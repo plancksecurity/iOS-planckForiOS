bab46c773991a616f4167372ee00221ce977d8a8
Dirk Zimmermann <dirk@pep-project.org>
2016-04-19 10:46:18 +0200

    Experiment with KVO to detect if message can be saved

--- pEpForiOS/Background/PersistentImapFolder.swift
+++ pEpForiOS/Background/PersistentImapFolder.swift
@@ -10,10 +10,20 @@ import Foundation
 import CoreData
 
 class PersistentImapFolder: CWIMAPFolder {
+    let comp = "PersistentImapFolder"
+
     let connectInfo: ConnectInfo
     let context: NSManagedObjectContext
     let backgroundQueue: NSOperationQueue
     let grandOperator: GrandOperator
+    let watchedMessages: NSMutableOrderedSet = []
+    let cache: PersistentEmailCache = PersistentEmailCache()
+
+    /**
+     - Note: What is typically fetched in IMAP is:
+     From To Cc Subject Date Message-ID References In-Reply-To
+     */
+    let pathsToWatch = ["from", "to", "recipients", "messageID", "receivedDate", "subject"]
 
     init(name: String, grandOperator: GrandOperator, connectInfo: ConnectInfo,
          backgroundQueue: NSOperationQueue) {
@@ -22,6 +32,14 @@ class PersistentImapFolder: CWIMAPFolder {
         self.grandOperator = grandOperator
         self.context = grandOperator.coreDataUtil.managedObjectContext
         super.init(name: name)
+        self.setCacheManager(cache)
+    }
+
+    func predicateAllMessages() -> NSPredicate {
+        let p = NSPredicate.init(format: "folder.account.email = %@ and folder.name = %@",
+                                 connectInfo.email,
+                                 self.name())
+        return p
     }
 
     /**
@@ -29,9 +47,8 @@ class PersistentImapFolder: CWIMAPFolder {
      be the case.
      */
     override func allMessages() -> [AnyObject] {
-        let p = NSPredicate.init(format: "folder.account.email = %@ and folder.name = %@", connectInfo.email,
-                                 self.name())
-        if let messages = Message.entitiesWithName(Message.entityName(), predicate: p,
+        if let messages = Message.entitiesWithName(Message.entityName(),
+                                                   predicate: self.predicateAllMessages(),
                                                    context: context) {
             return messages
         } else {
@@ -39,10 +56,62 @@ class PersistentImapFolder: CWIMAPFolder {
         }
     }
 
+    override func count() -> UInt {
+        let n = Message.countWithName(Message.entityName(), predicate: self.predicateAllMessages(),
+                                      context: context)
+        return UInt(n)
+    }
+
     override func appendMessage(theMessage: CWMessage) {
+        addAsObserver(theMessage)
+    }
+
+    func addAsObserver(message: CWMessage) {
+        watchedMessages.addObject(message)
+        for path in pathsToWatch {
+            message.addObserver(self, forKeyPath: path,
+                                options: .New,
+                                context: nil)
+        }
+    }
+
+    func removeAsObserver(message: CWMessage) {
+        watchedMessages.removeObject(message)
+        for path in pathsToWatch {
+            message.removeObserver(self, forKeyPath: path)
+        }
+    }
+
+    func saveMessage(message: CWMessage) {
         let op = StorePrefetchedMailOperation.init(grandOperator: self.grandOperator,
                                                    accountEmail: connectInfo.email,
-                                                   message: theMessage as! CWIMAPMessage)
-        //backgroundQueue.addOperation(op)
+                                                   message: message as! CWIMAPMessage)
+        backgroundQueue.addOperation(op)
+    }
+
+    func canPersistMail(message: CWMessage) -> Bool {
+        if message.recipients().count > 0 && message.from() != nil
+            && message.subject() != nil && message.receivedDate() != nil {
+            return true
+        }
+        return false
+    }
+
+    // MARK: -- KVO
+
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?,
+                                         change: [String : AnyObject]?,
+                                         context: UnsafeMutablePointer<Void>) {
+        if keyPath != nil && pathsToWatch.contains(keyPath!) {
+            if let message = object as? CWMessage {
+                Log.info(comp, "message (\(message)) changed \(keyPath!): \(change![NSKeyValueChangeNewKey])")
+                if canPersistMail(message) {
+                    saveMessage(message)
+                }
+            }
+        } else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change,
+                                         context: context)
+        }
     }
 }
