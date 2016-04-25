1c27e841d7509306d6cfc4e60b3ec9fdaa0137bf
Ana Rebollo <ana@pep-project.org>
2016-04-25 17:36:12 +0200

    added the multivalued field security that allows the user choose the security protocol for your map account

--- pEpForiOS/UI/ImapSetup/IMAPSettingsTableView.swift
+++ pEpForiOS/UI/ImapSetup/IMAPSettingsTableView.swift
@@ -24,6 +24,21 @@ class IMAPSettingsTableView: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+
+
+    @IBAction func alertWithSecurityValues(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Security protocol", message: "Choose an Security protocol  for your accont", preferredStyle: .ActionSheet)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in}
+        alertController.addAction(CancelAction)
+        let StartTLSAction = UIAlertAction(title: "Start TLS", style: .Default) { (action) in}
+        alertController.addAction(StartTLSAction)
+        let TLSAction = UIAlertAction(title: "TLS", style: .Default) { (action) in}
+        alertController.addAction(TLSAction)
+        let NONEAction = UIAlertAction(title: "NONE", style: .Default) { (action) in}
+        alertController.addAction(NONEAction)
+        self.presentViewController(alertController, animated: true) {}
+    }
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "SMTPSettings" {
             mailParameters.serverhostIMAP = serverValue.text!
