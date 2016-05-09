b993c92ee5807c02ce61944c0930b1f71bd4af8e
Ana Rebollo <ana@pep-project.org>
2016-05-09 11:30:28 +0200

    adding to the UI the option to choose multiple values for Security transport

--- pEpForiOS/UI/ImapSetup/IMAPSettingsTableView.swift
+++ pEpForiOS/UI/ImapSetup/IMAPSettingsTableView.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
+public struct ModelIMAPSettingsInfoTable {
+
+    public var IMAPServer:String = "default.imap.server"
+    public var IMAPPort:UInt16 = 993
+    public var transportSecurityIMAP:String = "NONE"
+
+    public init(IMAPServer:String,IMAPPort:UInt16,transportSecurityIMAP:String) {
+        self.IMAPServer = IMAPServer
+        self.IMAPPort = IMAPPort
+        self.transportSecurityIMAP = transportSecurityIMAP
+    }
+}
+
 class IMAPSettingsTableView: UITableViewController  {
 
     @IBOutlet weak var serverValue: UITextField!
     @IBOutlet weak var portValue: UITextField!
-    
+    @IBOutlet weak var transportSecurity: UIButton!
+
     var appConfig: AppConfig?
     var mailSettings = MailSettingParameters()
+    var model = ModelIMAPSettingsInfoTable(IMAPServer:"default.imap.server",IMAPPort: 993,transportSecurityIMAP: "NONE")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,21 +44,31 @@ class IMAPSettingsTableView: UITableViewController  {
     }
 
     @IBAction func alertWithSecurityValues(sender: AnyObject) {
-        let alertController = UIAlertController(title: "Security protocol", message: "Choose an Security protocol  for your accont", preferredStyle: .ActionSheet)
+        let alertController = UIAlertController(title: "Security protocol", message: "Choose a Security protocol for your accont", preferredStyle: .ActionSheet)
+
         let CancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in}
         alertController.addAction(CancelAction)
-        let StartTLSAction = UIAlertAction(title: "Start TLS", style: .Default) { (action) in}
+
+        let StartTLSAction = UIAlertAction(title: "Start TLS", style: .Default) { (action) in
+            self.transportSecurity.setTitle("Start TLS >", forState: .Normal)
+        }
         alertController.addAction(StartTLSAction)
-        let TLSAction = UIAlertAction(title: "TLS", style: .Default) { (action) in}
+        let TLSAction = UIAlertAction(title: "TLS", style: .Default) { (action) in
+             self.transportSecurity.setTitle("TLS >", forState: .Normal)
+        }
         alertController.addAction(TLSAction)
-        let NONEAction = UIAlertAction(title: "Plain", style: .Default) { (action) in}
+        let NONEAction = UIAlertAction(title: "NONE", style: .Default) { (action) in
+             self.transportSecurity.setTitle("NONE >", forState: .Normal)
+        }
         alertController.addAction(NONEAction)
         self.presentViewController(alertController, animated: true) {}
+        model.transportSecurityIMAP = (transportSecurity.titleLabel?.text)!
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "SMTPSettings" {
             mailSettings.serverhostIMAP = serverValue.text!
+            mailSettings.transportSecurityIMAP = model.transportSecurityIMAP
             let aux:String = portValue.text!
             mailSettings.portIMAP = UInt16(aux)!
             if let destination = segue.destinationViewController as? SMTPSettingsTableView {
--- pEpForiOS/UI/ImapSetup/SMTPSettingsTableView.swift
+++ pEpForiOS/UI/ImapSetup/SMTPSettingsTableView.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
+public struct ModelSMTPSettingsInfoTable {
+
+    public var SMTPServer:String = "default.imap.server"
+    public var SMTPPort:UInt16 = 993
+    public var transportSecuritySMTP:String = "NONE"
+
+    public init(SMTPServer:String,SMTPPort:UInt16,transportSecuritySMTP:String) {
+        self.SMTPServer = SMTPServer
+        self.SMTPPort = SMTPPort
+        self.transportSecuritySMTP = transportSecuritySMTP
+    }
+}
 
 class SMTPSettingsTableView: UITableViewController {
 
     @IBOutlet weak var serverValue: UITextField!
     @IBOutlet weak var portValue: UITextField!
+    @IBOutlet weak var transportSecurity: UIButton!
 
     var appConfig: AppConfig?
     var mailSettings = MailSettingParameters()
+    var model = ModelSMTPSettingsInfoTable(SMTPServer:"default.imap.server",SMTPPort: 993,transportSecuritySMTP: "NONE")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,16 +40,35 @@ class SMTPSettingsTableView: UITableViewController {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func alertWithSecurityValues(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Security protocol", message: "Choose a Security protocol for your accont", preferredStyle: .ActionSheet)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in}
+        alertController.addAction(CancelAction)
+        let StartTLSAction = UIAlertAction(title: "Start TLS", style: .Default) { (action) in
+            self.transportSecurity.setTitle("Start TLS >", forState: .Normal)
+        }
+        alertController.addAction(StartTLSAction)
+        let TLSAction = UIAlertAction(title: "TLS", style: .Default) { (action) in
+            self.transportSecurity.setTitle("TLS >", forState: .Normal)
+        }
+        alertController.addAction(TLSAction)
+        let NONEAction = UIAlertAction(title: "NONE", style: .Default) { (action) in
+            self.transportSecurity.setTitle("NONE >", forState: .Normal)
+        }
+        alertController.addAction(NONEAction)
+        self.presentViewController(alertController, animated: true) {}
+        model.transportSecuritySMTP = (transportSecurity.titleLabel?.text)!
+    }
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         mailSettings.serverhostSMTP = serverValue.text!
         let portSMTPAux:String = portValue.text!
         mailSettings.portSMTP = UInt16(portSMTPAux)!
 
-
-    let connect = ConnectInfo.init(
-        email: mailSettings.email!, imapPassword: mailSettings.password!,
+        var x = 3
+        let connect = ConnectInfo.init(email: mailSettings.email!, imapPassword: mailSettings.password!,
         imapServerName: mailSettings.serverhostIMAP!, imapServerPort: mailSettings.portIMAP!,
-        imapTransport:ConnectionTransport.Plain, smtpServerName: mailSettings.serverhostSMTP!,
+        imapTransport:ConnectionTransport(rawValue: x)!,smtpServerName: mailSettings.serverhostSMTP!,
         smtpServerPort: mailSettings.portSMTP!, smtpTransport: ConnectionTransport.Plain)
 
         _ = appConfig?.grandOperator.verifyConnection(connect, completionBlock: { (error) in
