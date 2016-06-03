2b7b14e1f85606968b27fc0131fbf608d7225462
Hernâni Marques <hernani@pep-project.org>
2016-06-03 23:21:50 +0200

    Add stub for pEpTests

---
+++ pEpForiOSTests/pEpTests.swift
@@ -0,0 +1,27 @@
+//
+//  pEpTests.swift
+//  pEpForiOS
+//
+//  Created by hernani on 03/06/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+import pEpForiOS
+
+class pEpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testPEpSession() {
+        //let pEpSession: PEPSession = PEPSession.init()
+        //XCTAssert(pEpSession == true)
+    }
+
+}
