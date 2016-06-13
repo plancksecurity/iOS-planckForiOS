32a4a6b749b2857d9f0a9e0b168f2fd61a8b5a95
Hernâni Marques <hernani@pep-project.org>
2016-06-13 15:33:28 +0200

    Add PEPUtil and DecryptMailOperation modules as stubs.

---
+++ pEpForiOS/Background/DecryptMailOperation.swift
@@ -0,0 +1,13 @@
+//
+//  DecryptMailOperation.swift
+//  pEpForiOS
+//
+//  Created by hernani on 13/06/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class DecryptMailOperation: NSOperation {
+    // Stub
+}
---
+++ pEpForiOS/Util/PEPUtil.swift
@@ -0,0 +1,13 @@
+//
+//  PEPUtil.swift
+//  pEpForiOS
+//
+//  Created by hernani on 13/06/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class PEPUtil {
+    // Stub
+}
