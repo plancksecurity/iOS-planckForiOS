9ec0fd36eb6c0ee1d6b6234b41ae40ef29e4c617
Dirk Zimmermann <dirk@pep-project.org>
2016-06-27 17:56:30 +0200

    Basic compose view text edit behavior

--- pEpForiOS/UI/ImapSetup/ComposeWithAutocompleteViewController.swift
+++ pEpForiOS/UI/ImapSetup/ComposeWithAutocompleteViewController.swift
@@ -13,7 +13,17 @@ public class ComposeViewControllerModel {
     var to: String? = nil
 }
 
-class ComposeWithAutocompleteViewController: UITableViewController, UITextFieldDelegate {
+/**
+ TODO:
+  - Figure out how to react text changes from the user
+  - When text changes, how to notify the table view so that it updates the height of just
+    the right cell? Does reloadRowsAtIndexPaths work well?
+  - How to correctly deal with ENTER so that it always enters newlines?
+  - What about keyboard handling: User taps any key, do you have to make sure the right
+    position of the text is still visible?
+ */
+class ComposeWithAutocompleteViewController: UITableViewController {
+    let textFieldRowNumber = 4
 
     var appConfig: AppConfig?
     var model = ComposeViewControllerModel.init()
@@ -22,18 +32,15 @@ class ComposeWithAutocompleteViewController: UITableViewController, UITextFieldD
     @IBOutlet weak var ccTextField: UITextField!
     @IBOutlet weak var bccTextField: UITextField!
     @IBOutlet weak var shortMessageTextField: UITextField!
-    @IBOutlet weak var longMessageTextField: UITextView!
+    @IBOutlet weak var longMessageTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIHelper.variableCellHeightsTableView(self.tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        return true 
     }
 
     func updateView() {
@@ -43,5 +50,28 @@ class ComposeWithAutocompleteViewController: UITableViewController, UITextFieldD
         if let to = model.to {
             toTextField.text = to
         }
+    }
+
+    // MARK: -- UITableViewDelegate
+
+    override func tableView(tableView: UITableView,
+                   heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let defaulHeight: CGFloat = 44
+        if indexPath.row == textFieldRowNumber {
+            if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                let cellSize = cell.bounds.size
+                let wantedSize = longMessageTextField.sizeThatFits(cellSize)
+                return wantedSize.height
+            }
+            return defaulHeight
+        } else {
+            return defaulHeight
+        }
+    }
+}
+
+extension ComposeWithAutocompleteViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return false
     }
 }
