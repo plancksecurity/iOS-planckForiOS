e8f67ec23920d0861a0260fe101868ed25f34da0
Ana Rebollo <ana@pep-project.org>
2016-07-15 12:01:24 +0200

    Addind the missing file LabelMailExplantionSecurityTableViewCell

---
+++ pEpForiOS/UI/Trustwords/LabelMailExplantionSecurityTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LabelMailExplantionSecurityTableViewCell.swift
+//  pEpForiOS
+//
+//  Created by ana on 14/7/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class LabelMailExplantionSecurityTableViewCell: UITableViewCell {
+    @IBOutlet weak var mailExplanationSecurityUILabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
