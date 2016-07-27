95a32e8e477ad9255f86761bb83626735f76e081
Dirk Zimmermann <dirk@pep-project.org>
2016-07-27 15:06:05 +0200

    Attachment debugDescription

--- pEpForiOS/Models/Attachment.swift
+++ pEpForiOS/Models/Attachment.swift
@@ -1,9 +1,25 @@
 import Foundation
 
-public protocol IAttachment: _IAttachment {
+public protocol IAttachment: _IAttachment, CustomStringConvertible, CustomDebugStringConvertible {
 }
 
 @objc(Attachment)
 public class Attachment: _Attachment, IAttachment {
 	// Custom logic goes here.
+
+    override public var description: String {
+        let s = NSMutableString()
+        s.appendString("Part \(size) bytes")
+        if let fn = filename {
+            s.appendString(", \(fn)")
+        }
+        if let ct = contentType {
+            s.appendString(", \(ct)")
+        }
+        return String(s)
+    }
+
+    override public var debugDescription: String {
+        return description
+    }
 }
