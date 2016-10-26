8ff5bf2150867306776fb1ff4e449010bce4e80f
Dirk Zimmermann <dirk@pep-project.org>
2016-10-26 16:30:41 +0200

    IOS-240 Implementing MessageFolderDelegate

--- pEpForiOS/UI/AccountsAndFolders/FolderListViewController.swift
+++ pEpForiOS/UI/AccountsAndFolders/FolderListViewController.swift
@@ -44,12 +44,31 @@ class FolderListViewController: UITableViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        MessageModelConfig.messageFolderDelegate = self
         updateModel()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        MessageModelConfig.messageFolderDelegate = nil
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Util
+
+    func index(ofFolder folder: Folder) -> Int? {
+        var counter = 0
+        for f in folders {
+            if f == folder {
+                return counter
+            }
+            counter += 1
+        }
+        return nil
     }
 
     func updateModel() {
@@ -65,7 +84,19 @@ class FolderListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if folders.isEmpty {
+            return 0
+        }
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folders.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: standardCell, for: indexPath)
         configureCell(cell, indexPath: indexPath)
@@ -87,9 +118,8 @@ class FolderListViewController: UITableViewController {
         }
     }
 
-    override func tableView(
-        _ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle,
-        forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
         let folder = folderAt(indexPath: indexPath)
         folder.delete()
     }
@@ -100,9 +130,8 @@ class FolderListViewController: UITableViewController {
 
     // MARK: - Table view delegate
 
-    override func tableView(
-        _ tableView: UITableView,
-        indentationLevelForRowAt indexPath: IndexPath) -> Int {
+    override func tableView(_ tableView: UITableView,
+                            indentationLevelForRowAt indexPath: IndexPath) -> Int {
         var folder = folderAt(indexPath: indexPath) as MessageFolder
         var count = 0
         while folder.parent != nil {
@@ -131,6 +160,41 @@ class FolderListViewController: UITableViewController {
                 return
             }
             vc.config = emailListConfig
+        }
+    }
+}
+
+// MARK: - MessageFolderDelegate
+
+extension FolderListViewController: MessageFolderDelegate {
+    func didChange(messageFolder: MessageFolder) {
+        guard let folder = messageFolder as? Folder else {
+            return
+        }
+
+        if folder.isGhost || folder.isOriginal {
+            if let index = index(ofFolder: folder) {
+                let indexPath = IndexPath.init(row: index, section: 0)
+                if messageFolder.isGhost {
+                    print("\(messageFolder.uuid) deleted")
+                    DispatchQueue.main.async {
+                        // Don't delete the row right off, because it's still part of the model
+                        // (otherwise we wouldn't know which index it has).
+                        // So delete it after this method has returned, and the message is removed
+                        // from the model.
+                        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                    }
+                } else if messageFolder.isOriginal {
+                    print("\(messageFolder.uuid) new object at \(index)")
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            } else {
+                print("ghost/original message without index")
+            }
+        } else if messageFolder.isChanged {
+            print("\(messageFolder.uuid) updated")
+        } else {
+            print("\(messageFolder.uuid) ???")
         }
     }
 }
