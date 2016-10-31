0ecd38b8abd96471d3c53376cca9653b86f4291d
Dirk Zimmermann <dirk@pep-project.org>
2016-10-31 15:18:34 +0100

    IOS-248 Account save test waiting for delegate call

--- pEpForiOSTests/MessageModelTests.swift
+++ pEpForiOSTests/MessageModelTests.swift
@@ -18,6 +18,19 @@ class MessageModelTests: XCTestCase {
     }
     
     func testAccountSave() {
+        // setup AccountDelegate
+        class CallBack: AccountDelegate {
+            var expVerifyCalled: XCTestExpectation?
+
+            func didVerify(account: Account, error: MessageModelError?) {
+                expVerifyCalled?.fulfill()
+            }
+        }
+        let callBack = CallBack()
+        callBack.expVerifyCalled = expectation(description: "expVerifyCalled")
+        MessageModelConfig.accountDelegate = callBack
+
+        // create account
         let id = Identity.create(address: "user1@example.com", userName: "User 1",
                                  userID: "user1")
         let server = Server.create(serverType: .imap, port: 993, address: "noserverhere",
@@ -25,5 +38,9 @@ class MessageModelTests: XCTestCase {
         let cred = ServerCredentials.create(userName: id.userID!, servers: [server])
         let acc = Account.create(identity: id, credentials: [cred])
         acc.save()
+
+        waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
+            XCTAssertNil(error)
+        })
     }
 }
