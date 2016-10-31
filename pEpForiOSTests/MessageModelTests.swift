091f2ad80d6db40bfe684e19d1a1d943e7316720
Dirk Zimmermann <dirk@pep-project.org>
2016-10-31 14:18:41 +0100

    IOS-248 Can save new CdAccount

--- pEpForiOSTests/MessageModelTests.swift
+++ pEpForiOSTests/MessageModelTests.swift
@@ -9,15 +9,21 @@
 import XCTest
 
 import pEpForiOS
+import MessageModel
 
 class MessageModelTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         let _ = PersistentSetup.init()
     }
     
     func testAccountSave() {
-        
+        let id = Identity.create(address: "user1@example.com", userName: "User 1",
+                                 userID: "user1")
+        let server = Server.create(serverType: .imap, port: 993, address: "noserverhere",
+                                   transport: .tls)
+        let cred = ServerCredentials.create(userName: id.userID!, servers: [server])
+        let acc = Account.create(identity: id, credentials: [cred])
+        acc.save()
     }
 }
