4fa1cbc69122b1e2d3c5b2d1b72c882ce317cafb
Dirk Zimmermann <dirk@pep-project.org>
2016-10-31 15:37:41 +0100

    IOS-248 Account.save() test with SendLayer

--- pEpForiOSTests/MessageModelTests.swift
+++ pEpForiOSTests/MessageModelTests.swift
@@ -8,25 +8,63 @@
 
 import XCTest
 
-import pEpForiOS
 import MessageModel
 
 class MessageModelTests: XCTestCase {
+    /**
+     Invoke completion block without errors for all actions.
+     */
+    class TestSendLayer: SendLayerProtocol {
+        func verify(account: CdAccount, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func send(message: CdMessage, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func saveDraft(message: CdMessage, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func syncFlagsToServer(folder: CdFolder, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func create(folderType: FolderType, account: CdAccount,
+                    completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func delete(folder: CdFolder, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+
+        func delete(message: CdMessage, completionBlock: SendLayerCompletionBlock?) {
+            completionBlock?(nil)
+        }
+    }
+
+    class TestAccountDelegate: AccountDelegate {
+        var expVerifyCalled: XCTestExpectation?
+        var error: MessageModelError?
+
+        func didVerify(account: Account, error: MessageModelError?) {
+            self.error = error
+            expVerifyCalled?.fulfill()
+        }
+    }
+
     override func setUp() {
         super.setUp()
         let _ = PersistentSetup.init()
     }
     
     func testAccountSave() {
-        // setup AccountDelegate
-        class CallBack: AccountDelegate {
-            var expVerifyCalled: XCTestExpectation?
+        CdAccount.sendLayer = TestSendLayer()
 
-            func didVerify(account: Account, error: MessageModelError?) {
-                expVerifyCalled?.fulfill()
-            }
-        }
-        let callBack = CallBack()
+        // setup AccountDelegate
+        let callBack = TestAccountDelegate()
         callBack.expVerifyCalled = expectation(description: "expVerifyCalled")
         MessageModelConfig.accountDelegate = callBack
 
@@ -41,6 +79,7 @@ class MessageModelTests: XCTestCase {
 
         waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
             XCTAssertNil(error)
+            XCTAssertNil(callBack.error)
         })
     }
 }
