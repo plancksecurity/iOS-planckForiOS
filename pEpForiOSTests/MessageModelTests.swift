eb4789bcd70185dea106312e5ed6e5959428c42e
Dirk Zimmermann <dirk@pep-project.org>
2016-10-31 17:47:32 +0100

    IOS-248 SendLayerTests.testVerifySMTP (fails)

--- pEpForiOSTests/MessageModelTests.swift
+++ pEpForiOSTests/MessageModelTests.swift
@@ -23,7 +23,7 @@ class MessageModelTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        let _ = PersistentSetup.init()
+        let _ = PersistentSetup()
     }
     
     func testAccountSave() {
@@ -34,14 +34,8 @@ class MessageModelTests: XCTestCase {
         callBack.expVerifyCalled = expectation(description: "expVerifyCalled")
         MessageModelConfig.accountDelegate = callBack
 
-        // create account
-        let id = Identity.create(address: "user1@example.com", userName: "User 1",
-                                 userID: "user1")
-        let server = Server.create(serverType: .imap, port: 993, address: "noserverhere",
-                                   transport: .tls)
-        let cred = ServerCredentials.create(userName: id.userID!, servers: [server])
-        let acc = Account.create(identity: id, credentials: [cred])
-        acc.save()
+        let account = TestData.createAccount()
+        account.save()
 
         waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
             XCTAssertNil(error)
--- pEpForiOSTests/SendLayerTests.swift
+++ pEpForiOSTests/SendLayerTests.swift
@@ -15,13 +15,35 @@ class SendLayerTests: XCTestCase {
     let coreDataUtil = CoreDataUtil()
     let connectionManager = ConnectionManager()
 
+    class TestAccountDelegate: AccountDelegate {
+        var expVerifyCalled: XCTestExpectation?
+        var error: MessageModelError?
+
+        func didVerify(account: Account, error: MessageModelError?) {
+            self.error = error
+            expVerifyCalled?.fulfill()
+        }
+    }
+
     override func setUp() {
         super.setUp()
-        let _ = PersistentSetup.init()
+        let _ = PersistentSetup()
     }
 
     func testVerifySMTP() {
+        let callBack = TestAccountDelegate()
+        callBack.expVerifyCalled = expectation(description: "expVerifyCalled")
+        MessageModelConfig.accountDelegate = callBack
+
         let grandOp = GrandOperator(connectionManager: connectionManager,
                                     coreDataUtil: coreDataUtil)
+        CdAccount.sendLayer = grandOp
+        let account = TestData.createAccount()
+        account.save()
+
+        waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
+            XCTAssertNil(error)
+            XCTAssertNil(callBack.error)
+        })
     }
 }
