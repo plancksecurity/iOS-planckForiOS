bc9bfc5ca26b6f820aa4111d9916264dfc2511cd
Xavier Algarra <xavier@pep-project.org>
2016-11-02 18:53:11 +0100

    delete with some part in old model

--- pEpForiOS/Background/DeleteMessageOperation.swift
+++ pEpForiOS/Background/DeleteMessageOperation.swift
@@ -14,35 +14,42 @@ import MessageModel
 open class DeleteMessageOperation: ConcurrentBaseOperation {
     let messageID: NSManagedObjectID
 
+    //public init(message: MessageModelConfig.)
+    
+    
+    
     public init(message: CdMessage, coreDataUtil: CoreDataUtil) {
+        
         self.messageID = message.objectID
         super.init(coreDataUtil: coreDataUtil)
     }
 
     override open func main() {
-        privateMOC.perform({
-            guard let message = self.privateMOC.object(with: self.messageID) as?
-                CdMessage
+        let bg = Record.Context.background
+        bg.perform {
+            guard let message = bg.object(with: self.messageID) as? CdMessage
                 else {
                     return
             }
-
+            
+            
             var targetFolder: CdFolder?
             targetFolder = self.model.folderByType(.trash, account: message.folder.account)
             if targetFolder == nil {
                 targetFolder = self.model.folderByType(
                     .archive, account: message.folder.account)
             }
-
+            
             guard let folder = targetFolder else {
                 // TODO: Just delete the mail
                 self.markAsFinished()
                 return
             }
-
+            
             let cwMail = PEPUtil.pantomimeMailFromMessage(message)
             let cwFolder = CWIMAPFolder.init(name: folder.name)
             cwFolder.copyMessages([cwMail], toFolder: cwFolder.name())
-        })
+        }
+        
     }
 }
