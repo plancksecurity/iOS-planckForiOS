671eeabc90e826c2207e51e509a9489aaaadbcb8
Dirk Zimmermann <dirk@pep-project.org>
2016-11-10 10:00:21 +0100

    IOS-277 + PrefetchEmailsOperationTest

---
+++ pEpForiOSTests/PrefetchEmailsOperationTest.swift
@@ -0,0 +1,20 @@
+//
+//  PrefetchEmailsOperationTest.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 10/11/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class PrefetchEmailsOperationTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        let _ = PersistentSetup()
+    }
+    
+    func testSimple() {
+    }
+}
