436561122229cad9f28f743fed369b6991b7f75a
Hernâni Marques <hernani@pep-project.org>
2016-11-13 13:44:28 +0100

    Remove BaseManagedObject.swift file (refactor).

--- pEpForiOS/Models/machine/_CdMessage.swift
+++ pEpForiOS/Models/machine/_CdMessage.swift
@@ -1,9 +1,11 @@
 // DO NOT EDIT. This file is machine-generated and constantly overwritten.
 // Make changes to CdMessage.swift instead.
 
+import CoreData
+
 import MessageModel
 
-open class _CdMessage: BaseManagedObject {
+open class _CdMessage: NSManagedObject {
 
     // MARK: - Properties
 
