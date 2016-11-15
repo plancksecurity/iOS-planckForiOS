2494ccfb6ef4ba95e67d449d8df935b9ff5d5b5a
Dirk Zimmermann <dirk@pep-project.org>
2016-11-15 10:56:20 +0100

    Cleanup

--- pEpForiOSTests/ObservableModelTest.swift
+++ pEpForiOSTests/ObservableModelTest.swift
@@ -10,11 +10,16 @@ import XCTest
 import MessageModel
 
 class ObservableModelTest: XCTestCase {
-    
+    var persistentSetup: PersistentSetup!
+
     override func setUp() {
         super.setUp()
-        let _ = PersistentSetup()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        persistentSetup = PersistentSetup()
+    }
+
+    override func tearDown() {
+        persistentSetup = nil
+        super.tearDown()
     }
 
     func testModelObserver() {
@@ -28,12 +33,12 @@ class ObservableModelTest: XCTestCase {
                     dirty = true;
                 }
             }
+
             func save() {
                 dirty = false
                 self.saveIsCalled?.fulfill()
 
             }
-
         }
 
         let mm = MockModel();
@@ -42,32 +47,9 @@ class ObservableModelTest: XCTestCase {
         mm.mockProperty = "new string"
         XCTAssertTrue(mm.dirty)
         
-
         waitForExpectations(timeout: TestUtil.waitTime, handler: { (error) in
             XCTAssertNil(error)
             XCTAssertFalse(mm.dirty)
         })
-
-
-
     }
-
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
