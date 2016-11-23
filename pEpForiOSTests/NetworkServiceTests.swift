e1f4c542a4a1193792bc09355d5bd618d3522db6
Hernâni Marques <hernani@pep-project.org>
2016-11-23 23:59:26 +0100

    IOS-230: Cancel thread after individual tests.

--- pEpForiOSTests/NetworkServiceTests.swift
+++ pEpForiOSTests/NetworkServiceTests.swift
@@ -20,8 +20,8 @@ class NetworkServiceTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        networkService.cancel()
     }
     
     func testNetworkServiceExistence() {
