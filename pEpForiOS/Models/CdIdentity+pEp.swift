011b477b1c8c5302b439e7b57cefe78615835313
Dirk Zimmermann <dirk@pep-project.org>
2016-11-23 10:13:45 +0100

    IOS-317 CdIdentity+pEp

--- pEpForiOS/Models/CdIdentity+pEp.swift
+++ pEpForiOS/Models/CdIdentity+pEp.swift
@@ -44,4 +44,35 @@ extension CdIdentity {
         }
         return contacts
     }
+
+    /**
+     Converts a `PEPContact` to a dictionary suitable for `Record`'s creation methods.
+     */
+    public static func recordDictionary(pEpContact: PEPContact) -> [String: Any] {
+        return [
+            "address": pEpContact[kPepAddress] as Any,
+            "userName": pEpContact[kPepUsername] as Any,
+            "isMySelf": pEpContact[kPepIsMe] as? Bool ?? false]
+    }
+
+    /**
+     Converts a `PEPContact`-like dictionary to a dictionary suitable
+     for `Record`'s creation methods.
+     */
+    public static func recordDictionary(pEpDictionary: NSDictionary) -> [String: Any] {
+        return [
+            "address": pEpDictionary[kPepAddress] as Any,
+            "userName": pEpDictionary[kPepUsername] as Any,
+            "isMySelf": pEpDictionary[kPepIsMe] as? Bool ?? false]
+    }
+
+    public static func firstOrCreate(pEpContact: PEPContact) -> CdIdentity {
+        let dict = recordDictionary(pEpContact: pEpContact)
+        return CdIdentity.firstOrCreate(with: dict)
+    }
+
+    public static func firstOrCreate(dictionary: NSDictionary) -> CdIdentity {
+        let dict = recordDictionary(pEpDictionary: dictionary)
+        return CdIdentity.firstOrCreate(with: dict)
+    }
 }
