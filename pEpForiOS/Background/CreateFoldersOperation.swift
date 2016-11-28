7a0554d317f61ac41b9301c51a01ca6022bead2e
Dirk Zimmermann <dirk@pep-project.org>
2016-11-28 15:49:09 +0100

    IOS-302 CreateFoldersOperation

---
+++ pEpForiOS/Background/CreateFoldersOperation.swift
@@ -0,0 +1,16 @@
+//
+//  CreateFoldersOperation.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 28/11/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Tries to create all local folders on the server.
+ */
+class CreateFoldersOperation: ConcurrentBaseOperation {
+    
+}
