ea6960536630c70ad42d31966a86f8ddf1499ec1
Dirk Zimmermann <dirk@pep-project.org>
2016-12-22 14:55:57 +0100

    IOS-363 move tests

--- pEpForiOSTests/AddressBookTests.swift
+++ pEpForiOSTests/AddressBookTests.swift
@@ -24,31 +24,6 @@ class AddressBookTests: XCTestCase {
         persistentSetup = nil
     }
 
-    func testSimpleContactSearch() {
-        MessageModelConfig.observer.delegate = ObserverDelegate(
-            expSaved: expectation(description: "saved"))
-
-        // Some contacts
-        for i in 1...5 {
-            let _ = Identity.create(
-                address: "email\(i)@xtest.de", userName: "name\(i)")
-        }
-        let _ = Identity.create(
-            address: "wha@wawa.com", userName: "Another")
-
-        waitForExpectations(timeout: waitTime, handler: { error in
-            XCTAssertNil(error)
-        })
-
-        XCTAssertGreaterThan(Identity.all().count, 0)
-
-        XCTAssertEqual(Identity.by(snippet: "xtes").count, 5)
-        XCTAssertEqual(Identity.by(snippet: "XtEs").count, 5)
-        XCTAssertEqual(Identity.by(snippet: "wha").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "Ano").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "ANO").count, 1)
-    }
-
     func testSplitContactName() {
         let ab = AddressBook()
         XCTAssertTrue(ab.splitContactNameInTuple("uiae dtrn qfgh") == ("uiae", "dtrn", "qfgh"))
@@ -58,30 +33,6 @@ class AddressBookTests: XCTestCase {
         XCTAssertTrue(ab.splitContactNameInTuple("") == (nil, nil, nil))
         XCTAssertTrue(ab.splitContactNameInTuple("uiae") == ("uiae", nil, nil))
         XCTAssertTrue(ab.splitContactNameInTuple("uiae   xvlc") == ("uiae", nil, "xvlc"))
-    }
-
-    func testAddressbook() {
-        MessageModelConfig.observer.delegate = ObserverDelegate(
-            expSaved: expectation(description: "saved"))
-
-        let _ = Identity.create(address: "none@test.com", userName: "Noone Particular")
-        let _ = Identity.create(address: "hahaha1@test.com", userName: "Hahaha1")
-        let _ = Identity.create(address: "hahaha2@test.com", userName: "Hahaha2")
-        let _ = Identity.create(address: "uhum3@test.com", userName: "This Is Not")
-
-        waitForExpectations(timeout: waitTime, handler: { error in
-            XCTAssertNil(error)
-        })
-
-        XCTAssertGreaterThan(Identity.all().count, 0)
-        XCTAssertEqual(Identity.by(snippet: "NONEAtyvAll").count, 0)
-        XCTAssertEqual(Identity.by(snippet: "NONE").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "none").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "hah").count, 2)
-        XCTAssertEqual(Identity.by(snippet: "hAHa2").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "This is").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "This").count, 1)
-        XCTAssertEqual(Identity.by(snippet: "test").count, 4)
     }
 
     func testAddressBookTransfer() {
