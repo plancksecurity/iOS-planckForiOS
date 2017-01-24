dd305b66e299e85628cebba8dc04d1b7f22c1464
Dirk Zimmermann <dirk@pep-project.org>
2017-01-24 14:45:23 +0100

    IOS-83 rm some questionable code, refactor the right one

--- pEpForiOS/Models/CdIdentity+pEp.swift
+++ pEpForiOS/Models/CdIdentity+pEp.swift
@@ -19,20 +19,23 @@ extension CdIdentity {
         guard let addr = pEpC[kPepAddress] as? String else {
             return nil
         }
-        var ident = CdIdentity.search(address: addr)
-        if ident == nil{
-            ident = CdIdentity.create()
+        var identOpt = CdIdentity.search(address: addr)
+        if identOpt == nil {
+            identOpt = CdIdentity.create()
         }
-        ident!.userName = pEpC[kPepUsername] as? String
-        if let mySelfNum = pEpC[kPepIsMe] as? NSNumber {
-            ident!.isMySelf = mySelfNum
+        if let theIdent = identOpt {
+            theIdent.userName = pEpC[kPepUsername] as? String
+            if let mySelfNum = pEpC[kPepIsMe] as? NSNumber {
+                theIdent.isMySelf = mySelfNum
+            }
+            if let ctNum = pEpC[kPepCommType] as? NSNumber {
+                theIdent.commType = ctNum
+            }
+            theIdent.userID = pEpC[kPepUserID] as? String
+            Record.saveAndWait()
+            return theIdent
         }
-        if let ctNum = pEpC[kPepCommType] as? NSNumber {
-            ident!.commType = ctNum
-        }
-        ident!.userID = pEpC[kPepUserID] as? String
-        Record.saveAndWait()
-        return ident
+        return nil
     }
 
     public static func from(pEpContacts: [PEPIdentity]?) -> [CdIdentity] {
@@ -46,37 +49,6 @@ extension CdIdentity {
             }
         }
         return contacts
-    }
-
-    /**
-     Converts a `PEPIdentity` to a dictionary suitable for `Record`'s creation methods.
-     */
-    public static func recordDictionary(pEpContact: PEPIdentity) -> [String: Any] {
-        return [
-            "address": pEpContact[kPepAddress] as Any,
-            "userName": pEpContact[kPepUsername] as Any,
-            "isMySelf": pEpContact[kPepIsMe] as? Bool ?? false]
-    }
-
-    /**
-     Converts a `PEPIdentity`-like dictionary to a dictionary suitable
-     for `Record`'s creation methods.
-     */
-    public static func recordDictionary(pEpDictionary: NSDictionary) -> [String: Any] {
-        return [
-            "address": pEpDictionary[kPepAddress] as Any,
-            "userName": pEpDictionary[kPepUsername] as Any,
-            "isMySelf": pEpDictionary[kPepIsMe] as? Bool ?? false]
-    }
-
-    public static func firstOrCreate(pEpContact: PEPIdentity) -> CdIdentity {
-        let dict = recordDictionary(pEpContact: pEpContact)
-        return CdIdentity.firstOrCreate(with: dict)
-    }
-
-    public static func firstOrCreate(dictionary: NSDictionary) -> CdIdentity {
-        let dict = recordDictionary(pEpDictionary: dictionary)
-        return CdIdentity.firstOrCreate(with: dict)
     }
 
     public func pEpRating(session: PEPSession = PEPSession()) -> PEP_rating {
