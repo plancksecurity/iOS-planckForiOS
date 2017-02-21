fb88a0ef331d8620d1ef6793791e57e45d2595d1
Dirk Zimmermann <dirk@pep-project.org>
2017-02-21 16:05:31 +0100

    IOS-450 AttachmentCounter

---
+++ pEpForiOS/UI/Compose/AttachmentCounter.swift
@@ -0,0 +1,20 @@
+//
+//  AttachmentCounter.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 21/02/2017.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class AttachmentCounter {
+    var attachmentCount = 0
+
+    public func filename(baseName: String, fileExtension: String) -> String {
+        //let newFileName = "\(baseName)_\(attachmentCount).\(fileExtension)"
+        let newFileName = String(format: "%@_%03d_%@", baseName, attachmentCount, fileExtension)
+        attachmentCount += 1
+        return newFileName
+    }
+}
