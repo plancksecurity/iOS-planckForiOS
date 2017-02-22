b6876c2bca7d66bdf10e9a0f0ccba1b0eb3618d6
Xavier Algarra <xavier@pep-project.org>
2017-02-22 12:46:30 +0100

    IOS-328 Adding new files needed for build

---
+++ pEpForiOS/UI/Folder/CellsAndSections/AccountSection.swift
@@ -0,0 +1,14 @@
+//
+//  AccountSection.swift
+//  pEpForiOS
+//
+//  Created by Xavier Algarra on 21/02/17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+public class AccountSection: UIView {
+
+    //UIView section tobe
+}
---
+++ pEpForiOS/UI/Folder/FolderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FolderTableViewCell.swift
+//  pEpForiOS
+//
+//  Created by Xavier Algarra on 21/02/17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class FolderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
