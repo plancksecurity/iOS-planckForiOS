e80bcabb80f277375699a097de494007665502db
Xavier Algarra <xavier@pep-project.org>
2017-02-22 11:42:37 +0100

    IOS-328 FolderViewController with viewModel

---
+++ pEpForiOS/UI/Folder/FolderTableViewController.swift
@@ -0,0 +1,112 @@
+//
+//  FolderTableViewController.swift
+//  pEpForiOS
+//
+//  Created by Xavier Algarra on 16/02/17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+import MessageModel
+
+class FolderTableViewController: UITableViewController {
+
+    var appConfig: AppConfig?
+
+    var folderVM = [FolderViewModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialConfig()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func initialConfig() {
+        self.tableView.sectionHeaderHeight = 45
+        self.tableView.rowHeight = 45
+        let accounts = Account.all()
+        for account in accounts {
+            let vm = FolderViewModel(account: account)
+            folderVM.append(vm)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return folderVM.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folderVM[section].count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return folderVM[section].title
+        //reimplement to a custom view and copy the view of mail app
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FolderCell", for: indexPath)
+        let fcvm = folderVM[indexPath.section][indexPath.item]
+        let txt = cell.textLabel
+        txt?.text = fcvm.name
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
