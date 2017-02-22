1fcae927599e1cad88b220217a4de99ba7b4f86b
Dirk Zimmermann <dirk@pep-project.org>
2017-02-22 16:04:57 +0100

    IOS-450 creating regex in playground

--- General.playground/Contents.swift
+++ General.playground/Contents.swift
@@ -2,6 +2,20 @@
 
 import UIKit
 
+let regex = try NSRegularExpression(pattern: "<img src=\"file:///Attachment(_\\d+)?.png\" alt=\"Attachment(_\\d+)?.png\">")
+
+let longMessage = "<span class=\"s1\"><img src=\"file:///Attachment.png\" alt=\"Attachment.png\"></span><span class=\"s1\"><img src=\"file:///Attachment_1.png\" alt=\"Attachment_1.png\"></span>"
+
+let matches = regex.matches(in: longMessage, range: NSMakeRange(0, (longMessage as NSString).length))
+matches.count
+
+var newText = longMessage
+for i in 0..<matches.count {
+    let m = matches[i]
+    newText = (newText as NSString).replacingCharacters(in: m.range, with: "<img src=\"file:///MyAttachment\(i).jpg\" alt=\"MyAttachment\(i).jpg\">")
+    print("range: \(m.range.location) newText: \(newText)\n")
+}
+
 protocol IName {
     var name: String? {get set}
 }
