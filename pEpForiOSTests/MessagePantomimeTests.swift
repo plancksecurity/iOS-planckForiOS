d532cf866a2ae8aa5042a31df2a5ddcce9232c09
Andreas Buff <andreas.buff@buffhaltestelle.de>
2017-03-13 17:49:51 +0100

    IOS-342 takes flagDraft into account

--- pEpForiOSTests/MessagePantomimeTests.swift
+++ pEpForiOSTests/MessagePantomimeTests.swift
@@ -94,7 +94,6 @@ class MessagePantomimeTests: XCTestCase {
         }
     }
 
-    //TODO: string representation of flags (like "\Deleted") should be an enum in Pantomime
     func testStoreCommandForFlagsToAdd() {
         let m = CdMessage.create()
         m.imap = CdImapFields.create()
@@ -102,6 +101,7 @@ class MessagePantomimeTests: XCTestCase {
         m.uid = 1024
         m.imap?.flagsFromServer = Int16.imapNoFlagsSet()
         m.imap?.flagAnswered = false
+        m.imap?.flagDraft = false
         m.imap?.flagDeleted = false
         m.imap?.flagSeen = false
         m.imap?.flagFlagged = false
@@ -112,17 +112,22 @@ class MessagePantomimeTests: XCTestCase {
         XCTAssertEqual(m.storeCommandForFlagsToAdd()?.0,
                        "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered)")
 
+        m.imap?.flagDraft = true
+        XCTAssertEqual(m.storeCommandForFlagsToAdd()?.0,
+                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Draft)")
+
         m.imap?.flagDeleted = true
         XCTAssertEqual(m.storeCommandForFlagsToAdd()?.0,
-                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Deleted)")
+                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Draft \\Deleted)")
 
         m.imap?.flagSeen = true
         XCTAssertEqual(m.storeCommandForFlagsToAdd()?.0,
-                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Seen \\Deleted)")
+                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Draft \\Seen \\Deleted)")
 
         m.imap?.flagFlagged = true
         XCTAssertEqual(m.storeCommandForFlagsToAdd()?.0,
-                       "UID STORE \(m.uid) +FLAGS.SILENT (\\Answered \\Flagged \\Seen \\Deleted)")
+                       "UID STORE \(m.uid) +FLAGS.SILENT " +
+                        "(\\Answered \\Draft \\Flagged \\Seen \\Deleted)")
     }
 
     func testStoreCommandForFlagsToRemove() {
@@ -132,6 +137,7 @@ class MessagePantomimeTests: XCTestCase {
         m.uid = 1024
         m.imap?.flagsFromServer = Int16.imapAllFlagsSet()
         m.imap?.flagAnswered = true
+        m.imap?.flagDraft = true
         m.imap?.flagDeleted = true
         m.imap?.flagSeen = true
         m.imap?.flagFlagged = true
@@ -142,17 +148,22 @@ class MessagePantomimeTests: XCTestCase {
         XCTAssertEqual(m.storeCommandForFlagsToRemove()?.0,
                        "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered)")
 
+        m.imap?.flagDraft = false
+        XCTAssertEqual(m.storeCommandForFlagsToRemove()?.0,
+                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Draft)")
+
         m.imap?.flagDeleted = false
         XCTAssertEqual(m.storeCommandForFlagsToRemove()?.0,
-                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Deleted)")
+                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Draft \\Deleted)")
 
         m.imap?.flagSeen = false
         XCTAssertEqual(m.storeCommandForFlagsToRemove()?.0,
-                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Seen \\Deleted)")
+                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Draft \\Seen \\Deleted)")
 
         m.imap?.flagFlagged = false
         XCTAssertEqual(m.storeCommandForFlagsToRemove()?.0,
-                       "UID STORE \(m.uid) -FLAGS.SILENT (\\Answered \\Flagged \\Seen \\Deleted)")
+                       "UID STORE \(m.uid) -FLAGS.SILENT " +
+                        "(\\Answered \\Draft \\Flagged \\Seen \\Deleted)")
     }
 
     func testReferences() {
