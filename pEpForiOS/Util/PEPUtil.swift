e53b1d047159ef950d5f8530ecd3d3373d0b54b8
Dirk Zimmermann <dirk@pep-project.org>
2017-03-23 15:09:30 +0100

    IOS-476 UIColor extension

--- pEpForiOS/Util/PEPUtil.swift
+++ pEpForiOS/Util/PEPUtil.swift
@@ -800,42 +800,6 @@ extension String {
     }
 }
 
-extension UIColor {
-    open class var pEpGreen: UIColor {
-        get {
-            return UIColor(hex: "#4CD964")
-        }
-    }
-
-    open class var pEpRed: UIColor {
-        get {
-            return UIColor(hex: "#FF3B30")
-        }
-    }
-
-    open class var pEpYellow: UIColor {
-        get {
-            return UIColor(hex: "#FFCC00")
-        }
-    }
-    
-    convenience init(hex: String) {
-        var hexstr = hex
-        if hexstr.hasPrefix("#") {
-            hexstr = String(hexstr.characters.dropFirst())
-        }
-
-        var rgbValue: UInt32 = 0
-        Scanner(string: hexstr).scanHexInt32(&rgbValue)
-
-        let r = CGFloat((rgbValue >> 16) & 0xff) / 255.0
-        let g = CGFloat((rgbValue >> 08) & 0xff) / 255.0
-        let b = CGFloat((rgbValue >> 00) & 0xff) / 255.0
-
-        self.init(red: r, green: g, blue: b, alpha: 1.0)
-    }
-}
-
 extension UIFont {
     open class var pEpInput: UIFont {
         get {
