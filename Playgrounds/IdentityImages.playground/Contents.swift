18c7499ade328623854026fe49fbe4b0ef86692d
Dirk Zimmermann <dirk@pep-project.org>
2017-03-24 10:07:35 +0100

    IOS-476 playground version that maximizes text size

--- Playgrounds/IdentityImages.playground/Contents.swift
+++ Playgrounds/IdentityImages.playground/Contents.swift
@@ -44,7 +44,7 @@ extension String {
     }
 
     func initials() -> String {
-        let words = self.components(separatedBy: " ").map() {
+        let words = self.characters.split(separator: " ").map(String.init).map() {
             return $0.trimmingCharacters(in: .whitespacesAndNewlines)
         }
         if words.count == 0 {
@@ -57,6 +57,43 @@ extension String {
         let word2 = words[words.count - 1]
         return word1.prefix(ofLength: 1) + word2.prefix(ofLength: 1)
     }
+
+    /**
+     Draws `text` in the context `ctx` in the given `color`, centered in a rectangle with
+     size `size`.
+     */
+    func draw(ctx: CGContext, centeredIn size: CGSize, color: UIColor, font: UIFont) {
+        let wholeRect = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        let nsString = self as NSString
+        let textAttributes: [String : Any] = [
+            NSStrokeColorAttributeName: color,
+            NSForegroundColorAttributeName: color,
+            NSFontAttributeName: font]
+        let stringSize = nsString.size(attributes: textAttributes)
+        let textRect = center(size: stringSize, inRect: wholeRect)
+        nsString.draw(in: textRect, withAttributes: textAttributes)
+    }
+}
+
+/**
+ - Returns: A font with a size that will make drawing `text` just barely fit into `size`.
+ */
+func maximize(text: String, inSize size: CGSize,
+              font: UIFont = UIFont.preferredFont(forTextStyle: .title1)) -> UIFont {
+    let nsString = text as NSString
+    var fontSize: CGFloat = 12.0
+    var lastFont = font
+
+    while true {
+        let currentFont = font.withSize(fontSize)
+        let textAttributes: [String : Any] = [NSFontAttributeName: currentFont]
+        let stringSize = nsString.size(attributes: textAttributes)
+        if stringSize.width > size.width || stringSize.height > size.height {
+            return lastFont
+        }
+        lastFont = currentFont
+        fontSize += 0.5
+    }
 }
 
 func drawCircle(ctx: CGContext, size: CGSize, color: UIColor) {
@@ -67,17 +104,44 @@ func drawCircle(ctx: CGContext, size: CGSize, color: UIColor) {
     ctx.fillEllipse(in: r)
 }
 
-func identityImageFromName(name: String, size: CGSize = CGSize(width: 64, height: 64)) -> UIImage? {
+/**
+ - Returns: A rectangle of size `size` in the center of `inRect`.
+ */
+func center(size: CGSize, inRect: CGRect) -> CGRect {
+    let xStart = inRect.size.width / 2 - size.width / 2
+    let yStart = inRect.size.height / 2 - size.height / 2
+    let o = CGPoint(x: xStart, y: yStart)
+    return CGRect(origin: o, size: size)
+}
+
+func drawInitialText(name: String, size: CGSize, padding: CGSize, ctx: CGContext) {
+    let textColor = UIColor.white
+    let text = name.initials()
+
+    var textSize = size
+    textSize.width -= padding.width
+    textSize.height -= padding.height
+
+    let font = maximize(text: text, inSize: textSize)
+    text.draw(ctx: ctx, centeredIn: size, color: textColor, font: font)
+}
+
+func identityImageFromName(name: String, size: CGSize = CGSize(width: 64, height: 64),
+                           padding: CGSize = CGSize(width: 10, height: 10)) -> UIImage? {
     return UIImage.generate(size: size) { ctx in
         drawCircle(ctx: ctx, size: size, color: UIColor(hex: "#c8c7cc"))
+        drawInitialText(name: name, size: size, padding: padding, ctx: ctx)
     }
 }
 
 identityImageFromName(name: "Bryant")
+
+identityImageFromName(name: "Eldon Tyrell")
 
 "Bryant".initials()
 "Priscilla Stratton".initials()
 "Eldon Tyrell".initials()
 "David Holden".initials()
 "J.F. Sebastian".initials()
+"Johann Sebastian Bach".initials()
 "".initials()
