b63a7f89bdc00b7da08f59fbc85e3144f91ba9dc
Dirk Zimmermann <dirk@pep-project.org>
2017-04-10 15:43:15 +0200

    IOS-113 dump constraints of views for debugging

---
+++ pEpForiOS/UI/Util/UIView+Util.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Util.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 10.04.17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    func dumpConstraints(axis: UILayoutConstraintAxis) {
+        let constrs = constraintsAffectingLayout(for: axis)
+        if constrs.isEmpty {
+            print("no constraints")
+        }
+        for con in constrs {
+            print("\(con)")
+        }
+    }
+}
