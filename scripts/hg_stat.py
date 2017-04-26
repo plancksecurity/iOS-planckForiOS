faaf8a66fae4a91579e610febe107056a0be467a
Dirk Zimmermann <dirk@pep-project.org>
2017-04-26 10:02:26 +0200

    script for quickly determining updates/changes

---
+++ scripts/hg_stat.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#
+# From pEp_for_iOS, invoke like this:
+# python3 scripts/hg_stat.py
+# Or:
+# ./scripts/hg_stat.py
+#
+
+import os
+from subprocess import call
+
+base = '../'
+dirs = ['pEp_for_iOS', 'MailModel', 'pEpiOSAdapter', 'pantomime-iOS',
+        'pEpEngine', 'netpgp-et', 'OpenSSL-for-iPhone']
+
+if __name__ == '__main__':
+    current_dir = os.getcwd()
+    for d in dirs:
+        path = base + d
+        os.chdir(path)
+        print("\n*** " + d)
+        git_dir = '.git/'
+        hg_dir = '.hg/'
+        if os.path.exists(git_dir):
+            call(['git', 'status'])
+        if os.path.exists(hg_dir):
+            call(['hg', 'status'])
+            call(['hg', 'out'])
+            call(['hg', 'in'])
+        os.chdir(current_dir)
+
