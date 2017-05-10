2ddd19d9ce880beed77886b766a5acee594d7a4c
Dirk Zimmermann <dirk@pep-project.org>
2017-05-10 08:50:42 +0200

    IOS-53 add .cpp dummy file so Xcode adds c++ runtime

---
+++ pEpForiOS/cpp/CppDummy.cpp
@@ -0,0 +1,12 @@
+//
+//  CppDummy.cpp
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 10.05.17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+/**
+ Entices Xcode to do some magic in order to add c++ runtime libraries to the project,
+ which are needed for using libAccountSettings.
+ */
