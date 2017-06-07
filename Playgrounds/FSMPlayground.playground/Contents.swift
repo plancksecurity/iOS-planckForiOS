f55738c0c501e9ab8a368ee5977f06a1eabf5d84
Dirk Zimmermann <dirk@pep-project.org>
2017-06-07 13:03:22 +0200

    IOS-578: working state machine

--- Playgrounds/FSMPlayground.playground/Contents.swift
+++ Playgrounds/FSMPlayground.playground/Contents.swift
@@ -2,163 +2,66 @@
 
 import Foundation
 
-open class MutableOrderedSet<T>: Sequence {
-    public init() {}
+class StateMachine<T: Hashable, U: Hashable> {
+    typealias DidEnterStateBlock = () -> ()
 
-    public init(array: [T]) {
-        for e in array {
-            elements.add(e)
+    class TransitionIndex: Hashable {
+        let source: T
+        let input: U
+
+        init(source: T, input: U) {
+            self.source = source
+            self.input = input
+        }
+
+        var hashValue: Int {
+            return 31 &* source.hashValue &* input.hashValue
+        }
+
+        static func ==(lhs: TransitionIndex, rhs: TransitionIndex) -> Bool {
+            return lhs.source == rhs.source && lhs.input == rhs.input
         }
     }
 
-    open var array: [T] {
-        return elements.array as? [T] ?? []
+    var currentState: T
+    var stateEventTable = [TransitionIndex: T]()
+    var stateExecutionBlocks = [T: DidEnterStateBlock]()
+
+    init(currentState: T) {
+        self.currentState = currentState
     }
 
-    open subscript(safe index: Int) -> T? {
-        get {
-            if elements.count == 0 {
-                return nil
-            }
-            if index >= elements.count {
-                return nil
-            }
-            if let e = elements.object(at: index) as? T {
-                return e
-            }
-            return nil
+    func add(transitionIndex: TransitionIndex, target: T) {
+        stateEventTable[transitionIndex] = target
+    }
+
+    func add(source: T, input: U, target: T) {
+        let ti = TransitionIndex(source: source, input: input)
+        add(transitionIndex: ti, target: target)
+    }
+
+    func executeOnEnter(state: T, block: @escaping DidEnterStateBlock) {
+        stateExecutionBlocks[state] = block
+    }
+
+    func start() {
+        executeActionForCurrentState()
+    }
+
+    func executeActionForCurrentState() {
+        if let action = stateExecutionBlocks[currentState] {
+            action()
         }
     }
 
-    open var count: Int {
-        get {
-            return elements.count
+    func accept(input: U) -> Bool {
+        let ti = TransitionIndex(source: currentState, input: input)
+        if let newState = stateEventTable[ti] {
+            currentState = newState
+            executeActionForCurrentState()
+            return true
         }
-    }
-
-    open var isEmpty: Bool {
-        get {
-            return elements.count == 0
-        }
-    }
-
-    open func append(_ element: T) {
-        elements.add(element)
-    }
-
-    open func insert(_ element: T) {
-        self.append(element)
-    }
-
-    open func contains(_ element: T) -> Bool {
-        return elements.contains(element)
-    }
-
-    open func remove(_ element: T) {
-        elements.remove(element)
-    }
-
-    open func indexOf(_ element: T) -> Int? {
-        let i = elements.index(of: element)
-        if i == NSNotFound {
-            return nil
-        } else {
-            return i
-        }
-    }
-
-    private var elements = NSMutableOrderedSet()
-
-    // MARK: - Sequence
-
-    public typealias Iterator = MutableOrderedSetIterator<T>
-
-    public func makeIterator() -> MutableOrderedSet.Iterator {
-        return MutableOrderedSetIterator.init(elements: elements.array as! [T])
-    }
-}
-
-public struct MutableOrderedSetIterator<T>: IteratorProtocol {
-    public typealias Element = T
-
-    private let elements: [T]
-    private var index = 0
-    private let maxIndex: Int
-
-    public init(elements: [T]) {
-        self.elements = elements
-        maxIndex = elements.count - 1
-    }
-
-    public mutating func next() -> MutableOrderedSetIterator.Element? {
-        if index > maxIndex {
-            return nil
-        } else {
-            let e = elements[index]
-            index += 1
-            return e
-        }
-    }
-}
-
-class StateMachine<T: Equatable> {
-    class State<T: Equatable>: Equatable {
-        let value: T
-        let didChangeToBlock: (() -> (Void))?
-
-        init(value: T, changedTo: (() -> (Void))? = nil) {
-            self.value = value
-            self.didChangeToBlock = changedTo
-        }
-
-        public static func ==(lhs: State, rhs: State) -> Bool {
-            return lhs.value == rhs.value
-        }
-    }
-
-    class Input<T: Equatable> {
-        let name: String
-        let from: State<T>
-        let to: State<T>
-
-        init(name: String, from: State<T>, to: State<T>) {
-            self.name = name
-            self.from = from
-            self.to = to
-        }
-    }
-
-    enum InputError: Error {
-        /**
-         An input was denied, because the current state does not match the source
-         state of the input.
-         */
-        case inputDenied
-    }
-    
-    var states = MutableOrderedSet<State<T>>()
-    var eventsByName = [String: Input<T>]()
-    var currentState: State<T>
-
-    init(states: [State<T>], startingState: State<T>) {
-        self.states = MutableOrderedSet(array: states)
-        currentState = startingState
-        signalActivation(state: currentState)
-    }
-
-    func input(_ input: Input<T>) throws {
-        if input.from != currentState {
-            throw InputError.inputDenied
-        }
-
-        currentState = input.to
-        signalActivation(state: currentState)
-    }
-
-    func signalActivation(state: State<T>) {
-        if let fn = state.didChangeToBlock {
-            fn()
-        }
+        return false
     }
 }
 
@@ -172,10 +75,41 @@ enum InboxSyncState {
     case saveDrafts
 }
 
-let inboxStartState: StateMachine.State<InboxSyncState>? = nil //= StateMachine<InboxSyncState>.State(value: .fetchFolders)
+enum InboxSyncInput {
+    case foldersFetched
+    case messagesFetched
 
-/*
-let inboxSyncStates = [
-    inboxStartState
-]
-*/
+    case idle
+    case sendMails
+    case saveDrafts
+    case repeatSync
+}
+
+func addFakeAction(
+    machine: StateMachine<InboxSyncState, InboxSyncInput>,
+    whenEnteringState: InboxSyncState, inputOnFinished: InboxSyncInput) {
+    machine.executeOnEnter(state: whenEnteringState) {
+        print("Executing: \(whenEnteringState)")
+        DispatchQueue.global().async {
+            sleep(1)
+            machine.accept(input: inputOnFinished)
+        }
+    }
+}
+
+let machine: StateMachine<InboxSyncState, InboxSyncInput> = StateMachine(currentState: InboxSyncState.fetchFolders)
+
+addFakeAction(machine: machine, whenEnteringState: .fetchFolders, inputOnFinished: .foldersFetched)
+addFakeAction(machine: machine, whenEnteringState: .fetchMessages, inputOnFinished: .messagesFetched)
+
+machine.add(source: .fetchFolders, input: .foldersFetched, target: .fetchMessages)
+machine.add(source: .fetchMessages, input: .messagesFetched, target: .syncMessages)
+machine.add(source: .idle, input: .repeatSync, target: .fetchFolders)
+
+machine.start()
+
+while machine.currentState != .syncMessages {
+    //print("state \(machine.currentState)")
+    Thread.sleep(forTimeInterval: 0.1)
+}
+print("finished state: \(machine.currentState)")
