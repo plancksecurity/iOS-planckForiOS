662a2840c1b446673b5f5ecd1078ff89e235a3e1
Dirk Zimmermann <dirk@pep-project.org>
2017-06-07 13:53:14 +0200

    IOS-578: state machine for inbox sync

--- Playgrounds/FSMPlayground.playground/Contents.swift
+++ Playgrounds/FSMPlayground.playground/Contents.swift
@@ -35,12 +35,12 @@ class StateMachine<T: Hashable, U: Hashable> {
         stateEventTable[transitionIndex] = target
     }
 
-    func add(source: T, input: U, target: T) {
-        let ti = TransitionIndex(source: source, input: input)
-        add(transitionIndex: ti, target: target)
+    func when(inState: T, onInput: U, transitTo: T) {
+        let ti = TransitionIndex(source: inState, input: onInput)
+        add(transitionIndex: ti, target: transitTo)
     }
 
-    func executeOnEnter(state: T, block: @escaping DidEnterStateBlock) {
+    func executeAfterEntering(state: T, block: @escaping DidEnterStateBlock) {
         stateExecutionBlocks[state] = block
     }
 
@@ -66,50 +66,85 @@ class StateMachine<T: Hashable, U: Hashable> {
 }
 
 enum InboxSyncState {
-    case fetchFolders
-    case fetchMessages
-    case syncMessages
-    case idle
-    case sendSmtp
-    case sendImap
-    case saveDrafts
+    case fetchingFolders
+    case fetchingMessages
+    case syncingMessages
+    case idling
+    case sendingSmtp
+    case sendingImap
+    case savingDrafts
 }
 
 enum InboxSyncInput {
-    case foldersFetched
-    case messagesFetched
+    case fetchingFoldersDone
+    case fetchingMessagesDone
+    case syncingMessagesDone
+    case sendingSmtpDone
+    case sendingImapDone
+    case savingDraftsDone
 
     case idle
     case sendMails
     case saveDrafts
     case repeatSync
+    case sendSmtp
 }
 
 func addFakeAction(
     machine: StateMachine<InboxSyncState, InboxSyncInput>,
     whenEnteringState: InboxSyncState, inputOnFinished: InboxSyncInput) {
-    machine.executeOnEnter(state: whenEnteringState) {
-        print("Executing: \(whenEnteringState)")
+    machine.executeAfterEntering(state: whenEnteringState) {
+        print("  Executing: \(whenEnteringState)")
         DispatchQueue.global().async {
-            sleep(1)
+            Thread.sleep(forTimeInterval: 0.1)
             machine.accept(input: inputOnFinished)
         }
     }
 }
 
-let machine: StateMachine<InboxSyncState, InboxSyncInput> = StateMachine(currentState: InboxSyncState.fetchFolders)
+func waitForState(machine: StateMachine<InboxSyncState, InboxSyncInput>, state: InboxSyncState) {
+    while machine.currentState != state {
+        //print("state \(machine.currentState)")
+        Thread.sleep(forTimeInterval: 0.1)
+    }
+    print("  > reached state: \(machine.currentState)")
+}
 
-addFakeAction(machine: machine, whenEnteringState: .fetchFolders, inputOnFinished: .foldersFetched)
-addFakeAction(machine: machine, whenEnteringState: .fetchMessages, inputOnFinished: .messagesFetched)
+let machine: StateMachine<InboxSyncState, InboxSyncInput> = StateMachine(currentState: InboxSyncState.fetchingFolders)
 
-machine.add(source: .fetchFolders, input: .foldersFetched, target: .fetchMessages)
-machine.add(source: .fetchMessages, input: .messagesFetched, target: .syncMessages)
-machine.add(source: .idle, input: .repeatSync, target: .fetchFolders)
+machine.when(inState: .fetchingFolders, onInput: .fetchingFoldersDone, transitTo: .fetchingMessages)
+machine.when(inState: .fetchingMessages, onInput: .fetchingMessagesDone, transitTo: .syncingMessages)
+machine.when(inState: .syncingMessages, onInput: .syncingMessagesDone, transitTo: .idling)
+machine.when(inState: .sendingSmtp, onInput: .sendingSmtpDone, transitTo: .sendingImap)
+machine.when(inState: .sendingImap, onInput: .sendingImapDone, transitTo: .idling)
+machine.when(inState: .savingDrafts, onInput: .savingDraftsDone, transitTo: .idling)
+machine.when(inState: .idling, onInput: .repeatSync, transitTo: .syncingMessages)
+machine.when(inState: .idling, onInput: .sendSmtp, transitTo: .sendingSmtp)
+machine.when(inState: .idling, onInput: .saveDrafts, transitTo: .savingDrafts)
 
+//////////////////////////
+// Testing
+//////////////////////////
+
+addFakeAction(machine: machine, whenEnteringState: .fetchingFolders, inputOnFinished: .fetchingFoldersDone)
+addFakeAction(machine: machine, whenEnteringState: .fetchingMessages, inputOnFinished: .fetchingMessagesDone)
+addFakeAction(machine: machine, whenEnteringState: .syncingMessages, inputOnFinished: .syncingMessagesDone)
+addFakeAction(machine: machine, whenEnteringState: .sendingSmtp, inputOnFinished: .sendingSmtpDone)
+addFakeAction(machine: machine, whenEnteringState: .sendingImap, inputOnFinished: .sendingImapDone)
+addFakeAction(machine: machine, whenEnteringState: .savingDrafts, inputOnFinished: .savingDraftsDone)
+
+print("* starting machine ...")
 machine.start()
 
-while machine.currentState != .syncMessages {
-    //print("state \(machine.currentState)")
-    Thread.sleep(forTimeInterval: 0.1)
-}
-print("finished state: \(machine.currentState)")
+waitForState(machine: machine, state: .idling)
+print("* requesting another sync ...")
+machine.accept(input: .repeatSync)
+waitForState(machine: machine, state: .idling)
+print("* requesting SMTP ...")
+machine.accept(input: .sendSmtp)
+waitForState(machine: machine, state: .idling)
+print("* requesting draft save ...")
+machine.accept(input: .saveDrafts)
+waitForState(machine: machine, state: .idling)
+
+print("* finished ...")
