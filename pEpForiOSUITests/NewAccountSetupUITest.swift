d2d9c191d43b6e5c7060262c731c62b1d2e93e4e
Dirk Zimmermann <dirk@pep-project.org>
2017-06-09 12:49:55 +0200

    IOS-139 fix UI test (new account)

--- pEpForiOSUITests/NewAccountSetupUITest.swift
+++ pEpForiOSUITests/NewAccountSetupUITest.swift
@@ -105,7 +105,7 @@ class NewAccountSetupUITest: XCTestCase {
     }
 
     func testNewAccountSetup() {
-        let apeiosccount = UITestData.workingAccount1
+        let account = UITestData.workingAccount1
         newAccountSetup(account: account)
         waitForever()
     }
