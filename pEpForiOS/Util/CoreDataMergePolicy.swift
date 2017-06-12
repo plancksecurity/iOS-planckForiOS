20af78385e5aab6800fa3369f3b7e55067229e72
Dirk Zimmermann <dirk@pep-project.org>
2017-06-12 14:26:18 +0200

    IOS-583: merge policy for debugging

--- pEpForiOS/Util/CoreDataMergePolicy.swift
+++ pEpForiOS/Util/CoreDataMergePolicy.swift
@@ -11,28 +11,49 @@ import CoreData
 
 class CoreDataMergePolicy: NSMergePolicy {
     override func resolve(mergeConflicts list: [Any]) throws {
-        do {
+        if let mcList = list as? [NSMergeConflict] {
+            try mergeConflicts(originalError: nil, mergeConflicts: mcList)
+        } else {
             try super.resolve(mergeConflicts: list)
-        } catch (let error) {
-            guard let mcList = list as? [NSMergeConflict] else {
-                throw error
-            }
-            try mergeConflicts(error: error, mergeConflicts: mcList)
         }
     }
 
     override func resolve(optimisticLockingConflicts list: [NSMergeConflict]) throws {
-        do {
-            try super.resolve(optimisticLockingConflicts: list)
-        } catch (let error) {
-            try mergeConflicts(error: error, mergeConflicts: list)
+        try mergeConflicts(originalError: nil, mergeConflicts: list)
+    }
+
+    func mergeConflicts(originalError: Error?, mergeConflicts: [NSMergeConflict]) throws {
+        print("originalError: \(String(describing: originalError))")
+        for mc in mergeConflicts {
+            print("mc \(mc)")
+            guard
+                let objSnapshot = mc.objectSnapshot, let cachedSnapshot = mc.cachedSnapshot else {
+                    print("No object snapshot or cached snapshot")
+                    return
+            }
+            if let persistedSnapshot = mc.persistedSnapshot {
+                print("2. Between the cached state at the Persistent store coordinator and the external store (file, database, etc.).")
+                diff(dict1: cachedSnapshot, dict2: persistedSnapshot)
+            } else {
+                print("1. Between the Managed object context and its in-memory cached state at the Persistent store coordinator layer.")
+                diff(dict1: objSnapshot, dict2: cachedSnapshot)
+            }
         }
     }
 
-    func mergeConflicts(error: Error, mergeConflicts: [NSMergeConflict]) throws {
-        print("error: \(error)")
-        for mc in mergeConflicts {
-            print("mc \(mc)")
+    func diff(dict1: [String:Any], dict2: [String:Any]) {
+        var keysDict1 = Set<String>()
+        for k in dict1.keys {
+            keysDict1.insert(k)
+            guard let v1 = dict1[k], let v2 = dict2[k] else {
+                print("key \(k) only in dict1")
+                continue
+            }
+            let o1 = v1 as AnyObject
+            print("Comparing \(k): \(o1)")
+            if !o1.isEqual(v2) {
+                print("*** Diff: \(k): \(v1) \(v2)")
+            }
         }
     }
 }
