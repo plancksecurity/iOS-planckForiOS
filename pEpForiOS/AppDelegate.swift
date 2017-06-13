5ccd4c7cdcf8bb16709f2d3302706ccdfc54cff6
Dirk Zimmermann <dirk@pep-project.org>
2017-06-13 09:15:26 +0200

    IOS-583 not using any special merge policy

--- pEpForiOS/AppDelegate.swift
+++ pEpForiOS/AppDelegate.swift
@@ -155,8 +155,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let objectModel = MessageModelData.MessageModelData()
         do {
             try Record.loadCoreDataStack(managedObjectModel: objectModel)
-            Record.set(
-                mergePolicy: CoreDataMergePolicy(merge: .mergeByPropertyStoreTrumpMergePolicyType))
         } catch {
             Log.error(component: comp, errorString: "Error while Loading DataStack")
         }
