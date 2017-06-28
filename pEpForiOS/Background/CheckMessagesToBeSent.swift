2691b1d3980b5e63d6d3ca7df5bb547c173b06e6
Dirk Zimmermann <dirk@pep-project.org>
2017-06-28 07:14:00 +0200

    IOS-575 op for determining if there are messages waiting to be sent

---
+++ pEpForiOS/Background/CheckMessagesToBeSent.swift
@@ -0,0 +1,37 @@
+//
+//  CheckMessagesToBeSent.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 27.06.17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+import MessageModel
+
+class CheckMessagesToBeSent: ConcurrentBaseOperation {
+    var hasMessagesReadyToBeSent = false
+
+    public override init(parentName: String? = nil,
+                errorContainer: ServiceErrorProtocol = ErrorContainer()) {
+        super.init(parentName: parentName, errorContainer: errorContainer)
+    }
+
+    override func main() {
+        let context = Record.Context.background
+        context.perform {
+            self.process(context: context)
+        }
+    }
+
+    func process(context: NSManagedObjectContext) {
+        if let _ = EncryptAndSendOperation.retrieveNextMessage(context: context) {
+            hasMessagesReadyToBeSent = true
+        } else {
+            hasMessagesReadyToBeSent = false
+        }
+        markAsFinished()
+    }
+}
