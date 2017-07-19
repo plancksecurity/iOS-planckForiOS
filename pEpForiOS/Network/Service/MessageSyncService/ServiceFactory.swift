6a7cff1ba9fe330c890a9b17359d73e45c7665f9
Dirk Zimmermann <dirk@pep-project.org>
2017-07-19 09:34:07 +0200

    IOS-233 more parentNames for better test debugging

--- pEpForiOS/Network/Service/MessageSyncService/ServiceFactory.swift
+++ pEpForiOS/Network/Service/MessageSyncService/ServiceFactory.swift
@@ -16,18 +16,18 @@ class ServiceFactory {
         imapSyncData: ImapSyncData, smtpSendData: SmtpSendData,
         smtpSendServiceDelegate: SmtpSendServiceDelegate?) -> ServiceExecutionProtocol {
         let fetchFoldersService = FetchFoldersService(
-            parentName: #function, backgrounder: backgrounder, imapSyncData: imapSyncData)
+            parentName: parentName, backgrounder: backgrounder, imapSyncData: imapSyncData)
 
         let smtpService = SmtpSendService(
-            parentName: #function, backgrounder: backgrounder,
+            parentName: parentName, backgrounder: backgrounder,
             imapSyncData: imapSyncData, smtpSendData: smtpSendData)
         smtpService.delegate = smtpSendServiceDelegate
 
         let fetchMessagesService = FetchMessagesService(
-            parentName: #function, backgrounder: backgrounder, imapSyncData: imapSyncData)
+            parentName: parentName, backgrounder: backgrounder, imapSyncData: imapSyncData)
 
         let syncMessagesService = SyncExistingMessagesService(
-            parentName: #function, backgrounder: backgrounder, imapSyncData: imapSyncData)
+            parentName: parentName, backgrounder: backgrounder, imapSyncData: imapSyncData)
 
         let chainedService = ServiceChainExecutor()
         chainedService.add(services: [fetchFoldersService, smtpService,
@@ -40,10 +40,10 @@ class ServiceFactory {
         parentName: String?, backgrounder: BackgroundTaskProtocol?,
         imapSyncData: ImapSyncData) -> ServiceExecutionProtocol {
         let fetchMessagesService = FetchMessagesService(
-            parentName: #function, backgrounder: backgrounder, imapSyncData: imapSyncData)
+            parentName: parentName, backgrounder: backgrounder, imapSyncData: imapSyncData)
 
         let syncMessagesService = SyncExistingMessagesService(
-            parentName: #function, backgrounder: backgrounder, imapSyncData: imapSyncData)
+            parentName: parentName, backgrounder: backgrounder, imapSyncData: imapSyncData)
 
         let chainedService = ServiceChainExecutor()
         chainedService.add(services: [fetchMessagesService, syncMessagesService])
