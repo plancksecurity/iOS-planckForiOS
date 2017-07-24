bd9bec4e73bb88d922d3f2059c76ab5e8dbed059
buff <andreas@pep-project.org>
2017-07-24 16:13:00 +0200

    seperates helper method from actual tests

--- pEpForiOSTests/AccountVerificationServiceTests.swift
+++ pEpForiOSTests/AccountVerificationServiceTests.swift
@@ -39,31 +39,6 @@ class AccountVerificationServiceTests: XCTestCase {
         persistentSetup = nil
     }
 
-    func testVerification(account: Account, expectedResult: AccountVerificationResult,
-                          testDirectly: Bool) {
-        let expVerified = expectation(description: "account verified")
-        let delegate = AccountVerificationTestDelegate(expVerified: expVerified)
-
-        let asService = AccountVerificationService()
-        let msService = MessageSyncService()
-
-        if testDirectly {
-            asService.delegate = delegate
-            asService.verify(account: account)
-        } else {
-            msService.requestVerification(account: account, delegate: delegate)
-        }
-
-        waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
-            XCTAssertNil(error)
-            guard let result = delegate.verificationResult else {
-                XCTFail()
-                return
-            }
-            XCTAssertEqual(result, expectedResult)
-        })
-    }
-
     func testDirectlySuccess() {
         testVerification(account: TestData().createWorkingAccount(),
                          expectedResult: AccountVerificationResult.ok,
@@ -100,5 +75,32 @@ class AccountVerificationServiceTests: XCTestCase {
             expectedResult: AccountVerificationResult.smtpError(
                 .connectionTimedOut("connectionTimedOut(_:theNotification:)")),
             testDirectly: false)
+    }
+
+    // MARK: HELPER
+
+    func testVerification(account: Account, expectedResult: AccountVerificationResult,
+                          testDirectly: Bool) {
+        let expVerified = expectation(description: "account verified")
+        let delegate = AccountVerificationTestDelegate(expVerified: expVerified)
+
+        let asService = AccountVerificationService()
+        let msService = MessageSyncService()
+
+        if testDirectly {
+            asService.delegate = delegate
+            asService.verify(account: account)
+        } else {
+            msService.requestVerification(account: account, delegate: delegate)
+        }
+
+        waitForExpectations(timeout: TestUtil.waitTime, handler: { error in
+            XCTAssertNil(error)
+            guard let result = delegate.verificationResult else {
+                XCTFail()
+                return
+            }
+            XCTAssertEqual(result, expectedResult)
+        })
     }
 }
