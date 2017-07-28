589628a54f91272bbece7c5fbf7aa25001deed23
buff <andreas@pep-project.org>
2017-07-28 11:21:24 +0200

    adds CWServiceClient error notification parser

---
+++ pEpForiOS/Util/Extensions/Notification+CWServiceClientNotificationParsing.swift
@@ -0,0 +1,25 @@
+//
+//  Notification+CWServiceClientNotificationParsing.swift
+//  pEpForiOS
+//
+//  Created by buff on 28.07.17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import MessageModel
+
+
+// MARK: - Parses notifications sent by CWServiceClient protocol methods for actual error messages.
+extension Notification {
+    func parseErrorMessageBadResponse() -> String {
+        guard let pantomimeError = self.userInfo?["PantomimeErrorInfo"] as? [String:String],
+            let errorMsg = pantomimeError["PantomimeBadResponseInfoKey"]
+            else {
+                return "Bad response"
+        }
+
+        return errorMsg
+    }
+}
+
