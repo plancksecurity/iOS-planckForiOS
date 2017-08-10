843c298f75b26d30cb581a75d643c73f886b1745
Dirk Zimmermann <dirk@pep-project.org>
2017-08-10 11:44:38 +0200

    IOS-288 HtmlSaxParser that doesn't parse HTML

--- pEpForiOS/HTMLParser/HtmlSaxParser.swift
+++ pEpForiOS/HTMLParser/HtmlSaxParser.swift
@@ -14,6 +14,8 @@ class HtmlSaxParser: NSObject {
     let inputData: Data
     var output: String?
 
+    var acceptCharacters = false
+
     init?(string: String) {
         if let data = string.data(using: String.Encoding.utf8) {
             self.inputData = data
@@ -25,6 +27,10 @@ class HtmlSaxParser: NSObject {
     func parse() {
         let parser = XMLParser(data: inputData)
         parser.delegate = self
+        let success = parser.parse()
+        if !success {
+            output = nil
+        }
     }
 }
 
@@ -32,14 +38,31 @@ extension HtmlSaxParser: XMLParserDelegate {
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?,
                 qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
         Log.shared.info(component: #function, content: "\(elementName)")
+        if elementName == "body" {
+            acceptCharacters = true
+        } else if elementName == "img" {
+            print("img!")
+        } else if elementName == "br" {
+            print("br!")
+        }
     }
 
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?,
                 qualifiedName qName: String?) {
         Log.shared.info(component: #function, content: "/\(elementName)")
+        if elementName == "body" {
+            acceptCharacters = false
+        }
     }
 
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         Log.shared.info(component: #function, content: "\(string)")
+        if acceptCharacters {
+            print("\(string)")
+        }
+    }
+
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("error: \(parseError)")
     }
 }
