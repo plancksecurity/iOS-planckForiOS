8be3d55d02799b632174b79a126ba6c08d64e826
Dirk Zimmermann <dirk@pep-project.org>
2017-08-18 12:28:26 +0200

    IOS-201 rm attach documents

--- pEpForiOS/UI/Compose/ComposeTableViewController.swift
+++ pEpForiOS/UI/Compose/ComposeTableViewController.swift
@@ -323,13 +323,6 @@ class ComposeTableViewController: UITableViewController {
         }
     }
 
-    public final func addAttachment() {
-        let documentPicker = UIDocumentPickerViewController(
-            documentTypes: ["public.data"], in: .import)
-        documentPicker.delegate = self
-        present(documentPicker, animated: true, completion: nil)
-    }
-
     // MARK: - Table view data source
 
     override func tableView(
@@ -599,11 +592,8 @@ extension ComposeTableViewController: MessageBodyCellDelegate {
             title: NSLocalizedString("Insert Photo",
                                      comment: "Insert photo/video in message text context menu"),
             action: #selector(addMediaToCell))
-        let attachment = UIMenuItem(
-            title: NSLocalizedString("Insert Document",
-                                     comment: "Insert document in message text context menu"),
-            action: #selector(addAttachment))
-        menuController.menuItems = [media, attachment]
+
+        menuController.menuItems = [media]
     }
 
     func didEndEditing(at indexPath: IndexPath) {
@@ -681,18 +671,6 @@ extension ComposeTableViewController: UIImagePickerControllerDelegate {
 
         tableView.updateSize()
         dismiss(animated: true, completion: nil)
-    }
-}
-
-// MARK: - UIDocumentPickerDelegate
-
-extension ComposeTableViewController: UIDocumentPickerDelegate {
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
-        guard let cell = tableView.cellForRow(at: currentCell) as? MessageBodyCell else {
-            return
-        }
-        cell.add(createAttachment(assetUrl: url))
-        tableView.updateSize()
     }
 }
 
