6afaeee34cbaffec31c6c466351595b9eadbcac2
Dirk Zimmermann <dirk@pep-project.org>
2017-08-21 10:45:45 +0200

    IOS-649 fix top constraint in attachments view

--- pEpForiOS/UI/EmailDisplay/Util/AttachmentsView.swift
+++ pEpForiOS/UI/EmailDisplay/Util/AttachmentsView.swift
@@ -55,8 +55,8 @@ class AttachmentsView: UIView {
         }
 
         // distance to the top
-        topAnchor.constraint(
-            equalTo: attachmentViewContainers[0].view.topAnchor, constant: spacing).isActive = true
+        attachmentViewContainers[0].view.topAnchor.constraint(
+            equalTo: topAnchor, constant: spacing).isActive = true
 
         // distance to the bottom
         let cBottom = bottomAnchor.constraint(
