b9041b324ed3ccdefdb91d3677ba5d0c39d1b08f
Xavier Algarra <xavier@pep-project.org>
2017-08-29 16:22:47 +0200

    IOS-116 extension moved from message model to pEp_f...

--- pEpForiOS/Util/Extensions/NSAttributedString+pEp.swift
+++ pEpForiOS/Util/Extensions/NSAttributedString+pEp.swift
@@ -52,3 +52,18 @@ extension NSAttributedString {
                 theDelegate.attachments)
     }
 }
+
+extension NSMutableAttributedString {
+    @discardableResult public func bold(_ text:String) -> NSMutableAttributedString {
+        let attrs:[String:AnyObject] = [NSFontAttributeName : UIFont.preferredFont(forTextStyle: .callout)]
+        let boldString = NSMutableAttributedString(string:"\(text)", attributes:attrs)
+        self.append(boldString)
+        return self
+    }
+
+    @discardableResult public func normal(_ text:String)->NSMutableAttributedString {
+        let normal =  NSAttributedString(string: text)
+        self.append(normal)
+        return self
+    }
+}
