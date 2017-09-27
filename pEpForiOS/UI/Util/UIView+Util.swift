bc1e44f59a3a3c463390843ccd8befb7ae3b2c6b
Dirk Zimmermann <dirk@pep-project.org>
2017-09-27 12:04:11 +0200

    IOS-740 rm manual gradient layer handling code

--- pEpForiOS/UI/Util/UIView+Util.swift
+++ pEpForiOS/UI/Util/UIView+Util.swift
@@ -58,16 +58,4 @@ extension UIView {
             v.removeFromSuperview()
         }
     }
-
-    func applyGradient(colours: [UIColor]) -> Void {
-        self.applyGradient(colours: colours, locations: nil)
-    }
-
-    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> Void {
-        let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.frame = self.bounds
-        gradient.colors = colours.map { $0.cgColor }
-        gradient.locations = locations
-        self.layer.insertSublayer(gradient, at: 0)
-    }
 }
