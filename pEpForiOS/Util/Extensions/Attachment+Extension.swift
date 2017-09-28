5caef19f1f2fbc97cdb1d75a035fd9b30258b146
Dirk Zimmermann <dirk@pep-project.org>
2017-09-28 14:50:02 +0200

    IOS-732 correctly filter by attachment

--- pEpForiOS/Util/Extensions/Attachment+Extension.swift
+++ pEpForiOS/Util/Extensions/Attachment+Extension.swift
@@ -15,12 +15,7 @@ extension Attachment {
      Can this attachment be shown in the app?
      */
     public func isViewable() -> Bool {
-        let unviewableMimeTypes = Set([
-            "application/pgp-keys",
-            "application/pgp-signature",
-            "image/gif"] // IOS-577
-        )
-        if data == nil || unviewableMimeTypes.contains(mimeType.lowercased()) {
+        if data == nil || Filter.unviewableMimeTypes.contains(mimeType.lowercased()) {
             return false
         }
         return true
