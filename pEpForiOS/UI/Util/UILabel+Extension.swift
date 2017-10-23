88c5cc19e09d9a16fc3c3c43211a044910e58b12
Xavier Algarra <xavier@pep-project.org>
2017-10-23 11:36:27 +0200

    IOS-719 body cell now can be copied

--- pEpForiOS/UI/Util/UILabel+Extension.swift
+++ pEpForiOS/UI/Util/UILabel+Extension.swift
@@ -18,3 +18,47 @@ extension UILabel {
         self.textColor = UIColor.red
     }
 }
+
+class CopyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    func sharedInit() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(
+            target: self, action:#selector(showMenu)))
+    }
+
+    @objc func showMenu(sender: AnyObject?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+    override func copy(_ sender: Any?) {
+        let board = UIPasteboard.general
+        board.string = text
+        let menu = UIMenuController.shared
+        menu.setMenuVisible(false, animated: true)
+    }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return action == #selector(UIResponderStandardEditActions.copy)
+    }
+
+}
