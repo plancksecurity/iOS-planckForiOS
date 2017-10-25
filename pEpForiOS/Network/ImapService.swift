89805c585da6ba3530c1308cf9f298ccbd8316b2
buff <andreas@pep-project.org>
2017-10-25 11:13:43 +0200

    IOS-694 re-sets maxFetchCount to 20

--- pEpForiOS/Network/ImapService.swift
+++ pEpForiOS/Network/ImapService.swift
@@ -111,7 +111,7 @@ open class ImapSync: Service {
     }
     //476-SOI
 
-    open var maxFetchCount: UInt = 2 //BUFF: 20
+    open var maxFetchCount: UInt = 20
 
     var capabilities: Set<String> {
         return service.capabilities()
