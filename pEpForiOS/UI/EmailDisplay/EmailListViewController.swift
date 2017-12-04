78f4d989c4c95f58575352d1fd5d4a951eb4d60d
buff <andreas@pep-project.org>
2017-12-04 13:55:14 +0100

    IOS-827 removes obsolete if clause

--- pEpForiOS/UI/EmailDisplay/EmailListViewController.swift
+++ pEpForiOS/UI/EmailDisplay/EmailListViewController.swift
@@ -267,39 +267,35 @@ class EmailListViewController: BaseTableViewController, SwipeTableViewCellDelega
     // MARK: - SwipeTableViewCellDelegate
 
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        if indexPath.section == 0 {
-            /// Create swipe actions, taking the currently displayed folder into account
-            var swipeActions = [SwipeAction]()
-            let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-                self.deleteAction(forCellAt: indexPath)
-            }
-            configure(action: deleteAction, with: .trash)
-            swipeActions.append(deleteAction)
-
-            let flagAction = SwipeAction(style: .default, title: "Flag") { action, indexPath in
-                self.flagAction(forCellAt: indexPath)
-            }
-            flagAction.hidesWhenSelected = true
-            configure(action: flagAction, with: .flag)
-            swipeActions.append(flagAction)
-
-            guard let folder = folderToShow else {
-                Log.shared.errorAndCrash(component: #function, errorString: "No folder")
-                return nil
-            }
-            if folder.folderType != .drafts {
-                // Do not add "more" actions (reply...) to drafted mails.
-                let moreAction = SwipeAction(style: .default, title: "More") { action, indexPath in
-                    self.moreAction(forCellAt: indexPath)
-                }
-                moreAction.hidesWhenSelected = true
-                configure(action: moreAction, with: .more)
-                swipeActions.append(moreAction)
-            }
-            return (orientation == .right ?   swipeActions : nil)
+        /// Create swipe actions, taking the currently displayed folder into account
+        var swipeActions = [SwipeAction]()
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            self.deleteAction(forCellAt: indexPath)
         }
+        configure(action: deleteAction, with: .trash)
+        swipeActions.append(deleteAction)
 
-        return nil
+        let flagAction = SwipeAction(style: .default, title: "Flag") { action, indexPath in
+            self.flagAction(forCellAt: indexPath)
+        }
+        flagAction.hidesWhenSelected = true
+        configure(action: flagAction, with: .flag)
+        swipeActions.append(flagAction)
+
+        guard let folder = folderToShow else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No folder")
+            return nil
+        }
+        if folder.folderType != .drafts {
+            // Do not add "more" actions (reply...) to drafted mails.
+            let moreAction = SwipeAction(style: .default, title: "More") { action, indexPath in
+                self.moreAction(forCellAt: indexPath)
+            }
+            moreAction.hidesWhenSelected = true
+            configure(action: moreAction, with: .more)
+            swipeActions.append(moreAction)
+        }
+        return (orientation == .right ?   swipeActions : nil)
     }
 
     func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
