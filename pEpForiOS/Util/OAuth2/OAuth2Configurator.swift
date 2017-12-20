9d78c0b16a7cf1fe3a6586cb071e6eb9d9d484bd
Dirk Zimmermann <dirk@pep-project.org>
2017-12-20 11:40:07 +0100

    IOS-570 secret OAuth2GmailConfig.swift

---
+++ pEpForiOS/Util/OAuth2/OAuth2Configurator.swift
@@ -0,0 +1,18 @@
+//
+//  OAuth2Configurator.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 20.12.17.
+//  Copyright © 2017 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class OAuth2Configurator {
+    func oauth2ConfigFor(oauth2Type: OAuth2Type) -> OAuth2ConfigurationProtocol {
+        switch oauth2Type {
+        case .google:
+            return OAuth2GmailConfig() // create your own OAuth2GmailConfig
+        }
+    }
+}
