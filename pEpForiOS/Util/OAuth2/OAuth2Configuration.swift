3c3aa7210f67a618458b0774ca14812ff091064a
Dirk Zimmermann <dirk@pep-project.org>
2018-01-11 12:36:02 +0100

    IOS-569 basic implementation of OAuth2ConfigurationProtocol

---
+++ pEpForiOS/Util/OAuth2/OAuth2Configuration.swift
@@ -0,0 +1,40 @@
+//
+//  OAuth2Configuration.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 11.01.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+struct OAuth2Configuration: OAuth2ConfigurationProtocol {
+    let oauth2Type: OAuth2Type
+    let scopes: [String]
+    let clientID: String
+    let redirectURL: URL
+
+    init?(oauth2Type: OAuth2Type, scopes: [String], plistName: String,
+         clientIDKey: String, redirectURLKey: String) {
+        self.oauth2Type = oauth2Type
+        self.scopes = scopes
+        guard let filePath = Bundle.main.path(forResource: plistName, ofType: "plist") else {
+            return nil
+        }
+        guard let settings = NSDictionary(contentsOfFile: filePath) else {
+            return nil
+        }
+        guard let clientID = settings.object(forKey: clientIDKey) as? String else {
+            return nil
+        }
+        guard let redirectURLString = settings.object(forKey: redirectURLKey) as? String else {
+            return nil
+        }
+        guard let redirectURL = URL(string: redirectURLString) else {
+            return nil
+        }
+
+        self.clientID = clientID
+        self.redirectURL = redirectURL
+    }
+}
