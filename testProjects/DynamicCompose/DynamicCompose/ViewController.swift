1d18881daf6ba4b1bf11d6358b8f4b8e440a0bf1
Dirk Zimmermann <dirk@pep-project.org>
2018-02-28 12:38:13 +0100

    IOS-833 test project (initial)

---
+++ testProjects/DynamicCompose/DynamicCompose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicCompose
+//
+//  Created by Dirk Zimmermann on 28.02.18.
+//  Copyright © 2018 pEp Security AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
