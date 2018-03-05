edf9be3791e5ac7d0942f8c4bac910759b6ec979
buff <andreas@pep-project.org>
2018-03-05 15:32:28 +0100

    IOS-873 adds settings view controller

---
+++ pEpForiOS/UI/Settings/SettingUnecryptedSubjectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingUnecryptedSubjectViewController.swift
+//  pEp
+//
+//  Created by Andreas Buff on 05.03.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class SettingUnecryptedSubjectViewController: SettingBaseViewController {
+    override func handleSwitchChange() {
+        AppSettings().unecryptedSubjectEnabled = !`switch`.isOn
+    }
+
+    override func setSwitchValue() {
+        `switch`.isOn = !AppSettings().unecryptedSubjectEnabled
+    }
+}
