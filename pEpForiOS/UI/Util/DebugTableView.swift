d76493e12ce55b1a277107eec1300cccaeb01d62
Dirk Zimmermann <dirk@pep-project.org>
2018-03-06 12:26:27 +0100

    IOS-833 DebugTableView with keyboard observer

--- pEpForiOS/UI/Util/DebugTableView.swift
+++ pEpForiOS/UI/Util/DebugTableView.swift
@@ -12,9 +12,55 @@ import UIKit
  Meant for debugging layout problems involving table views.
  */
 class DebugTableView: UITableView {
+    var keyboardObserver: Any?
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addObservers()
+    }
+
+    deinit {
+        removeObservers()
+    }
+
+    override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        if newWindow == nil {
+            removeObservers()
+        } else {
+            addObservers()
+        }
+    }
+
+    func addObservers() {
+        if keyboardObserver == nil {
+            keyboardObserver = NotificationCenter.default.addObserver(
+                forName: NSNotification.Name.UIKeyboardDidShow, object: nil,
+                queue: OperationQueue.main) { [weak self] notification in
+                    self?.keyboardDidShow(notification: notification)
+            }
+        }
+    }
+
+    func removeObservers() {
+        if let kObs = keyboardObserver {
+            NotificationCenter.default.removeObserver(kObs)
+            keyboardObserver = nil
+        }
+    }
+
+    func keyboardDidShow(notification: Notification) {
+        print("\(#function)")
+    }
+
     override var contentOffset: CGPoint {
         didSet {
             print("contentOffset: \(contentOffset)")
         }
+    }
+
+    override func scrollToRow(at indexPath: IndexPath, at scrollPosition: UITableViewScrollPosition,
+                              animated: Bool) {
+        super.scrollToRow(at: indexPath, at: scrollPosition, animated: animated)
     }
 }
