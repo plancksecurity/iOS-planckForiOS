850eae838f7b2df5d8e0158614ae4b8cab28b315
buff <andreas@pep-project.org>
2018-03-14 11:12:36 +0100

    IOS-836 adds docu

--- pEpForiOS/UI/Util/SecureWebViewController/CidHandler.swift
+++ pEpForiOS/UI/Util/SecureWebViewController/CidHandler.swift
@@ -10,11 +10,15 @@ import WebKit
 import MessageModel
 
 @available(iOS, introduced: 11.0)
+
 /// WKURLSchemeHandler subclass to handle cid: URLs (images inlined in mails).
-/// Provides content from MessageModel for a certain content ID.
+/// Provides content from MessageModel for a content IDs requested by a WKWebview instance.
 class CidHandler : NSObject {
     static let cidSchemeID = "security.pep.SecureWebViewController.setupSchemeHandler"
 
+    /// Call this method once to let this class handle dic: URLs.
+    ///
+    /// - Parameter config: webview configuration to setup this handler for
     static func setup(config: WKWebViewConfiguration) {
         config.setURLSchemeHandler(CidHandler(), forURLScheme: cidSchemeID)
     }
