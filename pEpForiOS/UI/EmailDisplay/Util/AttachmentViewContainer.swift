1c5dc68761b9ed1dab55add7c9863f51fea95a95
buff <andreas@pep-project.org>
2018-03-16 18:00:05 +0100

    IOS-872 removes now obsolete code

--- pEpForiOS/UI/EmailDisplay/Util/AttachmentViewContainer.swift
+++ pEpForiOS/UI/EmailDisplay/Util/AttachmentViewContainer.swift
@@ -13,7 +13,4 @@ import MessageModel
 struct AttachmentViewContainer {
     let view: UIView
     let attachment: Attachment
-
-    /** Should this be displayed as an image inline (that is, is the view an `UIImageView`)? */
-    let isInlineImage: Bool
 }
