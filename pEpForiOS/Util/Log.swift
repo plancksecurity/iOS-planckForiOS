0029f1caebeb35e0ccceb761051ad00e7eb23caa
Dirk Zimmermann <dirk@pep-project.org>
2018-03-22 14:04:42 +0100

    IOS-991 do not log to pEp session

--- pEpForiOS/Util/Log.swift
+++ pEpForiOS/Util/Log.swift
@@ -31,14 +31,6 @@ import MessageModel
             // If running in the debugger, dump to the console right away
             print("\(entity): \(description)")
         #endif
-        if !MiscUtil.isUnitTest() {
-            loggingQueue.addOperation() {
-                if self.logEnabled && !self.paused {
-                    self.session.logTitle(
-                        self.title, entity: entity, description: description, comment: comment)
-                }
-            }
-        }
     }
 
     func resume() {
