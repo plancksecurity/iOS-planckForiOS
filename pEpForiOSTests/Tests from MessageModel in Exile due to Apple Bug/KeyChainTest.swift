45f6604eda636c8cc8d6cd006a5a1476f36e1b78
buff <andreas@pep-project.org>
2018-04-25 12:42:05 +0200

    IOS-1055 tests for total number of keychain items

--- pEpForiOSTests/Tests from MessageModel in Exile due to Apple Bug/KeyChainTest.swift
+++ pEpForiOSTests/Tests from MessageModel in Exile due to Apple Bug/KeyChainTest.swift
@@ -15,16 +15,27 @@ import XCTest
  */
 class KeyChainTest: XCTestCase {
     let pass = "0001"
+    var numItemsBefore = 0
+    var numAdded = 0
+
+    override func setUp() {
+        super.setUp()
+        numItemsBefore = KeyChain.numKeychainItems(testCase: self)
+        numAdded = 0
+    }
 
     func testStore() {
         let key = UUID().uuidString
+        numAdded += 1
         XCTAssertTrue(KeyChain.updateCreateOrDelete(password: pass, forKey: key))
         XCTAssertEqual(pass, KeyChain.password(key: key))
+        XCTAssertTrue(isCorrectTotalNumberOfKeychainItems())
     }
 
     func testUpdate() {
         let key = UUID().uuidString
         // Save password
+        numAdded += 1
         XCTAssertTrue(KeyChain.updateCreateOrDelete(password: pass, forKey: key))
         // Assure it has been saved
         XCTAssertEqual(pass, KeyChain.password(key: key))
@@ -34,17 +45,27 @@ class KeyChainTest: XCTestCase {
         // Assure it has been updates
         XCTAssertEqual(newpass, KeyChain.password(key: key),
                        "Password has been updated")
+        XCTAssertTrue(isCorrectTotalNumberOfKeychainItems())
     }
 
     func testDelete() {
         let key = UUID().uuidString
         // Save password
+        numAdded += 1
         XCTAssertTrue(KeyChain.updateCreateOrDelete(password: pass, forKey: key))
         // Assure it has been saved
         XCTAssertEqual(pass, KeyChain.password(key: key))
-        // Delete Pasword
+        // Delete Password
+        numAdded -= 1
         XCTAssertTrue(KeyChain.updateCreateOrDelete(password: nil, forKey: key))
         // Assure it has been deleted
         XCTAssertNil(KeyChain.password(key: key))
+        XCTAssertTrue(isCorrectTotalNumberOfKeychainItems())
+    }
+
+    private func isCorrectTotalNumberOfKeychainItems() -> Bool {
+        let expected = numItemsBefore + numAdded
+        let actual = KeyChain.numKeychainItems(testCase: self)
+        return actual == expected
     }
 }
