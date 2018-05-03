4f89a947d9a130af2445bf572ed553626071aa0a
Dirk Zimmermann <dirk@pep-project.org>
2018-05-03 09:23:56 +0200

    IOS-1069 resize subject text view when text is entered

---
+++ pEpForiOS/UI/Compose/SubjectComposeTextView.swift
@@ -0,0 +1,15 @@
+//
+//  SubjectComposeTextView.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 02.05.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class SubjectComposeTextView: ComposeTextView {
+    public override func layoutAfterTextDidChange(tableView: UITableView) {
+        tableView.updateSize()
+    }
+}
