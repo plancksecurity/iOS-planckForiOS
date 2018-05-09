53245fd37024f5e48ef9a796efead75f9761c68a
buff <andreas@pep-project.org>
2018-05-09 11:09:18 +0200

    IOS-663 fixes: messages can be moved to folder it is already in

--- pEpForiOS/UI/MoveToFolder/FolderCellViewModel+MoveToFolder.swift
+++ pEpForiOS/UI/MoveToFolder/FolderCellViewModel+MoveToFolder.swift
@@ -18,12 +18,16 @@ extension FolderCellViewModel {
     /// - creates a copy of the original messages in the folder represented by this model
     ///
     /// - Parameter message: message to move here
-    func moveIn(message: Message) { //BUFF: HERE: tripple check, then append
+    func moveIn(message: Message) { //TODO: tripple check(better clone?), then append
+        if message.parent == folder {
+            // the message already is in the target folder. No need to move it.
+            return
+        }
         let orig = message
         // Create a new message in this folder
         let copy = Message(message: orig)
         copy.parent = folder
-        copy.save() //TODO: assure updateOrCreate doe *update* !
+        copy.save() //TODO: assure updateOrCreate *does create* !
         // Mark original msg /deleted
         orig.imapFlags?.deleted = true
         orig.save()
