97a82ad09965fe7fc53c6a553176cc62a5a88887
buff <andreas@pep-project.org>
2018-05-09 18:39:14 +0200

    IOS-663 we probably ain't gonna need it

--- pEpForiOS/UI/MoveToFolder/FolderCellViewModel+MoveToFolder.swift
+++ pEpForiOS/UI/MoveToFolder/FolderCellViewModel+MoveToFolder.swift
@@ -11,25 +11,27 @@ import MessageModel
 // MARK: - MoveToFolder Extentions
 
 extension FolderCellViewModel {
+    
+//IOS-663: wait for decition. Append or UIDPLUS. In any case, move this to Message extension.
 
-    /// Movesw a given message in the folder represented by this model.
-    ///
-    /// - marks original message as /deleted
-    /// - creates a copy of the original messages in the folder represented by this model
-    ///
-    /// - Parameter message: message to move here
-    func moveIn(message: Message) { //TODO: tripple check(better clone?), then append
-        if message.parent == folder {
-            // the message already is in the target folder. No need to move it.
-            return
-        }
-        let orig = message
-        // Create a new message in this folder
-        let copy = Message(message: orig)
-        copy.parent = folder
-        copy.save() //TODO: assure updateOrCreate *does create* !
-        // Mark original msg /deleted
-        orig.imapFlags?.deleted = true
-        orig.save()
-    }
+//    /// Movesw a given message in the folder represented by this model.
+//    ///
+//    /// - marks original message as /deleted
+//    /// - creates a copy of the original messages in the folder represented by this model
+//    ///
+//    /// - Parameter message: message to move here
+//    func moveIn(message: Message) { //TODO: tripple check(better clone?), then append
+//        if message.parent == folder {
+//            // the message is already in the target folder. No need to move it.
+//            return
+//        }
+//        let orig = message
+//        // Create a new message in this folder
+//        let copy = Message(message: orig)
+//        copy.parent = folder
+//        copy.save() //TODO: assure updateOrCreate *does create* !
+//        // Mark original msg /deleted
+//        orig.imapFlags?.deleted = true
+//        orig.save()
+//    }
 }
