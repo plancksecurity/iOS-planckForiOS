2ecc0ab44cfa13ef56380c05f452a17fc22c52e1
buff <andreas@pep-project.org>
2018-05-10 11:53:42 +0200

    IOS-663 adds MoveToFolderOperation skeleton

---
+++ pEpForiOS/Background/MoveToFolderOperation.swift
@@ -0,0 +1,13 @@
+//
+//  MoveToFolderOperation.swift
+//  pEp
+//
+//  Created by Andreas Buff on 10.05.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class MoveToFolderOperation: ImapSyncOperation {
+
+}
