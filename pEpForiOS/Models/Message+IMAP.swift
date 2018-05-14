78d40d301186a26c7897d542d8b5e1435611cb20
buff <andreas@pep-project.org>
2018-05-14 15:53:59 +0200

    IOS-663 on step further to ger rid of trashedStatus++

--- pEpForiOS/Models/Message+IMAP.swift
+++ pEpForiOS/Models/Message+IMAP.swift
@@ -11,24 +11,20 @@ import MessageModel
 extension Message {
     /// Sets flag "deleted" and sets trashed status to "trashed".
     /// Use this method if you do not want the message to be copied to trash folder.
-    func imapDeleteAndMarkTrashed() {
+    func imapDeleteAndMarkTrashed() {  //IOS-663: rename
         let theFlags = imapFlags ?? ImapFlags()
         theFlags.deleted = true
-        imapFields?.trashedStatus = .trashed
-        imapFlags = theFlags
         self.save()
     }
 
-    /// Sets flag "deleted" and marks the message to be copied to trash if appropriate.
-    func imapDelete() {
-        let theFlags = imapFlags ?? ImapFlags()
-        theFlags.deleted = true
-        imapFlags = theFlags
-        imapFields?.trashedStatus =
-            parent.shouldCopyDeletedMessagesToTrash ? .shouldBeTrashed : .trashed
-        imapFields?.uidMoveToTrashStatus =
-            parent.shouldUidMoveDeletedMessagesToTrash ? .shouldBeMoved : .none
-        self.save()
+    /// Marks message to move to Trash folder
+    func imapDelete() { //IOS-663: renmae (imapTrash?)
+        guard let trashFolder = parent.account.folder(ofType: .trash) else {
+            Log.shared.errorAndCrash(component: #function,
+                                     errorString: "We should have a trash folder at this point")
+            return
+        }
+        move(to: trashFolder)
     }
 
     /// Marks the message for moving to the given folder.
@@ -36,7 +32,7 @@ extension Message {
     /// - Parameter targetFolder: folder to move the message to
     func move(to targetFolder:Folder) {
         if parent == targetFolder {
-            // the message is already in the target folder. No need to move it.
+            // the message is in the target folder already. No need to move it.
             return
         }
         self.targetFolder = targetFolder
