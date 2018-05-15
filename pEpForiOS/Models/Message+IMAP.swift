8a4bbb3d8a246203e21b0a49b4ab87df89e4ada5
buff <andreas@pep-project.org>
2018-05-15 21:11:30 +0200

    IOS-663 one method handles "user has chosen to delete a message".

--- pEpForiOS/Models/Message+IMAP.swift
+++ pEpForiOS/Models/Message+IMAP.swift
@@ -9,22 +9,29 @@
 import MessageModel
 
 extension Message {
-    /// Sets flag "deleted" and sets trashed status to "trashed".
-    /// Use this method if you do not want the message to be copied to trash folder.
-    func imapDeleteAndMarkTrashed() {  //IOS-663: rename
+
+    /// Sets flag "deleted".
+    /// Use this method if you do not want the message to be moved to trash folder.
+    func imapMarkDeleted() {  //IOS-663: rename
         let theFlags = imapFlags ?? ImapFlags()
         theFlags.deleted = true
         self.save()
     }
 
-    /// Marks message to move to Trash folder
-    func imapDelete() { //IOS-663: renmae (imapTrash?)
+    /// Triggers trashing of the message, taking everithing in account (provider specific constrains and such).
+    /// Always use this method to handle "user has choosen to delete an e-mail".
+    func imapDelete() {
         guard let trashFolder = parent.account.folder(ofType: .trash) else {
             Log.shared.errorAndCrash(component: #function,
                                      errorString: "We should have a trash folder at this point")
             return
         }
-        move(to: trashFolder)
+
+        if parent.shouldUidMoveDeletedMessagesToTrash {
+            move(to: trashFolder)
+        } else {
+            imapMarkDeleted()
+        }
     }
 
     /// Marks the message for moving to the given folder.
