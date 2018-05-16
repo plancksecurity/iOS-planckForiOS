cfeb66f18b3e5a3ffce547ecbad8411610f045a0
buff <andreas@pep-project.org>
2018-05-16 15:04:27 +0200

    IOS-663 adds som docu

--- pEpForiOS/Models/Message+IMAP.swift
+++ pEpForiOS/Models/Message+IMAP.swift
@@ -9,7 +9,7 @@
 import MessageModel
 
 extension Message {
-
+    
     /// Sets flag "deleted".
     /// Use this method if you do not want the message to be moved to trash folder.
     func imapMarkDeleted() {
@@ -17,7 +17,7 @@ extension Message {
         theFlags.deleted = true
         self.save()
     }
-
+    
     /// Triggers trashing of the message, taking everithing in account (provider specific constrains and such).
     /// Always use this method to handle "user has choosen to delete an e-mail".
     func imapDelete() {
@@ -26,15 +26,20 @@ extension Message {
                                      errorString: "We should have a trash folder at this point")
             return
         }
-
+        
         if parent.shouldUidMoveDeletedMessagesToTrash {
             move(to: trashFolder)
         } else {
             imapMarkDeleted()
         }
     }
-
+    
     /// Marks the message for moving to the given folder.
+    ///
+    /// Does not actually move the message but set it's target folder.
+    /// The Backgound layer has to take care of the actual move.
+    /// 
+    /// Returns immediately in case the message is in the target folder already.
     ///
     /// - Parameter targetFolder: folder to move the message to
     func move(to targetFolder:Folder) {
