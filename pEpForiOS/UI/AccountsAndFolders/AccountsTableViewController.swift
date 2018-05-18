f573693270cefef879bfd842604a0d1ca61633c2
Hussein Kasem <devnull@localhost>
2018-05-18 16:40:59 +0200

    Remove unused import

--- pEpForiOS/UI/AccountsAndFolders/AccountsTableViewController.swift
+++ pEpForiOS/UI/AccountsAndFolders/AccountsTableViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2016 p≡p Security S.A. All rights reserved.
 //
 
-import MessageModel
 import SwipeCellKit
 
 class AccountsTableViewController: BaseTableViewController, SwipeTableViewCellDelegate {
