d82a021a4226e265f5c8460d522c8a24a4167831
Dirk Zimmermann <dirk@pep-project.org>
2018-05-24 10:20:11 +0200

    updated build test script

--- scripts/co_build.sh
+++ scripts/co_build.sh
@@ -1,5 +1,5 @@
 git clone https://pep-security.lu/gitlab/misc/ldns/
-git clone https://github.com/fdik/libetpan.git
+git clone https://github.com/dirkz/libetpan.git
 git clone https://github.com/x2on/OpenSSL-for-iPhone.git
 git clone https://github.com/SwipeCellKit/SwipeCellKit.git
 git clone https://github.com/openid/AppAuth-iOS
@@ -14,6 +14,7 @@ hg clone https://pep.foundation/dev/repos/libAccountSettings/
 hg clone https://pep-security.ch/dev/repos/pEp_for_iOS/
 
 pushd ldns && git co IOS-749 ; popd
+pushd SwipeCellKit && git co 2.0.1 ; popd
 
 pushd pEp_for_iOS && xcodebuild -workspace pEpForiOS.xcworkspace/ -scheme pEp ; popd
 
