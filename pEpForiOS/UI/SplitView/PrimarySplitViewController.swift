d60975c380ddda80d9710950836d16eff83c41ad
borja <borja@helm.cat>
2018-05-25 18:08:49 +0200

    Improve iphone plus split navigation

--- pEpForiOS/UI/SplitView/PrimarySplitViewController.swift
+++ pEpForiOS/UI/SplitView/PrimarySplitViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class PrimarySplitViewController: UISplitViewController, UISplitViewControllerDelegate {
-    
+
     override func viewDidLoad() {
         self.delegate = self
         preferredDisplayMode = .allVisible
     }
-    
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
-        return true
-    }
-    
-}
 
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController:UIViewController,
+                             onto primaryViewController:UIViewController) -> Bool {
+        guard let navigationController = secondaryViewController as? UINavigationController,
+            navigationController.rootViewController is EmailViewController
+            else {
+                return true
+        }
+
+        return false
+    }
+
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             separateSecondaryFrom primaryViewController: UIViewController)
+        -> UIViewController? {
+
+            guard let navigationController =
+                    splitViewController.viewControllers.first as? UINavigationController,
+                let secondaryNavigationController =
+                    navigationController.topViewController as? UINavigationController,
+                secondaryNavigationController.rootViewController is EmailViewController
+                else {
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "noMessagesViewController")
+                    return vc
+            }
+            return secondaryNavigationController
+    }
+}
