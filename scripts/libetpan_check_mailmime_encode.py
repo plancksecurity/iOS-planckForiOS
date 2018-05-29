92e7c100993fa998262ccb4b044fda0baae7cdfc
Dirk Zimmermann <dirk@pep-project.org>
2018-05-29 12:56:57 +0200

    ENGINE-2 libetpan bisect script: incorporate skip (125)

--- scripts/libetpan_check_mailmime_encode.py
+++ scripts/libetpan_check_mailmime_encode.py
@@ -19,11 +19,6 @@ def clean_xcodebuild():
     call(clean_command)
     return call(base_command)
 
-# Returns true if the given file name exists.
-def check_existence(file_name):
-    the_file = Path(file_name)
-    return the_file.is_file()
-
 # The build creates artefacts that are tracked. They must be removed
 # before being able to check out to another commit.
 def clean_repo():
@@ -35,14 +30,14 @@ if __name__ == '__main__':
     clean_repo() # clean up repo
 
     # Check what this means for bisect.
-    # We want to find out the commit that included mailmime_encode.h in
-    # the linkfarm.
-    # Old behavior is not having mailmime_encode.h (-> exit code 0),
-    # new behavior is having it (-> exit code 1).
-    the_file_name = 'include/libetpan/mailmime_encode.h'
-    if check_existence(the_file_name):
-        print('new: found {filename:s}'.format(filename = the_file_name))
-        sys.exit(1)
-    else:
-        print('old: did not find {filename:s}'.format(filename = the_file_name))
+    include_directory = Path('include/libetpan')
+    the_file_name = include_directory.joinpath('mailmime_encode.h')
+    if not include_directory.is_dir():
+        print('no include directory at all: {dir}'.format(dir = include_directory))
+        sys.exit(125)
+    elif the_file_name.is_file():
+        print('old: found {filename}'.format(filename = the_file_name))
         sys.exit(0)
+    else:
+        print('new: did not find {filename}'.format(filename = the_file_name))
+        sys.exit(1)
