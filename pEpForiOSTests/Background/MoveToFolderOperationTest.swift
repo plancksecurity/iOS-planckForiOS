facd79e0ea375adbb982e76d9f3758bb6f13277e
buff <andreas@pep-project.org>
2018-05-30 11:02:31 +0200

    cosmetic

--- pEpForiOSTests/Background/MoveToFolderOperationTest.swift
+++ pEpForiOSTests/Background/MoveToFolderOperationTest.swift
@@ -84,7 +84,7 @@ class MoveToFolderOperationTest: CoreDataDrivenTestBase {
     }
 
     private func move(messages:[Message], toFolerOfType type: FolderType) {
-        for msg:Message in messages {
+        for msg in messages {
             guard let targetFolder = msg.parent.account.folder(ofType: type) else {
                 // Can't seem to find the target folder. If this is an optional test
                 // (working on for certain accounts), ignore it.
