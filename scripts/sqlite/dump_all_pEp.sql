578a88a42bb0ac7abf4ee624fd043b357a3d4a8a
Dirk Zimmermann <dirk@pep-project.org>
2018-06-01 08:15:28 +0200

    IOS-1096 sql script for dumping pEp database

---
+++ scripts/sqlite/dump_all_pEp.sql
@@ -0,0 +1,20 @@
+.out alternate_user_id.txt
+select * from alternate_user_id;
+.out pgp_keypair.txt
+select * from pgp_keypair;
+.out trust.txt
+select * from trust;
+.out blacklist_keys.txt
+select * from blacklist_keys;
+.out mistrusted_keys.txt
+select * from mistrusted_keys;
+.out revoked_keys.txt
+select * from revoked_keys;
+.out version_info.txt
+select * from version_info;
+.out identity.txt
+select * from identity;
+.out person.txt
+select * from person;
+.out sequences.txt
+select * from sequences;
