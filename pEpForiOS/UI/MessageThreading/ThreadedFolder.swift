be828b50c86fe76db37bf2062bb2c46a17012982
Dirk Zimmermann <dirk@pep-project.org>
2018-06-05 09:43:24 +0200

    IOS-559 ThreadedFolder: noting top messages

---
+++ pEpForiOS/UI/MessageThreading/ThreadedFolder.swift
@@ -0,0 +1,56 @@
+//
+//  ThreadedFolder.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 05.06.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+import MessageModel
+
+class ThreadedFolder: ThreadAwareFolderProtocol {
+    let underlyingFolder: Folder
+
+    init(folder: Folder) {
+        underlyingFolder = folder
+    }
+
+    func allMessages() -> [Message] {
+        let originalMessages = underlyingFolder.allMessages()
+
+        var topMessages = [Message]()
+        var childMessagesAlreadyReferenced = Set<MessageID>()
+
+        for aMsg in originalMessages {
+            if !childMessagesAlreadyReferenced.contains(aMsg.messageID) {
+                // this is a top message
+                topMessages.append(aMsg)
+
+                // note all children, in order to prevent to interpret them as
+                // top messages when they are encountered
+                for ref in aMsg.references {
+                    childMessagesAlreadyReferenced.insert(ref)
+                }
+            }
+        }
+        return topMessages
+    }
+
+    func numberOfMessagesInThread(message: Message) -> Int {
+        return 0
+    }
+
+    func childMessagesInThread(message: Message) -> [Message] {
+        return []
+    }
+
+    func deleteSingle(message: Message) {
+        message.imapDelete()
+    }
+
+    func deleteThread(message: Message) {
+        deleteSingle(message: message)
+    }
+}
