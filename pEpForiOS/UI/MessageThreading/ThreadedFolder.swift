b2fb3afcd3d218e75b04b582cb880724388ea14e
Dirk Zimmermann <dirk@pep-project.org>
2018-06-20 10:15:58 +0200

    IOS-559 bye gatherTopMessagesWithReferences()

--- pEpForiOS/UI/MessageThreading/ThreadedFolder.swift
+++ pEpForiOS/UI/MessageThreading/ThreadedFolder.swift
@@ -67,31 +67,4 @@ class ThreadedFolder: ThreadedMessageFolderProtocol {
 
         return topMessages
     }
-
-    private func gatherTopMessagesWithReferences() -> (topMessages: [Message],
-        messageIdsReferenced: Set<MessageID>) {
-            let originalMessages = underlyingFolder.allMessagesNonThreaded()
-
-            var topMessages = [Message]()
-            var messageIdsReferenced = Set<MessageID>()
-
-            // gather references
-            for aMsg in originalMessages {
-                MessageModel.performAndWait {
-                    aMsg.referencedMessages().forEach {
-                        messageIdsReferenced.insert($0.messageID)
-                        print("*** \(aMsg.messageID) -> \($0.messageID)")
-                    }
-                }
-            }
-
-            // gather top messages
-            for aMsg in originalMessages {
-                if !messageIdsReferenced.contains(aMsg.messageID) {
-                    topMessages.append(aMsg)
-                }
-            }
-
-            return (topMessages: topMessages, messageIdsReferenced: messageIdsReferenced)
-    }
 }
