a535f469d27aede8521d4340c480b8f4d081702c
Dirk Zimmermann <dirk@pep-project.org>
2018-06-26 08:08:14 +0200

    IOS-559 EmailListViewModel+ThreadingTests skeleton

--- pEpForiOSTests/Models/EmailListViewModel+ThreadingTests.swift
+++ pEpForiOSTests/Models/EmailListViewModel+ThreadingTests.swift
@@ -15,6 +15,9 @@ class EmailListViewModel_ThreadingTests: CoreDataDrivenTestBase {
     var account: Account!
     var inbox: Folder!
     var topMessages = [Message]()
+    let emailListViewModelDelegate = MyEmailListViewModelDelegate()
+    let messageSyncServiceProtocol = MyMessageSyncServiceProtocol()
+    var emailListViewModel: EmailListViewModel!
 
     override func setUp() {
         super.setUp()
@@ -27,11 +30,75 @@ class EmailListViewModel_ThreadingTests: CoreDataDrivenTestBase {
         for i in 1...5 {
             let msg = Message.init(uuid: "\(i)", parentFolder: inbox)
             msg.save()
-
         }
+
+        emailListViewModel = EmailListViewModel(
+            emailListViewModelDelegate: emailListViewModelDelegate,
+            messageSyncService: messageSyncServiceProtocol,
+            folderToShow: inbox)
     }
 
     func testIncomingUnthreaded() {
         XCTAssertTrue(true)
+    }
+
+    // MARK - Internal
+
+    class MyMessageSyncServiceProtocol: MessageSyncServiceProtocol {
+        weak var errorDelegate: MessageSyncServiceErrorDelegate?
+        weak var sentDelegate: MessageSyncServiceSentDelegate?
+        weak var syncDelegate: MessageSyncServiceSyncDelegate?
+        weak var stateDelegate: MessageSyncServiceStateDelegate?
+        weak var flagsUploadDelegate: MessageSyncFlagsUploadDelegate?
+
+        func requestVerification(account: Account, delegate: AccountVerificationServiceDelegate) {
+        }
+
+        func requestFetchOlderMessages(inFolder folder: Folder) {
+        }
+
+        func requestDraft(message: Message) {
+        }
+
+        func requestSend(message: Message) {
+        }
+
+        func requestFlagChange(message: Message) {
+        }
+
+        func requestMessageSync(folder: Folder) {
+        }
+
+        func start(account: Account) {
+        }
+
+        func cancel(account: Account) {
+        }
+    }
+
+    class MyEmailListViewModelDelegate: EmailListViewModelDelegate {
+        func emailListViewModel(viewModel: EmailListViewModel,
+                                didInsertDataAt indexPath: IndexPath) {
+        }
+
+        func emailListViewModel(viewModel: EmailListViewModel,
+                                didUpdateDataAt indexPath: IndexPath) {
+        }
+
+        func emailListViewModel(viewModel: EmailListViewModel,
+                                didRemoveDataAt indexPath: IndexPath) {
+        }
+
+        func toolbarIs(enabled: Bool) {
+        }
+
+        func showUnflagButton(enabled: Bool) {
+        }
+
+        func showUnreadButton(enabled: Bool) {
+        }
+
+        func updateView() {
+        }
     }
 }
