b82ee2e117293c6274bac819cda8d81c6267f593
buff <andreas@pep-project.org>
2018-07-02 11:24:47 +0200

    IOS-33 test prototype up and running

--- pEpForiOSTests/Features/TrustedServerTest.swift
+++ pEpForiOSTests/Features/TrustedServerTest.swift
@@ -69,15 +69,23 @@ import CoreData
  - Mail is not Re-uploaded
  */
 class TrustedServerTest: CoreDataDrivenTestBase {
-    var senderAccount: Account!
-
     override func setUp() {
         super.setUp()
 
-        // Account 1 (sender)
+        // // Account on trusted server (sender)
         cdAccount.identity?.userName = "unittest.ios.3"
         cdAccount.identity?.userID = "unittest.ios.3_ID"
         cdAccount.identity?.address = "unittest.ios.3@peptest.ch"
+        guard
+            let cdServerImap = cdAccount.server(type: .imap),
+            let imapCredentials = cdServerImap.credentials,
+            let cdServerSmtp = cdAccount.server(type: .smtp),
+            let smtpCredentials = cdServerSmtp.credentials else {
+                XCTFail("Problem in setup")
+                return
+        }
+        imapCredentials.loginName = "unittest.ios.3@peptest.ch"
+        smtpCredentials.loginName = "unittest.ios.3@peptest.ch"
         try! TestUtil.importKeyByFileName(session,
                                           fileName:
             "unittest_ios_3_peptest_ch_550A_9E62_6822_040E_57CB_151A_651C_4A5D_B15B_77A3_sec.asc")
@@ -87,80 +95,163 @@ class TrustedServerTest: CoreDataDrivenTestBase {
         try! session.setOwnKey(cdAccount.identity!.pEpIdentity(),
                                fingerprint: "550A9E626822040E57CB151A651C4A5DB15B77A3")
         TestUtil.skipValidation()
-        Record.saveAndWait()
-        senderAccount = cdAccount.account()
-        senderAccount.save()
-        TrustedServerTestUtils.setServersTrusted(forAccount: senderAccount, testCase: self)
         cdAccount.createRequiredFoldersAndWait(testCase: self)
     }
 
-/*
+    /*
      1) Send unencrypted
      - Send unencrypted mail
      Check: Is msg in Sent folder unencrypted (*not* encrypted for self)
      */
-    func testMailSendUnencrypted() {
-        guard let sender = senderAccount.user, let id2 = cdAccount2.identity else {
-            XCTFail("We all loose identity ...")
+    func testMailSendUnencrypted_senderTrusted_receiverUntrusted() {
+        TrustedServerTestUtils.setServersTrusted(forCdAccount: cdAccount, testCase: self)
+        guard
+            let sender = cdAccount.identity,
+            let receiver = createForeignReceiverIdentityNoKnownKey().cdIdentity() else {
+                XCTFail("No identities")
+                return
+        }
+
+        // Send mail and sync
+        let sentMails = try! TestUtil.createOutgoingMails(cdAccount: cdAccount,
+                                                          fromIdentity: sender,
+                                                          toIdentity: receiver,
+                                                          setSentTimeOffsetForManualOrdering: false,
+                                                          testCase: self,
+                                                          numberOfMails: 1,
+                                                          withAttachments: false,
+                                                          attachmentsInlined: false,
+                                                          encrypt: false)
+        XCTAssertEqual(sentMails.count, 1)
+        guard let sentMail = sentMails.first, let sentUuid = sentMail.uuid else {
+            XCTFail("Problem")
+            return
+        }
+        TestUtil.makeFolderInteresting(folderType: .sent, cdAccount: cdAccount)
+        TestUtil.syncAndWait(numAccountsToSync: 1, testCase: self, skipValidation: true)
+
+        let sentFolder = TestUtil.cdFolder(ofType: .sent, in: cdAccount)
+        guard let sentFolderName = sentFolder.name else {
+            XCTFail("Problem")
             return
         }
 
-        // Sync both acocunts and remember what we got before starting the actual test
+        // Everything as expected?
+        guard
+            let cdMsg = CdMessage.search(uid: nil,
+                                         uuid: sentUuid,
+                                         folderName: sentFolderName,
+                                         inAccount: cdAccount),
+            let msg = cdMsg.message()
+            else {
+                XCTFail("Message not found")
+                return
+        }
+
+        let senderRatingOnServer = PEPUtil.pEpRatingFromInt(msg.pEpRatingInt)
+        XCTAssertEqual(senderRatingOnServer, PEP_rating_unencrypted,
+                       "assumed) stored rating on sever")
+        let senderRatingToDisplay = msg.pEpRating()
+        XCTAssertEqual(senderRatingToDisplay, PEP_rating_unencrypted,
+                       "Color to display to user is correct")
+
+        // Now lets see on receiver side.
+        guard let cdAccountReceiver = createAccountOfReceiverWithKeys().cdAccount() else {
+            XCTFail("No account")
+            return
+        }
+        // Fetch, maybe re-upload and fetch again.
         TestUtil.syncAndWait(numAccountsToSync: 2, testCase: self, skipValidation: true)
-//        let msgsInSentAccount1Before = cdAccount.allMessages(inFolderOfType: .sent, sendFrom: id1)
-//        let msgsInInboxAccount2Before = cdAccount2.allMessages(inFolderOfType: .inbox, sendFrom: id1)
 
-        // Create mail(s) from cdAccount to cdAccount2 ...
-        let numMailsToSend = 1
-        let sendKey = try! TestUtil.createOutgoingMails(cdAccount: cdAccount,
-                                                        fromIdentity: id2,
-                                                        toIdentity: id1,
-                                                        testCase: self,
-                                                        numberOfMails: numMailsToSend,
-                                                        withAttachments: false)
-        TestUtil.syncAndWait(numAccountsToSync: 2, testCase: self, skipValidation: true)
-
-        //
-        let mailsToSend = try! TestUtil.createOutgoingMails(cdAccount: cdAccount,
-                                                            fromIdentity: id1,
-                                                            toIdentity: id2,
-                                                            testCase: self,
-                                                            numberOfMails: numMailsToSend,
-                                                            withAttachments: false)
-        XCTAssertEqual(mailsToSend.count, numMailsToSend)
-        Record.saveAndWait()
-
-        // ... send them.
-        TestUtil.syncAndWait(numAccountsToSync: 2, testCase: self, skipValidation: true)
-
-
-
+        // Check inbox for the received message
+        let inbox = TestUtil.cdFolder(ofType: .inbox, in: cdAccountReceiver)
+        guard let inboxFolderName = inbox.name else {
+            XCTFail("Problem")
+            return
+        }
+        guard
+            let cdReceivedMsg = CdMessage.search(uid: nil,
+                                                 uuid: sentUuid,
+                                                 folderName: inboxFolderName,
+                                                 inAccount: cdAccountReceiver),
+            let receivedMsg = cdReceivedMsg.message()
+            else {
+                XCTFail("Message not found")
+                return
+        }
+        let receiverRatingOnServer = PEPUtil.pEpRatingFromInt(receivedMsg.pEpRatingInt)
+        XCTAssertEqual(receiverRatingOnServer, PEP_rating_unencrypted,
+                       "assumed) stored rating on sever")
+        let receiverRatingToDisplay = receivedMsg.pEpRating()
+        XCTAssertEqual(receiverRatingToDisplay, PEP_rating_unencrypted,
+                       "Color to display to user is correct")
     }
 
-    //        let msgsInSentAccount1After = cdAccount.allMessages(inFolderOfType: .sent, sendFrom: id1)
-    //            .sorted { (msg1: CdMessage, msg2: CdMessage) -> Bool in
-    //                return msg1.sent! < msg2.sent!
-    //            }
-    //            .map { $0.message()! }
-    //        let newMailsStartIdx = msgsInSentAccount1After.count - numMailsToSend
-    //        let newMailsEndIdx = msgsInSentAccount1After.count - 1
-    //        let mailsToSendInSentFolder = msgsInSentAccount1After[newMailsStartIdx...newMailsEndIdx]
-    //        // TODO: add test for sent folder
-    //
-    //
-    //        let msgsInInboxAccount2After = cdAccount2.allMessages(inFolderOfType: .inbox, sendFrom: id1)
-    //
-    //        XCTAssertEqual(msgsInSentAccount1After.count,
-    //                       msgsInSentAccount1Before.count + numMailsToSend,
-    //                       "Mails to send are in sent folder of sender")
-    //        let theFetchedOne = 1
-    //        let theOneToReUpload = 1
-    //        XCTAssertEqual(msgsInInboxAccount2After.count,
-    //                       msgsInInboxAccount2Before.count + theFetchedOne + theOneToReUpload)
-    //
-    //        // Sync again to append messages to reupload
-    //        TestUtil.syncAndWait(numAccountsToSync: 2, testCase: self, skipValidation: true)
-    //        let msgsInInboxAccount2AfterReupload = cdAccount2.allMessages(inFolderOfType: .inbox, sendFrom: id1)
+    // MARK: - HELPER
+
+    // MARK: Account / Identity 2 (receiver)
+
+    private func createForeignReceiverIdentityNoKnownKey() -> Identity {
+        let createe = Identity(address: "unittest.ios.4@peptest.ch",
+                                 userID: "unittest.ios.4_ID",
+                                 addressBookID: nil,
+                                 userName: "unittest.ios.4",
+                                 isMySelf: false)
+        createe.save()
+        return createe
+    }
+
+    private func createForeignReceiverIdentityKnownKey() -> Identity {
+        let createe = Identity(address: "unittest.ios.4@peptest.ch",
+                               userID: "unittest.ios.4_ID",
+                               addressBookID: nil,
+                               userName: "unittest.ios.4",
+                               isMySelf: false)
+        try! TestUtil.importKeyByFileName(session,
+                                          fileName:
+            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_pub.asc")
+        createe.save()
+        return createe
+    }
+
+    private func createOwnIdentityReceiverWithKeys() -> Identity {
+        let createe = Identity(address: "unittest.ios.4@peptest.ch",
+                               userID: "unittest.ios.4_ID",
+                               addressBookID: nil,
+                               userName: "unittest.ios.4",
+                               isMySelf: true)
+        createe.save()
+        try! TestUtil.importKeyByFileName(session,
+                                          fileName:
+            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_sec.asc")
+        try! TestUtil.importKeyByFileName(session,
+                                          fileName:
+            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_pub.asc")
+        try! session.setOwnKey(createe.pEpIdentity(),
+                               fingerprint: "66AF 5804 A879 1E01 B407 125A CAF0 D838 1542 49C4")
+        return createe
+    }
+
+    private func createAccountOfReceiverWithKeys() -> Account {
+        let receiver = createOwnIdentityReceiverWithKeys()
+        let cdAccount = SecretTestData().createWorkingCdAccount(number: 1)
+        guard
+            let cdServerImap = cdAccount.server(type: .imap),
+            let imapCredentials = cdServerImap.credentials,
+            let cdServerSmtp = cdAccount.server(type: .smtp),
+            let smtpCredentials = cdServerSmtp.credentials else {
+               fatalError()
+        }
+        imapCredentials.loginName = receiver.address
+        smtpCredentials.loginName = receiver.address
+
+        let createe = cdAccount.account()
+        createe.user = receiver
+        createe.save()
+        TestUtil.skipValidation()
+        createe.cdAccount()?.createRequiredFoldersAndWait(testCase: self)
+        return createe
+    }
 
     //IOS-1141
     //    func testAnonymizedRating() {
@@ -216,74 +307,4 @@ class TrustedServerTest: CoreDataDrivenTestBase {
     //            XCTAssertTrue(rating != PEP_rating_trusted_and_anonymized)
     //        }
     //    }
-
-    // MARK: - HELPER
-
-    // MARK: Account / Identity 2 (receiver)
-
-    private func createForeignReceiverIdentityNoKnownKey() -> Identity {
-        let createe = Identity(address: "unittest.ios.4@peptest.ch",
-                                 userID: "unittest.ios.4_ID",
-                                 addressBookID: nil,
-                                 userName: "unittest.ios.4",
-                                 isMySelf: false)
-        createe.save()
-        return createe
-    }
-
-    private func createForeignReceiverIdentityKnownKey() -> Identity {
-        let createe = Identity(address: "unittest.ios.4@peptest.ch",
-                               userID: "unittest.ios.4_ID",
-                               addressBookID: nil,
-                               userName: "unittest.ios.4",
-                               isMySelf: false)
-        try! TestUtil.importKeyByFileName(session,
-                                          fileName:
-            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_pub.asc")
-        createe.save()
-        return createe
-    }
-
-    private func createOwnIdentityReceiverWithKeys() -> Identity {
-        let createe = Identity(address: "unittest.ios.4@peptest.ch",
-                               userID: "unittest.ios.4_ID",
-                               addressBookID: nil,
-                               userName: "unittest.ios.4",
-                               isMySelf: true)
-        createe.save()
-        try! TestUtil.importKeyByFileName(session,
-                                          fileName:
-            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_sec.asc")
-        try! TestUtil.importKeyByFileName(session,
-                                          fileName:
-            "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_pub.asc")
-        try! session.setOwnKey(createe.pEpIdentity(),
-                               fingerprint: "66AF 5804 A879 1E01 B407 125A CAF0 D838 1542 49C4")
-        return createe
-    }
-
-    private func createAccountOfReceiverWithKeys() -> Account {
-        let receiver = createOwnIdentityReceiverWithKeys()
-        let createe = SecretTestData().createWorkingCdAccount(number: 1).account()
-        createe.user = receiver
-        createe.save()
-        createe.cdAccount()?.createRequiredFoldersAndWait(testCase: self)
-        return createe
-    }
-
-    /*
-     // Account 2
-     cdAccount2 = SecretTestData().createWorkingCdAccount(number: 1)
-     cdAccount2.identity?.userName = "unittest.ios.4"
-     cdAccount2.identity?.userID = "unittest.ios.4_ID"
-     cdAccount2.identity?.address = "unittest.ios.4@peptest.ch"
-     try! TestUtil.importKeyByFileName(session,
-     fileName:
-     "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_sec.asc")
-     try! TestUtil.importKeyByFileName(session,
-     fileName:
-     "unittest_ios_4_peptest_ch_66AF_5804_A879_1E01_B407_125A_CAF0_D838_1542_49C4_pub.asc")
-     try! session.setOwnKey(cdAccount2.identity!.pEpIdentity(),
-     fingerprint: "66AF 5804 A879 1E01 B407 125A CAF0 D838 1542 49C4")
-     */
 }
