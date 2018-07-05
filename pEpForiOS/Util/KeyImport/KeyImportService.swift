48eea3bcf9a6d3ea3074514c1eaddea5552f5548
buff <andreas@pep-project.org>
2018-07-05 18:49:52 +0200

    IOS-1030 send initial key import message

--- pEpForiOS/Util/KeyImport/KeyImportService.swift
+++ pEpForiOS/Util/KeyImport/KeyImportService.swift
@@ -10,9 +10,16 @@ import MessageModel
 
 public class KeyImportService {
     public weak var delegate: KeyImportServiceDelegate?
-
     /// Specifies how long a key import message is valid
     static let ttlKeyImportMessages: TimeInterval = 4 * 60 * 60
+    /// Work queue
+    let queue: OperationQueue
+
+    // MARK: - Life Cycle
+    init() {
+        queue = OperationQueue()
+        queue.name = "pep.security.KeyImportService"
+    }
 
     // MARK: - Working bees
 
@@ -51,6 +58,30 @@ public class KeyImportService {
             delegate?.receivedPrivateKey(forAccount: message.parent.account)
         }
     }
+
+    private func fingerprint(forAccount acc: Account) -> String? {
+        let pEpMe = acc.user.pEpIdentity()
+        do {
+            try PEPSession().mySelf(pEpMe)
+            return pEpMe.fingerPrint
+        } catch {
+            Log.shared.errorAndCrash(component: #function,
+                                     errorString: "myself error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+    private func smtpSendData(for account: Account) -> SmtpSendData? {
+        guard let smtpCI = account.smtpConnectInfo else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No connect info")
+            return nil
+        }
+        return  SmtpSendData(connectInfo: smtpCI)
+    }
+
+    private func smtpSend(for account: Account) -> SmtpSend? {
+        return smtpSendData(for: account)?.smtp
+    }
 }
 
 // MARK: - KeyImportServiceProtocol
@@ -63,9 +94,39 @@ extension KeyImportService: KeyImportServiceProtocol {
     }
 
     /// Call to inform the other device that we would love to start a Key Import session
-    public func sendInitKeyImportMessage(forAccount acccount: Account) {
-        //TODO: send unencrypted message to myself with header: "pEp-key-import: myPubKey_fpr" (assume: without appending to sent folder)
-        fatalError("Unimplemented stub")
+    public func sendInitKeyImportMessage(forAccount account: Account) {
+        //send unencrypted message to myself with header: "pEp-key-import: myPubKey_fpr" (assume: without appending to sent folder)
+        guard let dummyFolder = account.folder(ofType: .sent) else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No folder")
+            return
+        }
+
+        guard let myFpr = fingerprint(forAccount: account) else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No FPR")
+            return
+        }
+
+        let msg = Message(uuid: MessageID.generateUUID(), parentFolder: dummyFolder)
+        msg.optionalFields[Header.pEpKeyImport.rawValue] = myFpr
+
+        // Login OP
+        guard let sendData = smtpSendData(for: account) else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No send data")
+            return
+        }
+        let errorContainer = ErrorContainer()
+        let loginOp = LoginSmtpOperation(smtpSendData: sendData, errorContainer: errorContainer)
+
+        // send OP
+        guard let smtpSend = smtpSend(for: account) else {
+            Log.shared.errorAndCrash(component: #function, errorString: "No smtp")
+            return
+        }
+        let sendOp = SMTPSendOperation(errorrContainer: errorContainer,
+                                       messageToSend: msg,
+                                       smtpSend: smtpSend)
+        // Go!
+        queue.addOperations([loginOp, sendOp], waitUntilFinished: false)
     }
 
     /// Call after a newKeyImportMessage arrived to let the other device know
