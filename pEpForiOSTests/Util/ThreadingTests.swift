f4c39e41034f884fdd30aab182a5c063f1061e09
Dirk Zimmermann <dirk@pep-project.org>
2018-07-06 08:12:14 +0200

    IOS-559 ThreadingTests

---
+++ pEpForiOSTests/Util/ThreadingTests.swift
@@ -0,0 +1,65 @@
+//
+//  ThreadingTests.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 06.07.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+@testable import pEpForiOS
+@testable import MessageModel
+
+class ThreadingTests: CoreDataDrivenTestBase {
+    var account: Account!
+    var inbox: Folder!
+    var topMessages = [Message]()
+
+    // MARK: - setup/teardown
+
+    override func setUp() {
+        super.setUp()
+
+        account = cdAccount.account()
+
+        inbox = Folder.init(name: "INBOX", parent: nil, account: account, folderType: .inbox)
+        inbox.save()
+
+        let trash = Folder.init(name: "Trash", parent: nil, account: account, folderType: .trash)
+        trash.save()
+
+        topMessages.removeAll()
+
+        for i in 1...EmailListViewModel_ThreadingTests.numberOfTopMessages {
+            let msg = createMessage(number: i)
+            topMessages.append(msg)
+            msg.save()
+        }
+    }
+
+    // MARK: - Tests
+
+    func testNoThreads() {
+        FolderThreading.override(factory: ThreadAwareFolderFactory())
+        let threaded = inbox.threadAware()
+        XCTAssertEqual(threaded.allMessages().count, topMessages.count)
+
+        for msg in topMessages {
+            XCTAssertEqual(threaded.messagesInThread(message: msg).count, 0)
+        }
+    }
+
+    // MARK: - Helpers
+
+    func createMessage(number: Int, inFolder folder: Folder? = nil) -> Message {
+        let msg = Message.init(uuid: "\(number)",
+            uid: UInt(number),
+            parentFolder: folder ?? inbox)
+        XCTAssertEqual(msg.uid, UInt(number))
+        msg.pEpRatingInt = Int(PEP_rating_unreliable.rawValue)
+        msg.received = Date.init(timeIntervalSince1970: Double(number))
+        msg.sent = msg.received
+        return msg
+    }
+}
