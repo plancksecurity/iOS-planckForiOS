0516f77574444ea1432ae68d2e69b70ecb7648d5
buff <andreas@pep-project.org>
2018-07-23 12:39:41 +0200

    IOS-1033 adds legacy support

---
+++ pEpForiOS/Network/ConnectInfo/deprecated/LegacyConnectInfoProvider.swift
@@ -0,0 +1,74 @@
+//
+//  LegacyConnectInfoProvider.swift
+//  pEp
+//
+//  Created by Andreas Buff on 23.07.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import MessageModel
+import CoreData
+
+/// Supports usage of deprecated EmailConnectInfo.
+extension MMConnectInfo {
+    @available(*, deprecated, message: "use account instead")
+    var accountObjectID: NSManagedObjectID {
+        var result = NSManagedObjectID()
+        MessageModel.performAndWait { [weak self] in
+            guard let me = self else {
+                Log.shared.errorAndCrash(component: #function, errorString: "Lost myself")
+                return
+            }
+            guard let cdAccount = CdAccount.search(account: me.account) else {
+                Log.shared.errorAndCrash(component: #function, errorString: "No CdAccount")
+                return
+
+            }
+            result = cdAccount.objectID
+        }
+        return result
+    }
+
+    @available(*, deprecated, message: "use server instead")
+    var serverObjectID: NSManagedObjectID {
+        var result = NSManagedObjectID()
+        MessageModel.performAndWait { [weak self] in
+            guard let me = self else {
+                Log.shared.errorAndCrash(component: #function, errorString: "Lost myself")
+                return
+            }
+            let serverType = me.server.serverType
+            guard
+                let cdAccount = CdAccount.search(account: me.account),
+                let cdServer = cdAccount.server(type: serverType) else {
+                    Log.shared.errorAndCrash(component: #function, errorString: "No CdAccount")
+                    return
+            }
+            result = cdServer.objectID
+        }
+        return result
+    }
+
+    @available(*, deprecated, message: "use credentials instead")
+    var credentialsObjectID: NSManagedObjectID {
+        var result = NSManagedObjectID()
+        MessageModel.performAndWait { [weak self] in
+            guard let me = self else {
+                Log.shared.errorAndCrash(component: #function, errorString: "Lost myself")
+                return
+            }
+            let serverType = me.server.serverType
+            guard
+                let cdAccount = CdAccount.search(account: me.account),
+                let cdServer = cdAccount.server(type: serverType),
+                let cdCredentials = cdServer.credentials else {
+                    Log.shared.errorAndCrash(component: #function, errorString: "No CdAccount")
+                    // Return garbage
+                    return
+            }
+
+            result = cdCredentials.objectID
+        }
+        return result
+    }
+}
