ac3382de451d0dd1fb36615a4bfc4ec8b17ab005
buff <andreas@pep-project.org>
2018-08-03 13:07:06 +0200

    IOS-1222 adds mailto URL parsing util and tests

---
+++ pEpForiOS/Util/Extensions/URL+MailTo.swift
@@ -0,0 +1,33 @@
+//
+//  URL+MailTo.swift
+//  pEp
+//
+//  Created by Andreas Buff on 03.08.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+
+/// Handle mailto: URLs here.
+extension URL {
+    static let schemeMailto = "mailto:"
+
+    /// Primitive addess parser.
+    ///
+    /// Exapmle supported URLs:
+    /// "mailto:someone@example.com",
+    /// "mailto:someone@example.com?cc=someone_else@example.com&subject=This%20is%20the%20subject&body=This%20is%20the%20body"
+    ///
+    /// - Returns: the first address of a mailto: URL if parseable, nil otherwize
+    func firstRecipientAddress() -> String? {
+        let schemeStriped = self.absoluteString.replacingOccurrences(of: URL.schemeMailto, with: "")
+        var result: String?
+        if schemeStriped.contains(find: "?") {
+            result = schemeStriped.components(separatedBy: "?").first
+        } else {
+            result = schemeStriped
+        }
+        return result
+    }
+}
---
+++ pEpForiOSTests/Util/Extensions/URL+MailToTest.swift
@@ -0,0 +1,37 @@
+//
+//  URL+MailToTest.swift
+//  pEpForiOSTests
+//
+//  Created by Andreas Buff on 03.08.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+@testable import pEpForiOS
+
+class URL_MailToTest: XCTestCase {
+
+    func testFirstRecipientAddress_onlyOne() {
+        let url = URL(string: "mailto:someone@example.com")
+        let expected = "someone@example.com"
+        assertFirstRecipientAddress(url: url, expecteation: expected)
+    }
+
+    func testFirstRecipientAddress_complex_percentageEscaped() {
+        let url = URL(string: "mailto:someone@example.com" +
+            "?cc=elseone@example.com&subject=This%20is%20the%20subject&body=This%20is%20the%20body")
+        let expected = "someone@example.com"
+        assertFirstRecipientAddress(url: url, expecteation: expected)
+    }
+}
+
+// MARK: - HELPER
+
+private func assertFirstRecipientAddress(url: URL?, expecteation: String) {
+    guard let url = url else {
+        XCTFail("Nothing to test")
+        return
+    }
+    let testee = url.firstRecipientAddress()
+    XCTAssertEqual(testee, expecteation)
+}
