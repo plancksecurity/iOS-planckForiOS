b7a78f0efcd36e711ede964cc1ac934faf785275
Dirk Zimmermann <dirk@pep-project.org>
2018-08-10 11:57:49 +0200

    IOS-1226 SortedSet.replaceObject()

--- pEpForiOS/Util/SortedSet.swift
+++ pEpForiOS/Util/SortedSet.swift
@@ -57,6 +57,17 @@ class SortedSet<T: Equatable>: Sequence {
         set.removeObject(at: index)
     }
     
+    public func replaceObject(at index: Int, with object: T) {
+        objc_sync_enter(self)
+        defer { objc_sync_exit(self) }
+
+        guard isValidIndex(index) else {
+            Log.shared.errorAndCrash(component: #function, errorString: "Index out of range")
+            return
+        }
+        set.replaceObject(at: index, with: object)
+    }
+
     public func object(at index: Int) -> T? {
         objc_sync_enter(self)
         defer { objc_sync_exit(self) }
