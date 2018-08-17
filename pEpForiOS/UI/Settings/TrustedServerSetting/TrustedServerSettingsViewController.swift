6a4fa69f8bd68fdc18c3b4700201e68752804cf6
buff <andreas@pep-project.org>
2018-08-17 14:09:49 +0200

    IOS-1250 in the middle of ...

--- pEpForiOS/UI/Settings/TrustedServerSetting/TrustedServerSettingsViewController.swift
+++ pEpForiOS/UI/Settings/TrustedServerSetting/TrustedServerSettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TrustedServerSettingsViewController: BaseViewController {
+class TrustedServerSettingsViewController: BaseTableViewController {
     let viewModel = TrustedServerSettingsViewModel()
 
 
--- pEpForiOS/UI/Settings/TrustedServerSetting/TrustedServerSettingsViewModel.swift
+++ pEpForiOS/UI/Settings/TrustedServerSetting/TrustedServerSettingsViewModel.swift
@@ -10,10 +10,40 @@ import Foundation
 import MessageModel
 
 struct TrustedServerSettingsViewModel {
+    struct Row: Equatable {
+        let address: String
+        let trusted: Bool
+    }
 
-//    func serversThatHaveNotBeenInLocalNetworkWhenCreatingTheAccount() -> [String] {
-//        CdServer.al
-//
-//    }
+    private(set) var rows = [Row]()
 
+    init() {
+        reset()
+    }
+
+    mutating func toggleOnOff(forRowAt index: Int) {
+        guard index >= 0 && index < rows.count else {
+            Log.shared.errorAndCrash(component: #function, errorString: "Index out of bounds")
+            return
+        }
+        let oldRow = rows[index]
+        let toggeled = Row(address: oldRow.address, trusted: !oldRow.trusted)
+        rows[index] = toggeled
+    }
+
+    mutating private func reset() {
+        let servers = serversAllowedToManuallyTrust()
+        var createes = [Row]()
+        for address in servers {
+            let isTrusted = AppSettings.manuallyTrustedServers.contains(address)
+            createes.append(Row(address: address, trusted: isTrusted))
+        }
+        rows = createes
+    }
+
+    private func serversAllowedToManuallyTrust() -> [String] {
+        let servers = Server.Fetch.allAllowedToManuallyTrust()
+        let addresses = servers.map { $0.address }
+        return addresses
+    }
 }
