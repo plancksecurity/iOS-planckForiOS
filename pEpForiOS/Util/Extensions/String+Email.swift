215923aec126685a6b719979f67616a0000e4a7a
Dirk Zimmermann <dirk@pep-project.org>
2018-08-24 10:48:26 +0200

    IOS-1280 using Substring email checks

--- pEpForiOS/Util/Extensions/String+Email.swift
+++ pEpForiOS/Util/Extensions/String+Email.swift
@@ -41,7 +41,7 @@ extension String {
         let localPart = self[..<indexOfAt]
         let indexOfDomainPart = index(after: indexOfAt)
         let domainPart = self[indexOfDomainPart...]
-        return String(localPart).isValidEmailLocalPart() && String(domainPart).isValidDomain()
+        return localPart.isValidEmailLocalPart() && domainPart.isValidDomain()
     }
 
     /**
@@ -63,87 +63,21 @@ extension String {
      See https://en.wikipedia.org/wiki/Email_address#Domain
      */
     public func isValidDomainDnsLabel() -> Bool {
-        var currentIndex = 0
-
-        let theCount = count
-        if theCount > 63 {
-            return false
-        }
-
-        let lastIndex = theCount - 1
-
-        var haveSeenAlpha = false
-
-        for ch in self {
-            if ch == "-" {
-                if currentIndex == 0 || currentIndex == lastIndex {
-                    return false
-                }
-            } else if String.rangeLatinLetter.contains(ch) ||
-                String.rangeCapitalLatinLetter.contains(ch) {
-                haveSeenAlpha = true
-            } else if !String.rangeNumerical.contains(ch) {
-                return false
-            }
-
-            currentIndex += 1
-        }
-
-        return haveSeenAlpha
+        return self[startIndex..<endIndex].isValidDomainDnsLabel()
     }
 
     /**
      See https://en.wikipedia.org/wiki/Email_address#Domain
      */
     public func isValidDomain() -> Bool {
-        let labels = components(separatedBy: ".")
-        for label in labels {
-            if !label.isValidDomainDnsLabel() {
-                return false
-            }
-        }
-
-        return true
+        return self[startIndex..<endIndex].isValidDomain()
     }
 
     /**
      See https://en.wikipedia.org/wiki/Email_address#Local-part
      */
     public func isValidEmailLocalPart() -> Bool {
-        let theCount = count
-        if theCount > 64 {
-            return false
-        }
-
-        let lastIndex = theCount - 1
-
-        var previousCharWasDot = false
-
-        var currentIndex = 0
-        for ch in self {
-            if ch == "." {
-                if previousCharWasDot {
-                    return false
-                }
-                if currentIndex == 0 || currentIndex == lastIndex {
-                    return false
-                }
-                previousCharWasDot = true
-            } else {
-                previousCharWasDot = false
-
-                let specialChars = Set("!#$%&'*+-/=?^_`{|}~")
-                if !specialChars.contains(ch) &&
-                    !String.rangeNumerical.contains(ch) &&
-                    !String.rangeLatinLetter.contains(ch) &&
-                    !String.rangeCapitalLatinLetter.contains(ch) {
-                    return false
-                }
-            }
-            currentIndex += 1
-        }
-
-        return true
+        return self[startIndex..<endIndex].isValidEmailLocalPart()
     }
 
     /**
