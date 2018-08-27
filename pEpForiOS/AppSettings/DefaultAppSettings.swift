560a26e9ac4f560c0364847ae6b456a204292fc0
Dirk Zimmermann <dirk@pep-project.org>
2018-08-27 09:57:51 +0200

    IOS-1281 separate DefaultAppSettings

---
+++ pEpForiOS/AppSettings/DefaultAppSettings.swift
@@ -0,0 +1,137 @@
+//
+//  DefaultAppSettings.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 27.08.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+import MessageModel
+
+public class DefaultAppSettings: AppSettingsProtocol {
+    init() {
+        setup()
+    }
+
+    public var shouldReinitializePepOnNextStartup: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: AppSettings.keyReinitializePepOnNextStartup)
+        }
+        set {
+            UserDefaults.standard.set(newValue,
+                                      forKey: AppSettings.keyReinitializePepOnNextStartup)
+        }
+    }
+
+    public var unencryptedSubjectEnabled: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: AppSettings.keyUnencryptedSubjectEnabled)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: AppSettings.keyUnencryptedSubjectEnabled)
+            PEPObjCAdapter.setUnEncryptedSubjectEnabled(newValue)
+        }
+    }
+
+    public var threadedViewEnabled: Bool {
+        get {
+            return false
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: AppSettings.keyThreadedViewEnabled)
+        }
+    }
+
+    public var passiveMode: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: AppSettings.keyPassiveMode)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: AppSettings.keyPassiveMode)
+            PEPObjCAdapter.setPassiveModeEnabled(newValue)
+        }
+    }
+
+    /// Address of the default account
+    public var defaultAccount: String? {
+        get {
+            assureDefaultAccountIsSetAndExists()
+            return UserDefaults.standard.string(forKey: AppSettings.keyDefaultAccountAddress)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: AppSettings.keyDefaultAccountAddress)
+        }
+    }
+
+    // MARK: manuallyTrustedServers
+
+    /// Addresses of all accounts the user explicitly trusted
+    public var manuallyTrustedServers: [String] {
+        get {
+            return UserDefaults.standard.stringArray(
+                forKey: AppSettings.keyManuallyTrustedServers) ?? []
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: AppSettings.keyManuallyTrustedServers)
+        }
+    }
+
+    public func isManuallyTrustedServer(address: String) -> Bool {
+        return manuallyTrustedServers.contains(address)
+    }
+
+    public func addToManuallyTrustedServers(address: String) {
+        var addresses = Set(manuallyTrustedServers)
+        addresses.insert(address)
+        manuallyTrustedServers = Array(addresses)
+    }
+
+    public func removeFromManuallyTrustedServers(address: String) {
+        var addresses = Set(manuallyTrustedServers)
+        addresses.remove(address)
+        manuallyTrustedServers = Array(addresses)
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        registerDefaults()
+        setupObjcAdapter()
+    }
+
+    private func setupObjcAdapter() {
+        PEPObjCAdapter.setUnEncryptedSubjectEnabled(unencryptedSubjectEnabled)
+        PEPObjCAdapter.setPassiveModeEnabled(passiveMode)
+    }
+
+    private func registerDefaults() {
+        var defaults = [String: Any]()
+        defaults[AppSettings.keyReinitializePepOnNextStartup] = false
+        defaults[AppSettings.keyUnencryptedSubjectEnabled] = true
+        defaults[AppSettings.keyThreadedViewEnabled] = true
+        defaults[AppSettings.keyPassiveMode] = false
+
+        UserDefaults.standard.register(defaults: defaults)
+    }
+
+    // MARK: - Other
+
+    private func assureDefaultAccountIsSetAndExists() {
+        if UserDefaults.standard.string(forKey: AppSettings.keyDefaultAccountAddress) == nil {
+            // Default account is not set. Take the first MessageModel provides as a starting point
+            let initialDefault = Account.all().first?.user.address
+            UserDefaults.standard.set(initialDefault, forKey: AppSettings.keyDefaultAccountAddress)
+        }
+        // Assure the default account still exists. The user might have deleted it.
+        guard
+            let currentDefault = UserDefaults.standard.string(
+                forKey: AppSettings.keyDefaultAccountAddress),
+            let _ = Account.by(address: currentDefault)
+            else {
+                defaultAccount = nil
+                return
+        }
+    }
+}
