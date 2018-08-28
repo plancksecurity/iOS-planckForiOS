3290bfaf7719b3f765256f71df8ec93472579b1f
buff <andreas@pep-project.org>
2018-08-28 13:08:06 +0200

    IOS-729 moves IMAP related logic to IMAP extension

--- pEpForiOS/Models/FolderType+IMAP.swift
+++ pEpForiOS/Models/FolderType+IMAP.swift
@@ -18,4 +18,20 @@ extension FolderType {
                                             .spam,
                                             .archive,
                                             .all, .flagged] //IOS-729: sure about .all and .flagged?
+
+    /// Flags that should be used when appending mails to a folder of this type.
+    ///
+    /// - Returns:  If flags are defined for this type:: the flags.
+    ///             nil otherwize
+    func defaultAppendImapFlags() -> Message.ImapFlags? {
+        switch self {
+        case .sent:
+            let result = Message.ImapFlags()
+            result.seen = true
+            return result
+        case .archive, .drafts, .inbox, .normal, .trash, .spam, .all, .flagged, .outbox:
+            break
+        }
+        return nil
+    }
 }
