fd9dc015ffa207ce3b595a956e169b3632718a39
buff <andreas@pep-project.org>
2018-08-28 12:58:35 +0200

    IOS-729 removes dead code

--- pEpForiOS/Models/FolderType+IMAP.swift
+++ pEpForiOS/Models/FolderType+IMAP.swift
@@ -9,11 +9,4 @@
 import MessageModel
 
 extension FolderType {
-    /// Whether or not the folder type is most likely realized as virtual mailbox on server side.
-    /// See RFC-6154
-    var isMostLikelyVirtualMailbox: Bool {
-        let virtualMailboxesTypes = [FolderType.all,        // RFC for \All: "When this special use is supported, it is almost certain to represent a virtual mailbox"
-            FolderType.flagged]    // RFC for \Flagged: it is likely to represent a virtual mailbox collecting messages (from other mailboxes) that are marked with the "\Flagged" message flag.
-        return virtualMailboxesTypes.contains(self)
-    }
 }
