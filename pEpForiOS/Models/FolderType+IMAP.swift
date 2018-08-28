22c2a0f171d6cea5b71f852f8dcbd09b8b3cda5c
buff <andreas@pep-project.org>
2018-08-28 18:37:46 +0200

    IOS-729 moved here. It's not the stores business

--- pEpForiOS/Models/FolderType+IMAP.swift
+++ pEpForiOS/Models/FolderType+IMAP.swift
@@ -50,4 +50,99 @@ extension FolderType {
         }
         return nil
     }
+
+    /**
+     `FolderType`s that should be created if they don't yet exist.
+     */
+    public static let requiredTypes = [FolderType.drafts, .sent, .trash]
+
+    /**
+     A list of types to check folders from remote server for categorization.
+     Whenever a folder is created after having been parsed from the server,
+     those types should be checked for matches.
+     */
+    public static let allValuesToCheckFromServer = [drafts, sent, trash, spam, archive] //IOS-729: ckeck
+
+    /**
+     Each folder type has one or more human-readable name you can use to match
+     remote folder names.
+     - Note: This is used *only* for the local-only special folders, and for fuzzy-matching
+     against a known folder type when fetching from the remote server.
+     */
+    public func folderNames() -> [String] {
+        switch self {
+        case .normal:
+            return ["Normal"]
+        case .inbox:
+            // Don't actually use this for the INBOX, always use `ImapSync.defaultImapInboxName`!
+            return ["Inbox"]
+        case .sent:
+            return ["Sent"]
+        case .drafts:
+            return ["Drafts", "Draft"]
+        case .trash:
+            return ["Trash"]
+        case .archive:
+            return ["Archive"]
+        case .spam:
+            return ["Spam", "Junk"]
+        case .all:
+            return ["All"]
+        case .flagged:
+            return ["Flagged"]
+        case .outbox:
+            return [NSLocalizedString("Outbox",
+                                      comment:
+                "Name of folder that holds yet unsend messages (outbox)")]
+        }
+    }
+
+    //FIXME: after new Pantomime enum approach is implemented. It is ugly that MessageModel has to know int values of Pantomime enums
+    /**
+     Folder type for PantomimeSpecialUseMailboxType
+     */
+    public static func from(pantomimeSpecialUseMailboxType: Int) -> FolderType? {
+        switch pantomimeSpecialUseMailboxType {
+        case 0:
+            return .normal
+        case 1:
+            return .all
+        case 2:
+            return .archive
+        case 3:
+            return .drafts
+        case 4:
+            return .flagged
+        case 5:
+            return .spam
+        case 6:
+            return .sent
+        case 7:
+            return .trash
+        default:
+            // .outbox returns nil intentionally.
+            return nil
+        }
+    }
+
+    /**
+     Each kind has a human-readable name you can use to create a local folder object.
+     - Note: This is used *only* for the local-only special folders, and for fuzzy-matching
+     against a known folder type when fetching from the remote server.
+     */
+    public func folderName() -> String {
+        return folderNames()[0]
+    }
+
+    /**
+     Is a mail in that folder *typically* outgoing? This can only be a guess for some cases.
+     */
+    public func isOutgoing() -> Bool {
+        switch self {
+        case .inbox, .trash, .normal, .spam, .archive, .all, .flagged, .outbox:
+            return false
+        case .sent, .drafts:
+            return true
+        }
+    }
 }
