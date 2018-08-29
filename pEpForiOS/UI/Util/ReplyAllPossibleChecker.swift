e4914ec58a447629d4fb5b0aa3b0c9010a97d2b9
Dirk Zimmermann <dirk@pep-project.org>
2018-08-29 08:12:32 +0200

    IOS-1282 ReplyAllPossibleChecker

---
+++ pEpForiOS/UI/Util/ReplyAllPossibleChecker.swift
@@ -0,0 +1,29 @@
+//
+//  ReplyAllPossibleChecker.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 28.08.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+import MessageModel
+
+/**
+ Determins if a reply-all is possible, or even desirable for a given message.
+ */
+struct ReplyAllPossibleChecker {
+    func isReplyAllPossible(forMessage: Message?) -> Bool {
+        guard let theMessage = forMessage else {
+            return false
+        }
+
+        var allRecipients = Set<Identity>()
+        for recips in [theMessage.to, theMessage.cc, theMessage.bcc] {
+            allRecipients.formUnion(recips)
+        }
+
+        return allRecipients.count > 1
+    }
+}
