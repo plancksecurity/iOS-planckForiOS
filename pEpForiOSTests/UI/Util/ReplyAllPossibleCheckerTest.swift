a387b1b995f6be50ff518cb3173af52bca42ba35
Dirk Zimmermann <dirk@pep-project.org>
2018-08-29 08:12:32 +0200

    IOS-1282 ReplyAllPossibleCheckerTest

---
+++ pEpForiOSTests/UI/Util/ReplyAllPossibleCheckerTest.swift
@@ -0,0 +1,22 @@
+//
+//  ReplyAllPossibleCheckerTest.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 28.08.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class ReplyAllPossibleCheckerTest: CoreDataDrivenTestBase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
