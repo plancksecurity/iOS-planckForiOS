558b5ef4ea237443134bf3e25ff8ddee73547766
Dirk Zimmermann <dirk@pep-project.org>
2018-08-29 08:12:32 +0200

    IOS-1282 FAILING testSimplestMail

--- pEpForiOSTests/UI/Util/ReplyAllPossibleCheckerTest.swift
+++ pEpForiOSTests/UI/Util/ReplyAllPossibleCheckerTest.swift
@@ -8,15 +8,38 @@
 
 import XCTest
 
+@testable import pEpForiOS
+@testable import MessageModel
+
 class ReplyAllPossibleCheckerTest: CoreDataDrivenTestBase {
+    var account: Account!
+    var inbox: Folder!
+    var replyAllChecker = ReplyAllPossibleChecker()
+
     override func setUp() {
         super.setUp()
+
+        replyAllChecker = ReplyAllPossibleChecker()
+
+        account = cdAccount.account()
+
+        inbox = Folder(name: "INBOX", parent: nil, account: account, folderType: .inbox)
+        inbox.save()
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSimplestMail() {
+        let msg = Message.init(uuid: "1", uid: 1, parentFolder: inbox)
+        msg.from = Identity.create(address: "1@example.com",
+                                   userID: "1",
+                                   addressBookID: "1",
+                                   userName: "user1",
+                                   isMySelf: false)
+        msg.to = [account.user]
+
+        XCTAssertFalse(replyAllChecker.isReplyAllPossible(forMessage: msg))
     }
 }
