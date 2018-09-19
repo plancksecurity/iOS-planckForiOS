0e24ff88ce18d98a028ebe99ffec680e0896dde6
buff <andreas@pep-project.org>
2018-09-19 13:30:58 +0200

    IOS-1346 adds tests

--- pEpForiOSTests/Models/Filter/AttachmentFilterTest.swift
+++ pEpForiOSTests/Models/Filter/AttachmentFilterTest.swift
@@ -16,10 +16,10 @@ class AttachmentFilterTest: CoreDataDrivenTestBase {
     func testGetMessagesWithAttatchemnts() {
         let f1 = Folder(name: "inbox", parent: nil, account: account, folderType: .inbox)
         f1.save()
-        let messages = createMessages(in: f1, numMessages: 2)
-        let firstMessage = messages.first!
+        let messages = createDecryptedMessages(in: f1, numMessages: 2)
 
         let attachment = Attachment.create(data: nil, mimeType: "type", fileName: "name")
+        let firstMessage = messages.first!
         firstMessage.attachments = [attachment]
         firstMessage.save()
 
@@ -27,20 +27,55 @@ class AttachmentFilterTest: CoreDataDrivenTestBase {
         cf.add(filter: AttachmentFilter())
         let _ = f1.updateFilter(filter: cf)
 
-        XCTAssertEqual(f1.allCdMessagesNonThreaded().count, 1)
+         let numOfEncryptableMessagesWithAttchment = 1
+        XCTAssertEqual(f1.allCdMessagesNonThreaded().count, numOfEncryptableMessagesWithAttchment)
+    }
+
+    // MARK: - Undecryptable Messages
+
+    func testFilterShouldIgnoreUndecryptable_haveNoKey() {
+        assureMessagesDoNotPassFilter(with: PEP_rating_have_no_key)
+    }
+
+    func testFilterShouldIgnoreUndecryptable_canNotDecrypt() {
+        assureMessagesDoNotPassFilter(with: PEP_rating_cannot_decrypt)
     }
 
     // MARK: - Helper
+
+    func assureMessagesDoNotPassFilter(with pEpRating: PEP_rating) {
+        let f1 = Folder(name: "inbox", parent: nil, account: account, folderType: .inbox)
+        f1.save()
+        let messages = createMessages(in: f1, numMessages: 2)
+        let attachment = Attachment.create(data: nil, mimeType: "type", fileName: "name")
+        let firstMessage = messages.first!
+        firstMessage.attachments = [attachment]
+        firstMessage.save()
+
+        let _ = createMessages(in: f1,
+                               numMessages: 3,
+                               pepRating: pEpRating)
+
+        let cf = CompositeFilter<FilterBase>()
+        cf.add(filter: AttachmentFilter())
+        let _ = f1.updateFilter(filter: cf)
+
+        let numOfEncryptableMessagesWithAttchment = 1
+        XCTAssertEqual(f1.allCdMessagesNonThreaded().count, numOfEncryptableMessagesWithAttchment)
+    }
+
+    private func createDecryptedMessages(in folder: Folder, numMessages: Int) -> [Message] {
+        return createMessages(in: folder, numMessages: numMessages)
+    }
 
     private func createMessages(in folder: Folder, numMessages: Int,
                                 pepRating: PEP_rating = PEP_rating_trusted) -> [Message] {
         let id = Identity.create(address: "fake@mail.com")
         id.save()
 
-
         var messages = [Message]()
-        for i in 0..<numMessages {
-            let message = Message(uuid: String(i), parentFolder: folder)
+        for _ in 0..<numMessages {
+            let message = Message(uuid: UUID().uuidString, parentFolder: folder)
             message.from = id
             message.to = [account.user]
             message.imapFlags?.seen = false
