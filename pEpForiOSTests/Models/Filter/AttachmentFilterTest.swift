689f5c7ea03db0166d0a85aa0e594c1e03676a1d
buff <andreas@pep-project.org>
2018-09-19 13:08:56 +0200

    IOS-1346 prepare for reuse

--- pEpForiOSTests/Models/Filter/AttachmentFilterTest.swift
+++ pEpForiOSTests/Models/Filter/AttachmentFilterTest.swift
@@ -14,34 +14,40 @@ import pEpForiOS
 class AttachmentFilterTest: CoreDataDrivenTestBase {
 
     func testGetMessagesWithAttatchemnts() {
-        let acc = cdAccount.account()
-
-        let id = Identity.create(address: "fake@mail.com")
-        id.save()
-        let f1 = Folder(name: "inbox", parent: nil, account: acc, folderType: .inbox)
-        f1.account = acc
+        let f1 = Folder(name: "inbox", parent: nil, account: account, folderType: .inbox)
         f1.save()
+        let messages = createMessages(in: f1, numMessages: 2)
+        let firstMessage = messages.first!
 
         let attachment = Attachment.create(data: nil, mimeType: "type", fileName: "name")
-        let message = Message(uuid: "1", parentFolder: f1)
-        message.from = id
-        message.to = [acc.user]
-        message.imapFlags?.seen = false
-        message.pEpRatingInt = 16
-        message.attachments = [attachment]
-        message.save()
-
-        let message2 = Message(uuid: "2", parentFolder: f1)
-        message2.from = id
-        message2.to = [acc.user]
-        message2.imapFlags?.seen = false
-        message2.pEpRatingInt = 16
-        message2.save()
+        firstMessage.attachments = [attachment]
+        firstMessage.save()
 
         let cf = CompositeFilter<FilterBase>()
         cf.add(filter: AttachmentFilter())
         let _ = f1.updateFilter(filter: cf)
 
         XCTAssertEqual(f1.allCdMessagesNonThreaded().count, 1)
+    }
+
+    // MARK: - Helper
+
+    private func createMessages(in folder: Folder, numMessages: Int,
+                                pepRating: PEP_rating = PEP_rating_trusted) -> [Message] {
+        let id = Identity.create(address: "fake@mail.com")
+        id.save()
+
+
+        var messages = [Message]()
+        for i in 0..<numMessages {
+            let message = Message(uuid: String(i), parentFolder: folder)
+            message.from = id
+            message.to = [account.user]
+            message.imapFlags?.seen = false
+            message.pEpRatingInt = Int(pepRating.rawValue)
+            message.save()
+            messages.append(message)
+        }
+        return messages
     }
 }
