bb9e20f6f2d4958029fa167cc1d4e6f39d382697
buff <andreas@pep-project.org>
2018-09-19 13:30:34 +0200

    IOS-1346 undefined rating is another topic

--- pEpForiOS/Util/Extensions/PEP_rating+Extension.swift
+++ pEpForiOS/Util/Extensions/PEP_rating+Extension.swift
@@ -25,7 +25,8 @@ extension PEP_rating {
                                                        PEP_rating_cannot_decrypt,
                                                        PEP_rating_have_no_key]
 
-    static let neverShowAttachmentsForRatings: [PEP_rating] = retryDecriptionRatings
+    static let neverShowAttachmentsForRatings: [PEP_rating] = [PEP_rating_cannot_decrypt,
+                                                               PEP_rating_have_no_key]
 
     func neverShowAttachmentsFor() -> Bool {
         return PEP_rating.neverShowAttachmentsForRatings.contains(self)
