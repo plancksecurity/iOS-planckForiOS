ce6bbe9d66b7790122d908de53767ffcbff8fb00
borja <borja@helm.cat>
2018-09-26 18:13:01 +0200

    IOS-1285 possible bugfix

--- pEpForiOS/UI/EmailDisplayList/EmailListViewModel+MoveToFolderDelegate.swift
+++ pEpForiOS/UI/EmailDisplayList/EmailListViewModel+MoveToFolderDelegate.swift
@@ -11,22 +11,8 @@ import MessageModel
 
 extension EmailListViewModel: MoveToFolderDelegate {
     func didmove(messages: [Message]) {
-        var ips = [IndexPath]()
-        var pms = [MessageViewModel]()
         messages.forEach { (msg) in
-            let ind = index(of: msg)!
-            let pm = self.messages.object(at: ind)!
-            pms.append(pm)
-            let ip = IndexPath(row: ind, section: 0)
-            ips.append(ip)
-        }
-        deletePreviewMessagesHelper(previewMEssages: pms)
-        emailListViewModelDelegate?.emailListViewModel(viewModel: self, didRemoveDataAt: ips)
-    }
-
-    func deletePreviewMessagesHelper(previewMEssages: [MessageViewModel]) {
-        previewMEssages.forEach { (pm) in
-            self.messages.remove(object: pm)
+            emailDisplayDidDelete(message: msg)
         }
     }
 
