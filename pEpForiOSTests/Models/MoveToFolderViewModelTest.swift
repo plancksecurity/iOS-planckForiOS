8cdc610351100ecbef445a905a40f5fa240845f9
borja <borja@helm.cat>
2018-10-01 15:30:34 +0200

    IOS-1372 add test to check if message are moved.

---
+++ pEpForiOSTests/Models/MoveToFolderViewModelTest.swift
@@ -0,0 +1,109 @@
+//
+//  MoveToAccountViewControllerTest.swift
+//  pEpForiOSTests
+//
+//  Created by Borja González de Pablo on 01/10/2018.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+@testable import pEpForiOS
+import MessageModel
+
+class MoveToFolderViewModelTest: CoreDataDrivenTestBase {
+    var messagesToMove =  [Message]()
+    var accounts: [Account]!
+    var viewmodel: MoveToFolderViewModel!
+    var folders = [Folder]()
+
+    override func setUp() {
+        super.setUp()
+        let account = cdAccount.account()
+        givenThereAreTwoFolders()
+        viewmodel = MoveToFolderViewModel(account: account, messages: [Message]())
+    }
+
+
+    func testMessageAreMoved() {
+        givenThereIsAMoveToFolderDelegate(checkCall: true)
+        givenWeWantToMove(aNumberOfMessages: 10, currentlyInFolder: folders[0])
+
+        let moved = viewmodel.moveMessagesTo(index: 1)
+
+        XCTAssertTrue(moved)
+        waitForExpectations(timeout: TestUtil.waitTime)
+
+    }
+
+    func testMessageAreNotMovedIfTheyBelongToTheDestinationFolder() {
+        givenThereIsAMoveToFolderDelegate(checkCall: false)
+        givenWeWantToMove(aNumberOfMessages: 10, currentlyInFolder: folders[0])
+
+        let moved = viewmodel.moveMessagesTo(index: 0)
+
+        XCTAssertFalse(moved)
+        waitForExpectations(timeout: TestUtil.waitTime)
+
+    }
+
+
+
+
+//MARK: Initialization
+    
+    func givenWeWantToMove(aNumberOfMessages: Int, currentlyInFolder: Folder){
+        for i in 0..<aNumberOfMessages {
+          let message = TestUtil.createMessage(uid: i, inFolder: currentlyInFolder)
+            messagesToMove.append(message)
+        }
+        viewmodel.messages = messagesToMove
+    }
+
+    func givenThereIsAMoveToFolderDelegate(checkCall: Bool) {
+        let didMoveExpection =
+            expectation(description: MoveToFolderExpectations.DID_MOVE_EXPECTATION_DESCRIPTION)
+        if(!checkCall){
+            didMoveExpection.isInverted = true
+        }
+        let delegate = MoveToFolderExpectations(didMoveExpectation: didMoveExpection)
+        viewmodel.delegate = delegate
+    }
+
+    func givenThereAreTwoFolders() {
+        let folder = Folder(name: "inbox", parent: nil, account: account, folderType: .inbox)
+        folder.save()
+        let trashFolder =
+            Folder(name: "trash", parent: nil, account: folder.account, folderType: .trash)
+        trashFolder.save()
+        folders.append(folder)
+        folders.append(trashFolder)
+    }
+
+
+
+}
+
+//MARK: Delegate Expectations
+
+class MoveToFolderExpectations: MoveToFolderDelegate {
+    static let DID_MOVE_EXPECTATION_DESCRIPTION = "DID_MOVE_CALLED"
+    var expectationDidMoveCalled: XCTestExpectation?
+
+    public init (didMoveExpectation: XCTestExpectation? = nil) {
+        expectationDidMoveCalled = didMoveExpectation
+    }
+
+    func didMove() {
+        XCTFail()
+    }
+
+    func didmove(messages: [Message]) {
+        guard let expectation = expectationDidMoveCalled else {
+            XCTFail()
+            return
+        }
+        expectation.fulfill()
+    }
+
+
+}
