2c4b73ce1f87b087816da2bd425e6ca666b5436c
Dirk Zimmermann <dirk@pep-project.org>
2018-10-02 15:39:25 +0200

    IOS-1373 Log+Pantomime

--- pEpForiOS/Util/Log.swift
+++ pEpForiOS/Util/Log.swift
@@ -131,20 +131,6 @@ import MessageModel
     }
 }
 
-extension Log: CWLogging {
-    @objc open func infoComponent(_ component: String, message: String) {
-        Log.info(component: component, content: message)
-    }
-
-    @objc open func warnComponent(_ component: String, message: String) {
-        Log.warn(component: component, content: message)
-    }
-
-    @objc open func errorComponent(_ component: String, message: String) {
-        Log.error(component: component, errorString: message)
-    }
-}
-
 extension Log: MessageModelLogging {
     public func verbose(component: String, content: String) {
         Log.verbose(component: component, content: content)
