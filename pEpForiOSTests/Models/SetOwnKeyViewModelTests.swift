15c7b49294242f0619e4b11242d4c037518f7744
Dirk Zimmermann <dirk@pep-project.org>
2018-10-03 15:16:00 +0200

    IOS-1378 SetOwnKeyViewModelTests

---
+++ pEpForiOSTests/Models/SetOwnKeyViewModelTests.swift
@@ -0,0 +1,99 @@
+//
+//  SetOwnKeyViewModelTests.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 03.10.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+@testable import pEpForiOS
+@testable import MessageModel
+
+class SetOwnKeyViewModelTests: XCTestCase {
+    var persistentSetup: PersistentSetup!
+    var session: PEPSession {
+        return PEPSession()
+    }
+    var backgroundQueue: OperationQueue!
+
+    // MARK: - setUp, tearDown
+
+    override func setUp() {
+        super.setUp()
+
+        XCTAssertTrue(PEPUtil.pEpClean())
+
+        persistentSetup = PersistentSetup()
+
+        self.backgroundQueue = OperationQueue()
+    }
+
+    override func tearDown() {
+        persistentSetup = nil
+        backgroundQueue.cancelAllOperations()
+        backgroundQueue = nil
+        PEPSession.cleanup()
+        super.tearDown()
+    }
+
+    // MARK: - Tests
+
+    /**
+     - Note: If you need to manually verify something:
+     * The public/secret key pair of Leon Kowalski (subject)
+     is in `Leon Kowalski (19B9EE3B) – Private.asc`.
+     * The public/secret key pair of Harry Bryant (sender) is in
+     `Harry Bryant iostest002@peptest.ch (0x5716EA2D9AE32468) pub-sec.asc`.
+     */
+    func testSetOwnKey() {
+        let cdOwnAccount = DecryptionUtil.createLocalAccount(
+            ownUserName: "Rick Deckard",
+            ownUserID: "rick_deckard_uid",
+            ownEmailAddress: "iostest001@peptest.ch")
+
+        try! TestUtil.importKeyByFileName(fileName: "Rick Deckard (EB50C250) – Private.asc")
+
+        try! session.setOwnKey(cdOwnAccount.pEpIdentity(),
+                               fingerprint: "456B937ED6D5806935F63CE5548738CCEB50C250")
+
+        self.backgroundQueue = OperationQueue()
+        let cdMessage = DecryptionUtil.decryptTheMessage(
+            testCase: self,
+            backgroundQueue: backgroundQueue,
+            cdOwnAccount: cdOwnAccount,
+            fileName: "SimplifiedKeyImport_Harry_To_Rick_with_Leon.txt")
+
+        guard let theCdMessage = cdMessage else {
+            XCTFail()
+            return
+        }
+
+        // After ENGINE-465 is done, this should be PEP_rating_reliable
+        XCTAssertEqual(theCdMessage.pEpRating, Int16(PEP_rating_unreliable.rawValue))
+
+        XCTAssertEqual(theCdMessage.shortMessage, "Simplified Key Import")
+        XCTAssertEqual(
+            theCdMessage.longMessage,
+            "iostest003@peptest.ch\nLeon Kowalski\n63FC29205A57EB3AEB780E846F239B0F19B9EE3B\n\nSee the key of Leon attached.\n")
+
+        let attachments = theCdMessage.attachments?.array as? [CdAttachment] ?? []
+        XCTAssertEqual(attachments.count, 0)
+
+        guard let msg = theCdMessage.message() else {
+            XCTFail()
+            return
+        }
+
+        XCTAssertEqual(msg.attachments.count, 0)
+
+        let leon = PEPIdentity(address: "iostest002@peptest.ch",
+                               userID: PEP_OWN_USERID,
+                               userName: "Leon Kowalski",
+                               isOwn: true)
+        try! session.update(leon)
+
+        try! session.setOwnKey(leon, fingerprint: "63FC29205A57EB3AEB780E846F239B0F19B9EE3B")
+    }
+}
