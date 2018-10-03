42529e72ce1bdbe34b5e1333264469ee3ee95d2a
Dirk Zimmermann <dirk@pep-project.org>
2018-10-03 13:53:55 +0200

    IOS-1378 SetOwnKeyViewController view model implementation

--- pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewModel.swift
+++ pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewModel.swift
@@ -9,5 +9,34 @@
 import Foundation
 
 class SetOwnKeyViewModel {
+    public var userName: String?
+    public var fingerprint: String?
+    public var rawErrorString: String?
 
+    func setOwnKey() {
+        guard
+            let theUserName = userName,
+            let theFingerprint = fingerprint,
+            !theUserName.isEmpty,
+            !theFingerprint.isEmpty
+            else {
+                rawErrorString = NSLocalizedString(
+                    "Please provide a user name and a fingerprint",
+                    comment: "Validation error for set_own_key UI")
+                return
+        }
+
+        let session = PEPSession()
+
+        do {
+            let someIdent = PEPIdentity(
+                address: "",
+                userID: PEP_OWN_USERID,
+                userName: theUserName,
+                isOwn: true)
+            try session.setOwnKey(someIdent, fingerprint: theFingerprint.despaced())
+        } catch {
+            rawErrorString = error.localizedDescription
+        }
+    }
 }
