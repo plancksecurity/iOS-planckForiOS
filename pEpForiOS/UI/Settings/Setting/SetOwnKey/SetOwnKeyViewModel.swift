426b6f982e3599bd8fd43416ba9378c61295a9ef
Dirk Zimmermann <dirk@pep-project.org>
2018-10-03 13:53:55 +0200

    IOS-1378 view model for SetOwnKeyViewController

---
+++ pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SetOwnKeyViewModel.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 03.10.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class SetOwnKeyViewModel {
+
+}
