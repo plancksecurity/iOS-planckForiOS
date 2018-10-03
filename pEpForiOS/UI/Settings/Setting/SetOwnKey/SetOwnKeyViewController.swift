ac3e4ccd1673e3a4077a0394a20c1d7177270786
Dirk Zimmermann <dirk@pep-project.org>
2018-10-03 13:53:55 +0200

    IOS-1378 connect emailTextField

--- pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewController.swift
+++ pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SetOwnKeyViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var fingerprintTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var setOwnKeyButton: UIButton!
 
     override func viewDidLoad() {
