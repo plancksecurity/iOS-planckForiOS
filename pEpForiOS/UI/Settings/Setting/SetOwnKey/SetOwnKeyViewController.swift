a5d8b82cb6654738e9ddb261714d136f25510894
Dirk Zimmermann <dirk@pep-project.org>
2018-10-03 13:53:55 +0200

    IOS-1378 report errors back to the user

--- pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewController.swift
+++ pEpForiOS/UI/Settings/Setting/SetOwnKey/SetOwnKeyViewController.swift
@@ -35,6 +35,7 @@ class SetOwnKeyViewController: UIViewController {
         viewModel.fingerprint = fingerprintTextField.text
         viewModel.email = emailTextField.text
         viewModel.setOwnKey()
+        errorTextField.text = viewModel.rawErrorString
     }
 
     // MARK: - Private
