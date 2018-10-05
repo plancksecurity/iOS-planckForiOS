c0153b191267b3c019490000ffd520195d2e71b1
buff <andreas@pep-project.org>
2018-10-05 11:27:04 +0200

    IOS-1369 add common  protocol

---
+++ pEpForiOS/UI/Compose/Refactor/Util/CellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CellViewModel.swift
+//  pEp
+//
+//  Created by Andreas Buff on 05.10.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Conform to this if you:
+/// - are a view model
+/// - represent a UITableViewCell
+protocol CellViewModel { }
