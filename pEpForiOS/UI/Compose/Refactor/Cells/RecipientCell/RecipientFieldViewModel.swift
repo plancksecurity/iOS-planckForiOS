8fdddcc30d72059cd3431f566d32606549a22607
buff <andreas@pep-project.org>
2018-10-05 11:54:45 +0200

    IOS-1369 adds RecipientVM skeleton

---
+++ pEpForiOS/UI/Compose/Refactor/Cells/RecipientCell/RecipientFieldViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  RecipientFieldViewModel.swift
+//  pEp
+//
+//  Created by Andreas Buff on 05.10.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import MessageModel
+
+class RecipientFieldViewModel {
+    public let title: String
+    public var content: NSAttributedString
+    private var recipients = [Identity]()
+
+
+    init(title: String, content: NSAttributedString) {
+        self.title = title
+        self.content = content
+    }
+}
