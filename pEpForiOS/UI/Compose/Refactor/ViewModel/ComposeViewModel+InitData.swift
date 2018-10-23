6abe85b869c422c70bae6e996d70780f2e299ab7
buff <andreas@pep-project.org>
2018-10-23 12:07:16 +0200

    IOS-1369 set initial body part I

--- pEpForiOS/UI/Compose/Refactor/ViewModel/ComposeViewModel+InitData.swift
+++ pEpForiOS/UI/Compose/Refactor/ViewModel/ComposeViewModel+InitData.swift
@@ -12,84 +12,233 @@ import MessageModel
 
 //IOS-1369: wrap in extention when done to not polute the namespace
 //extension ComposeViewModel {
-    /// Wraps properties used for initial setup
-    struct InitData {
-        /// Recipient to set as "To:".
-        /// Is ignored if a originalMessage is set.
-        public let prefilledTo: Identity?
-        /// Original message to compute content and recipients from (e.g. a message we reply to).
-        public let originalMessage: Message?
+/// Wraps properties used for initial setup
+struct InitData {
+    /// Recipient to set as "To:".
+    /// Is ignored if a originalMessage is set.
+    public let prefilledTo: Identity?
+    /// Original message to compute content and recipients from (e.g. a message we reply to).
+    public let originalMessage: Message?
 
-        public let composeMode: ComposeUtil.ComposeMode
+    public let composeMode: ComposeUtil.ComposeMode
 
-        /// Whether or not the original message is in Drafts or Outbox
-        var isDraftsOrOutbox: Bool {
-            return isDrafts || isOutbox
+    /// Whether or not the original message is in Drafts or Outbox
+    var isDraftsOrOutbox: Bool {
+        return isDrafts || isOutbox
+    }
+
+    /// Whether or not the original message is in Drafts folder
+    var isDrafts: Bool {
+        if let om = originalMessage {
+            return om.parent.folderType == .drafts
         }
+        return false
+    }
 
-        /// Whether or not the original message is in Drafts folder
-        var isDrafts: Bool {
-            if let om = originalMessage {
-                return om.parent.folderType == .drafts
-            }
-            return false
+    /// Whether or not the original message is in Outbox
+    var isOutbox: Bool {
+        if let om = originalMessage {
+            return om.parent.folderType == .outbox
         }
+        return false
+    }
 
-        /// Whether or not the original message is in Outbox
-        var isOutbox: Bool {
-            if let om = originalMessage {
-                return om.parent.folderType == .outbox
-            }
-            return false
+    var from: Identity? {
+        return ComposeUtil.initialFrom(composeMode: composeMode,
+                                       originalMessage: originalMessage)
+    }
+
+    var toRecipients: [Identity] {
+        if let om = originalMessage {
+            return ComposeUtil.initialTos(composeMode: composeMode, originalMessage: om)
+        } else if let presetTo = prefilledTo {
+            return [presetTo]
         }
+        return []
+    }
 
-        var from: Identity? {
-            return ComposeUtil.initialFrom(composeMode: composeMode,
-                                           originalMessage: originalMessage)
-        }
-
-        var toRecipients: [Identity] {
-            if let om = originalMessage {
-                return ComposeUtil.initialTos(composeMode: composeMode, originalMessage: om)
-            } else if let presetTo = prefilledTo {
-                return [presetTo]
-            }
+    var ccRecipients: [Identity] {
+        guard let om = originalMessage else {
             return []
         }
+        return ComposeUtil.initialCcs(composeMode: composeMode, originalMessage: om)
+    }
 
-        var ccRecipients: [Identity] {
-            guard let om = originalMessage else {
-                return []
+    var bccRecipients: [Identity] {
+        guard let om = originalMessage else {
+            return []
+        }
+        return ComposeUtil.initialBccs(composeMode: composeMode, originalMessage: om)
+    }
+
+    var subject: String? {
+        return originalMessage?.shortMessage
+    }
+
+    var bodyPlaintext = " "
+    var bodyHtml: NSAttributedString?
+
+    //IOS-1369: all lazy private set?
+    public var nonInlinedAttachments: [Attachment] {
+        return ComposeUtil.initialAttachments(composeMode: composeMode,
+                                              contentDisposition: .attachment,
+                                              originalMessage: originalMessage)
+    }
+
+    public var inlinedAttachments: [Attachment] {
+        return ComposeUtil.initialAttachments(composeMode: composeMode,
+                                              contentDisposition: .inline,
+                                              originalMessage: originalMessage)
+    }
+
+    init(withPrefilledToRecipient prefilledTo: Identity? = nil,
+         orForOriginalMessage om: Message? = nil,
+         composeMode: ComposeUtil.ComposeMode? = nil) {
+        self.composeMode = composeMode ?? ComposeUtil.ComposeMode.normal
+        self.originalMessage = om
+        self.prefilledTo = om == nil ? prefilledTo : nil
+        setupInitialBody()
+    }
+
+    //IOS-1369: rethink. Move to BodyTextView(?)
+    mutating private func setupInitialBody() {
+        guard let om = originalMessage else {
+            // We have no original message. That's OK for compose mode .normal.
+            return
+        }
+
+        switch composeMode {
+        case .replyFrom:
+            setInitialBody(text: ReplyUtil.quotedMessageText(message: om, replyAll: false))
+        case .replyAll:
+            setInitialBody(text: ReplyUtil.quotedMessageText(message: om, replyAll: true))
+        case .forward:
+            setBodyPotetionallyTakingOverAttachments()
+            //                setBodyText(forMessage: om, to: messageBodyCell)
+            /*
+             private func setBodyText(forMessage msg: Message, to cell: MessageBodyCell) {
+             guard isOriginalMessageInDraftsOrOutbox || composeMode == .forward else {
+             Log.shared.errorAndCrash(component: #function,
+             errorString: "Unsupported mode or message")
+             return
+             }
+             if let html = msg.longMessageFormatted {
+             // We have HTML content. Parse it taking inlined attachments into account.
+             let attributedString = html.htmlToAttributedString(attachmentDelegate: self)
+             var result = attributedString
+             if composeMode == .forward {
+             // forwarded messges must have a cite header ("yxz wrote on ...")
+             result = ReplyUtil.citedMessageText(textToCite: attributedString,
+             fromMessage: msg)
+             }
+             cell.setInitial(text:result)
+             } else {
+             // No HTML available.
+             var result = msg.longMessage ?? ""
+             if composeMode == .forward {
+             // forwarded messges must have a cite header ("yxz wrote on ...")
+             result = ReplyUtil.citedMessageText(textToCite: msg.longMessage ?? "",
+             fromMessage: msg)
+             }
+             cell.setInitial(text: result)
+             }
+             }
+             */
+        case .normal:
+            if isDraftsOrOutbox {
+                setBodyPotetionallyTakingOverAttachments()
+                //                setBodyText(forMessage: om, to: messageBodyCell)
             }
-            return ComposeUtil.initialCcs(composeMode: composeMode, originalMessage: om)
-        }
-
-        var bccRecipients: [Identity] {
-            guard let om = originalMessage else {
-                return []
-            }
-            return ComposeUtil.initialBccs(composeMode: composeMode, originalMessage: om)
-        }
-
-        var subject: String? {
-            return originalMessage?.shortMessage
-        }
-
-        //IOS-1369: body needs before 9pm brain
-
-        var nonInlinedAttachments: [Attachment] {
-            return ComposeUtil.initialNonInlinedAttachments(composeMode: composeMode,
-                                                            originalMessage: originalMessage)
-        }
-
-
-
-        init(withPrefilledToRecipient prefilledTo: Identity? = nil,
-             orForOriginalMessage om: Message? = nil,
-             composeMode: ComposeUtil.ComposeMode? = nil) {
-            self.composeMode = composeMode ?? ComposeUtil.ComposeMode.normal
-            self.originalMessage = om
-            self.prefilledTo = om == nil ? prefilledTo : nil
+            // do nothing.
         }
     }
+
+    mutating private func setInitialBody(text: String) {
+        if text.isEmpty {
+            bodyPlaintext = " " //Workaround initial base line bug
+        } else {
+            bodyPlaintext = text
+        }
+    }
+
+    mutating private func setInitialBody(text: NSAttributedString) {
+        bodyHtml = text
+    }
+
+    /// Is sutable for isDraftsOrOutbox || composeMode == .forward only.
+    mutating private func setBodyPotetionallyTakingOverAttachments() {
+        guard let msg = originalMessage else {
+            Log.shared.errorAndCrash(component: #function, errorString: "Inconsitant state")
+            return
+        }
+        //        private func setBodyText(forMessage msg: Message, to cell: MessageBodyCell) {
+        guard isDraftsOrOutbox || composeMode == .forward else {
+            Log.shared.errorAndCrash(component: #function,
+                                     errorString: "Unsupported mode or message")
+            return
+        }
+        if let html = msg.longMessageFormatted {
+            // We have HTML content. Parse it taking inlined attachments into account.
+            let parserDelegate = InitDataHtmlToAttributedTextSaxParserAttachmentDelegate(
+                inlinedAttachments: inlinedAttachments)
+            let attributedString = html.htmlToAttributedString(attachmentDelegate: parserDelegate)
+            var result = attributedString
+            if composeMode == .forward {
+                // forwarded messges must have a cite header ("yxz wrote on ...")
+                result = ReplyUtil.citedMessageText(textToCite: attributedString,
+                                                    fromMessage: msg)
+            }
+            setInitialBody(text: result)
+        } else {
+            // No HTML available.
+            var result = msg.longMessage ?? ""
+            if composeMode == .forward {
+                // forwarded messges must have a cite header ("yxz wrote on ...")
+                result = ReplyUtil.citedMessageText(textToCite: msg.longMessage ?? "",
+                                                    fromMessage: msg)
+            }
+            setInitialBody(text: result)
+        }
+    }
+}
 //}
+
+// MARK: - HtmlToAttributedTextSaxParserAttachmentDelegate
+
+class InitDataHtmlToAttributedTextSaxParserAttachmentDelegate: HtmlToAttributedTextSaxParserAttachmentDelegate {
+    let inlinedAttachments: [Attachment]
+
+    init(inlinedAttachments: [Attachment]) {
+        self.inlinedAttachments = inlinedAttachments
+    }
+
+    func imageAttachment(src: String?, alt: String?) -> Attachment? {
+        for attachment in inlinedAttachments {
+            if attachment.contentID == src?.extractCid() {
+                // The attachment is inlined.
+                assertImage(inAttachment: attachment)
+                //IOS-1369: should not be contained
+                // Remove from non-inlined attachments if contained.
+                //                if let _ = nonInlinedAttachmentData.remove(attachment: attachment) {
+                //                    // dataSource has changed. Refresh tableView.
+                //                    tableView.reloadData()
+                //                }
+                return attachment
+            }
+        }
+        return nil
+    }
+
+    private func assertImage(inAttachment attachment: Attachment) {
+        // Assure the image is set ...
+        if attachment.image == nil {
+            guard let safeData = attachment.data else {
+                Log.shared.errorAndCrash(component: #function, errorString: "No data")
+                return
+            }
+            attachment.image = UIImage(data: safeData)
+        }
+        // ... and adjust its size.
+        //        attachment.image = attachment.image?.resized(newWidth: tableView.contentSize.width) //IOS-1369: Problem. Lets see.
+    }
+}
