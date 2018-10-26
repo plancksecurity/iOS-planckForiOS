9587ef5911c793e87b66f17de60812c6ad9e8ed7
Xavier Algarra <xavier@pep-project.org>
2018-10-26 11:12:21 +0200

    IOS-1396 unwind segue now works correctly

--- pEpForiOS/UI/EmailDisplayList/EmailListViewController.swift
+++ pEpForiOS/UI/EmailDisplayList/EmailListViewController.swift
@@ -977,6 +977,10 @@ extension EmailListViewController {
     @IBAction func segueUnwindAfterAccountCreation(segue:UIStoryboardSegue) {
         setup()
     }
+
+    @IBAction func segueUnwindLastAccountDeleted(segue:UIStoryboardSegue) {
+        setup()
+    }
 }
 
 // MARK: - SegueHandlerType
