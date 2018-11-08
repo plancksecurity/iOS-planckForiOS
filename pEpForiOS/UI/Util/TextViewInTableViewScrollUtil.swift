150ce7c56c31039d1d12b4c7a6c8f6d889108f32
buff <andreas@pep-project.org>
2018-11-08 12:25:16 +0100

    IOS-1369 fixes: tableview jumps caused by reloading attachment cells while updating size

--- pEpForiOS/UI/Util/TextViewInTableViewScrollUtil.swift
+++ pEpForiOS/UI/Util/TextViewInTableViewScrollUtil.swift
@@ -8,17 +8,25 @@
 
 import Foundation
 
-/**
- Small utility to share some scrolling behavior of text views contained
- in table views (basically "scroll-to-caret-on-edit").
- */
+/// Small utility to share some scrolling behavior of text views contained in table views
+/// (basically "scroll-to-caret-on-edit").
 class TextViewInTableViewScrollUtil {
 
-    /**
-     Makes sure that the given text view's cursor (if any) is visible, given that it is
-     contained in the given table view.
-     */
-    static func assureCaretVisibility(tableView: UITableView, textView: UITextView) {
+    typealias Height = NSNumber
+    /// Caches height for TextView.
+    private var sizeCache = NSMapTable<UITextView, Height>(keyOptions: .weakMemory,
+                                                           valueOptions: .strongMemory)
+
+    func layoutAfterTextDidChange(tableView: UITableView, textView: UITextView) {
+        if heightDidChange(for: textView) {
+            tableView.updateSize()
+        }
+        self.scrollCaretToVisible(tableView: tableView, textView: textView)
+    }
+
+    /// Makes sure that the given text view's cursor (if any) is visible, given that it is
+    /// contained in the given table view.
+    func scrollCaretToVisible(tableView: UITableView, textView: UITextView) {
         guard let uiRange = textView.selectedTextRange, uiRange.isEmpty else {
             // No selection, nothing to scroll to.
             return
@@ -42,5 +50,28 @@ class TextViewInTableViewScrollUtil {
         tvRect = tvRect.size.height <= maxCursorHeight ? tvRect : tvCaretRect
 
         tableView.scrollRectToVisible(tvRect, animated: false)
+    }
+
+    /// Tracks text view heights.
+    ///
+    /// - Parameter textView: text view to check
+    /// - Returns:  true if given textView is not known yet or it's height did change since last the
+    ///             call, false otherwize
+    private func heightDidChange(for textView: UITextView) -> Bool {
+        defer {
+            sizeCache.setObject(Height(value: Double(textView.frame.size.height)),
+                                forKey: textView)
+        }
+        var sizeChanged = true
+        guard let lastHeight = sizeCache.object(forKey: textView)?.doubleValue else {
+            // Not cached yet, so yes.
+            return sizeChanged
+        }
+        let newHeight = Double(textView.frame.size.height)
+        if lastHeight == newHeight {
+            sizeChanged = false
+        }
+
+        return sizeChanged
     }
 }
