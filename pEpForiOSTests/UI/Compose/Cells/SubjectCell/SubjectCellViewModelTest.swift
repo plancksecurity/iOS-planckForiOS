5d473030ab22daf25f4c50de130c65c925dd0665
buff <andreas@pep-project.org>
2018-11-12 16:39:20 +0100

    IOS-1399 SubjectCell: adds tests for shouldChangeText

--- pEpForiOSTests/UI/Compose/Cells/SubjectCell/SubjectCellViewModelTest.swift
+++ pEpForiOSTests/UI/Compose/Cells/SubjectCell/SubjectCellViewModelTest.swift
@@ -13,29 +13,90 @@ import MessageModel
 
 class SubjectCellViewModelTest: XCTestCase {
 
-    // MARK: - Helper
+    // MARK: - handleTextChanged
 
-
-
-}
-
-class TestSubjectCellViewModelResultDelegate: SubjectCellViewModelResultDelegate {
-    let expDidChangeSubjectCalled: XCTestExpectation?
-    let expectedSubject: String?
-
-    init(expDidChangeSubjectCalled: XCTestExpectation?, expectedSubject: String?)  {
-        self.expDidChangeSubjectCalled = expDidChangeSubjectCalled
-        self.expectedSubject = expectedSubject
+    func testHandleTextChanged_delegateInformed() {
+        let initialContent = ""
+        let changeContent = "changeContent"
+        let resultDelegate =
+            ResultDelegate(expDidChangeSubjectCalled: expDidChangeSubjectCalled(mustBeCalled: true),
+                           expectedSubject: changeContent)
+        let vm = SubjectCellViewModel(resultDelegate: resultDelegate)
+        vm.content = initialContent
+        vm.handleTextChanged(to: changeContent)
+        waitForExpectations(timeout: UnitTestUtils.waitTime)
     }
 
-    func subjectCellViewModelDidChangeSubject(_ vm: SubjectCellViewModel) {
-        guard let exp = expDidChangeSubjectCalled else {
-            // We ignore called or not
-            return
+    func testHandleTextChanged_delegateNotInformed() {
+        let initialContent = ""
+        let resultDelegate =
+            ResultDelegate(expDidChangeSubjectCalled: expDidChangeSubjectCalled(mustBeCalled: false),
+                           expectedSubject: nil)
+        let vm = SubjectCellViewModel(resultDelegate: resultDelegate)
+        vm.content = initialContent
+        waitForExpectations(timeout: UnitTestUtils.waitTime)
+    }
+
+    // MARK: - shouldChangeText
+
+    func testShouldChangeText_charsNotAllowed() {
+        let charactersNotAllowedToTypeInSubject = Array("\n")
+        assertShouldChangeText(testCharacters: charactersNotAllowedToTypeInSubject,
+                               mustChangeText: false)
+    }
+
+    func testShouldChangeText_charsAllowed() {
+        let charactersAllowedToTypeInSubject = Array("Aa !§$%&/()?`'ÄÖÜ*+üplöä\"@")
+        assertShouldChangeText(testCharacters: charactersAllowedToTypeInSubject,
+                               mustChangeText: true)
+    }
+
+    // MARK: - Helper
+
+    private func assertShouldChangeText(initialContent: String? = nil,
+                                testCharacters: [Character],
+                                mustChangeText: Bool) {
+        let initialContent = initialContent ?? ""
+        let resultDelegate =
+            ResultDelegate(expDidChangeSubjectCalled: expDidChangeSubjectCalled(mustBeCalled: false),
+                           expectedSubject: nil)
+        let vm = SubjectCellViewModel(resultDelegate: resultDelegate)
+        vm.content = initialContent
+
+        for char in testCharacters {
+            let testee = String(char)
+            let textChanged = vm.shouldChangeText(to: testee)
+            if mustChangeText {
+                XCTAssertTrue(textChanged)
+            } else {
+                XCTAssertFalse(textChanged)
+            }
         }
-        exp.fulfill()
-        if let expected = expectedSubject {
-            XCTAssertEqual(vm.content, expected)
+        waitForExpectations(timeout: UnitTestUtils.waitTime)
+    }
+
+    private func expDidChangeSubjectCalled(mustBeCalled: Bool) -> XCTestExpectation {
+        return expectation(inverted: !mustBeCalled)
+    }
+
+    class ResultDelegate: SubjectCellViewModelResultDelegate {
+        let expDidChangeSubjectCalled: XCTestExpectation?
+        let expectedSubject: String?
+
+        init(expDidChangeSubjectCalled: XCTestExpectation?, expectedSubject: String?)  {
+            self.expDidChangeSubjectCalled = expDidChangeSubjectCalled
+            self.expectedSubject = expectedSubject
+        }
+
+        func subjectCellViewModelDidChangeSubject(_ vm: SubjectCellViewModel) {
+            guard let exp = expDidChangeSubjectCalled else {
+                // We ignore called or not
+                return
+            }
+            exp.fulfill()
+            if let expected = expectedSubject {
+                XCTAssertEqual(vm.content, expected)
+            }
         }
     }
 }
