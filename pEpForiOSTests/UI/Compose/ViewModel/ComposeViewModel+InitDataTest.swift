b2ac32abeff1443d13b8a715a9afc5e9ef759876
buff <andreas@pep-project.org>
2018-11-13 17:02:34 +0100

    IOS-1369 InitData: adds test skeleton

---
+++ pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModel+InitDataTest.swift
@@ -0,0 +1,14 @@
+//
+//  ComposeViewModel+InitDataTest.swift
+//  pEpForiOSTests
+//
+//  Created by Andreas Buff on 13.11.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class ComposeViewModel_InitDataTest: XCTestCase {
+
+
+}
