c023dc78579be03f9ca2590447bea02ce2b4634f
buff <andreas@pep-project.org>
2018-11-15 10:39:59 +0100

    IOS-1369 ComposeViewModel: adds test delegate skeletons

--- pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModelTest.swift
+++ pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModelTest.swift
@@ -8,6 +8,79 @@
 
 import XCTest
 
+@testable import pEpForiOS
+import MessageModel
+
 class ComposeViewModelTest: XCTestCase {
+    private var testDelegate: TestDelegate?
+    private var testResultDelegate: TestResultDelegate?
+    var testee: ComposeViewModel?
+
+    // MARK: - Helper
+
+    private class TestResultDelegate: ComposeViewModelResultDelegate {
+        func composeViewModelDidComposeNewMail() {
+            fatalError()
+        }
+
+        func composeViewModelDidModifyMessage() {
+            fatalError()
+        }
+
+        func composeViewModelDidDeleteMessage() {
+            fatalError()
+        }
+
+
+    }
+    private class TestDelegate:  ComposeViewModelDelegate {
+        func contentChanged(inRowAt indexPath: IndexPath) {
+            fatalError()
+        }
+
+        func focusSwitched() {
+            fatalError()
+        }
+
+        func validatedStateChanged(to isValidated: Bool) {
+            fatalError()
+        }
+
+        func modelChanged() {
+            fatalError()
+        }
+
+        func sectionChanged(section: Int) {
+            fatalError()
+        }
+
+        func colorBatchNeedsUpdate(for rating: PEP_rating, protectionEnabled: Bool) {
+            fatalError()
+        }
+
+        func hideSuggestions() {
+            fatalError()
+        }
+
+        func showSuggestions(forRowAt indexPath: IndexPath) {
+            fatalError()
+        }
+
+        func showMediaAttachmentPicker() {
+            fatalError()
+        }
+
+        func hideMediaAttachmentPicker() {
+            fatalError()
+        }
+
+        func showDocumentAttachmentPicker() {
+            fatalError()
+        }
+
+        func documentAttachmentPickerDone() {
+            fatalError()
+        }
+    }
 
 }
