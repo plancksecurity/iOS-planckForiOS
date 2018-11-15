4dc515432aaadba5171a84922e2a08a1a7425657
buff <andreas@pep-project.org>
2018-11-15 12:00:39 +0100

    IOS-1369 ComposeViewModel: adds assertion helper

--- pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModelTest.swift
+++ pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModelTest.swift
@@ -18,6 +18,169 @@ class ComposeViewModelTest: XCTestCase {
 
     // MARK: - Helper
 
+    private func assert(composeMode: ComposeUtil.ComposeMode? = nil,
+                        prefilledTo: Identity? = nil,
+                        originalMessage: Message? = nil,
+                        contentChangedMustBeCalled: Bool? = nil,/*TestDelegate realted params*/
+                        expectedContentChangedIndexPath: IndexPath? = nil,
+                        focusSwitchedMustBeCalled: Bool? = nil,
+                        validatedStateChangedMustBeCalled: Bool? = nil,
+                        expectedIsValidated: Bool? = nil,
+                        modelChangedMustBeCalled: Bool? = nil,
+                        sectionChangedMustBeCalled: Bool? = nil,
+                        expectedSection: Int? = nil,
+                        colorBatchNeedsUpdateMustBeCalled: Bool? = nil,
+                        expectedRating: PEP_rating? = nil,
+                        expectedProtectionEnabled: Bool? = nil,
+                        hideSuggestionsMustBeCalled: Bool? = nil,
+                        showSuggestionsMustBeCalled: Bool? = nil,
+                        expectedShowSuggestionsIndexPath: IndexPath? = nil,
+                        showMediaAttachmentPickerMustBeCalled: Bool? = nil,
+                        hideMediaAttachmentPickerMustBeCalled: Bool? = nil,
+                        showDocumentAttachmentPickerMustBeCalled: Bool? = nil,
+                        documentAttachmentPickerDonePickerCalled: Bool? = nil,
+                        didComposeNewMailMustBeCalled: Bool? = nil,/*TestResultDelegate realted params*/
+                        didModifyMessageMustBeCalled: Bool? = nil,
+                        didDeleteMessageMustBeCalled: Bool?) {
+        // TestDelegate
+        var expContentChangedCalled: XCTestExpectation? = nil
+        if let exp = contentChangedMustBeCalled {
+            expContentChangedCalled =
+                expectation(description: "expContentChangedCalled")
+            expContentChangedCalled?.isInverted = !exp
+        }
+
+        var expFocusSwitchedCalled: XCTestExpectation? = nil
+        if let exp = focusSwitchedMustBeCalled {
+            expFocusSwitchedCalled =
+                expectation(description: "expFocusSwitchedCalled")
+            expFocusSwitchedCalled?.isInverted = !exp
+        }
+
+        var expValidatedStateChangedCalled: XCTestExpectation? = nil
+        if let exp = validatedStateChangedMustBeCalled {
+            expValidatedStateChangedCalled =
+                expectation(description: "expValidatedStateChangedCalled")
+            expValidatedStateChangedCalled?.isInverted = !exp
+        }
+
+        var expModelChangedCalled: XCTestExpectation? = nil
+        if let exp = modelChangedMustBeCalled {
+            expModelChangedCalled =
+                expectation(description: "expModelChangedCalled")
+            expModelChangedCalled?.isInverted = !exp
+        }
+
+        var expSectionChangedCalled: XCTestExpectation? = nil
+        if let exp = sectionChangedMustBeCalled {
+            expSectionChangedCalled =
+                expectation(description: "expSectionChangedCalled")
+            expSectionChangedCalled?.isInverted = !exp
+        }
+
+        var expColorBatchNeedsUpdateCalled: XCTestExpectation? = nil
+        if let exp = colorBatchNeedsUpdateMustBeCalled {
+            expColorBatchNeedsUpdateCalled =
+                expectation(description: "expColorBatchNeedsUpdateCalled")
+            expColorBatchNeedsUpdateCalled?.isInverted = !exp
+        }
+
+        var expHideSuggestionsCalled: XCTestExpectation? = nil
+        if let exp = hideSuggestionsMustBeCalled {
+            expHideSuggestionsCalled =
+                expectation(description: "expHideSuggestionsCalled")
+            expHideSuggestionsCalled?.isInverted = !exp
+        }
+
+        var expShowSuggestionsCalled: XCTestExpectation? = nil
+        if let exp = showSuggestionsMustBeCalled {
+            expShowSuggestionsCalled =
+                expectation(description: "expShowSuggestionsCalled")
+            expShowSuggestionsCalled?.isInverted = !exp
+        }
+
+        var expShowMediaAttachmentPickerCalled: XCTestExpectation? = nil
+        if let exp = showMediaAttachmentPickerMustBeCalled {
+            expShowMediaAttachmentPickerCalled =
+                expectation(description: "expShowMediaAttachmentPickerCalled")
+            expShowMediaAttachmentPickerCalled?.isInverted = !exp
+        }
+
+        var expHideMediaAttachmentPickerCalled: XCTestExpectation? = nil
+        if let exp = hideMediaAttachmentPickerMustBeCalled {
+            expHideMediaAttachmentPickerCalled =
+                expectation(description: "expHideMediaAttachmentPickerCalled")
+            expHideMediaAttachmentPickerCalled?.isInverted = !exp
+        }
+
+        var expShowDocumentAttachmentPickerCalled: XCTestExpectation? = nil
+        if let exp = showDocumentAttachmentPickerMustBeCalled {
+            expShowDocumentAttachmentPickerCalled =
+                expectation(description: "expShowDocumentAttachmentPickerCalled")
+            expShowDocumentAttachmentPickerCalled?.isInverted = !exp
+        }
+
+        var expDocumentAttachmentPickerDonePickerCalled: XCTestExpectation? = nil
+        if let exp = documentAttachmentPickerDonePickerCalled {
+            expDocumentAttachmentPickerDonePickerCalled =
+                expectation(description: "expDocumentAttachmentPickerDonePickerCalled")
+            expDocumentAttachmentPickerDonePickerCalled?.isInverted = !exp
+        }
+
+        testDelegate =
+            TestDelegate(expContentChangedCalled: expContentChangedCalled,
+                         expectedContentChangedIndexPath: expectedContentChangedIndexPath,
+                         expFocusSwitchedCalled: expFocusSwitchedCalled,
+                         expValidatedStateChangedCalled: expValidatedStateChangedCalled,
+                         expectedIsValidated: expectedIsValidated,
+                         expModelChangedCalled: expModelChangedCalled,
+                         expSectionChangedCalled: expSectionChangedCalled,
+                         expectedSection: expectedSection,
+                         expColorBatchNeedsUpdateCalled: expColorBatchNeedsUpdateCalled,
+                         expectedRating: expectedRating,
+                         expectedProtectionEnabled: expectedProtectionEnabled,
+                         expHideSuggestionsCalled: expHideSuggestionsCalled,
+                         expShowSuggestionsCalled: expShowSuggestionsCalled,
+                         expectedShowSuggestionsIndexPath: expectedShowSuggestionsIndexPath,
+                         expShowMediaAttachmentPickerCalled: expShowMediaAttachmentPickerCalled,
+                         expHideMediaAttachmentPickerCalled: expHideMediaAttachmentPickerCalled,
+                         expShowDocumentAttachmentPickerCalled: expShowDocumentAttachmentPickerCalled,
+                         expDocumentAttachmentPickerDonePickerCalled:
+                expDocumentAttachmentPickerDonePickerCalled)
+
+        // TestResultDelegate
+
+        var expDidComposeNewMailCalled: XCTestExpectation? = nil
+        if let exp = didComposeNewMailMustBeCalled {
+            expDidComposeNewMailCalled =
+                expectation(description: "expDidComposeNewMailCalled")
+            expDidComposeNewMailCalled?.isInverted = !exp
+        }
+
+        var expDidModifyMessageCalled: XCTestExpectation? = nil
+        if let exp = didModifyMessageMustBeCalled {
+            expDidModifyMessageCalled =
+                expectation(description: "expDidModifyMessageCalled")
+            expDidModifyMessageCalled?.isInverted = !exp
+        }
+
+        var expDidDeleteMessageCalled: XCTestExpectation? = nil
+        if let exp = didDeleteMessageMustBeCalled {
+            expDidDeleteMessageCalled =
+                expectation(description: "expDidDeleteMessageCalled")
+            expDidDeleteMessageCalled?.isInverted = !exp
+        }
+
+        testResultDelegate =
+            TestResultDelegate(expDidComposeNewMailCalled: expDidComposeNewMailCalled,
+                               expDidModifyMessageCalled: expDidModifyMessageCalled,
+                               expDidDeleteMessageCalled: expDidDeleteMessageCalled)    
+        testee = ComposeViewModel(resultDelegate: testResultDelegate,
+                                  composeMode: composeMode,
+                                  prefilledTo: prefilledTo,
+                                  originalMessage: originalMessage)
+    }
+
     private class TestResultDelegate: ComposeViewModelResultDelegate {
         let expDidComposeNewMailCalled: XCTestExpectation?
         let expDidModifyMessageCalled: XCTestExpectation?
@@ -55,9 +218,8 @@ class ComposeViewModelTest: XCTestCase {
             }
             exp.fulfill()
         }
-
-
     }
+
     private class TestDelegate:  ComposeViewModelDelegate {
         let expContentChangedCalled: XCTestExpectation?
         let expectedContentChangedIndexPath: IndexPath?
