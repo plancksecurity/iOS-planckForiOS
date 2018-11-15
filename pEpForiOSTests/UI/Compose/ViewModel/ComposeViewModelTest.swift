931285c475d61b494a94f31578f5d803f8b1939a
buff <andreas@pep-project.org>
2018-11-15 10:33:36 +0100

    IOS-1369 ComposeViewModel: adds test skeleton

---
+++ pEpForiOSTests/UI/Compose/ViewModel/ComposeViewModelTest.swift
@@ -0,0 +1,13 @@
+//
+//  ComposeViewModelTest.swift
+//  pEpForiOSTests
+//
+//  Created by Andreas Buff on 15.11.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class ComposeViewModelTest: XCTestCase {
+
+}
