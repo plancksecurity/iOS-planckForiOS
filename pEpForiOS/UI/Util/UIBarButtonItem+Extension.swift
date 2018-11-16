47e21ea651d81f3cae6043a37cf3466f6efaea76
Dirk Zimmermann <dirk@pep-project.org>
2018-11-16 10:37:37 +0100

    IOS-1301 formatting

--- pEpForiOS/UI/Util/UIBarButtonItem+Extension.swift
+++ pEpForiOS/UI/Util/UIBarButtonItem+Extension.swift
@@ -10,7 +10,6 @@ import Foundation
 
 extension UIBarButtonItem {
     public static func getpEpButton(action: Selector, target: Any) -> UIBarButtonItem {
-
         let img = UIImage(named: "icon-settings")
         let pEpButton = UIBarButtonItem(image: img, style: .plain, target: target, action: action)
         return pEpButton
