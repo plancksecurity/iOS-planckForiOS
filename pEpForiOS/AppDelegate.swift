fa3cf85e2b80a583814ac321835eca38b883aa23
buff <andreas@pep-project.org>
2018-11-28 11:54:14 +0100

    IOS-1415 fixes: transition to inactive state unhandled

--- pEpForiOS/AppDelegate.swift
+++ pEpForiOS/AppDelegate.swift
@@ -276,6 +276,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        gracefullyRestartServices()
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
@@ -401,8 +402,13 @@ extension AppDelegate: NetworkServiceDelegate {
 
     func networkServiceDidCancel(service: NetworkService) {
         if UIApplication.shared.applicationState == .background {
+            // We have been cancelled because we are entering background.
+            // Quickly sync local changes and clean up.
             stopUsingPepSession()
+        } else if UIApplication.shared.applicationState == .inactive {
+            // We re inactive. Keep services paused -> Do nothing
         } else {
+            // We have been cancelled soley to assure a clean restart.
             startServices()
         }
     }
