7a39f93bb609814d0b3112ae6106f432b51a9981
Dirk Zimmermann <dz@pep.security>
2018-12-03 11:41:44 +0100

    IOS-629 rm arm64e from dylibs

---
+++ scripts/python/rm_arm64e.py
@@ -0,0 +1,35 @@
+import glob
+import os
+import subprocess
+import argparse
+
+official_files = {'libswiftCoreGraphics.dylib', 'libswiftCoreMedia.dylib',
+    'libswiftDarwin.dylib', 'libswiftCoreAudio.dylib',
+    'libswiftos.dylib', 'libswiftPhotos.dylib',
+    'libswiftQuartzCore.dylib', 'libswiftCoreData.dylib',
+    'libswiftContacts.dylib', 'libswiftFoundation.dylib',
+    'libswiftMetal.dylib', 'libswiftCoreLocation.dylib',
+    'libswiftCoreFoundation.dylib', 'libswiftAVFoundation.dylib',
+    'libswiftCoreImage.dylib', 'libswiftObjectiveC.dylib',
+    'libswiftsimd.dylib', 'libswiftSwiftOnoneSupport.dylib',
+    'libswiftUIKit.dylib', 'libswiftDispatch.dylib',
+    'libswiftCore.dylib'}
+
+if __name__ == '__main__':
+    current_dir = os.getcwd()
+
+    parser = argparse.ArgumentParser(description='Remove arm64e from *.dylib')
+    parser.add_argument('archive_path',
+        help='SwiftSupport/iphoneos/ of the target archive')
+    args = parser.parse_args()
+
+    os.chdir(args.archive_path)
+
+    files = glob.glob('*.dylib')
+    for file in files:
+        if file not in official_files:
+            print("Extra: " + file)
+        else:
+            subprocess.call(['lipo', file, '-remove', 'arm64e', '-output', file])
+
+    os.chdir(current_dir)
