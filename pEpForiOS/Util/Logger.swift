ec90e410e437269a06a07d153814f5a4611f2fb5
Dirk Zimmermann <dz@pep.security>
2018-12-18 12:27:59 +0100

    IOS-1276 class Logger

---
+++ pEpForiOS/Util/Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 18.12.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+public class Logger {
+}
