20b26ff652a5e4cbe029386a41a45d6185274115
Dirk Zimmermann <dz@pep.security>
2018-12-18 12:27:59 +0100

    IOS-1276 dancing with the swift compiler

--- pEpForiOS/Util/Logger.swift
+++ pEpForiOS/Util/Logger.swift
@@ -10,4 +10,56 @@ import Foundation
 import os.log
 
 public class Logger {
+    public enum Severity {
+        /**
+         OSLog.default
+         */
+        case `default`
+        case info
+        case debug
+        case error
+        case fault
+
+        @available(iOS 10.0, macOS 10.12, tvOS 10.0, watchOS 3.0, *)
+        public func osLogType() -> OSLogType {
+            switch self {
+            case .default:
+                return .default
+            case .info:
+                return .info
+            case .debug:
+                return .debug
+            case .error:
+                return .error
+            case .fault:
+                return .fault
+            }
+        }
+    }
+
+    public init(subsystem: String, category: String) {
+        if #available(iOS 10.0, macOS 10.12, tvOS 10.0, watchOS 3.0, *) {
+            self.subsystem = nil
+            self.category = nil
+            osLogger = OSLog(subsystem: subsystem, category: category)
+        } else {
+            self.subsystem = subsystem
+            self.category = category
+            osLogger = nil
+        }
+    }
+
+    private let subsystem: String?
+    private let category: String?
+
+    private let osLogger: Any?
+
+    private func saveLog(severity: Severity,
+                         function: String = #function,
+                         filePath: String = #file,
+                         fileLine: Int = #line,
+                         message: StaticString,
+                         args: CVarArg) {
+        // TODO: Invoke os_log()
+    }
 }
