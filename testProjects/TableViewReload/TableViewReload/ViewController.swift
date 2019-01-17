ab6169d34c4b49ea26c733d8fb6e1940db922f82
Dirk Zimmermann <dz@pep.security>
2019-01-17 08:33:29 +0100

    IOS-1442 try to reproduce in test project

---
+++ testProjects/TableViewReload/TableViewReload/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  TableViewReload
+//
+//  Created by Dirk Zimmermann on 16.01.19.
+//  Copyright © 2019 pEp Security AG. All rights reserved.
+//
+
+import UIKit
+
+class Model {
+    var shouldIncrease = true
+    var numRows = 6
+}
+
+class ViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func changeModel() {
+        if model.shouldIncrease {
+            model.numRows += 1
+        } else {
+            model.numRows -= 1
+        }
+        model.shouldIncrease = !model.shouldIncrease
+        (view as! UITableView).reloadData()
+    }
+
+    // MARK: - UITableViewDataSource
+
+    let model = Model()
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return model.numRows
+        }
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "the_cell_id"
+
+        if indexPath.row >= model.numRows {
+            print("*** ERROR")
+        }
+
+        if indexPath.section == 0 {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+            cell.textLabel?.text = "\(indexPath.row)"
+
+            if indexPath.row == model.numRows - 1 {
+                DispatchQueue.main.async {
+                    self.changeModel()
+                }
+            }
+
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+            cell.textLabel?.text = "ERROR: Wrong section"
+            return cell
+        }
+    }
+}
