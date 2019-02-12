5a4962c3550e33fb34b13861424bb2e255b8a3a8
Xavier Algarra <xavier@pep-project.org>
2019-02-12 12:19:16 +0100

    IOS-1455 collection from mm

---
+++ pEpUtilities/pEpUtilities/pEpUtilities/UIKit/Collection+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Collection+Extensions.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 11/10/16.
+//  Copyright © 2016 pEp Security S.A. All rights reserved.
+//
+
+import UIKit
+
+extension Collection {
+    /*
+     * Returns the element at the specified index if it is within bounds, otherwise nil.
+     */
+    public subscript(safe index: Index) -> Iterator.Element? {
+        return index >= startIndex && index < endIndex ? self[index] : nil
+    }
+}
