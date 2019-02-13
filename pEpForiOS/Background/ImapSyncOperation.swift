cffa3187639a0d3876a1ae455484f8a12a21b518
buff <andreas@pep-project.org>
2019-02-13 11:42:55 +0100

    Backed out changeset 754b387c57be

---
+++ pEpForiOS/Background/ImapSyncOperation.swift
@@ -0,0 +1,51 @@
+//
+//  ImapSyncOperation.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 16/12/16.
+//  Copyright © 2016 p≡p Security S.A. All rights reserved.
+//
+
+import MessageModel
+
+public class ImapSyncOperation: ConcurrentBaseOperation {
+    let imapSyncData: ImapSyncData
+
+    init(parentName: String = #function, errorContainer: ServiceErrorProtocol = ErrorContainer(),
+                imapSyncData: ImapSyncData) {
+        self.imapSyncData = imapSyncData
+        super.init(parentName: parentName, errorContainer: errorContainer)
+    }
+
+    public func checkImapSync() -> Bool {
+        if imapSyncData.sync == nil {
+            addError(BackgroundError.ImapError.invalidConnection(info: comp))
+            markAsFinished()
+            return false
+        }
+        return true
+    }
+
+    public func addIMAPError(_ error: Error) {
+        addError(error)
+        imapSyncData.sync?.imapState.hasError = true
+    }
+
+    override func markAsFinished() {
+        imapSyncData.sync?.delegate = nil
+        super.markAsFinished()
+    }
+
+    override public func waitForBackgroundTasksToFinish() {
+        imapSyncData.sync?.delegate = nil
+        super.waitForBackgroundTasksToFinish()
+    }
+}
+
+extension ImapSyncOperation: ImapSyncDelegateErrorHandlerProtocol {
+    func handle(error: Error) {
+        Logger.backendLogger.error("%{public}@", error.localizedDescription)
+        addIMAPError(error)
+        markAsFinished()
+    }
+}
