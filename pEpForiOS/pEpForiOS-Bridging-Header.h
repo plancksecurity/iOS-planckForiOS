1cc5a104e074781b1cd45dc899521af45c88152a
Dirk Zimmermann <dz@pep.security>
2019-02-15 08:41:48 +0100

    IOS-1480 Remove Pantomime. Backed out changeset acd3c41e30d9

--- pEpForiOS/pEpForiOS-Bridging-Header.h
+++ pEpForiOS/pEpForiOS-Bridging-Header.h
@@ -9,32 +9,6 @@
 #ifndef pEpForiOS_Bridging_Header_h
 #define pEpForiOS_Bridging_Header_h
 
-#pragma mark - Pantomime headers
-
-#import "Pantomime/CWLogger.h"
-#import "Pantomime/CWConstants.h"
-#import "Pantomime/CWFolder.h"
-#import "Pantomime/CWService.h"
-#import "Pantomime/CWConnection.h"
-#import "Pantomime/CWTCPConnection.h"
-#import "Pantomime/CWIMAPFolder.h"
-#import "Pantomime/CWCacheRecord.h"
-
-#import "Pantomime/CWCacheManager.h"
-#import "Pantomime/CWIMAPCacheManager.h"
-
-#import "Pantomime/CWIMAPStore.h"
-#import "Pantomime/CWIMAPMessage.h"
-#import "Pantomime/CWMessage.h"
-#import "Pantomime/CWFlags.h"
-
-#import "Pantomime/CWSMTP.h"
-#import "Pantomime/CWInternetAddress.h"
-#import "Pantomime/NSData+Extensions.h"
-
-#import "Pantomime/CWMIMEMultipart.h"
-#import "Pantomime/CWMIMEUtility.h"
-
 #pragma mark - pEp Headers
 
 #import "pEpObjCAdapter/PEPObjCAdapter.h"
