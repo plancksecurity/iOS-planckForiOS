3f6769bc29a204ffab2b2cc29efb56897ff57a76
Dirk Zimmermann <dz@pep.security>
2019-02-22 14:22:38 +0100

    IOS-1480 Publish Contants.*.

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/Constants.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/Constants.swift
@@ -10,43 +10,43 @@ import Foundation
 
 public struct Constants {
     /** MIME content type for plain text */
-    static let contentTypeText = "text/plain"
+    public static let contentTypeText = "text/plain"
 
     /** MIME content type for HTML */
-    static let contentTypeHtml = "text/html"
+    public static let contentTypeHtml = "text/html"
 
     /**
      Mime type for the "Version" attachment of PGP/MIME.
      */
-    static let contentTypePGPEncrypted = "application/pgp-encrypted"
+    public static let contentTypePGPEncrypted = "application/pgp-encrypted"
 
     /**
      Content type for MIME multipart/mixed.
      */
-    static let contentTypeMultipartMixed = "multipart/mixed"
+    public static let contentTypeMultipartMixed = "multipart/mixed"
 
     /**
      Content type for MIME multipart/related.
      */
-    static let contentTypeMultipartRelated = "multipart/related"
+    public static let contentTypeMultipartRelated = "multipart/related"
 
     /**
      Content type for MIME multipart/encrypted.
      */
-    static let contentTypeMultipartEncrypted = "multipart/encrypted"
+    public static let contentTypeMultipartEncrypted = "multipart/encrypted"
 
     /**
      Protocol for PGP/MIME application/pgp-encrypted.
      */
-    static let protocolPGPEncrypted = "application/pgp-encrypted"
+    public static let protocolPGPEncrypted = "application/pgp-encrypted"
 
     /**
      Content type for MIME multipart/alternative.
      */
-    static let contentTypeMultipartAlternative = "multipart/alternative"
+    public static let contentTypeMultipartAlternative = "multipart/alternative"
 
     /**
      The MIME type for attached emails (e.g., when forwarding).
      */
-    static let attachedEmailMimeType = "message/rfc822"
+    public static let attachedEmailMimeType = "message/rfc822"
 }
