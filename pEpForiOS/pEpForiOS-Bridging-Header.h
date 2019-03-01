b3d040625b19c089742ae65930cd1e1aa8cdb203
Dirk Zimmermann <dz@pep.security>
2019-03-01 16:05:22 +0100

    IOS-1480 Only import main adapter header.

--- pEpForiOS/pEpForiOS-Bridging-Header.h
+++ pEpForiOS/pEpForiOS-Bridging-Header.h
@@ -11,15 +11,7 @@
 
 #pragma mark - pEp Headers
 
-#import "pEpObjCAdapter/PEPObjCAdapter.h"
-#import "pEpObjCAdapter/PEPSessionProtocol.h"
-#import "pEpObjCAdapter/PEPLanguage.h"
-#import "pEpObjCAdapter/PEPLanguage.h"
-#import "pEpObjCAdapter/NSDictionary+Extension.h"
-#import "pEpObjCAdapter/NSDictionary+Debug.h"
 #import <pEpObjCAdapterFramework/pEpObjCAdapterFramework.h>
-#import "pEpObjCAdapter/PEPMessage.h"
-#import "pEpObjCAdapter/PEPAttachment.h"
 
 #pragma mark - pEp AccountSettings Headers
 
