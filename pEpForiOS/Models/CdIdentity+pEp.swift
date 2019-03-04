6612edf5a034d64632dc78811c18241d1a133ee8
Dirk Zimmermann <dz@pep.security>
2019-03-04 16:06:01 +0100

    IOS-1480 Move extension methods.

--- pEpForiOS/Models/CdIdentity+pEp.swift
+++ pEpForiOS/Models/CdIdentity+pEp.swift
@@ -13,43 +13,6 @@ import pEpIOSToolbox
  pEp extension for CdIdentity
  */
 extension CdIdentity {
-    public static func from(pEpContact: PEPIdentity?) -> CdIdentity? {
-        guard let pEpC = pEpContact else {
-            return nil
-        }
-        var identity: Identity
-        if let existing = Identity.by(address: pEpC.address) {
-            identity = existing
-            if !identity.isMySelf {
-                identity.userName = pEpC.userName
-            }
-        } else {
-            // this identity has to be created
-            identity = Identity.create(address: pEpC.address, userID: pEpC.userID,
-                                       userName: pEpC.userName)
-        }
-        identity.save()
-
-        guard let result = CdIdentity.search(address: pEpC.address) else {
-            Logger.modelLogger.errorAndCrash(
-                "We have just saved this identity. It has to exist.")
-            return CdIdentity.create()
-        }
-
-        return result
-    }
-
-    public static func from(pEpContacts: [PEPIdentity]?) -> [CdIdentity] {
-        let theContacts = pEpContacts ?? []
-        var contacts = [CdIdentity]()
-        for p in theContacts {
-            if let c = from(pEpContact: p) {
-                contacts.append(c)
-            }
-        }
-        return contacts
-    }
-
     public func pEpRating(session: PEPSession = PEPSession()) -> PEPRating {
         return PEPUtil.pEpRating(cdIdentity: self, session: session)
     }
