5447935f30e0f8af2bb125018eface26b46b8e4f
Dirk Zimmermann <dz@pep.security>
2019-03-05 15:49:57 +0100

    IOS-1480 Prove that PEPUtil methods can be used.

--- pEpForiOS/Models/CdAccount+pEp.swift
+++ pEpForiOS/Models/CdAccount+pEp.swift
@@ -5,10 +5,12 @@
 //  Created by Dirk Zimmermann on 25/11/16.
 //  Copyright © 2016 p≡p Security S.A. All rights reserved.
 //
+
 import MessageModel
 
 extension CdAccount {
     open func pEpIdentity() -> PEPIdentity {
+        let _ = PEPUtil.identity(string: "hi")
         return PEPUtil.identity(account: self)
     }
 }
