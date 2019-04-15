e55fb7aa4d308bc107d8ea13dc6fa016e672d5a5
Dirk Zimmermann <dz@pep.security>
2019-04-15 16:17:33 +0200

    IOS-1542 Move BasicVerificationServiceTest.

---
+++ pEpForiOSTests/Service/BasicVerificationServiceTest.swift
@@ -0,0 +1,45 @@
+//
+//  BasicVerificationServiceTest.swift
+//  MessageModelTests
+//
+//  Created by Dirk Zimmermann on 12.04.19.
+//  Copyright © 2019 pEp Security S.A. All rights reserved.
+//
+
+import XCTest
+
+import MessageModel
+
+class TestDelegate: BasicVerificationServiceDelegate {
+    private let expectation: XCTestExpectation
+    public var error: Error?
+
+    init(expectation: XCTestExpectation) {
+        self.expectation = expectation
+    }
+
+    func verified(connectInfo: BasicConnectInfo, error: Error?,
+                  service: BasicVerificationServiceProtocol) {
+        self.error = error
+        expectation.fulfill()
+    }
+}
+
+class BasicVerificationServiceTest: XCTestCase {
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func test(service: inout BasicVerificationServiceProtocol,
+              connectInfo: BasicConnectInfo,
+              verify: (Error?) -> ()) {
+        let expVerificationDone = expectation(description: "expVerificationDone")
+        let myDelegate = TestDelegate(expectation: expVerificationDone)
+        service.delegate = myDelegate
+        service.verify(connectInfo: connectInfo)
+        wait(for: [expVerificationDone], timeout: TestUtil.waitTime)
+        verify(myDelegate.error)
+    }
+}
