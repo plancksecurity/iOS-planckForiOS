f580512436f8c852c13d9fdf63624be163fa743b
Dirk Zimmermann <dz@pep.security>
2019-04-16 16:24:07 +0200

    IOS-1542 FAILING VerifiableAccountTest.

---
+++ pEpForiOSTests/Service/VerifiableAccountTest.swift
@@ -0,0 +1,45 @@
+//
+//  VerifiableAccountTest.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 16.04.19.
+//  Copyright © 2019 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+import MessageModel
+@testable import pEpForiOS
+
+class VerifiableAccountTest: XCTestCase {
+    func testBasicSuccess() {
+        let verifier = VerifiableAccount()
+        var verifierType: VerifiableAccountProtocol = verifier
+        SecretTestData().populateWorkingAccount(
+            verifiableAccount: &verifierType)
+        let expDidVerify = expectation(description: "expDidVerify")
+        let delegate = VerifiableAccountTestDelegate(expDidVerify: expDidVerify)
+        try! check(verifier: &verifierType, delegate: delegate)
+        wait(for: [expDidVerify], timeout: TestUtil.waitTime)
+    }
+
+    // MARK: Helpers
+
+    func check(verifier: inout VerifiableAccountProtocol,
+               delegate: VerifiableAccountDelegate?) throws {
+        verifier.verifiableAccountDelegate = delegate
+        try verifier.verify()
+    }
+}
+
+class VerifiableAccountTestDelegate: VerifiableAccountDelegate {
+    let expDidVerify: XCTestExpectation
+
+    init(expDidVerify: XCTestExpectation) {
+        self.expDidVerify = expDidVerify
+    }
+
+    func didEndVerification(result: Result<Void, Error>) {
+        expDidVerify.fulfill()
+    }
+}
