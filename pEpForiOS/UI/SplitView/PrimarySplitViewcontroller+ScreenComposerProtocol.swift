13540cf17444ed7ef9828eddf40ea094b2863f50
buff <andreas@pep-project.org>
2019-04-17 11:04:46 +0200

    IOS-1495 comments out message thread related code

--- pEpForiOS/UI/SplitView/PrimarySplitViewcontroller+ScreenComposerProtocol.swift
+++ pEpForiOS/UI/SplitView/PrimarySplitViewcontroller+ScreenComposerProtocol.swift
@@ -1,95 +1,97 @@
+ //Commented out as is message thread related (currently unimplemented feature)
+
+////
+////  PrimarySplitViewcontroller+ScreenComposerProtocol.swift
+////  pEp
+////
+////  Created by Borja González de Pablo on 20/07/2018.
+////  Copyright © 2018 p≡p Security S.A. All rights reserved.
+////
 //
-//  PrimarySplitViewcontroller+ScreenComposerProtocol.swift
-//  pEp
+//import Foundation
+//import MessageModel
+//import pEpIOSToolbox
 //
-//  Created by Borja González de Pablo on 20/07/2018.
-//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//extension PrimarySplitViewController: ScreenComposerProtocol {
+//    func emailListViewModel(_ emailListViewModel: EmailListViewModel,
+//                            requestsShowThreadViewFor message: Message) {
+//        let storyboard = UIStoryboard(name: "Thread", bundle: nil)
 //
-
-import Foundation
-import MessageModel
-import pEpIOSToolbox
-
-extension PrimarySplitViewController: ScreenComposerProtocol {
-    func emailListViewModel(_ emailListViewModel: EmailListViewModel,
-                            requestsShowThreadViewFor message: Message) {
-        let storyboard = UIStoryboard(name: "Thread", bundle: nil)
-
-        guard let singleViewController = getDetailViewController() as? EmailViewController else {
-            //Do nothing as it is not showing the detail we want
-            return
-        }
-
-        guard let nav = singleViewController.navigationController,
-            let folder = singleViewController.folderShow,
-            let vc: ThreadViewController =
-            storyboard.instantiateViewController(withIdentifier: "threadViewController")
-                as? ThreadViewController
-            else {
-                Logger.frontendLogger.errorAndCrash("Segue issue")
-                return
-        }
-        vc.appConfig = singleViewController.appConfig
-        let viewModel = ThreadedEmailViewModel(tip:message, folder: folder)
-        viewModel.emailDisplayDelegate = emailListViewModel as! EmailDisplayDelegate
-        vc.model = viewModel
-        nav.viewControllers[nav.viewControllers.count - 1] = vc
-        emailListViewModel.currentDisplayedMessage = viewModel
-        emailListViewModel.updateThreadListDelegate = viewModel
-    }
-
-    func emailListViewModel(_ emailListViewModel: EmailListViewModel,
-                            requestsShowEmailViewFor message: Message) {
-
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
-        guard let threadViewController = getDetailViewController() as? ThreadViewController else {
-            //Do nothing as it is not showing the detail we want
-            return
-        }
-        fatalError()
-            guard let nav = threadViewController.navigationController,
-            let vc: EmailViewController =
-            storyboard.instantiateViewController(withIdentifier: "emailDetail")
-                as? EmailViewController
-        //!!!:, let index = emailListViewModel.index(of: message)
-
-            else {
-                Logger.frontendLogger.errorAndCrash("Segue issues")
-                return
-        }
-
-        vc.appConfig = threadViewController.appConfig
-        vc.message = message
-        //vc.folderShow = emailListViewModel.folderToShow
-        //vc.messageId = index
-        //vc.delegate = emailListViewModel
-        emailListViewModel.currentDisplayedMessage = vc
-        nav.viewControllers[nav.viewControllers.count - 1 ] = vc
-    }
-
-    private func getDetailViewController() -> UIViewController? {
-        let viewControllers = self.viewControllers
-        let last = viewControllers.last
-
-        if isCollapsed {
-            guard let nav = last as? UINavigationController,
-                let emailNav = nav.topViewController as? UINavigationController,
-                let viewController = emailNav.rootViewController
-                else {
-                    return nil
-            }
-            return viewController
-
-        } else {
-            guard let nav = last as? UINavigationController,
-                let viewController = nav.rootViewController
-                else {
-                    return nil
-            }
-            return viewController
-        }
-    }
-
-
-}
+//        guard let singleViewController = getDetailViewController() as? EmailViewController else {
+//            //Do nothing as it is not showing the detail we want
+//            return
+//        }
+//
+//        guard let nav = singleViewController.navigationController,
+//            let folder = singleViewController.folderShow,
+//            let vc: ThreadViewController =
+//            storyboard.instantiateViewController(withIdentifier: "threadViewController")
+//                as? ThreadViewController
+//            else {
+//                Logger.frontendLogger.errorAndCrash("Segue issue")
+//                return
+//        }
+//        vc.appConfig = singleViewController.appConfig
+//        let viewModel = ThreadedEmailViewModel(tip:message, folder: folder)
+//        viewModel.emailDisplayDelegate = emailListViewModel as! EmailDisplayDelegate
+//        vc.model = viewModel
+//        nav.viewControllers[nav.viewControllers.count - 1] = vc
+//        emailListViewModel.currentDisplayedMessage = viewModel
+//        emailListViewModel.updateThreadListDelegate = viewModel
+//    }
+//
+//    func emailListViewModel(_ emailListViewModel: EmailListViewModel,
+//                            requestsShowEmailViewFor message: Message) {
+//
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//
+//        guard let threadViewController = getDetailViewController() as? ThreadViewController else {
+//            //Do nothing as it is not showing the detail we want
+//            return
+//        }
+//        fatalError()
+//            guard let nav = threadViewController.navigationController,
+//            let vc: EmailViewController =
+//            storyboard.instantiateViewController(withIdentifier: "emailDetail")
+//                as? EmailViewController
+//        //!!!:, let index = emailListViewModel.index(of: message)
+//
+//            else {
+//                Logger.frontendLogger.errorAndCrash("Segue issues")
+//                return
+//        }
+//
+//        vc.appConfig = threadViewController.appConfig
+//        vc.message = message
+//        //vc.folderShow = emailListViewModel.folderToShow
+//        //vc.messageId = index
+//        //vc.delegate = emailListViewModel
+//        emailListViewModel.currentDisplayedMessage = vc
+//        nav.viewControllers[nav.viewControllers.count - 1 ] = vc
+//    }
+//
+//    private func getDetailViewController() -> UIViewController? {
+//        let viewControllers = self.viewControllers
+//        let last = viewControllers.last
+//
+//        if isCollapsed {
+//            guard let nav = last as? UINavigationController,
+//                let emailNav = nav.topViewController as? UINavigationController,
+//                let viewController = emailNav.rootViewController
+//                else {
+//                    return nil
+//            }
+//            return viewController
+//
+//        } else {
+//            guard let nav = last as? UINavigationController,
+//                let viewController = nav.rootViewController
+//                else {
+//                    return nil
+//            }
+//            return viewController
+//        }
+//    }
+//
+//
+//}
