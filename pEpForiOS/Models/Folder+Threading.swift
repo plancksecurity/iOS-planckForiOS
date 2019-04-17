be28dd9b16a95bd08d7a4746e1f25a631e91ac60
buff <andreas@pep-project.org>
2019-04-17 14:58:51 +0200

    IOS-1422 debug: UnifiedInbox shows *all* messages in DB, other folders show *all* messages in this folder

--- pEpForiOS/Models/Folder+Threading.swift
+++ pEpForiOS/Models/Folder+Threading.swift
@@ -15,9 +15,18 @@ extension Folder {
      that is no threading involved.
      */
     public func allMessagesNonThreaded() -> [Message] {
-        return allCdMessagesNonThreaded(ignoringPepRating: showsMessagesNeverSeenByEngine)
-            .compactMap {
-                return $0.message()
+        if self is UnifiedInbox {
+            let cdMessages = CdMessage.all() ?? []
+            return cdMessages.compactMap { ($0 as! CdMessage).message() }
+        } else {
+            guard let cdFolder = self.cdFolder() else {
+                UIUtils.showAlertWithOnlyPositiveButton(title: "No CdFolder", message: "No CdFolder exists for folder: \(self)", inViewController: UIApplication.topViewController()!)
+                return []
+            }
+
+            let belongsToFolder = NSPredicate(format: "parent = %@", cdFolder)
+            let cdMessages = CdMessage.all(predicate: belongsToFolder) ?? []
+            return cdMessages.compactMap { ($0 as! CdMessage).message() }
         }
     }
 }
