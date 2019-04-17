3c2fe5e7251894a3c14b600a0bb7e948ecde7382
buff <andreas@pep-project.org>
2019-04-17 14:56:57 +0200

    IOS-1422 adds debug alert presenter

--- pEpForiOS/AppDelegate.swift
+++ pEpForiOS/AppDelegate.swift
@@ -421,3 +421,23 @@ extension AppDelegate {
         }
     }
 }
+
+
+//DEBUG version
+
+extension UIApplication {
+    class func topViewController(base: UIViewController? = (UIApplication.shared.delegate as! AppDelegate).window?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
