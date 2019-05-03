d98f53108bcaf1ba53a4ff8cadba41bfc975fdf6
Xavier Algarra <xavier@pep-project.org>
2019-05-03 14:28:48 +0200

    IOS-1495 temp commit

--- pEpForiOS/UI/EmailDisplayList/EmailListViewModel.swift
+++ pEpForiOS/UI/EmailDisplayList/EmailListViewModel.swift
@@ -119,7 +119,7 @@ class EmailListViewModel {
 
         // We intentionally do *not* start monitoring. Respiosibility is on currently on VC.
         messageQueryResults = MessageQueryResults(withFolder: folderToShow,
-                                                       filter: nil,
+                                                       filter: folderToShow.defaultFilter,
                                                        search: nil)
         //!!!: changed filter to nil take care
         messageQueryResults.delegate = self
