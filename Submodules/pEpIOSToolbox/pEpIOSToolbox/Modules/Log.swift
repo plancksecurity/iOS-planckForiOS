7fefb081416dfcddb0f54762f225994f40a76b3c
Alejandro Gelos <agp@pep.security>
2019-05-03 17:20:34 +0200

    IOS-1467 fix Log in tookbox

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/Modules/Log.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/Modules/Log.swift
@@ -2,7 +2,7 @@
 //  Log.swift
 //  pEpIOSToolbox
 //
-//  Created by Alejandro Gelos on 15/04/2019.
+//  Created by Alejandro Gelos on 03/05/2019.
 //  Copyright © 2019 pEp Security SA. All rights reserved.
 //
 
@@ -10,7 +10,7 @@ import Foundation
 
 /// Shared instance of logger.
 class Log {
-    static let shared = Logger(subsystem: "security.pEp.MessageModel", category: "MessageModule")
+    static let shared = Logger(subsystem: "security.pEp.pEpForiOS", category: "pEpIOSToolbox")
 
     /// Init is forbidden. Singleton...
     private init() {}
