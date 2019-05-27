5725d9f728d69710588cbeea61917cb5574b9c57
Alejandro Gelos <alejandro@pep-project.org>
2019-05-27 11:56:12 +0200

    IOS-1605 add outbox to folderTypesNotAllowedToMoveTo

--- pEpForiOS/UI/MoveToFolder/ViewModel/MoveToFolderViewModel.swift
+++ pEpForiOS/UI/MoveToFolder/ViewModel/MoveToFolderViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import pEpIOSToolbox
 import MessageModel
 
-let folderTypesNotAllowedToMoveTo = [FolderType.drafts, .sent]
+let folderTypesNotAllowedToMoveTo = [FolderType.drafts, .sent, .outbox]
 
 class MoveToAccountViewModel {
 
