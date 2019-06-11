dfccf80af29c1b43da756dfaa6f804dd8d7d874c
buff <andreas@pep-project.org>
2019-06-11 18:11:29 +0200

    IOS-1576 adds huss info

--- pEpForiOS/KeySync/KeySyncHandshakeService.swift
+++ pEpForiOS/KeySync/KeySyncHandshakeService.swift
@@ -9,47 +9,65 @@
 import MessageModel
 import PEPObjCAdapterFramework
 
-class KeySyncHandshakeService: KeySyncServiceHandshakeDelegate {
+class KeySyncHandshakeService {
     weak var presenter: UIViewController?
-    
-    func handleHandshakeRequest(_ object: UnsafeMutableRawPointer?,
-                                meFpr: String?,
-                                partnerFpr: String?,
-                                signal: PEPSyncHandshakeSignal) {
-        fatalError()
-        //TODO:
-        /*
-         PEPSyncHandshakeSignalUndefined = 0, // SYNC_NOTIFY_UNDEFINED = 0,
 
-         // request show handshake dialog
-         PEPSyncHandshakeSignalInitAddOurDevice = 1, // SYNC_NOTIFY_INIT_ADD_OUR_DEVICE = 1,
-         PEPSyncHandshakeSignalInitAddOtherDevice = 2, // SYNC_NOTIFY_INIT_ADD_OTHER_DEVICE = 2,
-         PEPSyncHandshakeSignalInitFormGroup = 3, // SYNC_NOTIFY_INIT_FORM_GROUP = 3,
-         // SYNC_NOTIFY_INIT_MOVE_OUR_DEVICE = 4,
+    // ALWAYS MUST close existing
+//    func handleHandshakeRequest(_ object: UnsafeMutableRawPointer?,
+//                                meFpr: String?,
+//                                partnerFpr: String?,
+//                                signal: PEPSyncHandshakeSignal) {
+//        fatalError()
+//        //TODO:
+//        /*
+//         PEPSyncHandshakeSignalUndefined = 0, // SYNC_NOTIFY_UNDEFINED = 0,
+    //yet undefined
+//
+//         // request show handshake dialog
+//         PEPSyncHandshakeSignalInitAddOurDevice = 1, // SYNC_NOTIFY_INIT_ADD_OUR_DEVICE = 1,
+//         PEPSyncHandshakeSignalInitAddOtherDevice = 2, // SYNC_NOTIFY_INIT_ADD_OTHER_DEVICE = 2,
+//         PEPSyncHandshakeSignalInitFormGroup = 3, // SYNC_NOTIFY_INIT_FORM_GROUP = 3,
+//         // SYNC_NOTIFY_INIT_MOVE_OUR_DEVICE = 4,
+//
+//         // handshake process timed out
+//         PEPSyncHandshakeSignalTimeout = 5, // SYNC_NOTIFY_TIMEOUT = 5,
+//
+//         // handshake accepted by user
+//         PEPSyncHandshakeSignalAcceptedDeviceAdded = 6, // SYNC_NOTIFY_ACCEPTED_DEVICE_ADDED = 6,
+//         PEPSyncHandshakeSignalAcceptedGroupCreated = 7, // SYNC_NOTIFY_ACCEPTED_GROUP_CREATED = 7,
+//         // SYNC_NOTIFY_ACCEPTED_DEVICE_MOVED = 8,
+    // allert: thumbs up
+//
+//         // handshake dialog must be closed
+//         PEPSyncHandshakeSignalOvertaken = 9, // SYNC_NOTIFY_OVERTAKEN = 9,
+//
+//         // notificaton of actual group status
+//         PEPSyncHandshakeSignalSole = 254, // SYNC_NOTIFY_SOLE = 254,
+//         PEPSyncHandshakeSignalInGroup = 255 // SYNC_NOTIFY_IN_GROUP = 255
+//         */
+//
+//
+//        /*
+//        PEPSession().deliver(<#T##result: PEPSyncHandshakeResult##PEPSyncHandshakeResult#>, identitiesSharing: <#T##[PEPIdentity]?#>)
+//
+//         PEPSyncHandshakeResultCancel = -1, // SYNC_HANDSHAKE_CANCEL = -1,
+//         PEPSyncHandshakeResultAccepted = 0, // SYNC_HANDSHAKE_ACCEPTED = 0,
+//         PEPSyncHandshakeResultRejected = 1 // SYNC_HANDSHAKE_REJECTED = 1
+// */
+//    }
+}
 
-         // handshake process timed out
-         PEPSyncHandshakeSignalTimeout = 5, // SYNC_NOTIFY_TIMEOUT = 5,
+extension KeySyncHandshakeService: KeySyncServiceHandshakeDelegate {
+    func showHandshake(meFpr: String, partnerFpr: String) { //BUFF: HERE: add completionhandler
+        fatalError("unimplemented stub")
 
-         // handshake accepted by user
-         PEPSyncHandshakeSignalAcceptedDeviceAdded = 6, // SYNC_NOTIFY_ACCEPTED_DEVICE_ADDED = 6,
-         PEPSyncHandshakeSignalAcceptedGroupCreated = 7, // SYNC_NOTIFY_ACCEPTED_GROUP_CREATED = 7,
-         // SYNC_NOTIFY_ACCEPTED_DEVICE_MOVED = 8,
+        // Show handshake.
+        // report result
+    }
 
-         // handshake dialog must be closed
-         PEPSyncHandshakeSignalOvertaken = 9, // SYNC_NOTIFY_OVERTAKEN = 9,
+    func cancelHandshake() {
+        fatalError("unimplemented stub")
 
-         // notificaton of actual group status
-         PEPSyncHandshakeSignalSole = 254, // SYNC_NOTIFY_SOLE = 254,
-         PEPSyncHandshakeSignalInGroup = 255 // SYNC_NOTIFY_IN_GROUP = 255
-         */
-
-
-        /*
-        PEPSession().deliver(<#T##result: PEPSyncHandshakeResult##PEPSyncHandshakeResult#>, identitiesSharing: <#T##[PEPIdentity]?#>)
-
-         PEPSyncHandshakeResultCancel = -1, // SYNC_HANDSHAKE_CANCEL = -1,
-         PEPSyncHandshakeResultAccepted = 0, // SYNC_HANDSHAKE_ACCEPTED = 0,
-         PEPSyncHandshakeResultRejected = 1 // SYNC_HANDSHAKE_REJECTED = 1
- */
+        // dismiss possibly shown, KeySync related views
     }
 }
