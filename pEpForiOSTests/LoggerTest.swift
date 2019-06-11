057244d0c88121610177e5143d17925d0594d31b
Dirk Zimmermann <dz@pep.security>
2019-06-11 16:28:31 +0200

    IOS-1540 Back out fd9ab3617937: Remove LoggerTest.

---
+++ pEpForiOSTests/LoggerTest.swift
@@ -0,0 +1,22 @@
+//
+//  LoggerTest.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 18.12.18.
+//  Copyright © 2018 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+@testable import pEpForiOS
+@testable import MessageModel
+@testable import pEpIOSToolbox
+
+class LoggerTest: XCTestCase {
+    func testSimple() {
+        let log1 = Logger(subsystem: "sys1", category: "cat1")
+        log1.log("1 hi (standalone)")
+        log1.log("2 hi (one number): %d", 2)
+        log1.log("3 hi (one number plus string): %d %@", 2, "parameters")
+    }
+}
