7dc27953b74a1d66e607feef443bb44a51db1e26
Dirk Zimmermann <dz@pep.security>
2019-06-12 11:27:11 +0200

    IOS-1646 Have to log all errors in order to catch internal ones.

--- pEpForiOS/UI/Util/UIUtils.swift
+++ pEpForiOS/UI/Util/UIUtils.swift
@@ -19,10 +19,10 @@ struct UIUtils {
     ///   - error: error to preset to user
     ///   - vc: ViewController to present the error on
     static func show(error: Error, inViewController vc: UIViewController) {
+        Log.shared.error("Will display error to user: (interpolate) %@", "\(error)")
+
         guard let displayError = DisplayUserError(withError: error) else {
             // Do nothing. The error type is not suitable to bother the user with.
-            // Log it, though.
-            Log.shared.error("*** Internal error: %@", error as CVarArg)
             return
         }
         showAlertWithOnlyPositiveButton(title: displayError.title,
