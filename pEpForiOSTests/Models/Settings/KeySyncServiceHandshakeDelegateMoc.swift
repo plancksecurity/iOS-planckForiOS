0bcff9c441e4d45da85024a7771ab2c5149e391a
Alejandro Gelos <agp@pep.security>
2019-06-19 16:29:57 +0200

    IOS-1611 add KeySyncServiceHandshakeDelegateMoc

---
+++ pEpForiOSTests/Models/Settings/KeySyncServiceHandshakeDelegateMoc.swift
@@ -0,0 +1,19 @@
+//
+//  KeySyncServiceHandshakeDelegateMoc.swift
+//  pEpForiOSTests
+//
+//  Created by Alejandro Gelos on 19/06/2019.
+//  Copyright © 2019 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import MessageModel
+
+final class KeySyncServiceHandshakeDelegateMoc: KeySyncServiceHandshakeDelegate{
+    var presenter: UIViewController?
+
+    func showHandshake(meFpr: String, partnerFpr: String) {}
+
+    func cancelHandshake() {}
+
+}
