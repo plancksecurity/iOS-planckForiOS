4bda73cb05e6d08f3ef8454ce231bf30b6f05854
buff <andreas@pep-project.org>
2019-07-01 10:15:27 +0200

    IOS-1576 debug (no UI involved) version to find working timeout threshold

--- pEpForiOS/KeySync/KeySyncHandshakeService.swift
+++ pEpForiOS/KeySync/KeySyncHandshakeService.swift
@@ -16,62 +16,71 @@ class KeySyncHandshakeService {
 }
 
 extension KeySyncHandshakeService: KeySyncServiceHandshakeDelegate {
-    func showHandshake(me: PEPIdentity, partner: PEPIdentity) { //BUFF: HERE: add completionhandler
-        DispatchQueue.main.async { [weak self] in
-            guard let safeSelf = self else {
-                Log.shared.errorAndCrash("Lost myself")
-                return
-            }
 
-            guard let meFPR = me.fingerPrint, let partnerFPR = partner.fingerPrint else {
-                Log.shared.errorAndCrash("Missing FPRs")
-                try? PEPSession().deliver(PEPSyncHandshakeResult.cancel,
-                                          identitiesSharing: [me, partner])
-                return
-            }
+    func showHandshake(me: PEPIdentity, partner: PEPIdentity) {
 
-            let trustwords = try! PEPSession().getTrustwordsFpr1(meFPR,
-                                                                 fpr2: partnerFPR,
-                                                                 language: nil,
-                                                                 full: true)
+        //BUFF: debug without UI
+        try! PEPSession().deliver(PEPSyncHandshakeResult.accepted, identitiesSharing: [me, partner])
 
-            // Show Handshake
-            let newAlertView = UIAlertController.pEpAlertController(title: "Handshake",
-                                                                    message: trustwords,
-                                                                    preferredStyle: .alert)
-            newAlertView.addAction(UIAlertAction(title: "Confirm", style: .default) { action in
-                do {
-                    try PEPSession().deliver(PEPSyncHandshakeResult.accepted,
-                                             identitiesSharing: [me, partner])
-                } catch {
-                    return
-                }
-            })
-            newAlertView.addAction(UIAlertAction(title: "Wrong Trustwords", style: .destructive) { action in
-                do {
-                    try PEPSession().deliver(PEPSyncHandshakeResult.rejected,
-                                             identitiesSharing: [me, partner])
-                } catch {
-                    return
-                }
-            })
-            newAlertView.addAction(UIAlertAction(title: "Cancel", style: .cancel) { action in
-                do {
-                    try PEPSession().deliver(PEPSyncHandshakeResult.cancel,
-                                             identitiesSharing: [me, partner])
-                } catch {
-                    return
-                }
-            })
-            safeSelf.alertView = newAlertView
+        return
+            //FFUB
 
-            guard let vc = safeSelf.presenter else {
-                Log.shared.errorAndCrash("No Presenter")
-                return
-            }
-            vc.present(newAlertView, animated: true, completion: nil)
-
-        }
+//            //BUFF: HERE: add completionhandler
+//            DispatchQueue.main.async { [weak self] in
+//                guard let safeSelf = self else {
+//                    Log.shared.errorAndCrash("Lost myself")
+//                    return
+//                }
+//
+//                guard let meFPR = me.fingerPrint, let partnerFPR = partner.fingerPrint else {
+//                    Log.shared.errorAndCrash("Missing FPRs")
+//                    try? PEPSession().deliver(PEPSyncHandshakeResult.cancel,
+//                                              identitiesSharing: [me, partner])
+//                    return
+//                }
+//
+//            let trustwords = try! PEPSession().getTrustwordsFpr1(meFPR,
+//                                                                 fpr2: partnerFPR,
+//                                                                 language: nil,
+//                                                                 full: true)
+//
+//            // Show Handshake
+//            let newAlertView = UIAlertController.pEpAlertController(title: "Handshake",
+//                                                                    message: trustwords,
+//                                                                    preferredStyle: .alert)
+//            newAlertView.addAction(UIAlertAction(title: "Confirm", style: .default) { action in
+//                do {
+//                    try PEPSession().deliver(PEPSyncHandshakeResult.accepted,
+//                                             identitiesSharing: [me, partner])
+//                } catch {
+//                    return
+//                }
+//            })
+//            newAlertView.addAction(UIAlertAction(title: "Wrong Trustwords", style: .destructive) { action in
+//                do {
+//                    try PEPSession().deliver(PEPSyncHandshakeResult.rejected,
+//                                             identitiesSharing: [me, partner])
+//                } catch {
+//                    return
+//                }
+//            })
+//            newAlertView.addAction(UIAlertAction(title: "Cancel", style: .cancel) { action in
+//                do {
+//                    try PEPSession().deliver(PEPSyncHandshakeResult.cancel,
+//                                             identitiesSharing: [me, partner])
+//                } catch {
+//                    return
+//                }
+//            })
+//            safeSelf.alertView = newAlertView
+//
+//            guard let vc = safeSelf.presenter else {
+//                Log.shared.errorAndCrash("No Presenter")
+//                return
+//            }
+//            vc.present(newAlertView, animated: true, completion: nil)
+//
+//        }
     }
 
     func cancelHandshake() {
