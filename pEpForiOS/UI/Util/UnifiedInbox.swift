2622314d04043b87c93694493bd9b69449d66998
Dirk Zimmermann <dz@pep.security>
2019-07-29 11:43:55 +0200

    IOS-1596 Rename.

--- pEpForiOS/UI/Util/UnifiedInbox.swift
+++ pEpForiOS/UI/Util/UnifiedInbox.swift
@@ -33,7 +33,7 @@ public class UnifiedInbox: VirtualFolderProtocol {
             predicates.append(CdMessage.PredicateFactory.isInInbox())
             predicates.append(CdMessage.PredicateFactory.existingMessages())
             predicates.append(CdMessage.PredicateFactory.processed())
-            predicates.append(CdMessage.PredicateFactory.isNotAutoconsumable())
+            predicates.append(CdMessage.PredicateFactory.isNotAutoConsumable())
             return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
         }
     }
