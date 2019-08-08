cc004c497dac0484b08eee734e5ea33060242b30
buff <andreas@pep-project.org>
2019-08-08 14:34:39 +0200

    rm test that doesnot make sense any more with wrapped CdObjects.

--- pEpForiOSTests/UI/Util/AccountPickerViewModel/AccountPickerViewModelTest.swift
+++ pEpForiOSTests/UI/Util/AccountPickerViewModel/AccountPickerViewModelTest.swift
@@ -65,14 +65,6 @@ class AccountPickerViewModelTest: CoreDataDrivenTestBase {
                             mustEqualSelected: true)
     }
 
-    func testHandleUserSelected_oneAccount_shouldFail() {
-        let firstRowIdx = 0
-        let accountNotSaved = SecretTestData().createWorkingAccount(number: 1)
-        assertUserSelection(selectIdx: firstRowIdx,
-                            accountToCompare: accountNotSaved,
-                            mustEqualSelected: false)
-    }
-
     func testHandleUserSelected_twoAccounts_correct() {
         let secondRowIdx = 1
         let _ = createAndSaveSecondAccount()
