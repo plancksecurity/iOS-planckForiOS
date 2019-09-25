7a89c3b7aceb1df277b75744ebd4418023c89e90
Xavier Algarra <xavier@pep-project.org>
2019-09-25 16:48:39 +0200

    IOS-1796 extension for delegates + table functions

--- pEpForiOS/UI/Settings/Setting/TrustSetting/ResetTrustViewController.swift
+++ pEpForiOS/UI/Settings/Setting/TrustSetting/ResetTrustViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ResetTrustViewController: UIViewController {
 
-    let viewModel = ResetTrustViewModel()
+    let cellId = "ResetTrustSettingCell"
+    let model = ResetTrustViewModel()
 
     @IBOutlet var tableView: UITableView!
 
@@ -18,7 +19,9 @@ class ResetTrustViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        model.startMonitoring()
     }
 
     func setupView() {
@@ -27,29 +30,87 @@ class ResetTrustViewController: UIViewController {
         tableView.delegate = self
         ///Hide toolbar
         navigationController?.setToolbarHidden(true, animated: false)
+        model.delegate = self
     }
 
 }
 
 extension ResetTrustViewController: UITableViewDataSource, UITableViewDelegate {
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return viewModel.numberOfSections()
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfRowsPerSection(section: section)
-    }
-
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return viewModel.titleForSection(section: section)
+        return model.numberOfRowsPerSection(section: section)
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        cell?.textLabel?.text = model.nameFor(indexPath: indexPath)
+        return cell ?? UITableViewCell()
     }
 
-    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return viewModel.indexElements()
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
+
+/*
+
+ let cell = tableView.dequeueReusableCell(withIdentifier: EmailListViewCell.storyboardId,
+ for: indexPath)
+ if let theCell = cell as? EmailListViewCell {
+ theCell.delegate = self
+
+ guard let viewModel = model?.viewModel(for: indexPath.row) else {
+ return cell
+ }
+ theCell.configure(for: viewModel)
+ } else {
+ Log.shared.errorAndCrash("dequeued wrong cell")
+ }
+
+ //restores selection state for updated or replaced cells.
+ if lastSelectedIndexPath == indexPath {
+ tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+ }
+
+ return cell
+ */
+
+extension ResetTrustViewController: ResetTrustViewModelDelegate {
+    func willReceiveUpdates(viewModel: ResetTrustViewModel) {
+        tableView.beginUpdates()
+    }
+
+    func allUpdatesReceived(viewModel: ResetTrustViewModel) {
+        tableView.endUpdates()
+    }
+
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didInsertDataAt indexPaths: [IndexPath]) {
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didUpdateDataAt indexPaths: [IndexPath]) {
+        tableView.reloadRows(at: indexPaths, with: .none)
+    }
+
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didRemoveDataAt indexPaths: [IndexPath]) {
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didMoveData atIndexPath: IndexPath, toIndexPath: IndexPath) {
+        tableView.moveRow(at: atIndexPath, to: toIndexPath)
+    }
+
+    func updateView() {
+        tableView.reloadData()
+    }
+
+
 }
