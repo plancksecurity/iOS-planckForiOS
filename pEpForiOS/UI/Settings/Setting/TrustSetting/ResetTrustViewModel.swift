d59261de862587fd3f9cd7e667ce4b5cfb48bf46
Xavier Algarra <xavier@pep-project.org>
2019-09-25 16:48:59 +0200

    IOS-1796 using identityqueryresults

--- pEpForiOS/UI/Settings/Setting/TrustSetting/ResetTrustViewModel.swift
+++ pEpForiOS/UI/Settings/Setting/TrustSetting/ResetTrustViewModel.swift
@@ -9,7 +9,33 @@
 import Foundation
 import MessageModel
 
+protocol ResetTrustViewModelDelegate: class, TableViewUpdate {
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didInsertDataAt indexPaths: [IndexPath])
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didUpdateDataAt indexPaths: [IndexPath])
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didRemoveDataAt indexPaths: [IndexPath])
+    func resetTrustViewModel(viewModel: ResetTrustViewModel, didMoveData atIndexPath: IndexPath, toIndexPath: IndexPath)
+    func willReceiveUpdates(viewModel: ResetTrustViewModel)
+    func allUpdatesReceived(viewModel: ResetTrustViewModel)
+}
+
 class ResetTrustViewModel {
+
+    var identityQueryResult: IdentityQueryResults
+    var delegate: ResetTrustViewModelDelegate?
+
+    init() {
+        identityQueryResult = IdentityQueryResults()
+        identityQueryResult.delegate = self
+    }
+
+    func startMonitoring() {
+        try? identityQueryResult.startMonitoring()
+    }
+
+    func nameFor(indexPath: IndexPath) -> String {
+        let id = identityQueryResult[indexPath.row]
+        return id.userNameOrAddress
+    }
 
     func numberOfSections() -> Int {
         //!!!: To Be Implemented
@@ -22,12 +48,41 @@ class ResetTrustViewModel {
     }
 
     func numberOfRowsPerSection(section: Int) -> Int {
-        //!!!: To Be Implemented
-        return 1
+        do {
+            return try identityQueryResult.count()
+        } catch {
+            return 0
+        }
     }
 
     func indexElements() -> [String] {
         //!!!: To Be Implemented
         return ["T"]
+    }
+}
+
+extension ResetTrustViewModel: QueryResultsDelegate {
+    func didInsert(indexPath: IndexPath) {
+        delegate?.resetTrustViewModel(viewModel: self, didInsertDataAt: [indexPath])
+    }
+
+    func didUpdate(indexPath: IndexPath) {
+        delegate?.resetTrustViewModel(viewModel: self, didUpdateDataAt: [indexPath])
+    }
+
+    func didDelete(indexPath: IndexPath) {
+        delegate?.resetTrustViewModel(viewModel: self, didRemoveDataAt: [indexPath])
+    }
+
+    func didMove(from: IndexPath, to: IndexPath) {
+        delegate?.resetTrustViewModel(viewModel: self, didMoveData: from, toIndexPath: to)
+    }
+
+    func willChangeResults() {
+        delegate?.willReceiveUpdates(viewModel: self)
+    }
+
+    func didChangeResults() {
+        delegate?.allUpdatesReceived(viewModel: self)
     }
 }
