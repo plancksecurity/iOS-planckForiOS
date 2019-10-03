f31620d9111dbf09ec84b40fcc82abb02be4381d
Xavier Algarra <xavier@pep-project.org>
2019-10-03 13:18:38 +0200

    IOS-1889 account settings table new design

--- pEpForiOS/UI/Settings/Setting/AccountSettings/AccountSettingsTableViewController.swift
+++ pEpForiOS/UI/Settings/Setting/AccountSettings/AccountSettingsTableViewController.swift
@@ -208,10 +208,18 @@ UIPickerViewDataSource, UITextFieldDelegate {
         return viewModel?[section]
     }
     
-    override func tableView(
-        _ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+//    override func tableView(
+//        _ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 50
+//    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return viewModel?.footerFor(section: section)
     }
+
+//    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return 50
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let origCount = super.tableView(tableView, numberOfRowsInSection: section)
