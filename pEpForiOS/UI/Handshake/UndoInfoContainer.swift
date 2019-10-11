18f1ecb38ab0fe24a1c1177748123e834cef39a7
Dirk Zimmermann <dz@pep.security>
2019-10-11 08:36:21 +0200

    IOS-1823 ObjC container for storing undo information.

---
+++ pEpForiOS/UI/Handshake/UndoInfoContainer.swift
@@ -0,0 +1,21 @@
+//
+//  UndoInfoContainer.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 10.10.19.
+//  Copyright © 2019 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Contains trust or mistrust undo information suitable for being used
+/// in connection with an undo manager, being able to being cast to Any.
+@objc class UndoInfoContainer: NSObject {
+    let indexPath: IndexPath
+    let viewModel: HandshakePartnerTableViewCellViewModel
+
+    init(indexPath: IndexPath, viewModel: HandshakePartnerTableViewCellViewModel) {
+        self.indexPath = indexPath
+        self.viewModel = viewModel
+    }
+}
