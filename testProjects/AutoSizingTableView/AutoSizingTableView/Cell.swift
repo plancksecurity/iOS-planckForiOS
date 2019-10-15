0d15b80c59f7a0c39dc72b24cf0591c59277a991
Dirk Zimmermann <dz@pep.security>
2019-10-15 17:01:51 +0200

    IOS-1823 Test project handshake screen.

---
+++ testProjects/AutoSizingTableView/AutoSizingTableView/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  AutoSizingTableView
+//
+//  Created by Dirk Zimmermann on 15.10.19.
+//  Copyright © 2019 pEp Security. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Cell: UITableViewCell {
+    @IBOutlet weak var theTextLabel: UILabel!
+}
