dd351db4ac0683235352be621dcbc6be9939a6ed
buff <andreas@pep-project.org>
2019-10-17 09:27:36 +0200

    adapts MM changes: pEpRatingNone does not exist any more

--- pEpForiOSTests/TestUtils/TestUtil.swift
+++ pEpForiOSTests/TestUtils/TestUtil.swift
@@ -664,7 +664,7 @@ class TestUtil {
                                                             XCTFail()
                                                             return nil
             }
-            XCTAssertEqual(cdMessage.pEpRating, CdMessage.pEpRatingNone)
+            XCTAssertEqual(cdMessage.pEpRating, Int16(PEPRating.undefined.rawValue))
 
             guard let cdM = CdMessage.first(in: context) else {
                 XCTFail("Expected the one message in the DB that we imported")
@@ -741,7 +741,7 @@ class TestUtil {
                 XCTFail()
                 return nil
         }
-        XCTAssertEqual(cdMessage.pEpRating, CdMessage.pEpRatingNone)
+        XCTAssertEqual(cdMessage.pEpRating, Int16(PEPRating.undefined.rawValue))
 
         return cdMessage
     }
