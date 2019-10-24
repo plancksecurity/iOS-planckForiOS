382b00b5a542969d837e5c4064551f37e965917d
Dirk Zimmermann <dz@pep.security>
2019-10-24 11:28:55 +0200

    IOS-1911 Back out: Trying alignment rect, blow up to 24.

--- pEpForiOS/Util/Appearance.swift
+++ pEpForiOS/Util/Appearance.swift
@@ -66,20 +66,9 @@ class Appearance {
         appearance.largeTitleTextAttributes = titleTextAttributes
         appearance.doneButtonAppearance.normal.titleTextAttributes = titleTextAttributes
 
-        if var chevronLeftImg = UIImage(named: "chevron-left-original") {
-            let optimumHeight: CGFloat = 24
-            let height = chevronLeftImg.size.height
-            if height < optimumHeight {
-                let diff = optimumHeight - height
-                let halveDiff = round(diff/2)
-                let edgeInsets = UIEdgeInsets(top: -halveDiff,
-                                              left: 0,
-                                              bottom: -halveDiff,
-                                              right: 0)
-                chevronLeftImg = chevronLeftImg.withAlignmentRectInsets(edgeInsets)
-            }
-            appearance.setBackIndicatorImage(chevronLeftImg, transitionMaskImage: chevronLeftImg)
-        }
+        let chevronLeftImg = UIImage(named: "chevron-left-original")
+        let chevronLeftImg2 = chevronLeftImg?.withBaselineOffset(fromBottom: 0)
+        appearance.setBackIndicatorImage(chevronLeftImg2, transitionMaskImage: chevronLeftImg2)
 
         return appearance
     }
