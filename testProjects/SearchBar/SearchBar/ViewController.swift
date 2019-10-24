a49cd5be0678398d99ef02a6efcb80772e96b189
Dirk Zimmermann <dz@pep.security>
2019-10-24 15:17:57 +0200

    IOS-1949 SAMPLE: UISearchController test project.

---
+++ testProjects/SearchBar/SearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBar
+//
+//  Created by Dirk Zimmermann on 24.10.19.
+//  Copyright © 2019 pEp Security. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
