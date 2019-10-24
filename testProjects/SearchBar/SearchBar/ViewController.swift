1614409dd4671904ceeabee40d49e8850f7ba848
Dirk Zimmermann <dz@pep.security>
2019-10-24 15:17:57 +0200

    IOS-1949 SAMPLE: Table view.

--- testProjects/SearchBar/SearchBar/ViewController.swift
+++ testProjects/SearchBar/SearchBar/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "ReuseString")
+
+        cell.textLabel?.text = "Title \(indexPath.row)"
+        cell.detailTextLabel?.text = "[1] Non eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret."
+
+        return cell
+    }
+}
