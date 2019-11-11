3d291fd83e7a036657b1d4a36f6e4d3332015a92
Dirk Zimmermann <dz@pep.security>
2019-11-11 10:46:09 +0100

    IOS-1962 Back out 1e845431d311: Show noMessage only in appropriate situations

--- pEpForiOS/UI/EmailDisplayList/EmailListViewController.swift
+++ pEpForiOS/UI/EmailDisplayList/EmailListViewController.swift
@@ -81,17 +81,11 @@ class EmailListViewController: BaseTableViewController, SwipeTableViewCellDelega
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        if let spvc = splitViewController {
-            switch spvc.currentDisplayMode {
-            case .onlyMaster: break
-            case .masterAndDetail:
-                // When the message list disappears for, e.g., settings,
-                // then the "no message" should be displayed.
-                if !splitViewControllerWillHidePrimary {
-                    performSegue(withIdentifier: .showNoMessage, sender: nil)
-                }
-            case .onlyDetail: break
-            }
+        guard let isIphone = splitViewController?.isCollapsed else {
+            return
+        }
+        if !isIphone {
+            performSegue(withIdentifier: .showNoMessage, sender: nil)
         }
     }
 
