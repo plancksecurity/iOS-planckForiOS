495fd6745dfcf9561a0b37f264c99344ccd1f818
Dirk Zimmermann <dz@pep.security>
2019-11-12 14:27:48 +0100

    IOS-1962 Docs

--- pEpForiOS/UI/SplitView/NoMessagesViewController.swift
+++ pEpForiOS/UI/SplitView/NoMessagesViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class NoMessagesViewController: UIViewController {
     @IBOutlet weak var labelMessage: UILabel!
 
+    /// The message to display when this VC is shown in the details view.
+    /// - Note: On some devices, the master VC has no chance to decide for this message
+    ///   in all circunstances, unless involving more support code,
+    ///   so this default should be neutral enough to cover all views that need it.
     var message: String = NSLocalizedString(
         "Nothing Selected",
         comment: "Default message in detail view when nothing has been selected")
