1b24f450bce2e40a9be50fecf2581ad6876cbf64
buff <andreas@pep-project.org>
2019-11-13 13:54:07 +0100

    IOS-1942 adds evaluation app as pEp4iOS is currently unable to build (Sequoia)

---
+++ MacCatalystEvaluation/MacCatalystEvaluation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacCatalystEvaluation
+//
+//  Created by Andreas Buff on 13.11.19.
+//  Copyright © 2019 pEp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
