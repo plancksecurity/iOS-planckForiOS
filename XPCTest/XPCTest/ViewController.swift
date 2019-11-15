f2e3f14a5cdbd0dd60be948609e024c17578de59
buff <andreas@pep-project.org>
2019-11-15 09:26:57 +0100

    IOS-1942 adds XPC test project

---
+++ XPCTest/XPCTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  XPCTest
+//
+//  Created by Andreas Buff on 14.11.19.
+//  Copyright © 2019 pEp. All rights reserved.
+//
+
+import Cocoa
+import XPC
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
