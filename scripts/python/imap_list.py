5bb7cd309a54bf45951c5e69583a18d135ec2a1b
Dirk Zimmermann <dz@pep.security>
2019-11-21 15:56:18 +0100

    Format

--- scripts/python/imap_list.py
+++ scripts/python/imap_list.py
@@ -8,12 +8,16 @@ from pprint import pprint
 
 from imap_ini import connect_account
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='List INBOX on IMAP accounts')
-    parser.add_argument('--credentials', required=True, help='INI file containing account credentials')
-    parser.add_argument('account_name', help='The account to list the INBOX for')
+    parser.add_argument('--credentials', required=True,
+                        help='INI file containing account credentials')
+    parser.add_argument(
+        'account_name', help='The account to list the INBOX for')
     args = parser.parse_args()
     return args
+
 
 def process(credentials, account_name):
     con = connect_account(credentials, account_name)
@@ -21,8 +25,10 @@ def process(credentials, account_name):
     if status == 'OK':
         typ, data = con.search(None, 'ALL')
         for num in data[0].split():
-            typ, data = con.fetch(num, '(UID, BODY.PEEK[HEADER.FIELDS (SUBJECT)])')
+            typ, data = con.fetch(
+                num, '(UID, BODY.PEEK[HEADER.FIELDS (SUBJECT)])')
             pprint(data)
+
 
 if __name__ == '__main__':
     args = parse_args()
