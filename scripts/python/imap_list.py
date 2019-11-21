8cc3a3187e3dceb33e8d744bbb8b35721fd96033
Dirk Zimmermann <dz@pep.security>
2019-11-21 15:24:35 +0100

    Include the actual UID in the dump

--- scripts/python/imap_list.py
+++ scripts/python/imap_list.py
@@ -22,7 +22,7 @@ def process(credentials, account_name):
         typ, data = con.search(None, 'ALL')
         for num in data[0].split():
             typ, data = con.fetch(num, '(UID, BODY.PEEK[HEADER.FIELDS (SUBJECT)])')
-            print('Message %s %s' % (num, data[0][1]))
+            pprint(data)
 
 if __name__ == '__main__':
     args = parse_args()
