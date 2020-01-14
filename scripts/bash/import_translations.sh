9beb6c6f0070f28207512b12ed5613853082d7e6
Dirk Zimmermann <dz@pep.security>
2020-01-14 11:15:42 +0100

    IOS-2038 The sed command to be used

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -5,5 +5,6 @@ translationdir=../pEp-Translate/
 mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
 cp $translationdir/*.xliff $mytmpdir
+sed -i '' 's/<target\/>//' $mytmpdir/*.xliff
 
 rm -fr $mytmpdir
