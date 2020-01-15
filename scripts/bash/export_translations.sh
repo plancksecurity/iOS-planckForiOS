b45c5c9297cad01f26f4e67de1cdcc1d59ccf33c
Dirk Zimmermann <dz@pep.security>
2020-01-15 15:17:19 +0100

    IOS-2038 Fix spacing

--- scripts/bash/export_translations.sh
+++ scripts/bash/export_translations.sh
@@ -16,9 +16,10 @@ do
   xcodebuild -exportLocalizations -project pEpForiOS.xcodeproj -exportLanguage $lang -localizationPath $appdir
 
   cp "$appdir/$lang.xcloc/Localized Contents/$lang.xliff" $translationdir
-    xcodebuild -exportLocalizations -project ../MessageModel/MessageModel/MesssageModel.xcodeproj -exportLanguage $lang -localizationPath $mmdir
+  
+  xcodebuild -exportLocalizations -project ../MessageModel/MessageModel/MesssageModel.xcodeproj -exportLanguage $lang -localizationPath $mmdir
 
-cp "$mmdir/$lang.xcloc/Localized Contents/$lang.xliff" $translationdir/MessageModel
+  cp "$mmdir/$lang.xcloc/Localized Contents/$lang.xliff" $translationdir/MessageModel
  done
 
 rm -fr $mytmpdir
