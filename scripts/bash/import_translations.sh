452b8748b3222b87824e74f9a56e8e9f11f4d50f
Dirk Zimmermann <dz@pep.security>
2020-01-15 15:17:19 +0100

    IOS-2038 Make import depend on import_in_place

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -11,21 +11,16 @@ function import() {
     mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
     cp $2/*.xliff $mytmpdir
-    sed -i '' 's/<target\/>//' $mytmpdir/*.xliff
 
-    for filename in $mytmpdir/*.xliff; do
-        echo \*\*\* xcodebuild -importLocalizations -project $1 -localizationPath $filename
-        xcodebuild -importLocalizations -project $1 -localizationPath $filename
-    done
+    import_in_place $1 $mytmpdir
 
     rm -fr $mytmpdir
 }
 
-# Imports translations in place, that is, it may change original files.
+# Imports translations directly from a given directory, maybe changing the originals.
 # Param 1: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
-# Param 2: The source directory ($translationdir or $translationdir/MessageModel)
+# Param 2: The source directory
 function import_in_place() {
-    echo \*\*\* import $1 $2
     for filename in $2/*.xliff; do
         sed -i '' 's/<target\/>//' $filename
         echo \*\*\* xcodebuild -importLocalizations -project $1 -localizationPath $filename
