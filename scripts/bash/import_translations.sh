4768a406836d3d95c0af5cc76c97593b1cbcd627
Dirk Zimmermann <dz@pep.security>
2020-01-15 15:17:19 +0100

    IOS-2038 Some logging, correct path to MM

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -7,12 +7,14 @@ translationdir=../pEp-Translate/
 # Param 1: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
 # Param 2: The source directory ($translationdir or $translationdir/MessageModel)
 function import() {
+    echo \*\*\* import $1 $2
     mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
     cp $2/*.xliff $mytmpdir
     sed -i '' 's/<target\/>//' $mytmpdir/*.xliff
 
     for filename in $mytmpdir/*.xliff; do
+        echo \*\*\* xcodebuild -importLocalizations -project $1 -localizationPath $filename
         xcodebuild -importLocalizations -project $1 -localizationPath $filename
     done
 
@@ -23,16 +25,17 @@ function import() {
 # Param 1: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
 # Param 2: The source directory ($translationdir or $translationdir/MessageModel)
 function import_in_place() {
+    echo \*\*\* import $1 $2
     for filename in $2/*.xliff; do
         sed -i '' 's/<target\/>//' $filename
+        echo \*\*\* xcodebuild -importLocalizations -project $1 -localizationPath $filename
         xcodebuild -importLocalizations -project $1 -localizationPath $filename
     done
 }
 
-import_in_place pEpForiOS.xcodeproj $translationdir
-import_in_place MessageModel/MessageModel.xcodeproj $translationdir/MessageModel
+import pEpForiOS.xcodeproj $translationdir
+import ../MessageModel/MessageModel/MessageModel.xcodeproj $translationdir/MessageModel
 
 echo
-echo ! Beware of changes in $translationdir. Verify !
-echo ! Verify the changes in pEpForiOS and MessageModel and commit !
+echo \*\*\* Verify the changes in pEpForiOS and MessageModel and commit \*\*\*
 echo
