09ebe496b53fcc174fd9262472eb3a6a934bf723
Dirk Zimmermann <dz@pep.security>
2020-01-15 15:17:19 +0100

    IOS-2038 In-place import (sed on original files)

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -19,9 +19,20 @@ function import() {
     rm -fr $mytmpdir
 }
 
-import pEpForiOS.xcodeproj $translationdir
-import MessageModel/MessageModel.xcodeproj $translationdir/MessageModel
+# Imports translations in place, that is, it may change original files.
+# Param 1: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
+# Param 2: The source directory ($translationdir or $translationdir/MessageModel)
+function import_in_place() {
+    for filename in $2/*.xliff; do
+        sed -i '' 's/<target\/>//' $filename
+        xcodebuild -importLocalizations -verbose -project $1 -localizationPath $filename
+    done
+}
+
+import_in_place pEpForiOS.xcodeproj $translationdir
+import_in_place MessageModel/MessageModel.xcodeproj $translationdir/MessageModel
 
 echo
+echo ! Beware of changes in $translationdir. Veryfy !
 echo ! Verify the changes in pEpForiOS and MessageModel and commit !
 echo
