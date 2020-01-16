60e1803bf038adc941d3a8b76df59e6fe0694335
Xavier Algarra <xavier@pep-project.org>
2020-01-16 12:12:20 +0100

    IOS-2041 sections test + rows in section 1 test

--- pEpForiOSTests/Models/Settings/ActionCellViewModelTest.swift
+++ pEpForiOSTests/Models/Settings/ActionCellViewModelTest.swift
@@ -1,65 +1,65 @@
+////
+////  ActionCellViewModelTest.swift
+////  pEpForiOSTests
+////
+////  Created by Alejandro Gelos on 18/06/2019.
+////  Copyright © 2019 p≡p Security S.A. All rights reserved.
+////
 //
-//  ActionCellViewModelTest.swift
-//  pEpForiOSTests
+//import XCTest
+//import PEPObjCAdapterFramework
 //
-//  Created by Alejandro Gelos on 18/06/2019.
-//  Copyright © 2019 p≡p Security S.A. All rights reserved.
+//@testable import pEpForiOS
 //
-
-import XCTest
-import PEPObjCAdapterFramework
-
-@testable import pEpForiOS
-
-class ActionCellViewModelTest: XCTestCase {
-    var actionCellViewModels: [SettingsActionCellViewModel]?
-
-    override func setUp() {
-        actionCellViewModels = [SettingsActionCellViewModel]()
-        actionCellViewModels?.append(SettingsActionCellViewModel(type: .keySyncSetting))
-    }
-
-    override func tearDown() {
-        actionCellViewModels = nil
-    }
-
-    func testTitleText() {
-        // GIVEN
-        guard let actionCellViewModels = actionCellViewModels else {
-            XCTFail()
-            return
-        }
-
-        // WHEN
-        for actionCellViewModel in actionCellViewModels {
-            switch actionCellViewModel.type {
-            // THEN
-            case .keySyncSetting:
-                XCTAssertEqual(actionCellViewModel.title,
-                               NSLocalizedString("Leave Device Group",
-                                                 comment: "Settings: Cell (button) title for leaving device group"))
-            case .resetAllIdentities:
-                XCTFail()
-            }
-        }
-    }
-
-    func testTitleColor() {
-        // GIVEN
-        guard let actionCellViewModels = actionCellViewModels else {
-            XCTFail()
-            return
-        }
-
-        // WHEN
-        for actionCellViewModel in actionCellViewModels {
-            switch actionCellViewModel.type {
-            // THEN
-            case .keySyncSetting:
-                XCTAssertEqual(actionCellViewModel.titleColor, UIColor.AppleRed)
-            case .resetAllIdentities:
-                XCTFail()
-            }
-        }
-    }
-}
+//class ActionCellViewModelTest: XCTestCase {
+//    var actionCellViewModels: [SettingsActionCellViewModel]?
+//
+//    override func setUp() {
+//        actionCellViewModels = [SettingsActionCellViewModel]()
+//        actionCellViewModels?.append(SettingsActionCellViewModel(type: .keySyncSetting))
+//    }
+//
+//    override func tearDown() {
+//        actionCellViewModels = nil
+//    }
+//
+//    func testTitleText() {
+//        // GIVEN
+//        guard let actionCellViewModels = actionCellViewModels else {
+//            XCTFail()
+//            return
+//        }
+//
+//        // WHEN
+//        for actionCellViewModel in actionCellViewModels {
+//            switch actionCellViewModel.type {
+//            // THEN
+//            case .keySyncSetting:
+//                XCTAssertEqual(actionCellViewModel.title,
+//                               NSLocalizedString("Leave Device Group",
+//                                                 comment: "Settings: Cell (button) title for leaving device group"))
+//            case .resetAllIdentities:
+//                XCTFail()
+//            }
+//        }
+//    }
+//
+//    func testTitleColor() {
+//        // GIVEN
+//        guard let actionCellViewModels = actionCellViewModels else {
+//            XCTFail()
+//            return
+//        }
+//
+//        // WHEN
+//        for actionCellViewModel in actionCellViewModels {
+//            switch actionCellViewModel.type {
+//            // THEN
+//            case .keySyncSetting:
+//                XCTAssertEqual(actionCellViewModel.titleColor, UIColor.AppleRed)
+//            case .resetAllIdentities:
+//                XCTFail()
+//            }
+//        }
+//    }
+//}
--- pEpForiOSTests/UI/Settings/SettingsViewModelV2Test.swift
+++ pEpForiOSTests/UI/Settings/SettingsViewModelV2Test.swift
@@ -7,43 +7,46 @@
 //
 
 import XCTest
+@testable import pEpForiOS
+@testable import MessageModel
 
 class SettingsViewModelV2Test: CoreDataDrivenTestBase {
 
+    var settingsVM : SettingsViewModelV2!
+
     override func setUp() {
         super.setUp()
+        setupViewModel()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-
-    func testExample() {
-
-    }
+    //Number of sections corresponding to SettingsViewModelV2.SectionType count
+    let sections = 5
 
     func testNumberOfSections() {
-        setupViewModel()
+        XCTAssertEqual(settingsVM.count, sections)
     }
 
-
-    func setupViewModel() {
-
+    func testNumberOfRowsInSection1() {
+        let numberOfAccounts = Account.all().count
+        XCTAssertEqual(settingsVM[0].rows?.count, numberOfAccounts)
     }
 }
 
 // MARK: - Private
 extension SettingsViewModelV2Test {
     private func setupViewModel() {
-        settingsVM = Sett
-    }
-
-    private func keySyncSection() -> SettingsSectionViewModel? {
-        for section in settingsVM.sections {
-            guard section.type == SettingsSectionViewModel.SectionType.keySync else { continue }
-            return section
+        if settingsVM == nil {
+            settingsVM = SettingsViewModelV2()
         }
-        return nil
+
     }
 }
+
+//    private func keySyncSection() -> SettingsSectionViewModel? {
+//        for section in settingsVM.sections {
+//            guard section.type == SettingsSectionViewModel.SectionType.keySync else { continue }
+//            return section
+//        }
+//        return nil
+//    }
