831066a535b9f9ab9a714149dd30c70d67fd2b74
Alejandro Gelos <agp@pep.security>
2020-01-28 17:06:29 +0100

    IOS-1672 Add some test to PEPLoggerTest

--- Submodules/PEPLogger/PEPLoggerTests/PEPLoggerTests.swift
+++ Submodules/PEPLogger/PEPLoggerTests/PEPLoggerTests.swift
@@ -9,10 +9,16 @@
 import XCTest
 @testable import PEPLogger
 
+/// For DEBUG only.
 final class LoggerTest: XCTestCase {
 
-    var actual: State?
-    var expected: State?
+    private var actual: State?
+    private var expected: State?
+
+    //Message to test log
+    private let messageToLog = "Test message to log"
+    //Error to test log
+    private let errorToLog = TestError.testError
 
     override func setUp() {
         super.setUp()
@@ -27,14 +33,50 @@ final class LoggerTest: XCTestCase {
         super.tearDown()
     }
 
-    /// Test shared as a Singleton
+    /// Singleton test
     func testShared() {
         XCTAssertNotNil(Logger.shared)
         XCTAssertTrue(Logger.shared === Logger.shared)
     }
 
+    /// Test initial value
+    func testModeDefaultValue() {
+        XCTAssertEqual(Logger.shared.mode, .normal)
+    }
+
+    /// Test changing mode
     func testMode() {
-        
+        // GIVEN
+        let initialValue = Logger.shared.mode
+
+        // WHEN
+        Logger.shared.mode = .verbose
+
+        //THEN
+        XCTAssertEqual(Logger.shared.mode, .verbose)
+        XCTAssertNotEqual(Logger.shared.mode, initialValue)
+    }
+
+    /// <#Description#>
+    func testdebug(){
+        // GIVEN
+        Logger.shared.mode = .normal
+        guard let errorDescription = errorToLog.errorDescription else {
+            XCTFail()
+            return
+        }
+
+        // WHEN
+        Logger.shared.debug(message: messageToLog)
+        Logger.shared.debug(error: errorToLog)
+
+        // THEN
+        let log = Logger.shared.log
+        XCTAssertTrue(log.contains(messageToLog))
+        XCTAssertTrue(log.contains(#function))
+        XCTAssertTrue(log.contains(#file))
+
+        XCTAssertTrue(log.contains(errorDescription))
     }
 
     func testPerformanceExample() {
@@ -49,15 +91,28 @@ final class LoggerTest: XCTestCase {
 
 extension LoggerTest {
     /// State of the logger.
-    struct State: Equatable {
+    private struct State: Equatable {
         var log: String = ""
         var mode: Logger.Mode = .normal
+    }
+
+    /// Error to test log
+    private enum TestError: Error, LocalizedError {
+        case testError
+
+        var errorDescription: String? {
+            switch self {
+            case .testError:
+                return "Test error description to log"
+            }
+        }
     }
 }
 
 // MARK: - Private
 
 extension LoggerTest {
-private func setDefaultActualState() {
-    actual = State()
+    private func setDefaultActualState() {
+        actual = State()
+    }
 }
