6d6bfc3addc83969a694d0624d3c6e9f983221a7
Dirk Zimmermann <dz@pep.security>
2020-01-29 17:09:18 +0100

    IOS-2002 Move ImapConnection.close to test extension

---
+++ pEpForiOSTests/TestUtils/ImapConnection+Close.swift
@@ -0,0 +1,19 @@
+//
+//  ImapConnection+Close.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 29.01.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+@testable import MessageModel
+
+extension ImapConnection {
+    func close() {
+        imapState.currentFolder = nil
+        imapStore.close()
+        imapStore.setDelegate(nil)
+    }
+}
