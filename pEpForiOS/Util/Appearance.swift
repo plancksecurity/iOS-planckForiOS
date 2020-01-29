3c239c135f36f479089037a62e78a5b148d73879
Martin Brude <devnull@localhost>
2020-01-29 10:06:13 +0100

    Remove imageset not used

--- pEpForiOS/Util/Appearance.swift
+++ pEpForiOS/Util/Appearance.swift
@@ -67,8 +67,8 @@ class Appearance {
         let appearance = UINavigationBarAppearance()
 
         appearance.configureWithOpaqueBackground()
-        
-        //TODO: set the proper font. --MB
+
+        //TODO: set the proper font for the navigation bar. --MB
         let titleTextAttributes: [NSAttributedString.Key : Any] = [.foregroundColor: color, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
         appearance.buttonAppearance.normal.titleTextAttributes = titleTextAttributes
         appearance.backButtonAppearance.normal.titleTextAttributes = titleTextAttributes
@@ -78,7 +78,7 @@ class Appearance {
 
         //TODO: change this asset: chevron-left-original. --MB
         //TODO: change next / prev icons in Main.storyboard, EmailDetailViewController
-        //TODO: change font size of the subject title on MessageSubjectCell on  Main.storyboard. --MB.
+        //TODO: change font size of the subject title on MessageSubjectCell on Main.storyboard. --MB.
         let chevronLeftImg = UIImage(named: "chevron-left-original")
         appearance.setBackIndicatorImage(chevronLeftImg, transitionMaskImage: chevronLeftImg)
 
