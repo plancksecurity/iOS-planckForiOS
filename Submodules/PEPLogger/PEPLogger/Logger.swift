2ca7cdded67eb2bd5de6f350cdf57c9035061952
Alejandro Gelos <agp@pep.security>
2020-01-31 10:39:58 +0100

    IOS-1672 approach using path and FileManager to create

--- Submodules/PEPLogger/PEPLogger/Logger.swift
+++ Submodules/PEPLogger/PEPLogger/Logger.swift
@@ -42,6 +42,18 @@ public class Logger: NSObject {
         return ""
     }
 
+    private let appGroup = "group.security.pep.pep4ios"
+    private let containerFolderName = "PEPLogger"
+    private let fileName = "Log.txt"
+
+    private var fileFolderURL: URL? {
+        let appGroupURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)
+        return appGroupURL?.appendingPathComponent(containerFolderName)
+    }
+    private var fileURL: URL? {
+        return fileFolderURL?.appendingPathComponent(fileName)
+    }
+
     /// Logs only when build for DEBUG.
     ///
     /// - Parameters:
@@ -54,6 +66,9 @@ public class Logger: NSObject {
     ///   - message: a message to log.
     public func debug(file: String = #file, line: Int = #line, function: String = #function, message: String) {
 
+
+
+        print("pEp[DEBUG] \(message) (\(file):\(line) - \(function))")
     }
 
     /// Logs only when build for DEBUG.
@@ -67,6 +82,7 @@ public class Logger: NSObject {
     ///   function where the method was called.
     ///   - error: the error to log.
     public func debug(file: String = #file, line: Int = #line, function: String = #function, error: Error) {
+        print("** INFO ** ")
     }
 
 
@@ -159,6 +175,7 @@ public class Logger: NSObject {
     ///   function where the method was called.
     ///   - message: a message to log.
     public func errorAndCrash(file: String = #file, line: Int = #line, function: String = #function, message: String) {
+        log(level: .errorAndCrash, file: file, line: String(line), function: function, message: message)
     }
 
     /// Always logs. And crash only if build for DEBUG.
@@ -175,6 +192,132 @@ public class Logger: NSObject {
     }
 }
 
-//private enum LogType {
-//    case log, debug, info, warn, error, errorAndCrash
-//}
+// MARK: - Private
+
+extension Logger {
+    private func log(level: Level, file: String, line: String, function: String, message: String) {
+        guard let filePath = fileURL?.absoluteString else {
+            debugCrash("Fail to get file URL absoluteString")
+            return
+        }
+
+        let data = dataToLog(level: level,
+                             file: file,
+                             line: line,
+                             function: function,
+                             message: message)
+
+        if !FileManager.default.fileExists(atPath: filePath) {
+            createFile(atPath: filePath, data: data)
+        } else {
+            guard let fileHandler = FileHandle(forWritingAtPath: filePath) else {
+                debugCrash("Fail to write to file URL absoluteString")
+                return
+            }
+            fileHandler.seekToEndOfFile()
+            fileHandler.write(data)
+            fileHandler.closeFile()
+        }
+    }
+
+    private func textToLog(level: Level,
+                           file: String,
+                           line: String,
+                           function: String,
+                           message: String) -> String {
+
+        return "\(level.rawValue) \(message) (\(file):\(line) - \(function))"
+    }
+
+    private func dataToLog(level: Level,
+                           file: String,
+                           line: String,
+                           function: String,
+                           message: String) -> Data {
+
+        let text = textToLog(level: level,
+                             file: file,
+                             line: line,
+                             function: function,
+                             message: message)
+        guard let data = text.data(using: .utf8) else {
+            debugCrash("Fail to conver String to Data")
+            return Data()
+        }
+        return data
+    }
+
+
+    /// Crash in DEGUB if fails
+    /// - Parameters:
+    ///   - atPath: file path
+    ///   - data: data to write
+    private func createFile(atPath: String, data: Data) {
+        guard let fileFolderPath = fileFolderURL?.absoluteString else {
+            debugCrash("Fail to get fileFolderPath")
+            return
+        }
+        do {
+            try FileManager.default.createDirectory(atPath: fileFolderPath,
+                                                    withIntermediateDirectories: true)
+        }
+        catch {
+            debugCrash(error.localizedDescription)
+            return
+        }
+        if FileManager.default.createFile(atPath: atPath, contents: data) {
+            return
+        } else {
+            debugCrash("Fail to create log file")
+            return
+        }
+    }
+
+
+    /// Crash in DEBUG only
+    /// - Parameter message: crash information
+    private func debugCrash(_ message: String) {
+        #if DEBUG
+        fatalError("pEpLogger: \(message)")
+        #endif
+    }
+
+
+    //            guard let fileURL = fileURL else {
+    //                #if DEBUG
+    //                fatalError("PEPLogger: Fail to get fileURL")
+    //                #endif
+    //                return
+    //            }
+    //            do {
+    //                try textToLog.write(to: fileURL, atomically: true, encoding: .utf8)
+    //            }
+    //            catch {
+    //                #if DEBUG
+    //                fatalError("PEPLogger: Fail to create log file")
+    //                #endif
+    //            }
+    //        }
+
+    //        "pEp[DEBUG] \(message) (\(file):\(line) - \(function))".write(to: <#T##URL#>, atomically: <#T##Bool#>, encoding: <#T##String.Encoding#>)
+
+
+    private func log(level: Level, error: Error) {
+
+    }
+
+
+
+}
+
+// MARK: - Helping Structures
+
+extension Logger {
+    enum Level: String {
+        case debug = "pEp[DEBUG]"
+        case info = "pEp[INFO]"
+        case warn = "pEp[WARN]"
+        case error = "pEp[ERROR]"
+        case errorAndCrash = "pEp[ERRORandCRASH]"
+    }
+}
