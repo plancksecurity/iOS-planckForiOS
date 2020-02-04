c3417e9b6ad6c50e63929715736d0dbaae2af032
Martin Brude <devnull@localhost>
2020-02-04 11:52:27 +0100

    IOS-2064 Add documentation

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/UIKit/UIFont+Extension.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/UIKit/UIFont+Extension.swift
@@ -9,13 +9,18 @@
 import Foundation
 import UIKit
 
+/// UIFont extension
+/// It has static vars that use text styles like body or callout.
+/// As it is based on the system fonts, it allows to take advantage of Dynamic Type.
+/// System fonts automatically react to accessibility features like bold text and larger type.
+/// To add a new font, include the font files, add it on Info.plist, and add access methods here.
 extension UIFont {
     private static let name = "SFUIText-Regular"
     
     public static var pepBody: UIFont {
         return UIFont(name: name, size: preferredFontSize(for: .body))!
     }
-
+    
     public static var pepCallout: UIFont {
         return UIFont(name: name, size: preferredFontSize(for: .callout))!
     }
