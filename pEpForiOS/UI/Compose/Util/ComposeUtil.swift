6270209783ed0535bd52b352afa4a3105589b35f
buff <andreas@pep-project.org>
2020-02-12 11:54:46 +0100

    IOS-2039 moves private stuff to private exténsion, marks public stuff explicitly

--- pEpForiOS/UI/Compose/Util/ComposeUtil.swift
+++ pEpForiOS/UI/Compose/Util/ComposeUtil.swift
@@ -13,135 +13,11 @@ import PEPObjCAdapterFramework
 /// Utils for composing a message. Helps finding out values depending on the original message
 /// (the correct recipients, cancle actions ...).
 struct ComposeUtil {
-    enum ComposeMode: CaseIterable {
+    public enum ComposeMode: CaseIterable {
         case normal
         case replyFrom
         case replyAll
         case forward
-    }
-
-    // MARK: - Recipients
-
-    static func initialTos(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
-        var result = [Identity]()
-        switch composeMode {
-        case .replyFrom:
-            if om.parent.folderType == .sent || om.parent.folderType == .drafts {
-                result = om.to.allObjects
-            } else if om.parent.folderType != .sent, let omFrom = om.from {
-                result = [omFrom]
-            }
-        case .replyAll:
-            if om.parent.folderType == .sent || om.parent.folderType == .drafts  {
-                result = om.to.allObjects
-            } else if om.parent.folderType != .sent, let omFrom = om.from {
-                guard let me = initialFrom(composeMode: composeMode, originalMessage: om) else {
-                    Log.shared.errorAndCrash("No from")
-                    return result
-                }
-                let origTos = om.to
-                let originalTosWithoutMe = origTos.filter { $0 != me}
-                result = originalTosWithoutMe + [omFrom]
-            }
-        case .normal:
-            if om.parent.folderType == .sent ||
-                om.parent.folderType == .drafts ||
-                om.parent.folderType == .outbox  {
-                result = om.to.allObjects
-            }
-        case .forward:
-            break
-        }
-        return result
-    }
-
-    static func initialCcs(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
-        var result = [Identity]()
-        switch composeMode {
-        case .replyAll:
-            if om.parent.folderType == .sent || om.parent.folderType == .drafts {
-                result = om.cc.allObjects
-            } else {
-                guard let me = initialFrom(composeMode: composeMode, originalMessage: om) else {
-                    Log.shared.errorAndCrash("No from")
-                    return result
-                }
-                let origCcs = om.cc
-                result = origCcs.filter { $0 != me}
-            }
-        case .replyFrom, .forward:
-            break
-        case .normal:
-            if om.parent.folderType == .sent ||
-                om.parent.folderType == .drafts ||
-                om.parent.folderType == .outbox  {
-                result = om.cc.allObjects
-            }
-        }
-        return result
-    }
-
-    static func initialBccs(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
-        var result = [Identity]()
-        switch composeMode {
-        case .normal:
-            if om.parent.folderType == .sent ||
-                om.parent.folderType == .drafts ||
-                om.parent.folderType == .outbox {
-                result = om.bcc.allObjects
-            }
-        case .replyFrom, .forward, .replyAll:
-            break
-        }
-        return result
-    }
-
-    static func initialFrom(composeMode: ComposeMode, originalMessage om: Message?) -> Identity? {
-        switch composeMode {
-        case .replyFrom, .replyAll, .forward:
-            return om?.parent.account.user
-        case .normal:
-            if let om = om,
-                om.parent.folderType == .sent ||
-                    om.parent.folderType == .drafts ||
-                    om.parent.folderType == .outbox  {
-                return om.from
-            }
-            return Account.defaultAccount()?.user
-        }
-    }
-
-    // MARK: - Attachments
-
-    /// - Returns: attachments appropriate for the given compose mode
-    static func initialAttachments(composeMode: ComposeMode,
-                                   contentDisposition: Attachment.ContentDispositionType,
-                                   originalMessage om: Message?) -> [Attachment] {
-        guard shouldTakeOverAttachments(composeMode: composeMode, originalMessage: om ) else {
-            return []
-        }
-        guard let om = om else {
-            // No om, no initial attachments
-            return []
-        }
-        let viewAbleAttachments = om.viewableAttachments()
-            .filter { $0.contentDisposition == contentDisposition }
-
-        let privateSession = Session()
-        let result = viewAbleAttachments.map { $0.clone(for: privateSession) }
-        return result
-    }
-
-    /// Computes whether or not attachments must be taken over in current compose mode
-    ///
-    /// - Returns: true if we must take over attachments from the original message, false otherwize
-    static private func shouldTakeOverAttachments(composeMode: ComposeMode,
-                                                  originalMessage om: Message?) -> Bool {
-        var isInDraftsOrOutbox = false
-        if let om = om {
-            isInDraftsOrOutbox = om.isInDraftsOrOutbox
-        }
-        return composeMode == .forward || isInDraftsOrOutbox
     }
 
     /// Creates a message from the given ComposeView State
@@ -182,9 +58,140 @@ struct ComposeUtil {
             message.pEpRatingInt = Int(state.rating.rawValue)
         }
 
+        //BUFF: add references?
+
         message.imapFlags.seen = imapSeenState(forMessageToSend: message)
 
         return message
+    }
+
+    // MARK: - Recipients
+
+    static public func initialTos(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
+        var result = [Identity]()
+        switch composeMode {
+        case .replyFrom:
+            if om.parent.folderType == .sent || om.parent.folderType == .drafts {
+                result = om.to.allObjects
+            } else if om.parent.folderType != .sent, let omFrom = om.from {
+                result = [omFrom]
+            }
+        case .replyAll:
+            if om.parent.folderType == .sent || om.parent.folderType == .drafts  {
+                result = om.to.allObjects
+            } else if om.parent.folderType != .sent, let omFrom = om.from {
+                guard let me = initialFrom(composeMode: composeMode, originalMessage: om) else {
+                    Log.shared.errorAndCrash("No from")
+                    return result
+                }
+                let origTos = om.to
+                let originalTosWithoutMe = origTos.filter { $0 != me}
+                result = originalTosWithoutMe + [omFrom]
+            }
+        case .normal:
+            if om.parent.folderType == .sent ||
+                om.parent.folderType == .drafts ||
+                om.parent.folderType == .outbox  {
+                result = om.to.allObjects
+            }
+        case .forward:
+            break
+        }
+        return result
+    }
+
+    static public func initialCcs(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
+        var result = [Identity]()
+        switch composeMode {
+        case .replyAll:
+            if om.parent.folderType == .sent || om.parent.folderType == .drafts {
+                result = om.cc.allObjects
+            } else {
+                guard let me = initialFrom(composeMode: composeMode, originalMessage: om) else {
+                    Log.shared.errorAndCrash("No from")
+                    return result
+                }
+                let origCcs = om.cc
+                result = origCcs.filter { $0 != me}
+            }
+        case .replyFrom, .forward:
+            break
+        case .normal:
+            if om.parent.folderType == .sent ||
+                om.parent.folderType == .drafts ||
+                om.parent.folderType == .outbox  {
+                result = om.cc.allObjects
+            }
+        }
+        return result
+    }
+
+    static public func initialBccs(composeMode: ComposeMode, originalMessage om: Message) -> [Identity] {
+        var result = [Identity]()
+        switch composeMode {
+        case .normal:
+            if om.parent.folderType == .sent ||
+                om.parent.folderType == .drafts ||
+                om.parent.folderType == .outbox {
+                result = om.bcc.allObjects
+            }
+        case .replyFrom, .forward, .replyAll:
+            break
+        }
+        return result
+    }
+
+    static public func initialFrom(composeMode: ComposeMode, originalMessage om: Message?) -> Identity? {
+        switch composeMode {
+        case .replyFrom, .replyAll, .forward:
+            return om?.parent.account.user
+        case .normal:
+            if let om = om,
+                om.parent.folderType == .sent ||
+                    om.parent.folderType == .drafts ||
+                    om.parent.folderType == .outbox  {
+                return om.from
+            }
+            return Account.defaultAccount()?.user
+        }
+    }
+
+    // MARK: - Attachments
+
+    /// - Returns: attachments appropriate for the given compose mode
+    static public func initialAttachments(composeMode: ComposeMode,
+                                   contentDisposition: Attachment.ContentDispositionType,
+                                   originalMessage om: Message?) -> [Attachment] {
+        guard shouldTakeOverAttachments(composeMode: composeMode, originalMessage: om ) else {
+            return []
+        }
+        guard let om = om else {
+            // No om, no initial attachments
+            return []
+        }
+        let viewAbleAttachments = om.viewableAttachments()
+            .filter { $0.contentDisposition == contentDisposition }
+
+        let privateSession = Session()
+        let result = viewAbleAttachments.map { $0.clone(for: privateSession) }
+        return result
+    }
+}
+
+// MARK: - Private
+
+extension ComposeUtil {
+
+    /// Computes whether or not attachments must be taken over in current compose mode
+    ///
+    /// - Returns: true if we must take over attachments from the original message, false otherwize
+    static private func shouldTakeOverAttachments(composeMode: ComposeMode,
+                                                  originalMessage om: Message?) -> Bool {
+        var isInDraftsOrOutbox = false
+        if let om = om {
+            isInDraftsOrOutbox = om.isInDraftsOrOutbox
+        }
+        return composeMode == .forward || isInDraftsOrOutbox
     }
 
     static private func imapSeenState(forMessageToSend msg: Message) -> Bool {
