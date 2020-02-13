0ab3a8d53848258e0801f8b60c8ee8ec3582307d
Xavier Algarra <xavier@pep-project.org>
2020-02-13 12:42:02 +0100

    IOS-2008 splitview fix

--- pEpForiOS/UI/SplitView/PrimarySplitViewController.swift
+++ pEpForiOS/UI/SplitView/PrimarySplitViewController.swift
@@ -9,46 +9,69 @@
 import UIKit
 import pEpIOSToolbox
 
-//!!!: The concept is very dirty. PrimarySplitViewController should not be aware of EmailListViewController.
-// According to //XAVIER, the implementation will change to be generic (emilaiVC independent) with the new SplitViewController concept which is WIP.
-class PrimarySplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+//rename to pepSplitViewController
+
+class PEPSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.delegate = self
         preferredDisplayMode = .allVisible
     }
-
+    
     func splitViewController(_ splitViewController: UISplitViewController,
                              collapseSecondary secondaryViewController:UIViewController,
                              onto primaryViewController:UIViewController) -> Bool {
-        guard
-            let navigationController = secondaryViewController as? UINavigationController,
-            navigationController.rootViewController is EmailViewController
-            else {
-                return true
+        if let navigationController = secondaryViewController as? UINavigationController,
+            let top = navigationController.topViewController, top.collapsedBehavior == .needed,
+            let primaryNavigationController = primaryViewController as? UINavigationController {
+            
+            navigationController.popViewController(animated: false)
+            primaryNavigationController.pushViewController(top, animated: false)
         }
-
-        return false
+        return true
     }
-
+    
     func splitViewController(_ splitViewController: UISplitViewController,
                              separateSecondaryFrom primaryViewController: UIViewController)
         -> UIViewController? {
-            guard
-                let navigationController =
-                splitViewController.viewControllers.first as? UINavigationController,
-                let secondaryNavigationController =
-                navigationController.topViewController as? UINavigationController,
-                secondaryNavigationController.topViewController is EmailViewController
-                else {
-                    let storyboard = UIStoryboard(
-                        name: UIStoryboard.noSelectionStoryBoard,
-                        bundle: nil)
-                    let vc = storyboard.instantiateViewController(
-                        withIdentifier: UIStoryboard.nothingSelectedViewController)
-                    return vc
+            let navigationController = UINavigationController()
+            guard let primaryView = primaryViewController as? UINavigationController else {
+                Log.shared.errorAndCrash(message: "root view is not nav Controller?")
+                return nil
             }
-            return secondaryNavigationController
+            if let topView = primaryView.topViewController, topView.isKind(of: EmailViewController.self) {
+                primaryView.popViewController(animated: false)
+                navigationController.pushViewController(topView, animated: false)
+            } else {
+                let storyboard = UIStoryboard(
+                    name: UIStoryboard.noSelectionStoryBoard,
+                    bundle: nil)
+                let vc = storyboard.instantiateViewController(
+                    withIdentifier: UIStoryboard.nothingSelectedViewController)
+                navigationController.pushViewController(vc, animated: false)
+            }
+            return navigationController
+    }
+    
+    func splitViewController(_ svc: UISplitViewController, willChangeTo displayMode: UISplitViewController.DisplayMode) {
+        guard let nav = viewControllers.last as? UINavigationController,
+            let emailViewController = nav.rootViewController as? EmailViewController else {
+                return
+        }
+        //emailViewController.splitViewController(willChangeTo: displayMode)
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, showDetail vc: UIViewController, sender: Any?) -> Bool {
+        if !onlySplitViewMasterIsShown {
+            //apple docs say detailView will be always in the position 1 of the .viewcontrollers array
+            let detail = 1
+            guard splitViewController.viewControllers.count == 2, let controller = splitViewController.viewControllers[detail] as? UINavigationController else {
+                return false
+            }
+            controller.viewControllers = [vc]
+            return true
+        }
+        return false
     }
 }
