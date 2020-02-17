b2dfffc57afbd806c8aba5c5351aeff6fecd6be5
Adam Kowalski <adam@pep-project.org>
2020-02-17 10:09:44 +0100

    IOS-2067 Naming

--- pEpForiOS/UI/KeySync/KeySyncHandshake/KeySyncHandshakeViewController.swift
+++ pEpForiOS/UI/KeySync/KeySyncHandshake/KeySyncHandshakeViewController.swift
@@ -15,17 +15,17 @@ final class KeySyncHandshakeViewController: UIViewController {
 
     static let storyboardId = "KeySyncHandshakeViewController"
 
-    @IBOutlet weak var keySyncWordsContainer: UIView! {
+    @IBOutlet weak var trustwordsView: UIView! {
         didSet {
-            keySyncWordsContainer.backgroundColor = .white
-            keySyncWordsContainer.layer.borderColor = UIColor.pEpGreyLines.cgColor
-            keySyncWordsContainer.layer.cornerRadius = 3
-            keySyncWordsContainer.layer.borderWidth = 1
+            trustwordsView.backgroundColor = .white
+            trustwordsView.layer.borderColor = UIColor.pEpGreyLines.cgColor
+            trustwordsView.layer.cornerRadius = 3
+            trustwordsView.layer.borderWidth = 1
         }
     }
-    @IBOutlet weak var keySyncWords: UILabel! {
+    @IBOutlet weak var trustwordsLabel: UILabel! {
         didSet {
-            keySyncWords.backgroundColor = .white
+            trustwordsLabel.backgroundColor = .white
         }
     }
     @IBOutlet weak var contentView: KeyInputView! {
@@ -39,7 +39,8 @@ final class KeySyncHandshakeViewController: UIViewController {
     }
     @IBOutlet weak var alertTitle: UILabel! {
         didSet {
-            let alertTittle = NSLocalizedString("p≡p Sync", comment: "keySync handshake alert title")
+            let alertTittle = NSLocalizedString("p≡p Sync",
+                                                comment: "keySync handshake alert title")
             alertTitle.attributedText = alertTittle.paintPEPToPEPColour()
         }
     }
@@ -52,7 +53,8 @@ final class KeySyncHandshakeViewController: UIViewController {
         didSet {
             setFont(button: accept)
             accept.setTitleColor(.pEpGreen, for: .normal)
-            accept.setTitle(NSLocalizedString("Confirm", comment: "accept hand shake confirm button"), for: .normal)
+            accept.setTitle(NSLocalizedString("Confirm",
+                                              comment: "accept hand shake confirm button"), for: .normal)
             accept.backgroundColor = .pEpGreyBackground
         }
     }
@@ -60,7 +62,8 @@ final class KeySyncHandshakeViewController: UIViewController {
         didSet {
             setFont(button: decline)
             decline.setTitleColor(.pEpRed, for: .normal)
-            decline.setTitle(NSLocalizedString("Reject", comment: "reject hand shake button"), for: .normal)
+            decline.setTitle(NSLocalizedString("Reject",
+                                               comment: "reject hand shake button"), for: .normal)
             decline.backgroundColor = .pEpGreyBackground
         }
     }
@@ -68,7 +71,8 @@ final class KeySyncHandshakeViewController: UIViewController {
         didSet {
             setFont(button: cancel)
             cancel.setTitleColor(.pEpGreyText, for: .normal)
-            cancel.setTitle(NSLocalizedString("Not Now", comment: "not now button"), for: .normal)
+            cancel.setTitle(NSLocalizedString("Not Now",
+                                              comment: "not now button"), for: .normal)
             cancel.backgroundColor = .pEpGreyBackground
         }
     }
@@ -106,7 +110,8 @@ final class KeySyncHandshakeViewController: UIViewController {
     }
 
     private func setFont(button: UIButton) {
-        button.titleLabel?.font = UIFont.pepFont(style: .body, weight: .regular)
+        button.titleLabel?.font = UIFont.pepFont(style: .body,
+                                                 weight: .regular)
     }
 
     @IBAction func didPress(_ sender: UIButton) {
@@ -150,7 +155,7 @@ extension KeySyncHandshakeViewController: KeySyncHandshakeViewModelDelegate {
 
     func change(handshakeWordsTo: String) {
         DispatchQueue.main.async { [weak self] in
-            self?.keySyncWords.text = handshakeWordsTo
+            self?.trustwordsLabel.text = handshakeWordsTo
         }
     }
 }
