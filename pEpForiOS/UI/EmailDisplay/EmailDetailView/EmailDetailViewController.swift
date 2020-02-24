1fdf4b05fd7fc45e46c7dba4706176b6e8d63a93
Martin Brude <devnull@localhost>
2020-02-24 16:13:25 +0100

    IOS-2064 remove whitespaces

--- pEpForiOS/UI/EmailDisplay/EmailDetailView/EmailDetailViewController.swift
+++ pEpForiOS/UI/EmailDisplay/EmailDetailView/EmailDetailViewController.swift
@@ -763,10 +763,7 @@ extension EmailDetailViewController {
         let size = CGSize(width: 15, height: 25)
         nextButton?.image = nextButton?.image?.resizeImage(targetSize: size)
         previousButton?.image = previousButton?.image?.resizeImage(targetSize: size)
-        
         let isIpad = UIDevice.current.userInterfaceIdiom == .pad
-
-
         if !onlySplitViewMasterIsShown && isIpad {
             let nextPrevButtonSize = CGRect(x: 0, y: 0, width: 27, height: 15)
 
