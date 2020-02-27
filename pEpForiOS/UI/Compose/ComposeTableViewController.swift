63d11e779d0b81fedf9f1c15da6f35f8c44e2ac9
Martin Brude <devnull@localhost>
2020-02-27 14:59:36 +0100

    IOS-1823 - add whitespace to string

--- pEpForiOS/UI/Compose/ComposeTableViewController.swift
+++ pEpForiOS/UI/Compose/ComposeTableViewController.swift
@@ -324,7 +324,7 @@ extension ComposeTableViewController: SegueHandlerType {
             }
 
             destination.appConfig = appConfig
-            destination.backButtonTitle = NSLocalizedString("Cancel",
+            destination.backButtonTitle = NSLocalizedString(" Cancel",
                                                             comment: "Cancel Back Button Title")
             guard let trustManagementViewModel = vm.trustManagementViewModel() else {
                 Log.shared.error("Message not found")
