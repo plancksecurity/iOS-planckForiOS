d5928d1115a086fafc785e07bb080a0435726839
Martin Brude <devnull@localhost>
2020-03-11 17:16:59 +0100

    IOS-2062 undo debug line

--- pEpForiOS/UI/EmailDisplay/EmailDisplayList/EmailListViewModel.swift
+++ pEpForiOS/UI/EmailDisplay/EmailDisplayList/EmailListViewModel.swift
@@ -111,8 +111,7 @@ class EmailListViewModel: EmailDisplayViewModel {
 
     /// Whether or not to show the Tutorial
     public var shouldShowTutorialWizard: Bool {
-        return true
-//        return AppSettings.shared.shouldShowTutorialWizard
+        return AppSettings.shared.shouldShowTutorialWizard
     }
 
     /// Call when the tutorial has been displayed to the user
