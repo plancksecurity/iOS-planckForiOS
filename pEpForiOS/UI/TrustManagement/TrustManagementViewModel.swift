6565100469690b9c37e2a5c8dfe3528c63752a92
buff <andreas@pep-project.org>
2020-03-12 16:02:42 +0100

    refactors TrustManagement scene. Also betters stocking scrolling. The rest is caused by getting pEpColor. ToDo.

--- pEpForiOS/UI/TrustManagement/TrustManagementViewModel.swift
+++ pEpForiOS/UI/TrustManagement/TrustManagementViewModel.swift
@@ -14,6 +14,9 @@ import PEPObjCAdapterFramework
 protocol TrustManagementViewModelDelegate: class {
     /// Delegate method to notify that an action ends and the view must be reloaded.
     func reload()
+
+    /// Called when data changed.
+    func dataChanged(forRowAt indexPath: IndexPath)
 }
 
 protocol TrustmanagementProtectionStateChangeDelegate: class {
@@ -23,22 +26,34 @@ protocol TrustmanagementProtectionStateChangeDelegate: class {
 
 extension TrustManagementViewModel {
     /// The item that represents the handshake partner
-    public struct Row {
+    public class Row {
+        private let trustManagementUtil: TrustManagementUtilProtocol
+        public typealias LanguageCode = String
+
+        public init(language: LanguageCode,
+                    handshakeCombination: TrustManagementUtil.HandshakeCombination,
+                    trustManagementUtil: TrustManagementUtilProtocol? = nil) {
+            self.language = language
+            self.handshakeCombination = handshakeCombination
+            self.trustManagementUtil = trustManagementUtil ?? TrustManagementUtil()
+            setupTrustwords(combination: handshakeCombination, language: language)
+        }
+
         /// Indicates the handshake partner's name
-        var name: String {
+        public var name: String {
             let name = handshakeCombination.partnerIdentity.userName
             let address = handshakeCombination.partnerIdentity.address
             return name ?? address
         }
         /// The description for the row
-        var description: String {
+        public var description: String {
             if forceRed {
                 return PEPColor.red.privacyStatusDescription
             }
             return color.privacyStatusDescription
         }
         /// The privacy status name
-        var privacyStatusName: String {
+        public var privacyStatusName: String {
             if (forceRed) {
                 return String.trustIdentityTranslation(pEpRating: .underAttack).title
             }
@@ -47,16 +62,20 @@ extension TrustManagementViewModel {
             return translations.title
         }
         /// The privacy status image
-        var privacyStatusImage: UIImage? {
+        public var privacyStatusImage: UIImage? {
             if forceRed {
                 return PEPColor.red.statusIconForMessage(enabled: true, withText: false)
             }
             return color.statusIconForMessage(enabled: true, withText: false)
         }
         /// The current language
-        var currentLanguage: String
-        /// Indicates if the trustwords are long
-        var longTrustwords: Bool = false
+        fileprivate var language: String {
+            didSet {
+                setupTrustwords(combination: handshakeCombination, language: language)
+            }
+        }
+        /// Indicates if the long (or the short) version of the trustwords should be shown
+        var showLongTrustwordVersion: Bool = false
         /// The privacy status in between the current user and the partner
         var privacyStatus: String?
         /// Status indicator
@@ -66,13 +85,49 @@ extension TrustManagementViewModel {
             }
             return handshakeCombination.partnerIdentity.pEpColor()
         }
-        var trustwords : String?
+
+        public typealias TrustWords = String
+
+        public var trustwords: TrustWords? {
+            return showLongTrustwordVersion ? trustwordsLong : trustwordsShort
+        }
+
+        private var trustwordsShort: String?
+        private var trustwordsLong: String?
         //Prevents the overkill of require the trustwords when it's not necesary.
-        fileprivate var shouldUpdateTrustwords : Bool = true
         fileprivate var forceRed: Bool = false
         /// The identity of the user to do the handshake
         fileprivate var handshakeCombination: TrustManagementUtil.HandshakeCombination
         fileprivate var fingerprint: String?
+
+        private func setupTrustwords(combination: TrustManagementUtil.HandshakeCombination,
+                                     language: LanguageCode) {
+            let longTw =  try? trustManagementUtil.getTrustwords(for: combination.ownIdentity,
+                                                                 and: combination.partnerIdentity,
+                                                                 language: language,
+                                                                 long: true)
+            trustwordsLong = prepareTrustwordStringForDisplay(trustwords: longTw)
+            var shortTw =  try? trustManagementUtil.getTrustwords(for: combination.ownIdentity,
+                                                                  and: combination.partnerIdentity,
+                                                                  language: language,
+                                                                  long: false)
+            if let tmp = shortTw {
+                // Short TWs must have three dots for signaling as truncated
+                shortTw = "\(tmp)…"
+            }
+            trustwordsShort = prepareTrustwordStringForDisplay(trustwords: shortTw)
+        }
+
+        private func prepareTrustwordStringForDisplay(trustwords: TrustWords?) -> TrustWords? {
+            guard let trustwords = trustwords else {
+                // This is a valid case. Eg. contacts with no color have no trustwords.
+                return nil
+            }
+            let oneSpace = " "
+            let threeSpaces = "   "
+            return trustwords.replacingOccurrences(of: oneSpace, with: threeSpaces)
+        }
+
     }
 }
 
@@ -87,7 +142,7 @@ final class TrustManagementViewModel {
     }
     var shouldShowOptionsButton: Bool = false
     private var message: Message
-    private var trustManagementUtil : TrustManagementUtilProtocol?
+    private var trustManagementUtil : TrustManagementUtilProtocol
     private let undoManager = UndoManager()
     private var actionPerformed = [String]()
     
@@ -102,14 +157,14 @@ final class TrustManagementViewModel {
                 pEpProtectionModifyable: Bool,
                 delegate : TrustManagementViewModelDelegate? = nil,
                 protectionStateChangeDelegate: TrustmanagementProtectionStateChangeDelegate? = nil,
-                trustManagementUtil: TrustManagementUtilProtocol? = TrustManagementUtil()) {
+                trustManagementUtil: TrustManagementUtilProtocol? = nil) {
         self.message = message
-        self.trustManagementUtil = trustManagementUtil
+        self.trustManagementUtil = trustManagementUtil ?? TrustManagementUtil()
         self.pEpProtected = message.pEpProtected
         self.shouldShowOptionsButton = pEpProtectionModifyable
         self.delegate = delegate
         self.protectionStateChangeDelegate = protectionStateChangeDelegate
-        generateRows()
+        setupRows()
     }
 
     ///MARK - Actions
@@ -117,38 +172,30 @@ final class TrustManagementViewModel {
     /// Reject the handshake
     /// - Parameter indexPath: The indexPath of the item to get the user to reject the handshake
     public func handleRejectHandshakePressed(at indexPath: IndexPath) {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModel is nil")
-            return
-        }
         let actionName = NSLocalizedString("Trust Rejection", comment: "Action name to be suggested at the moment of revert")
         actionPerformed.append(actionName)
         registerUndoAction(at: indexPath)
         let row = rows[indexPath.row]
         let identity : Identity = row.handshakeCombination.partnerIdentity.safeForSession(Session.main)
-        rows[indexPath.row].fingerprint = trustManagementViewModel.getFingerprint(for: identity)
+        rows[indexPath.row].fingerprint = trustManagementUtil.getFingerprint(for: identity)
         rows[indexPath.row].forceRed = true
-        trustManagementViewModel.denyTrust(for: identity)
+        trustManagementUtil.denyTrust(for: identity)
         reevaluateMessage()
-        delegate?.reload()
+        delegate?.dataChanged(forRowAt: indexPath)
     }
     
     /// Confirm the handshake
     /// - Parameter indexPath: The indexPath of the item to get the user to confirm the handshake
     public func handleConfirmHandshakePressed(at indexPath: IndexPath) {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModel is nil")
-            return
-        }
         let actionName = NSLocalizedString("Trust Confirmation", comment: "Action name to be suggested at the moment of revert")
         actionPerformed.append(actionName)
         registerUndoAction(at: indexPath)
         let row = rows[indexPath.row]
         rows[indexPath.row].forceRed = false
         let identity : Identity = row.handshakeCombination.partnerIdentity.safeForSession(Session.main)
-        trustManagementViewModel.confirmTrust(for: identity)
+        trustManagementUtil.confirmTrust(for: identity)
         reevaluateMessage()
-        delegate?.reload()
+        delegate?.dataChanged(forRowAt: indexPath)
     }
     
     /// Handles the undo action.
@@ -156,62 +203,41 @@ final class TrustManagementViewModel {
     /// So it is not important what action in concrete was performed.
     /// - Parameter indexPath: The index path of the row from where the last action has been performed.
     @objc public func handleUndo(forRowAt indexPath: IndexPath) {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModel is nil")
-            return
-        }
         let row = rows[indexPath.row]
-        rows[indexPath.row].shouldUpdateTrustwords = true
         rows[indexPath.row].forceRed = false
-        trustManagementViewModel.undoMisstrustOrTrust(for: row.handshakeCombination.partnerIdentity,
-                                                      fingerprint: row.fingerprint)
+        trustManagementUtil.undoMisstrustOrTrust(for: row.handshakeCombination.partnerIdentity,
+                                                 fingerprint: row.fingerprint)
         reevaluateMessage()
+        delegate?.dataChanged(forRowAt: indexPath)
     }
     
     /// Handles the redey action
     /// - Parameter indexPath: The indexPath of the item to get the user to undo last action.
     public func handleResetPressed(forRowAt indexPath: IndexPath) {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModel is nil")
-            return
-        }
         let row = rows[indexPath.row]
         rows[indexPath.row].forceRed = false
-        trustManagementViewModel.resetTrust(for: row.handshakeCombination.partnerIdentity)
+        trustManagementUtil.resetTrust(for: row.handshakeCombination.partnerIdentity)
         reevaluateMessage()
-        delegate?.reload()
+        delegate?.dataChanged(forRowAt: indexPath)
     }
 
     /// - returns: the available languages.
-    public func handleChangeLanguagePressed(forRowAt indexPath : IndexPath) -> [String] {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModelDelegate is nil")
-            return [String]()
-        }
-        rows[indexPath.row].shouldUpdateTrustwords = true
-        guard let list = trustManagementViewModel.languagesList() else {
-            Log.shared.error("The list of languages could be retrieved.")
-            return [String]()
-        }
-        return list
+    public var languages: [String] {
+        return trustManagementUtil.languagesList() ?? []
     }
     
     /// Updates the selected language for that row.
     /// - Parameters:
     ///   - indexPath: The index path of the row
     ///   - language: The chosen language
-    public func didSelectLanguage(forRowAt indexPath: IndexPath, language: String) {
-        guard let trustManagementViewModelDelegate = delegate else {
-            Log.shared.errorAndCrash("TrustManagementViewModelDelegate is nil")
-            return
-        }
-        rows[indexPath.row].currentLanguage = language
-        trustManagementViewModelDelegate.reload()
+    public func handleDidSelect(language: String, forRowAt indexPath: IndexPath) {
+        rows[indexPath.row].language = language
+        delegate?.dataChanged(forRowAt: indexPath)
     }
     
     /// Toogle pEp protection status
     public func handleToggleProtectionPressed() {
-        pEpProtected = !pEpProtected
+        pEpProtected.toggle()
     }
 
     /// Informs if is it possible to undo an action.
@@ -228,70 +254,13 @@ final class TrustManagementViewModel {
     /// Method that makes the trustwords long or short (more or less trustwords in fact).
     /// - Parameter indexPath: The indexPath to get the row to toogle the status (long/short)
     public func handleToggleLongTrustwords(forRowAt indexPath: IndexPath) {
-        guard let trustManagementViewModelDelegate = delegate else {
-            Log.shared.errorAndCrash("TrustManagementViewModelDelegate is nil")
-            return
-        }
-        rows[indexPath.row].shouldUpdateTrustwords = true
-        rows[indexPath.row].longTrustwords.toggle()
-        trustManagementViewModelDelegate.reload()
-    }
-
-    public typealias TrustWords = String
-
-    /// Generate the trustwords
-    /// - Parameters:
-    ///   - indexPath: The indexPath of the row to toogle the status (long/short)
-    ///   - long: Indicates if the trustwords MUST be long (more words)
-    ///   - completion: the completion block to be executed once the trustwords are generated.
-    /// If the trustwords passed are nil the UI must no be updated.
-    /// If the completion block is nil, only the model in the row at the provided indexPath will be updated.
-    public func generateTrustwords(forRowAt indexPath: IndexPath,
-                                   long : Bool = false,
-                                   completion: ((TrustWords?) -> Void)?)  {
-        guard rows[indexPath.row].shouldUpdateTrustwords else {
-            if let trustwords = rows[indexPath.row].trustwords {
-                completion?(trustwords)
-                return
-            }
-            completion?(nil)
-            return
-        }
-        rows[indexPath.row].shouldUpdateTrustwords = false
-        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
-            guard let me = self,
-                let trustManagementViewModel = me.trustManagementUtil else {
-                completion?(nil)
-                Log.shared.errorAndCrash("Lost myself or the trustManagement ViewModel")
-                return
-            }
-            let complete: (TrustWords?) -> Void = { trustwords in
-                DispatchQueue.main.async {
-                    completion?(trustwords)
-                }
-            }
-            let handshakeItem = me.rows[indexPath.row]
-            handshakeItem.handshakeCombination.ownIdentity.session.performAndWait {
-                let selfIdentity = handshakeItem.handshakeCombination.ownIdentity
-                let partnerIdentity = handshakeItem.handshakeCombination.partnerIdentity
-                guard let trustwords = try? trustManagementViewModel.getTrustwords(for: selfIdentity,
-                                                                                   and: partnerIdentity,
-                                                                                   language: handshakeItem.currentLanguage,
-                                                                                   long: long)
-                    else {
-                        Log.shared.errorAndCrash("No Trustwords")
-                        completion?(nil)
-                        return
-                }
-                me.rows[indexPath.row].trustwords = trustwords
-                complete(trustwords)
-            }
-        }
+        rows[indexPath.row].showLongTrustwordVersion.toggle()
+        delegate?.dataChanged(forRowAt: indexPath)
     }
 
     /// Method that reverts the last action performed by the user
     /// After the execution of this method there won't be any action to un-do.
-    public func shakeMotionDidEnd() {
+    public func handleShakeMotionDidEnd() {
        /// Evaluate it the undo manager can undo means if it has something registerd to undo.
         /// If so, undo it and reload the view.
         if (undoManager.canUndo) {
@@ -316,23 +285,17 @@ final class TrustManagementViewModel {
     }
 
     /// Method that generates the rows to be used by the VC
-    private func generateRows() {
-        guard let trustManagementViewModel = trustManagementUtil else {
-            Log.shared.errorAndCrash("TrustManagementViewModel is nil")
-            return
-        }
-        let combinations = trustManagementViewModel.handshakeCombinations(message: message)
+    private func setupRows() {
+        let combinations = trustManagementUtil.handshakeCombinations(message: message)
 
-        for (index, combination) in combinations.enumerated() {
+        for combination in combinations{
             let backupLanguage = "en"
             let language =
             combination.partnerIdentity.language ?? Locale.current.languageCode ?? backupLanguage
-            let row = Row(currentLanguage: language,
-                          longTrustwords: false,
-                          handshakeCombination: combination)
+            let row = Row(language: language,
+                          handshakeCombination: combination,
+                          trustManagementUtil: trustManagementUtil)
             rows.append(row)
-            let indexPath = IndexPath(row: index, section: 0)
-            generateTrustwords(forRowAt: indexPath, completion: nil)
         }
     }
 
