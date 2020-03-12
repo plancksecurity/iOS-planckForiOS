75f24a848a3aeb1aa96067bbe023d08c1dcef65f
Dirk Zimmermann <dz@pep.security>
2020-03-12 16:58:22 +0100

    IOS-2160 Back out: Make verifiable account semi-persistent

--- pEpForiOS/UI/Login/ViewModel/AccountTypeSelectorViewModel.swift
+++ pEpForiOS/UI/Login/ViewModel/AccountTypeSelectorViewModel.swift
@@ -19,8 +19,6 @@ protocol AccountTypeSelectorViewModelDelegate: class {
 class AccountTypeSelectorViewModel {
     private let clientCertificateUtil: ClientCertificateUtilProtocol
 
-    private var verifiableAccount: VerifiableAccountProtocol?
-
     public weak var delegate: AccountTypeSelectorViewModelDelegate?
 
     /// list of providers to show
@@ -84,16 +82,10 @@ class AccountTypeSelectorViewModel {
     }
 
     public func clientCertificateManagementViewModel() -> ClientCertificateManagementViewModel {
-        // Client certificate handling will insert a client certificate into the given verifiable
-        // account (hidden side effect), therefore from this point on it needs to be
-        // persistent.
-        verifiableAccount = verifiableAccount ?? VerifiableAccount.verifiableAccout(for: chosenAccountType)
-        return ClientCertificateManagementViewModel(verifiableAccount: verifiableAccount)
+        return ClientCertificateManagementViewModel(verifiableAccount: VerifiableAccount.verifiableAccout(for: chosenAccountType))
        }
 
     public func loginViewModel() -> LoginViewModel {
-        // If we handled certificates, then the verifiable account is already set and must be used.
-        verifiableAccount = verifiableAccount ?? VerifiableAccount.verifiableAccout(for: chosenAccountType)
-        return LoginViewModel(verifiableAccount: verifiableAccount)
+           return LoginViewModel(verifiableAccount: VerifiableAccount.verifiableAccout(for: chosenAccountType))
        }
 }
