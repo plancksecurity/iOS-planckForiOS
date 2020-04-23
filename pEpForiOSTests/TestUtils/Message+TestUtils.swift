a1e5cad4b2823b6188f2a6bb2a4852f30dfa5231
Dirk Zimmermann <dz@pep.security>
2020-04-23 17:40:21 +0200

    IOS-1518 Remove dead code

--- pEpForiOSTests/TestUtils/Message+TestUtils.swift
+++ pEpForiOSTests/TestUtils/Message+TestUtils.swift
@@ -7,7 +7,6 @@
 //
 
 @testable import MessageModel
-import CoreData
 @testable import pEpForiOS
 import PEPObjCAdapterFramework
 
@@ -17,21 +16,5 @@ extension Message {
             || self.longMessageFormatted != nil
             || self.attachments.count > 0
             || self.shortMessage != nil
-    }
-
-    public static func by(uid: Int,
-                          folderName: String,
-                          accountAddress: String,
-                          context: NSManagedObjectContext) -> Message? {
-        let pAccount =
-            CdMessage.PredicateFactory.belongingToAccountWithAddress(address: accountAddress)
-        let pUid = NSPredicate(format: "%K = %d", CdMessage.AttributeName.uid, uid)
-        let pFolder =
-            CdMessage.PredicateFactory.belongingToParentFolderNamed(parentFolderName: folderName)
-        let p = NSCompoundPredicate(andPredicateWithSubpredicates: [pAccount, pUid, pFolder])
-        guard let cdMessage = CdMessage.all(predicate: p, in: context)?.first as? CdMessage else {
-            return nil
-        }
-        return MessageModelObjectUtils.getMessage(fromCdMessage: cdMessage)
     }
 }
