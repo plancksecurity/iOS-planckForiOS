dae9ca8080f7d51deba8d3357e1d376acd06d9c6
Xavier Algarra <xavier@pep-project.org>
2020-04-29 16:33:30 +0200

    IOS-2257 test improves

--- Submodules/MessageModel/MessageModel/MessageModelTests/CdObjectExtensions/CdIdentityPredicateFactoryTest.swift
+++ Submodules/MessageModel/MessageModel/MessageModelTests/CdObjectExtensions/CdIdentityPredicateFactoryTest.swift
@@ -19,6 +19,7 @@ class CdIdentityPredicateFactoryTest: PersistentStoreDrivenTestBase {
         let name4 = "Dr. Peter Falk"
         let name5 = "Falk Anselm Peter"
         let name6 = "Peter Anselm Falk"
+        let name7 = "DrPeterAnselmFalk"
         let address1 = "peter@pep.security"
         let address2 = "falk.peter@pep.security"
         let address3 = "falk-peter@pep.security"
@@ -35,32 +36,34 @@ class CdIdentityPredicateFactoryTest: PersistentStoreDrivenTestBase {
         id5.save()
         let id6 = Identity(address: "no6@no6.com", userName: name6)
         id6.save()
-        let id7 = Identity(address: address1, userName: "no7")
+        let id7 = Identity(address: "no7@no7.com", userName: name7)
         id7.save()
-        let id8 = Identity(address: address2, userName: "no8")
+        let id8 = Identity(address: address1, userName: "no8")
         id8.save()
-        let id9 = Identity(address: address3, userName: "no9")
+        let id9 = Identity(address: address2, userName: "no9")
         id9.save()
+        let id10 = Identity(address: address3, userName: "no10")
+        id10.save()
         
-        let totalCdIdentities = CdIdentity.all(in: moc)?.count
-        XCTAssertGreaterThan(totalCdIdentities ?? 0, 0)
-        var predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "pet")
-        var result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 9)
-        predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "falk")
-        result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 8)
-        predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "dr.")
-        result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 2)
-        predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "ter")
-        result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 0)
-        predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "elm")
-        result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 0)
-        predicate = CdIdentity.PredicateFactory.recipientSuggestions(searchTerm: "ans")
-        result = CdIdentity.all(predicate: predicate, in: moc)
-        XCTAssertEqual(result?.count, 2)
+        let totalCdIdentities = CdIdentity.all(in: moc)?.count ?? 0
+        XCTAssertGreaterThan(totalCdIdentities, 0)
+        var predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "pet")
+        var resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 9)
+        predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "falk")
+        resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 8)
+        predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "dr.")
+        resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 2)
+        predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "ter")
+        resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 0)
+        predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "elm")
+        resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 0)
+        predicate = CdIdentity.PredicateFactory.recipientSuggestions(for: "ans")
+        resultCount = CdIdentity.all(predicate: predicate, in: moc)?.count ?? 0
+        XCTAssertEqual(resultCount, 2)
     }
 }
