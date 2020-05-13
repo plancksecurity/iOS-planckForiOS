0ff79ec04584ee80f1c9003d2308c28ab31c75ad
Dirk Zimmermann <dz@pep.security>
2020-05-13 16:42:21 +0200

    IOS-2274 Rename the enum case

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/FileBrowser.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/FileBrowser.swift
@@ -12,13 +12,13 @@ import Foundation
 public class FileBrowser {
     public enum FileType {
         /// ASCII-armored public/private keys
-        case keys
+        case key
 
         /// Supported file extensions (minus the dot).
         /// - Returns: The file extensions for a file type.
         public func fileExtensions() -> Set<String> {
             switch self {
-            case .keys:
+            case .key:
                 return Set(["asc"])
             }
         }
