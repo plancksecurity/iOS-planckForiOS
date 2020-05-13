013c7f48732288092d2bab301d688c35e97097ee
Dirk Zimmermann <dz@pep.security>
2020-05-13 16:42:21 +0200

    IOS-2274 Implement listFileUrls (initial)

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/FileBrowser.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/FileBrowser.swift
@@ -13,6 +13,15 @@ public class FileBrowser {
     public enum FileType {
         /// ASCII-armored public/private keys
         case keys
+
+        /// Supported file extensions (minus the dot).
+        /// - Returns: The file extensions for a file type.
+        public func fileExtensions() -> Set<String> {
+            switch self {
+            case .keys:
+                return Set(["asc"])
+            }
+        }
     }
 
     /// Retrieves an array of file URLs found in the documents directories,
@@ -24,12 +33,24 @@ public class FileBrowser {
     public func listFileUrls(fileTypes: [FileType]) throws -> [URL] {
         var resultUrls = [URL]()
 
+        var allExtensions = Set<String>()
+        for ftype in fileTypes {
+            let additionalExtensions = ftype.fileExtensions()
+            for newFType in additionalExtensions {
+                allExtensions.insert(newFType)
+            }
+        }
+
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         for theUrl in urls {
             let fileUrls = try FileManager.default.contentsOfDirectory(at: theUrl,
                                                                        includingPropertiesForKeys: nil,
                                                                        options: [.skipsHiddenFiles])
             for theFileUrl in fileUrls {
+                if theFileUrl.pathExtension != "" &&
+                    allExtensions.contains(theFileUrl.pathExtension) {
+                    resultUrls.append(theFileUrl)
+                }
             }
         }
 
