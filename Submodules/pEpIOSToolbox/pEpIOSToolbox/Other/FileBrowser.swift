ebf55f810495bb0a3f75a3f4782a7b9956163aa1
Dirk Zimmermann <dz@pep.security>
2020-05-13 16:42:21 +0200

    IOS-2274 FileBrowser

---
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/Other/FileBrowser.swift
@@ -0,0 +1,17 @@
+//
+//  FileBrowser.swift
+//  pEpIOSToolbox
+//
+//  Created by Dirk Zimmermann on 13.05.20.
+//  Copyright © 2020 pEp Security SA. All rights reserved.
+//
+
+import Foundation
+
+/// Methods for listing files in the app's documents directory.
+public class FileBrowser {
+    public enum FileType {
+        /// ASCII-armored public/private keys
+        case keys
+    }
+}
