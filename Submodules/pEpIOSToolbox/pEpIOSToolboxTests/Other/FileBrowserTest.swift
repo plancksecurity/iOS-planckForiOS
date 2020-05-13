3a74ab816ee6108d92bd3bdcb848110f290444bc
Dirk Zimmermann <dz@pep.security>
2020-05-13 16:42:21 +0200

    IOS-2274 Add FileBrowserTest skeleton

---
+++ Submodules/pEpIOSToolbox/pEpIOSToolboxTests/Other/FileBrowserTest.swift
@@ -0,0 +1,23 @@
+//
+//  FileBrowserTest.swift
+//  pEpIOSToolboxTests
+//
+//  Created by Dirk Zimmermann on 13.05.20.
+//  Copyright © 2020 pEp Security SA. All rights reserved.
+//
+
+import XCTest
+
+import pEpIOSToolbox
+
+class FileBrowserTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testNoFiles() throws {
+    }
+}
