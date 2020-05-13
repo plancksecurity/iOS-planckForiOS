4c1e4ded35a32a51fb1eb406076348f1938e0eba
Dirk Zimmermann <dz@pep.security>
2020-05-13 16:42:21 +0200

    IOS-2274 Load rows in init

--- pEpForiOS/UI/KeyImport/KeyImportModel.swift
+++ pEpForiOS/UI/KeyImport/KeyImportModel.swift
@@ -8,12 +8,35 @@
 
 import Foundation
 
+import pEpIOSToolbox
+
 /// Model for importing keys from the filesystem, and setting them as own keys.
 class KeyImportModel {
     public private(set) var rows = [Row]()
+
+    init() {
+        loadRows()
+    }
 }
 
 extension KeyImportModel {
     struct Row {
+        public let fileUrl: URL
+
+        public var fileName: String {
+            fileUrl.fileName(includingExtension: true)
+        }
+    }
+}
+
+extension KeyImportModel {
+    private func loadRows() {
+        do {
+            let urls = try FileBrowser.listFileUrls(fileTypes: [.key])
+            rows = urls.map { Row(fileUrl: $0) }
+        } catch {
+            Log.shared.errorAndCrash(error: error)
+            rows = []
+        }
     }
 }
