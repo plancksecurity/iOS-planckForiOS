c831bfea137861c8007d212d9f754aa33b292d59
Dirk Zimmermann <dz@pep.security>
2020-05-14 12:09:32 +0200

    IOS-2274 Start to implement KeyImportUtilProtocol

---
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/KeyImport/KeyImportUtil.swift
@@ -0,0 +1,41 @@
+//
+//  KeyImportUtil.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 14.05.20.
+//  Copyright © 2020 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+import PEPObjCAdapterFramework
+
+class KeyImportUtil {
+}
+
+extension KeyImportUtil: KeyImportUtilProtocol {
+    func importKey(url: URL) throws -> KeyImportUtilProtocolKeyData {
+        guard let dataString = try? String(contentsOf: url) else {
+            throw KeyImportUtilProtocolImportError.cannotLoadKey
+        }
+
+        let session = PEPSession()
+
+        let identities = try session.importKey(dataString)
+
+        guard let firstIdentity = identities.first else {
+            throw KeyImportUtilProtocolImportError.malformedKey
+        }
+
+        guard let fingerprint = firstIdentity.fingerPrint else {
+            throw KeyImportUtilProtocolImportError.malformedKey
+        }
+
+        return KeyImportUtilProtocolKeyData(address: firstIdentity.address,
+                                            fingerprint: fingerprint,
+                                            keyDataString: dataString)
+    }
+
+    func setOwnKey(keyData: KeyImportUtilProtocolKeyData) throws {
+    }
+}
