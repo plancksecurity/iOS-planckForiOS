1b8de423d089c0ae0b40690a906dd67182401068
buff <andreas@pep-project.org>
2020-05-14 11:43:53 +0200

    IOS-2274 adds structure for setting UI

---
+++ pEpForiOS/UI/Settings/Setting/PGPKeyImport/PGPKeyImportViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PGPKeyImportViewController.swift
+//  pEp
+//
+//  Created by Andreas Buff on 14.05.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class PGPKeyImportViewController: BaseViewController {
+    <#code#>
+}
