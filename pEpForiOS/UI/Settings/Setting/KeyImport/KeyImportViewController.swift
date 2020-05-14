fa814bbddc772c6aadda53b1047bf7bf938cddac
Dirk Zimmermann <dz@pep.security>
2020-05-14 16:14:44 +0200

    IOS-2274 Add KeyImportViewController

---
+++ pEpForiOS/UI/Settings/Setting/KeyImport/KeyImportViewController.swift
@@ -0,0 +1,12 @@
+//
+//  KeyImportViewController.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 14.05.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class KeyImportViewController: BaseViewController {
+}
