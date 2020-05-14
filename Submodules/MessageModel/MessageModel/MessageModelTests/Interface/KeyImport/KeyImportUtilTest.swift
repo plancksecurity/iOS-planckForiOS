2fafe664a6b9820a59f3329c23426df623c3c23a
Dirk Zimmermann <dz@pep.security>
2020-05-14 15:47:00 +0200

    IOS-2274 testSuccessfulImport

--- Submodules/MessageModel/MessageModel/MessageModelTests/Interface/KeyImport/KeyImportUtilTest.swift
+++ Submodules/MessageModel/MessageModel/MessageModelTests/Interface/KeyImport/KeyImportUtilTest.swift
@@ -31,12 +31,43 @@ class KeyImportUtilTest: XCTestCase {
     func testSuccessfulImportButNoAccount() throws {
         let keyImport = KeyImportUtil()
 
-        guard let url = Bundle.main.url(forResource: "IOS-1432_keypair.asc", withExtension: nil) else {
-            XCTFail()
-            return
+        let testBundle = Bundle(for: KeyImportUtilTest.self)
+
+        guard let url = testBundle.url(forResource: "IOS-1432_keypair.asc",
+                                       withExtension: nil) else {
+                                        XCTFail()
+                                        return
         }
 
         let keyData = try keyImport.importKey(url: url)
+
+        do {
+            try keyImport.setOwnKey(keyData: keyData)
+        } catch KeyImportUtil.SetOwnKeyError.noMatchingAccount {
+            // expected
+        }
+    }
+
+    func testSuccessfulImport() throws {
+        let keyImport = KeyImportUtil()
+
+        let testBundle = Bundle(for: KeyImportUtilTest.self)
+
+        guard let url = testBundle.url(forResource: "IOS-1432_keypair.asc",
+                                       withExtension: nil) else {
+                                        XCTFail()
+                                        return
+        }
+
+        let keyData = try keyImport.importKey(url: url)
+
+        let ident = Identity(address: keyData.address,
+                             userID: "some_user_id",
+                             addressBookID: nil,
+                             userName: "some name",
+                             session: nil)
+
+        let account = Account(user: ident, servers: [])
 
         do {
             try keyImport.setOwnKey(keyData: keyData)
