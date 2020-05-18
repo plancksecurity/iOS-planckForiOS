16c9f6c1278735a33c92716e06a2a9831eb22b07
Dirk Zimmermann <dz@pep.security>
2020-05-18 11:40:32 +0200

    IOS-2274 Add bare KeyImportViewModelTest

---
+++ pEpForiOSTests/UI/Settings/Setting/KeyImport/KeyImportViewModelTest.swift
@@ -0,0 +1,19 @@
+//
+//  KeyImportViewModelTest.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 18.05.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class KeyImportViewModelTest: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
