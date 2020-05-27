bc0d6f411bf77a4b475e8c782473067af3b7df26
Dirk Zimmermann <dz@pep.security>
2020-05-27 18:00:56 +0200

    IOS-2259 Whitespace

--- pEpForiOSTests/TrustManagementViewModelTest.swift
+++ pEpForiOSTests/TrustManagementViewModelTest.swift
@@ -8,6 +8,7 @@
 //
 
 import XCTest
+
 @testable import MessageModel
 @testable import pEpForiOS
 
@@ -183,7 +184,6 @@ extension TrustManagementViewModelTest {
         }
     }
 }
-
 
 ///MARK: - Mock Util Classes
 
