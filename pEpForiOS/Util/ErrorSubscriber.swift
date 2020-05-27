6a327000fc508ba00e1779e383cdc9e5ae66eb7c
Xavier Algarra <xavier@pep-project.org>
2020-05-27 10:20:39 +0200

    IOS-2196 rm comments and imp of servers in subscriber

--- pEpForiOS/Util/ErrorSubscriber.swift
+++ pEpForiOS/Util/ErrorSubscriber.swift
@@ -11,9 +11,86 @@ import MessageModel
 
 public class ErrorSubscriber {
     public func errorShouldBeDisplayed(error: Error) -> Bool{
+        if let smtpError = error as? SmtpSendError {
+            switch smtpError {
+            case .authenticationFailed( _, let account):
+                return accountErrorShouldBeShown(account: account, serverType: .smtp)
+            case .illegalState(_):
+                break
+            case .connectionLost(_):
+                break
+            case .connectionTerminated(_):
+                break
+            case .connectionTimedOut(_):
+                break
+            case .badResponse(_):
+                break
+            case .clientCertificateNotAccepted:
+                break
+            }
+        } else if let imapError = error as? ImapSyncOperationError {
+            switch imapError {
+            case .authenticationFailed(_, let account):
+                return accountErrorShouldBeShown(account: account, serverType: .imap)
+            case .illegalState(_):
+                break
+            case .connectionLost(_):
+                break
+            case .connectionTerminated(_):
+                break
+            case .connectionTimedOut(_):
+                break
+            case .folderAppendFailed:
+                break
+            case .badResponse(_):
+                break
+            case .actionFailed:
+                break
+            case .clientCertificateNotAccepted:
+                break
+            }
+        }
         return true
     }
+    private enum serverError {
+        case smtp, imap
+    }
     
+    private func accountErrorShouldBeShown(account: String, serverType: serverError) -> Bool {
+        guard let account = Account.by(address: account) else {
+            return true
+        }
+        var server: Server
+        
+        switch serverType {
+        case .imap:
+            guard let imapServer = Server.by(account: account, serverType: .imap) else {
+                return true
+            }
+            server = imapServer
+            
+        case .smtp:
+            guard let smtpServer = Server.by(account: account, serverType: .smtp) else {
+                return true
+            }
+            server = smtpServer
+        }
+        let actualDate = Date()
+        guard let lastErrorShownDate = server.dateLastAuthenticationErrorShown,
+        let minimumDateBeforeShowinAnotherError = Calendar.current.date(byAdding: .minute,
+                                                                        value: 2,
+                                                                        to: lastErrorShownDate)
+            else {
+                return true
+        }
+        if actualDate > minimumDateBeforeShowinAnotherError {
+            server.dateLastAuthenticationErrorShown = actualDate
+            Session.main.commit()
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
 extension ErrorSubscriber: ErrorPropagatorSubscriber {
@@ -24,28 +101,3 @@ extension ErrorSubscriber: ErrorPropagatorSubscriber {
         }
     }
 }
-
-/*
- let moc = Session.main.moc
- let actualDate = Date()
- let server = CdServer.first(predicate: CdServer.PredicateFactory.smtpServerForAccount(account: smtpConnection.accountAddress), in: moc)
- guard let lastErrorShownDate = server?.dateLastAuthenticationErrorShown,
- let minimumDateBeforeShowinAnotherError = Calendar.current.date(byAdding: .minute,
- value: 2,
- to: lastErrorShownDate)
- else {
- addError(SmtpSendError.authenticationFailed(
- #function,
- smtpConnection.accountAddress))
- waitForBackgroundTasksAndFinish()
- return
- }
- if actualDate > minimumDateBeforeShowinAnotherError {
- addError(SmtpSendError.authenticationFailed(
- #function,
- smtpConnection.accountAddress))
- waitForBackgroundTasksAndFinish()
- server?.dateLastAuthenticationErrorShown = actualDate
- moc.saveAndLogErrors()
- }
- */
