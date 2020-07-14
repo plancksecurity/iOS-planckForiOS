2064b33ccfe0edc9e74779b887f285ce3ff86301
Martin Brude <devnull@localhost>
2020-07-14 16:31:22 +0200

    IOS-2244 add some tests

--- Submodules/MessageModel/MessageModel/MessageModelTests/Interface/MessageModelObjects/FolderTest.swift
+++ Submodules/MessageModel/MessageModel/MessageModelTests/Interface/MessageModelObjects/FolderTest.swift
@@ -36,4 +36,36 @@ class FolderTest: PersistentStoreDrivenTestBase {
                        CdMessage.all(predicate: predicate, in: moc)?.count)
 
     }
+
+    func testCountUnreads() {
+        let inbox = TestUtil.createFolder(name: "inbox", folderType: .inbox, moc: moc)
+        inbox.account = cdAccount
+        let inboxFolder = inbox.folder()
+        let message = TestUtil.createCdMessage(cdFolder: inboxFolder.cdFolder()! , moc: moc)
+        XCTAssert(inboxFolder.countUnread == 0)
+        message.imap = CdImapFields(context: moc)
+        message.imap?.localFlags = CdImapFlags(context: moc)
+        message.imap?.localFlags?.flagSeen = false
+        XCTAssert(inboxFolder.countUnread == 1)
+    }
+
+    func testAllCountUnreads() {
+        let localInbox = TestUtil.createFolder(name: "inbox", folderType: .inbox, moc: moc)
+        localInbox.account = cdAccount
+
+        let m = TestUtil.createCdMessage(withText: "a", sentDate: nil, cdFolder: localInbox, moc: moc)
+        m.imap = CdImapFields(context: moc)
+
+        let localFlags = CdImapFlags(context: moc)
+        m.imap?.localFlags = localFlags
+
+        let serverFlags = CdImapFlags(context: moc)
+        m.imap?.serverFlags = serverFlags
+
+        m.imap?.localFlags?.flagSeen = false
+        moc.saveAndLogErrors()
+
+        let result = Folder.countUnread(folderType: .inbox)
+        XCTAssertEqual(result, 1)
+     }
 }
