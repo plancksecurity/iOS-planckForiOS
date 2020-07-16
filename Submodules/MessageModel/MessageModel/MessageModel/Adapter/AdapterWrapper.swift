49eace5cd9d5e4e06d429fd0ef833965eb479286
Dirk Zimmermann <dz@pep.security>
2020-07-16 14:46:09 +0200

    IOS-2325 Docs

--- Submodules/MessageModel/MessageModel/MessageModel/Adapter/AdapterWrapper.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Adapter/AdapterWrapper.swift
@@ -10,6 +10,16 @@ import Foundation
 
 import PEPObjCAdapterFramework
 
+/// Wraps UI calls into the adapter.
+///
+/// UI code cannot access adapter methods directly, since the adapter may
+/// trigger passphrase UI and wait for an answer, which leads to deadlocks.
+///
+/// For that reason, the UI must not use `PEPSession` directly, but instead
+/// use the methods from this class with a completion block.
+///
+/// The adapter will be called on a background queue and invoke the
+/// completion block on the main queue with the result.
 public class AdapterWrapper {
     public static func pEpColor(cdIdentity: CdIdentity,
                                 completion: @escaping (_ error: Error?, _ color: PEPColor?) -> Void) {
