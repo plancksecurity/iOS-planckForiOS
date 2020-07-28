469dc377b69d96ca4f048c2e02ef5b2ce99451ea
Dirk Zimmermann <dz@pep.security>
2020-07-28 16:11:21 +0200

    IOSAD-178 Add sync languageList

--- Submodules/MessageModel/MessageModel/MessageModelTests/TestUtils/XCTest+SyncAdapter.swift
+++ Submodules/MessageModel/MessageModel/MessageModelTests/TestUtils/XCTest+SyncAdapter.swift
@@ -51,4 +51,20 @@ extension XCTestCase {
         }
         return identity
     }
+
+    func languageList() -> [PEPLanguage] {
+        var languages = [PEPLanguage]()
+
+        let expHaveLanguages = expectation(description: "expHaveLanguages")
+        PEPAsyncSession().languageList({ error in
+            XCTFail()
+            expHaveLanguages.fulfill()
+        }) { langs in
+            languages = langs
+            expHaveLanguages.fulfill()
+        }
+        wait(for: [expHaveLanguages], timeout: TestUtil.waitTime)
+
+        return languages
+    }
 }
