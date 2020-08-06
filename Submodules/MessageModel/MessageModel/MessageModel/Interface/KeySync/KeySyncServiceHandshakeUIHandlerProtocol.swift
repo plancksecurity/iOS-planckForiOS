66384e6c1b2c253a4844898e18dfc4a6f3dba62e
Dirk Zimmermann <dz@pep.security>
2020-08-06 13:13:12 +0200

    IOS-2328 No adapter in the interface

--- Submodules/MessageModel/MessageModel/MessageModel/Interface/KeySync/KeySyncServiceHandshakeUIHandlerProtocol.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/KeySync/KeySyncServiceHandshakeUIHandlerProtocol.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2019 pEp Security S.A. All rights reserved.
 //
 
-import PEPObjCAdapterFramework
-
 public protocol KeySyncServiceHandshakeHandlerProtocol: class {
 
     /// Show pEp Sync Wizzard
