6b15356b7b5577f2fd85c5c78a1761148da40dde
Martin Brude <devnull@localhost>
2020-08-07 12:10:30 +0200

    IOS-2368 save context

--- pEpForiOS/UI/Settings/Setting/AccountSettings/ViewModel/AccountSettingsViewModel.swift
+++ pEpForiOS/UI/Settings/Setting/AccountSettings/ViewModel/AccountSettingsViewModel.swift
@@ -201,6 +201,7 @@ extension AccountSettingsViewModel {
     public func handleSwitchChanged(isIncludedInUnifiedFolders: Bool) {
         includeInUnifiedFolders = isIncludedInUnifiedFolders
         account.isIncludedInUnifiedFolders = isIncludedInUnifiedFolders
+        account.session.commit()
     }
 
     /// [En][Dis]able the pEpSync status
