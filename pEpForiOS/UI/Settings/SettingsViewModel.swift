1d5f09d700e5ba312621fa4a88befe9de7f96d6b
buff <andreas@pep-project.org>
2020-08-11 12:25:15 +0200

    IOS-2366 fixes: now async leaveDeviceGroup() not processed due to calling shutdown() sync imediatelly

--- pEpForiOS/UI/Settings/SettingsViewModel.swift
+++ pEpForiOS/UI/Settings/SettingsViewModel.swift
@@ -240,7 +240,7 @@ extension SettingsViewModel {
                     Log.shared.lostMySelf()
                     return
                 }
-                me.setPEPSyncEnabled(to: value)//!!!: IOS-2325_!
+                me.setPEPSyncEnabled(to: value)
             })
             rows.append(generateSwitchRow(type: .usePEPFolder,
                                           isDangerous: false,
@@ -416,9 +416,12 @@ extension SettingsViewModel {
             KeySyncUtil.enableKeySync()
         } else {
             if grouped {
-                KeySyncUtil.leaveDeviceGroup()
+                KeySyncUtil.leaveDeviceGroup() {
+                    // Nothing to do.
+                }
+            } else {
+                KeySyncUtil.disableKeySync()
             }
-            KeySyncUtil.disableKeySync()
         }
     }
 
