75c82f402b6d24b19cba68ddbdb3e8e51e29c5c3
buff <andreas@pep-project.org>
2020-08-12 13:48:26 +0200

    IOS-2361 I do not think its needed and it makes the code very complex, but lets be on the save side. Maybe backout if the real root of the issue is found.

--- Submodules/MessageModel/MessageModel/MessageModel/Services/ReplicationService/IMAP/ImapReplicationService.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Services/ReplicationService/IMAP/ImapReplicationService.swift
@@ -45,7 +45,52 @@ class ImapReplicationService: OperationBasedService {
         privateMoc.performAndWait {
             cdAccount = privateMoc.object(with: cdAccountObjectID) as? CdAccount
         }
+
+        // Custom finisBlock to make sure all local changes (made by the user) are synced with the server.
+        finishBlock = { [weak self] in
+            guard let me = self else {
+                Log.shared.errorAndCrash("Lost myself")
+                return
+            }
+            Log.shared.info("%@ - custom finishBlock called with state: %@)",
+                            "\(type(of: self))", "\(me.state)")
+            
+            me.backgroundQueue.cancelAllOperations()
+            me.syncLocalChanges()
+            me.state = .finshing
+            me.doNotRestart()
+        }
     }
+
+    //BUFF: move
+    private func syncLocalChanges() {
+        let toDos = internalOperations(syncOnlyUserChanges: true)
+        Log.shared.info("%@ - syncLocalChanges called with state: %@ numOperations: %d",
+                        "\(type(of: self))", "\(state)", toDos.count)
+        guard !toDos.isEmpty else {
+                // Nothing to do. Let everyone know we are ready.
+                // Do not call next(). That would result in an endless loop
+                // (lasstCommand == start, state == .ready, -> nothing todo, next(), ...).
+                // Wait for QRC to trigger or client to call `start()` again instead.
+                doNotRestart()
+                return
+        }
+        let group = DispatchGroup()
+        let toDosManagedByGroup = addCompletionOperations(handling: group, to: toDos)
+        group.notify(queue: DispatchQueue.global(qos: .background)) { [weak self] in
+            guard let me = self else {
+                Log.shared.errorAndCrash("Lost myself")
+                return
+            }
+            me.doNotRestart()
+        }
+        // registerBackgroundTask should already have been started when starting the service (in
+        //startNextProcessingRound). To make sure we call it anyway just to make sure.
+        // It does not hurt. BackgroundTaskManager will ignore it.
+        registerBackgroundTask()
+        backgroundQueue.addOperations(toDosManagedByGroup, waitUntilFinished: false)
+    }
+    //
 
     private func internalOperations(syncOnlyUserChanges: Bool = false) -> [Operation] {
         var createes = [Operation]()
