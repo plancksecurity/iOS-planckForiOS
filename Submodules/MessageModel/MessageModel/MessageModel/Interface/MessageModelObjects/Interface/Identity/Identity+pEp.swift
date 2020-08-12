2a8da28be107197467e6b223ac4b142d92a6adba
Dirk Zimmermann <dz@pep.security>
2020-08-12 17:07:46 +0200

    IOS-2328 Dead code

--- Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Identity/Identity+pEp.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Identity/Identity+pEp.swift
@@ -11,14 +11,6 @@ import PEPObjCAdapterFramework
 //!!: re-think & cleanup. Imo PEPIdentity should never be used in App. Actually the Adapter should be used at least as little as possible in the app. At least all the functtionallity should be moved to CdIdentity and then being forwarded.
 
 extension Identity {
-
-    public static func from(pEpIdentity: PEPIdentity) -> Identity {
-        let id = Identity(address: pEpIdentity.address,
-                          userID: pEpIdentity.userID,
-                          userName: pEpIdentity.userName)
-        return id
-    }
-
     public func pEpRating(completion: @escaping (PEPRating)->Void) {
         cdObject.pEpRating(completion: completion)
     }
