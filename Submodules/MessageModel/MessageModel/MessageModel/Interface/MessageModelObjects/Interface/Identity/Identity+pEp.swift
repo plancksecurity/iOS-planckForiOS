4f1d4b38043218317ecde6cd7336be0d04c9f96f
Dirk Zimmermann <dz@pep.security>
2020-08-13 16:58:04 +0200

    IOS-2328 This is clean. No adapter.

--- Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Identity/Identity+pEp.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Identity/Identity+pEp.swift
@@ -6,10 +6,6 @@
 //  Copyright © 2016 p≡p Security S.A. All rights reserved.
 //
 
-import PEPObjCAdapterFramework
-
-//!!: re-think & cleanup. Imo PEPIdentity should never be used in App. Actually the Adapter should be used at least as little as possible in the app. At least all the functtionallity should be moved to CdIdentity and then being forwarded.
-
 extension Identity {
     public func pEpRating(completion: @escaping (Rating) -> Void) {
         cdObject.pEpRating() { pEpRating in
