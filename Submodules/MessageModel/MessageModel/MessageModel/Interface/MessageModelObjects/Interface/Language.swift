79de28095e4d165d1b9cd1fa5c7f8ebfa982bc18
Dirk Zimmermann <dz@pep.security>
2020-08-19 16:58:07 +0200

    IOS-2328 Language.languages()

--- Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Language.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Language.swift
@@ -8,14 +8,30 @@
 
 import Foundation
 
+import PEPObjCAdapterFramework
+
 /// Wraps `PEPLanguage`, indicating a trustwords language.
 public struct Language {
     /// ISO 639-1 language code
     public let code: String
 
-    /// Name of the language.
+    /// Name of the language
     public let name: String
 
-    /// Sentence of the form "I want to display the trustwords in <lang>".
+    /// Sentence of the form "I want to display the trustwords in <lang>"
     public let sentence: String
+
+    /// Retrieves all known languages, calling the completion block on the main queue.
+    static public func languages(completion: @escaping ([Language]) -> ()) {
+        PEPAsyncSession().languageList({ error in
+            Log.shared.errorAndCrash("%@", error.localizedDescription)
+            DispatchQueue.main.async {
+                completion([])
+            }
+        }) { theLangs in
+            DispatchQueue.main.async {
+                completion(theLangs.map { Language.from(pEpLanguage: $0) })
+            }
+        }
+    }
 }
