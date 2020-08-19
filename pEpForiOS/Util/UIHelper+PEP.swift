cfa04bbc5fda0483342213233a32b06ca1ab3eb2
Dirk Zimmermann <dz@pep.security>
2020-08-19 16:58:07 +0200

    IOS-2328 Dead code

--- pEpForiOS/Util/UIHelper+PEP.swift
+++ pEpForiOS/Util/UIHelper+PEP.swift
@@ -11,25 +11,6 @@ import MessageModel
 // MARK: - UIHelper+PEP
 
 extension UIHelper {
-    /// Get the UIColor for the background image of a send button for an (abstract) pEp color.
-    static func sendButtonBackgroundColorFromPepColor(_ color: Color) -> UIColor? {
-        switch color {
-        case .green:
-            return UIColor.green
-        case .yellow:
-            return UIColor.yellow
-        case .red:
-            return UIColor.red
-        default:
-            return nil
-        }
-    }
-
-    /// Cell background color in trustwords cell for indicating the rating of a contact.
-    static func trustWordsCellBackgroundColorFromPepColor(_ color: Color) -> UIColor? {
-        return sendButtonBackgroundColorFromPepColor(color)
-    }
-
     /// Get the UIColor for an identity (in a text field or label) for an (abstract) pEp color.
     /// This might, or might not, be the same,
     /// as `sendButtonBackgroundColorFromPepColor:PrivacyColor`.
