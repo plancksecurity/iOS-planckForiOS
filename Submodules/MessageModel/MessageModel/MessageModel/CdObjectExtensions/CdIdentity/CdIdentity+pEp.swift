00fab4d3573baab4557772146afb7770d5cf7f2a
Dirk Zimmermann <dz@pep.security>
2020-08-19 16:58:07 +0200

    IOS-2328 Make internal

--- Submodules/MessageModel/MessageModel/MessageModel/CdObjectExtensions/CdIdentity/CdIdentity+pEp.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/CdObjectExtensions/CdIdentity/CdIdentity+pEp.swift
@@ -56,9 +56,8 @@ extension CdIdentity {
                            language: nil)
     }
 
-    //!!!: MUST become internal after tests have been moved
-    public static func from(pEpContact: PEPIdentity?,
-                            context: NSManagedObjectContext) -> CdIdentity? {
+    static func from(pEpContact: PEPIdentity?,
+                     context: NSManagedObjectContext) -> CdIdentity? {
         guard let pEpC = pEpContact else {
             return nil
         }
