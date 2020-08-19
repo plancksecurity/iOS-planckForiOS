839df9c4cbab4ce81bb83d9a14e64406b060745a
Xavier Algarra <xavier@pep-project.org>
2020-08-19 10:12:06 +0200

    IOS-1124 viewModel

---
+++ pEpForiOS/UI/Settings/Setting/AccountSettings/EditSignature/EditSignatureViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  EditSignatureViewModel.swift
+//  pEp
+//
+//  Created by Xavier Algarra on 19/08/2020.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import MessageModel
+
+class EditSignatureViewModel {
+    
+    private let account: Account
+    
+    public var numberOfRows: Int {
+        return 1
+    }
+    
+    init(account: Account) {
+        self.account = account
+    }
+    
+    public func updateSignature(newSignature: String) {
+        AppSettings.shared.storeSignatureForAddress(address: account.user.address,
+                                                    signature: newSignature)
+    }
+    
+    public func actualSignature() -> String {
+        return AppSettings.shared.loadSignatureForAddress(address: account.user.address)
+    }
+}
