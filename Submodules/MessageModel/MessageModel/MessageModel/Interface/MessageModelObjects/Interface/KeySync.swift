2e3b181d7197687093e8e920f715aced1d1e05b7
Dirk Zimmermann <dz@pep.security>
2020-08-19 16:58:07 +0200

    IOS-2328 --Import Adapter

---
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/KeySync.swift
@@ -0,0 +1,19 @@
+//
+//  KeySync.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 19.08.20.
+//  Copyright © 2020 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+import PEPObjCAdapterFramework
+
+public class KeySync {
+    /// Wraps `PEPAsyncSession.leaveDeviceGroup`
+    public static func leaveDeviceGroup(_ errorCallback: @escaping (Error) -> Void,
+                                        successCallback: @escaping () -> Void) {
+        PEPAsyncSession().leaveDeviceGroup(errorCallback, successCallback: successCallback)
+    }
+}
