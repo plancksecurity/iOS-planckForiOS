28e225454420890f7ea4ab116ccfadaa07123e13
Adam Kowalski <adam@pep-project.org>
2020-09-17 13:16:01 +0200

    IOS-2429 Weird scroll in compose view when replying a long mail.

--- pEpForiOS/UI/Compose/ComposeTableViewController.swift
+++ pEpForiOS/UI/Compose/ComposeTableViewController.swift
@@ -243,6 +243,10 @@ extension ComposeTableViewController: ComposeViewModelDelegate {
             }
         } else if cell is BodyCell {
             cell.textView.sizeToFit()
+            // IOS-2429
+            // It is important to set focus before layoutAfterTextDidChange(...)!
+            setInitialFocus()
+            // We call this function only when focus is set (not before that)
             scrollUtil.layoutAfterTextDidChange(tableView: tableView, textView: cell.textView)
             tableView.updateSize()
         } else {
