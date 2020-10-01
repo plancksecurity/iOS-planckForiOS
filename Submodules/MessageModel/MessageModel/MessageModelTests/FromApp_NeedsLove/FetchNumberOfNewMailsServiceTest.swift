c2521f3a2dd304fc5a27acf66b3d11d1c30d35dd
Dirk Zimmermann <dz@pep.security>
2020-10-01 16:52:22 +0200

    IOS-2259 Back out: Remove failing tests

--- Submodules/MessageModel/MessageModel/MessageModelTests/FromApp_NeedsLove/FetchNumberOfNewMailsServiceTest.swift
+++ Submodules/MessageModel/MessageModel/MessageModelTests/FromApp_NeedsLove/FetchNumberOfNewMailsServiceTest.swift
@@ -61,6 +61,16 @@ class FetchNumberOfNewMailsServiceTest: PersistentStoreDrivenTestBase {
     func testUnreadMail_autoconsumableOnly_many() {
         assertNewMailsService(numNewNormalMessages: 0, numNewAutoconsumableMessages: 5)
     }
+
+    // MARK: - Normal & Auto-Consumable Mails Exist
+
+    func testUnreadMail_normalAndAutoconsumable_one() {
+        assertNewMailsService(numNewNormalMessages: 1, numNewAutoconsumableMessages: 1)
+    }
+
+    func testUnreadMail_normalAndAutoconsumable_multi_sameCount() {
+        assertNewMailsService(numNewNormalMessages: 2, numNewAutoconsumableMessages: 2)
+    }
 }
 
 // MARK: - HELPERS
