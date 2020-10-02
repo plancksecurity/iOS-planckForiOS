c682bc1dbd2c23735890fe005ccaeeb619f08291
Dirk Zimmermann <dz@pep.security>
2020-10-02 17:01:16 +0200

    IOS-2259 Remove comments

--- pEpForiOSTests/UI/Compose/Cells/BodyCell/BodyCellViewModelTest.swift
+++ pEpForiOSTests/UI/Compose/Cells/BodyCell/BodyCellViewModelTest.swift
@@ -651,17 +651,7 @@ class BodyCellViewModelTest: XCTestCase {
             }
         }
 
-        func bodyCellViewModel(_ vm: BodyCellViewModel, bodyAttributedString: NSAttributedString) { //!!!: ADAM: commented due to the changes
-//            guard let exp = expBodyChangedCalled else {
-//                // We ignore called or not
-//                return
-//            }
-//            exp.fulfill()
-//            let expBody = bodyAttributedString.toHtml()
-//            let expPlainText = expBody.plainText
-//            if let expected1 = exectedPlain {
-//                XCTAssertEqual(expPlainText, expected1)
-//            }
+        func bodyCellViewModel(_ vm: BodyCellViewModel, bodyAttributedString: NSAttributedString) {
         }
     }
 }
