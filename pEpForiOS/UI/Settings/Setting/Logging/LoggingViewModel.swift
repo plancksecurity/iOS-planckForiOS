66b7fee3182c86035f8bceb92255ddd8b993bcf4
Dirk Zimmermann <dz@pep.security>
2020-10-07 16:04:10 +0200

    IOS-2457 Implement the VM

--- pEpForiOS/UI/Settings/Setting/Logging/LoggingViewModel.swift
+++ pEpForiOS/UI/Settings/Setting/Logging/LoggingViewModel.swift
@@ -8,20 +8,46 @@
 
 import Foundation
 
-protocol LogViewModelDelegate: class {
+import pEpIOSToolbox
+
+public protocol LogViewModelDelegate: class {
     /// The view should be updated with the latest log contents.
     /// - Note: The updates _may_ occur every `updateInterval`, regardless
     /// if the log changed or not.
     func updateLogContents(logString: String)
 }
 
-class LoggingViewModel {
+public class LoggingViewModel {
     /// Update interval in seconds.
     ///
     /// The VM will pull the latest log string every interval and inform
     /// the delegate via `updateLogContents`.
-    var updateInterval: TimeInterval = 2.0
+    public var updateInterval: TimeInterval = 2.0
 
     /// The delegate for log updates
-    weak var delegate: LogViewModelDelegate?
+    public weak var delegate: LogViewModelDelegate?
+
+    public init() {
+        setupTimers()
+    }
+
+    private var timer: Timer?
+
+    private func setupTimers() {
+        let theTimer = Timer(fire: Date(timeIntervalSinceNow: updateInterval),
+                             interval: updateInterval,
+                             repeats: true) { [weak self] timer in
+            guard let me = self else {
+                // can happen, e.g. owning VC goes out of view/scope
+                return
+            }
+            let logString = Log.shared.getLatestLogString()
+            me.delegate?.updateLogContents(logString: logString)
+        }
+        timer = theTimer
+    }
+
+    deinit {
+        timer?.invalidate()
+    }
 }
