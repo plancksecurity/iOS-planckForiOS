ada30df830369855b123ab678e3979d34732b0c7
Dirk Zimmermann <dz@pep.security>
2020-10-07 16:04:10 +0200

    IOS-2457 LoggingViewController

---
+++ pEpForiOS/UI/Settings/Setting/Logging/LoggingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoggingViewController.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 07.10.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class LoggingViewController: UIViewController {
+}
