54fb68b777554760c6a26a8cea6a6f6fdbe8f5e8
Martin Brude <devnull@localhost>
2020-10-08 10:36:41 +0200

    IOS-2462 - address comments from code review

--- pEpForiOS/UI/Compose/Util/UIUtils+Compose.swift
+++ pEpForiOS/UI/Compose/Util/UIUtils+Compose.swift
@@ -25,45 +25,10 @@ extension UIUtils {
                     Log.shared.errorAndCrash("Missing required data")
                     return
             }
-            composeVc.viewModel = composeViewModel(with: mailto)
+            composeVc.viewModel = ComposeViewModel.from(mailTo: mailto)
             composeVc.appConfig = appConfig
             present(composeNavigationController: composeNavigationController)
         }
-    }
-
-    private static func composeViewModel(with mailTo: Mailto?) -> ComposeViewModel {
-        func identities(addresses: [String]) -> [Identity]? {
-            return addresses.map { return Identity(address: $0) }
-        }
-        var tos: [Identity]? = nil
-        if let mailtos = mailTo?.tos {
-            tos = identities(addresses: mailtos)
-        }
-        var ccs: [Identity]? = nil
-        if let mailccs = mailTo?.ccs {
-            ccs = identities(addresses: mailccs)
-        }
-        var bccs: [Identity]? = nil
-        if let mailbccs = mailTo?.bccs {
-            bccs = identities(addresses: mailbccs)
-        }
-        var initData = ComposeViewModel.InitData(prefilledTos: tos,
-                                                 prefilledCCs: ccs,
-                                                 prefilledBCCs: bccs)
-        if let body = mailTo?.body {
-            initData.bodyPlaintext = body
-        } else if let firstTo = mailTo?.tos?.first, firstTo == Constants.supportMail {
-            /// To give more precise information to Support, we inform the device and OS version.
-            let deviceField = NSLocalizedString("Device", comment: "Device field, reporting issue")
-            initData.bodyPlaintext = "\n\n\(deviceField): \(UIDevice().type.rawValue)" + "\n" + "OS: \(UIDevice.current.systemVersion)"
-            initData.subject = NSLocalizedString("Help", comment: "Contact Support - Mail subject")
-        }
-
-        let state = ComposeViewModel.ComposeViewModelState(initData: initData)
-        if let subject = mailTo?.subject {
-            state.subject = subject
-        }
-        return ComposeViewModel(state: state)
     }
 
     // MARK: - Private - Present
