9bfd89c7a9cd8eba330bf095a4824fc90fe7d8ce
Dirk Zimmermann <dz@pep.security>
2020-10-15 08:35:12 +0200

    IOS-2259 Newline

--- Submodules/MessageModel/MessageModel/MessageModel/Submodule/CoreData/Stack/Stack.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Submodule/CoreData/Stack/Stack.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreData
+
 import pEpIOSToolbox
 import pEp4iosIntern
 
@@ -169,55 +170,6 @@ extension Stack {
                                             at: url,
                                             options: options)
     }
-}
-
-// MARK: - Tear Down
-//!!!: move to test MM target somehow after pEp4IOS stopped using CD
-extension Stack {
-
-    /// Resets the Stack.
-    /// MUST be used in tests only.
-    /// - note: After calling this method, you MUST NOT use any privateConcurrentContext that has been created before calling this method.
-    func reset() {
-        objc_sync_enter(Stack.unitTestLock)
-        defer { objc_sync_exit(Stack.unitTestLock) }
-        guard MiscUtil.isUnitTest() else { fatalError("Not permited to use in production code.") }
-
-        // A `reset(context:)` can involve a merge, which can crash a test.
-        // Not wanted.
-        stopReceivingContextNotifications()
-
-        mainContext.hasBeenReset = true
-        changePropagatorContext.hasBeenReset = true
-
-        reset(context: mainContext)
-        reset(context: changePropagatorContext)
-
-        do {
-            try loadCoreDataStack(storeType: NSInMemoryStoreType)
-        } catch {
-            fatalError("No Stack, no running app, sorry.")
-        }
-    }
-
-    private func reset(context: NSManagedObjectContext) {
-        for store in context.persistentStoreCoordinator?.persistentStores ?? [] {
-            do {
-                try context.persistentStoreCoordinator?.remove(store)
-            } catch {
-                Log.shared.errorAndCrash(error: error)
-            }
-        }
-        context.performAndWait {
-            context.reset()
-        }
-    }
-}
-
-extension Stack {
-
-    /// Lock to synchronize reset() calls (in unit tests)
-    static let unitTestLock = NSObject()
 }
 
 // MARK: - Merge Contexts
