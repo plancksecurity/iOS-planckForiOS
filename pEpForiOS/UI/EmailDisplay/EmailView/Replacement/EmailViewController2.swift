234a73f644e9ff618d72c684038029e79cc04bae
Martin Brude <devnull@localhost>
2020-10-19 11:43:03 +0200

    IOS-2235 - add files

---
+++ pEpForiOS/UI/EmailDisplay/EmailView/Replacement/EmailViewController2.swift
@@ -0,0 +1,14 @@
+//
+//  EmailViewController2.swift
+//  pEp
+//
+//  Created by Martín Brude on 19/10/2020.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class EmailViewController2: UIViewController {
+    var viewModel: EmailViewModel?
+
+}
---
+++ pEpForiOS/UI/EmailDisplay/EmailView/Replacement/EmailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EmailViewModel.swift
+//  pEp
+//
+//  Created by Martín Brude on 19/10/2020.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+struct EmailViewModel {
+    
+}
