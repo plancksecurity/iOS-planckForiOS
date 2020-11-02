898e7b498902b30e63f41f6966c347d87a5bbf42
Martin Brude <devnull@localhost>
2020-11-02 11:54:29 +0100

    IOS-2519 - prevent wrong UI when rotating certain devices in iOS 12 on account type selection or login view

--- Submodules/pEpIOSToolbox/pEpIOSToolbox/UIKit/UiSplitViewController+DisplayMode.swift
+++ Submodules/pEpIOSToolbox/pEpIOSToolbox/UIKit/UiSplitViewController+DisplayMode.swift
@@ -22,28 +22,40 @@ public extension UISplitViewController {
         case onlyDetail
     }
 
-    /// value that represent tha actual display mode.
     var currentDisplayMode : SplitViewDisplayMode {
         get {
-            if isCollapsed {
+            guard #available(iOS 13.0, *) else {
+                /// Works around a UI glitch:
+                /// displayMode in iOS 12 does not updates in certain cases.
+                /// As Apple doc says: 'Collapsing happens when the split view controller transitions from a horizontally regular to a horizontally compact environment'
+                /// In Plus size devices (iPhone 6 Plus, 7 Plus, 8 Plus, iPhone XS Max, iPhone XR) with IOS 12 the isCollapsed property is not updated when rotating from horizontally regular to horizontally compact environment, which leads UI issues that makes the app un-usable.
+                /// For more information, please go to https://pep.foundation/jira/browse/IOS-2519.
+                return  isIphone && isPortrait ? .onlyMaster : getDisplayMode()
+            }
+            return getDisplayMode()
+        }
+    }
+
+    /// value that represent tha actual display mode.
+    private func getDisplayMode() -> UISplitViewController.SplitViewDisplayMode {
+        if isCollapsed {
+            return .onlyMaster
+        } else {
+            switch displayMode {
+            case .automatic:
+                //this case is never posible as splitviewcontroller.displaymode never will return that
                 return .onlyMaster
-            } else {
-                switch displayMode {
-                case .automatic:
-                    //this case is never posible as splitviewcontroller.displaymode never will return that
-                    return .onlyMaster
-                case .secondaryOnly:
-                    return .onlyDetail
-                case .oneOverSecondary,
-                     .oneBesideSecondary,
-                     .twoOverSecondary,
-                     .twoDisplaceSecondary,
-                     .twoBesideSecondary:
-                    return .masterAndDetail
-                @unknown default:
-                    //this case is because apple do not assures other posible cases for displayMode.
-                    return .onlyMaster
-                }
+            case .secondaryOnly:
+                return .onlyDetail
+            case .oneOverSecondary,
+                 .oneBesideSecondary,
+                 .twoOverSecondary,
+                 .twoDisplaceSecondary,
+                 .twoBesideSecondary:
+                return .masterAndDetail
+            @unknown default:
+                //this case is because apple do not assures other posible cases for displayMode.
+                return .onlyMaster
             }
         }
     }
