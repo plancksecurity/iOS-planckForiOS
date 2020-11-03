1d6f75bf8a844572110eca82a8fd528eca8191fa
Dirk Zimmermann <dz@pep.security>
2020-11-03 10:41:38 +0100

    IOS-1672 Add lumberjack license

--- pEpForiOS/UI/Settings/Setting/Credits/CreditsWebViewController.swift
+++ pEpForiOS/UI/Settings/Setting/Credits/CreditsWebViewController.swift
@@ -57,6 +57,10 @@ class CreditsWebViewController: PEPWebViewController {
         \(eula())
         </p>
 
+        <p>&nbsp;</p>
+        <p>
+        \(licenseLumberjack())
+        </p>
         </blockquote>
         </body>
         </html>
@@ -154,6 +158,23 @@ class CreditsWebViewController: PEPWebViewController {
     """
         return eula
     }
+
+    private func licenseLumberjack() -> String {
+        let license = p("BSD 3-Clause License") +
+            p("Copyright (c) 2010-2020, Deusty, LLC<br>All rights reserved.") +
+            p("Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:") +
+            p("1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.") +
+            p("2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.") +
+            p("3. Neither the name of Deusty nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission of Deusty, LLC.") +
+            p("THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.")
+        return license
+    }
+
+    private func p(_ text: String) -> String {
+        return """
+            <p style="margin-bottom: 0.14in; line-height: 100%;" align="center"><span style="color: #000000;"><span style="font-family: Times New Roman, serif;"><span style="font-size: x-large;"><span lang="en">
+\(text)
+</p>
+"""
+    }
 }
-
-
