526b5d91a19649398cce2641370919b8a01c770a
Martin Brude <devnull@localhost>
2020-11-13 15:14:55 +0100

    IOS-2481 - improve comment to explain fix

--- pEpForiOS/UI/Folder/FolderTableViewController.swift
+++ pEpForiOS/UI/Folder/FolderTableViewController.swift
@@ -275,7 +275,10 @@ extension FolderTableViewController: SegueHandlerType {
     }
 
     private func updateRefreshControl() {
-        /// Works around a layout issue that stucks refresh control.
+        /// This fixes a UI glitch.
+        /// The refresh control gets stucked when a view controller is pushed over the current one and dismissed.
+        /// This works around that issue. If the refresh control is refreshing, make it spin again.
+        /// If not, it is already hidden, so nothing to do.
         if refreshControl?.isRefreshing ?? false {
             refreshControl?.endRefreshing()
             refreshControl?.beginRefreshing()
