272ba85f8195f3716074bc0c0f05504ebf29fc21
Dirk Zimmermann <dz@pep.security>
2020-11-16 17:01:40 +0100

    IOS-1557 Move to interface

--- Submodules/MessageModel/MessageModel/MessageModel/Services/NetworkService/Util/FolderType+IMAP.swift
+++ Submodules/MessageModel/MessageModel/MessageModel/Services/NetworkService/Util/FolderType+IMAP.swift
@@ -29,8 +29,6 @@ extension FolderType {
         return nil
     }
 
-    //!!!: make all the below internal 
-
     // MARK: - Required IMAP folders
 
     /// FolderType`s that should be created if they don't yet exist.
@@ -66,13 +64,6 @@ extension FolderType {
             // should not be shown to the user, has a fixed name
             return []
         }
-    }
-
-    /// Each kind has a human-readable name you can use to create a local folder object.
-    /// - Note: This is used *only* for the local-only special folders, and for fuzzy-matching
-    ///         against a known folder type when fetching from the remote server.
-    public func folderName() -> String {
-        return folderNames()[0]
     }
 
     // MARK: - PantomimeSpecialUseMailboxType <-> FolderType
