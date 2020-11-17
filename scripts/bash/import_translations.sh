b59a901608bc35f6856fde0db64215d1886bef81
Dirk Zimmermann <dz@pep.security>
2020-11-17 16:49:02 +0100

    IOS-2536 Can do the import directly

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -4,22 +4,6 @@ languages="en de fr es ca tr nl sk"
 
 translationdir=../pEp-Translate/
 
-# Imports translations, using a temporary directory in order to not change
-# original files.
-# Param 1: The language to import
-# Param 2: The project (pEpForiOS.xcodeproj)
-# Param 3: The source directory ($translationdir)
-function import() {
-    echo \*\*\* import $1 $2 $3
-    mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
-
-    cp $3/$1.xliff $mytmpdir
-
-    import_in_place $1 $2 $mytmpdir
-
-    rm -fr $mytmpdir
-}
-
 # Imports translations directly from a given directory, maybe changing the originals.
 # Param 1: The language to import
 # Param 2: The project (pEpForiOS.xcodeproj)
@@ -37,5 +21,6 @@ do
 done
 
 echo
+echo \*\*\* Make sure $translationdir doesn not have changes \*\*\*
 echo \*\*\* Verify the changes in pEpForiOS and commit \*\*\*
 echo
