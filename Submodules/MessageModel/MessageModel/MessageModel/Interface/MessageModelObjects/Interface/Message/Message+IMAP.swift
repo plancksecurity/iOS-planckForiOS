a0b10852d5a9b26377546a19f336b911ad6b7160
Dirk Zimmermann <dz@pep.security>
2020-11-17 11:54:35 +0100

    IOS-1557 Placeholder for public IMAP message handling

---
+++ Submodules/MessageModel/MessageModel/MessageModel/Interface/MessageModelObjects/Interface/Message/Message+IMAP.swift
@@ -0,0 +1,12 @@
+//
+//  Message+IMAP.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 17.11.20.
+//  Copyright © 2020 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+extension Message {
+}
