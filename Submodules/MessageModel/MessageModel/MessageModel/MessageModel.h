43012e9247ebe24ebd1124817855149e8bfc9a83
Dirk Zimmermann <dz@pep.security>
2020-11-26 17:05:46 +0100

    IOSAD-185 Putting deps here must be wrong

--- Submodules/MessageModel/MessageModel/MessageModel/MessageModel.h
+++ Submodules/MessageModel/MessageModel/MessageModel/MessageModel.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double MessageModelVersionNumber;
 FOUNDATION_EXPORT const unsigned char MessageModelVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MessageModel/PublicHeader.h>
-
-#import "PEPSession.h"
