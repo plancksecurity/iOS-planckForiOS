14b879d7d2befbc8e174e345a6c7e92cb386429a
Dirk Zimmermann <dz@pep.security>
2020-11-26 17:05:46 +0100

    IOSAD-185 Add MM bridging header

---
+++ Submodules/MessageModel/MessageModel/MessageModel/MessageModel-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MessageModel-Bridging-Header.h
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 26.11.20.
+//  Copyright © 2020 pEp Security S.A. All rights reserved.
+//
+
+#ifndef MessageModel_Bridging_Header_h
+#define MessageModel_Bridging_Header_h
+
+
+#endif /* MessageModel_Bridging_Header_h */
