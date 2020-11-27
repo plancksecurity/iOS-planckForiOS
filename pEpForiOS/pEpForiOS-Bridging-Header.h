27da08a7b15d1ce9a36761c4b9441888fa03e858
Dirk Zimmermann <dz@pep.security>
2020-11-27 18:19:28 +0100

    IOSAD-185 Remove unneeded imports

--- pEpForiOS/pEpForiOS-Bridging-Header.h
+++ pEpForiOS/pEpForiOS-Bridging-Header.h
@@ -9,23 +9,10 @@
 #ifndef pEpForiOS_Bridging_Header_h
 #define pEpForiOS_Bridging_Header_h
 
-#pragma mark - pEp AccountSettings Headers
-
-#import "AccountSettings.h"
-#import "AccountSettingsServer.h"
-
 #pragma mark - HTML/Markdown
 
 #import "Axt.h"
 #import "cmark.h"
 #import "NSString+Markdown.h"
-
-#pragma mark - AppAuth
-
-#import "AppAuth/AppAuth.h"
-
-#pragma mark - Apple System Log facility
-
-#import <asl.h>
 
 #endif /* pEpForiOS_Bridging_Header_h */
