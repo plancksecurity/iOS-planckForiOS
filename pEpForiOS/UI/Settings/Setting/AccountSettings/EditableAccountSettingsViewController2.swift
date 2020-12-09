30cde839d2234a6de8512dead84d56ccd108b499
Martin Brude <devnull@localhost>
2020-12-09 10:30:31 +0100

    IOS-2562 - start table view implementation

--- pEpForiOS/UI/Settings/Setting/AccountSettings/EditableAccountSettingsViewController2.swift
+++ pEpForiOS/UI/Settings/Setting/AccountSettings/EditableAccountSettingsViewController2.swift
@@ -7,29 +7,100 @@
 //
 
 import UIKit
+import pEpIOSToolbox
 
 class EditableAccountSettingsViewController2: UIViewController {
 
+    override var collapsedBehavior: CollapsedSplitViewBehavior {
+        return .needed
+    }
+
+    override var separatedBehavior: SeparatedSplitViewBehavior {
+        return .detail
+    }
+
     var viewModel : EditableAccountSettingsViewModel2?
+    @IBOutlet private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.register(PEPHeaderView.self, forHeaderFooterViewReuseIdentifier: PEPHeaderView.reuseIdentifier)
+        tableView.hideSeperatorForEmptyCells()
+        UIHelper.variableContentHeight(tableView)
+
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismissYourself()
+    }
 }
+
+// MARK: - UITableViewDataSource
+
+extension EditableAccountSettingsViewController2: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let vm = viewModel else {
+            Log.shared.errorAndCrash("VM not found")
+            return 0
+        }
+
+        return vm.sections[section].rows.count
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        guard let vm = viewModel else {
+            Log.shared.errorAndCrash("VM not found")
+            return 0
+        }
+        return vm.sections.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let vm = viewModel else {
+            Log.shared.errorAndCrash("VM not found")
+            return UITableViewCell()
+        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AccountSettingsTableViewCell.identifier) as? AccountSettingsTableViewCell else {
+            return UITableViewCell()
+        }
+        guard let row = vm.sections[indexPath.section].rows[indexPath.row] as? AccountSettingsViewModel.DisplayRow else {
+            Log.shared.errorAndCrash("Can't dequeue row")
+            return cell
+        }
+        cell.configure(with: row, for: traitCollection)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension EditableAccountSettingsViewController2: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: PEPHeaderView.reuseIdentifier) as? PEPHeaderView else {
+            Log.shared.errorAndCrash("pEpHeaderView doesn't exist!")
+            return nil
+        }
+        guard let vm = viewModel else {
+            Log.shared.errorAndCrash("VM not found")
+            return nil
+        }
+        headerView.title = vm.sections[section].title.uppercased()
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+// MARK: - EditableAccountSettingsDelegate2
 
 extension EditableAccountSettingsViewController2: EditableAccountSettingsDelegate2 {
     func setLoadingView(visible: Bool) {
@@ -45,6 +116,18 @@ extension EditableAccountSettingsViewController2: EditableAccountSettingsDelegat
     }
 
     func dismissYourself() {
-        dismiss(animated: true)
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Accessibility
+
+extension EditableAccountSettingsViewController2 {
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if previousTraitCollection?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
+            tableView.reloadData()
+        }
     }
 }
