3012001be466616f09eac3ee4af58f7f03bbc427
Martin Brude <devnull@localhost>
2021-01-12 15:40:06 +0100

    IOS-2571 - text can't be selected

---
+++ pEpForiOS/UI/Util/TextFieldWithoutSelection.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldWithoutSelection.swift
+//  pEp
+//
+//  Created by Martín Brude on 12/1/21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// UITextView property 'isSelectable', when it's false, doesn't allow the user to tap links.
+/// This allows the interaction with the link and disable the selection.
+class TextFieldWithoutSelection: UITextView {
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        guard let pos = closestPosition(to: point) else { return false }
+        guard let range = tokenizer.rangeEnclosingPosition(pos, with: .character, inDirection: .layout(.left)) else { return false }
+        let startIndex = offset(from: beginningOfDocument, to: range.start)
+        return attributedText.attribute(.link, at: startIndex, effectiveRange: nil) != nil
+    }
+}
