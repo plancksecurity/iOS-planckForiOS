abe76bf584d385f8bd95c79558770f30d0589442
Dirk Zimmermann <dz@pep.security>
2021-01-14 12:28:57 +0100

    IOS-2524 Comment

--- scripts/bash/import_translations.sh
+++ scripts/bash/import_translations.sh
@@ -7,7 +7,8 @@ translationdir=../pEp-Translate/
 # Imports translations, using a temporary directory in order to not change
 # original files.
 # Param 1: The language to import
-# Param 2: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
+# Param 2: The project (pEpForiOS.xcodeproj or
+#          ./SubProjects/MessageModel/MessageModel/MessageModel.xcodeproj
 # Param 3: The source directory ($translationdir or $translationdir/MessageModel)
 function import() {
     echo \*\*\* import $1 $2 $3
@@ -22,7 +23,8 @@ function import() {
 
 # Imports translations directly from a given directory, maybe changing the originals.
 # Param 1: The language to import
-# Param 2: The project (pEpForiOS.xcodeproj or MessageModel/MessageModel.xcodeproj)
+# Param 2: The project (pEpForiOS.xcodeproj or
+#          ./SubProjects/MessageModel/MessageModel/MessageModel.xcodeproj
 # Param 3: The source directory
 function import_in_place() {
     filename=$3/$1.xliff
