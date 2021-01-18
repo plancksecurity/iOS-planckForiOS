f3ca7760e4860a5d5df8746ae84c4b34ec207448
Dirk Zimmermann <dz@pep.security>
2021-01-18 17:37:42 +0100

    Remove MM from translation export

--- scripts/bash/export_translations.sh
+++ scripts/bash/export_translations.sh
@@ -15,10 +15,6 @@ do
   xcodebuild -exportLocalizations -project pEpForiOS.xcodeproj -exportLanguage $lang -localizationPath $appdir
 
   cp "$appdir/$lang.xcloc/Localized Contents/$lang.xliff" $translationdir
-
-  xcodebuild -exportLocalizations -project ./SubProjects/MessageModel/MessageModel/MessageModel.xcodeproj -exportLanguage $lang -localizationPath $mmdir
-
-  cp "$mmdir/$lang.xcloc/Localized Contents/$lang.xliff" $translationdir/MessageModel
 done
 
 rm -fr $mytmpdir
