382ad7fc8ab8de015f708907d4b7ad04e0f9b4f3
Dirk Zimmermann <dz@pep.security>
2021-01-19 12:16:12 +0100

    IOS-2295 Add an empty bridging header, MM works?

---
+++ pEp-share/pEp-share-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  pEp-share-Bridging-Header.h
+//  pEp-share
+//
+//  Created by Dirk Zimmermann on 19.01.21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+#ifndef pEp_share_Bridging_Header_h
+#define pEp_share_Bridging_Header_h
+
+
+#endif /* pEp_share_Bridging_Header_h */
