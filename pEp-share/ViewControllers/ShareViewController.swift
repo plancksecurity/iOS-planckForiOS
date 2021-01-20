5a51f45ffe005b6717d5dc1e62a030c9c895ae1b
Dirk Zimmermann <dz@pep.security>
2021-01-20 10:41:53 +0100

    IOS-2295 Don't even try to send the mail

--- pEp-share/ViewControllers/ShareViewController.swift
+++ pEp-share/ViewControllers/ShareViewController.swift
@@ -40,8 +40,9 @@ final class ShareViewController: UIViewController {
 extension ShareViewController {
     private func checkInputItems() {
 
-        let sendMail = SendMailHelper.shared
-        sendMail.sendMessage()
+        // TODO: Send mail
+        //let sendMail = SendMailHelper.shared
+        //sendMail.sendMessage()
 
         guard let context = extensionContext else {
             Log.shared.errorAndCrash(message: "Lost extension context!")
