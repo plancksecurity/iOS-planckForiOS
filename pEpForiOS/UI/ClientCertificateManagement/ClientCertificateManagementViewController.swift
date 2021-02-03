c6a291d95319d15be753639dfef42470cc33f0c5
Dirk Zimmermann <dz@pep.security>
2021-02-03 15:27:43 +0100

    IOS-2620 Limit the file browser to certificates

--- pEpForiOS/UI/ClientCertificateManagement/ClientCertificateManagementViewController.swift
+++ pEpForiOS/UI/ClientCertificateManagement/ClientCertificateManagementViewController.swift
@@ -53,7 +53,7 @@ final class ClientCertificateManagementViewController: UIViewController {
     }
 
     @IBAction func addCertificateButtonPressed(_ sender: Any) {
-        let picker = UIDocumentPickerViewController(documentTypes: ["public.data"], in: .import)
+        let picker = UIDocumentPickerViewController(documentTypes: ["security.pep.certificates.client"], in: .import)
         picker.delegate = self
         picker.modalPresentationStyle = .fullScreen
         present(picker, animated: true)
