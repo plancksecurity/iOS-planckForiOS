ca2fa929e4d65c7d96c276356bdd680e0ad489d4
Dirk Zimmermann <dz@pep.security>
2021-02-05 11:20:46 +0100

    IOS-2633 Better "resize" on orientation changes

--- pEpForiOS/UI/Settings/Setting/Credits/CreditsWebViewController.swift
+++ pEpForiOS/UI/Settings/Setting/Credits/CreditsWebViewController.swift
@@ -36,7 +36,7 @@ class CreditsWebViewController: PEPWebViewController {
         <html>
          <head>
            \(style)
-           <meta name="viewport" content="initial-scale=1.0">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
            <meta charset="utf-8"/>
         </head>
         <body>
