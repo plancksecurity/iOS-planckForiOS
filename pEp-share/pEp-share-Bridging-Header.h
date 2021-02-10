e9179073f39c3fa6831b005d402905cbb4070b06
Dirk Zimmermann <dz@pep.security>
2021-02-10 12:37:17 +0100

    IOS-2295 Add pEp-share bridging header

---
+++ pEp-share/pEp-share-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  pEp-share-Bridging-Header.h
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 10.02.21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+#ifndef pEp_share_Bridging_Header_h
+#define pEp_share_Bridging_Header_h
+
+
+#endif /* pEp_share_Bridging_Header_h */
