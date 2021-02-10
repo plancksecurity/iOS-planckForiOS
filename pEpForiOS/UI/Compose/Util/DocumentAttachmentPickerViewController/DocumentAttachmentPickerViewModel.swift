0ec100bdd63befcce3f8f35d8a81ee077ecd7f61
Dirk Zimmermann <dz@pep.security>
2021-02-10 10:41:14 +0100

    IOS-2295 Fix import

--- pEpForiOS/UI/Compose/Util/DocumentAttachmentPickerViewController/DocumentAttachmentPickerViewModel.swift
+++ pEpForiOS/UI/Compose/Util/DocumentAttachmentPickerViewController/DocumentAttachmentPickerViewModel.swift
@@ -6,8 +6,17 @@
 //  Copyright © 2018 p≡p Security S.A. All rights reserved.
 //
 
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
 import MessageModel
-import pEpIOSToolbox
+#endif
+
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
+import MessageModel
+#endif
 
 protocol DocumentAttachmentPickerViewModelResultDelegate: class {
     func documentAttachmentPickerViewModel(_ vm: DocumentAttachmentPickerViewModel,
