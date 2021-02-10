34cbefb427415e4b0ed3bd17bc6d33110f3b30ba
Dirk Zimmermann <dz@pep.security>
2021-02-10 15:41:26 +0100

    IOS-2295 Fix imports

--- pEpForiOS/Models/Attachment+Extensions.swift
+++ pEpForiOS/Models/Attachment+Extensions.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2018 p≡p Security S.A. All rights reserved.
 //
 
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
 import MessageModel
+#endif
 
 extension Attachment {
 
