b80d2ccb6f077fec604b78ca3f612d068eee0b15
Dirk Zimmermann <dz@pep.security>
2021-02-10 10:49:27 +0100

    IOS-2295 Fix import

--- pEpForiOS/UI/Util/Color+UI.swift
+++ pEpForiOS/UI/Util/Color+UI.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
 import MessageModel
+#endif
 
 extension Color {
     /// The icon suitable for indicating the pEp rating of a message.
