16a7177403fd2607535051f767b31e8fbf005919
Dirk Zimmermann <dz@pep.security>
2021-02-10 13:46:39 +0100

    IOS-2295 Separate UIUtils+ShowError

--- pEpForiOS/UI/Util/UIUtils/UIUtils.swift
+++ pEpForiOS/UI/Util/UIUtils/UIUtils.swift
@@ -22,42 +22,6 @@ import pEpIOSToolbox
 #endif
 
 class UIUtils {
-
-    /// Converts the error to a user frienldy DisplayUserError and presents it to the user
-    ///
-    /// - Parameters:
-    ///   - error: error to preset to user
-    static public func show(error: Error) {
-        let workBlock = {
-            // Do not show alerts when app is in background.
-            if UIApplication.shared.applicationState != .active {
-                #if DEBUG
-                // show alert in background when in debug.
-                #else
-                return
-                #endif
-            }
-
-            Log.shared.info("May or may not display error to user: (interpolate) %@", "\(error)")
-
-            guard let displayError = DisplayUserError(withError: error) else {
-                // Do nothing. The error type is not suitable to bother the user with.
-                return
-            }
-            DispatchQueue.main.async {
-                showAlertWithOnlyPositiveButton(title: displayError.title, message: displayError.errorDescription)
-            }
-        }
-
-        if Thread.current == Thread.main {
-            workBlock()
-        } else {
-            DispatchQueue.main.async {
-                workBlock()
-            }
-        }
-    }
-
     /// Shows the navigation controller passed by parameter
     /// - Parameter navigationController: The Navigation Controller to present.
     public static func show(navigationController: UINavigationController) {
