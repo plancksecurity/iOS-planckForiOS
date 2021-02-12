37d42cf972ca08a6286a22ecfe689e9764847be5
Martin <martin.brude@gmail.com>
2021-02-12 11:17:38 +0100

    IOS-2377 - disable pepsync before deleting accounts

--- pEpForiOS/UI/Settings/SettingsViewModel.swift
+++ pEpForiOS/UI/Settings/SettingsViewModel.swift
@@ -463,6 +463,14 @@ extension SettingsViewModel {
     /// It also updates the default account if necessary.
     /// - Parameter account: The account to be deleted
     private func delete(account: Account) {
+        account.setKeySyncEnabled(enable: false,
+                                  errorCallback: { error in
+                                    if !MiscUtil.isUnitTest() {
+                                        Log.shared.errorAndCrash("Can't disable key sync")
+                                    }
+                                  }, successCallback: {
+                                    //All good, nothing to do.
+                                  })
         let oldAddress = account.user.address
         account.delete()
         Session.main.commit()
