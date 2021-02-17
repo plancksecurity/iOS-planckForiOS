27d2365a0ad2af06ca2f28b6ddcf2c2eda448255
Dirk Zimmermann <dz@pep.security>
2021-02-17 14:39:09 +0000

    IOS-2295 Fix import(s)

--- SubProjects/MessageModel/MessageModel/MessageModel/Util/Internal/KeyGeneratorService.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Util/Internal/KeyGeneratorService.swift
@@ -11,7 +11,12 @@ import CoreData
 
 import PEPObjCAdapterTypes_iOS
 import PEPObjCAdapter_iOS
+
+#if EXT_SHARE
+import PEPIOSToolboxForAppExtensions
+#else
 import pEpIOSToolbox
+#endif
 
 struct KeyGeneratorService {
 
