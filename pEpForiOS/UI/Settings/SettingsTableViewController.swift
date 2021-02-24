75d20134553d045db9ef2572759ad3b9137bad9f
Dirk Zimmermann <dz@pep.security>
2021-02-24 13:27:28 +0100

    IOS-2295 Fix import(s)

--- pEpForiOS/UI/Settings/SettingsTableViewController.swift
+++ pEpForiOS/UI/Settings/SettingsTableViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 import SwipeCellKit
+
+#if EXT_SHARE
+import PEPIOSToolboxForAppExtensions
+#else
 import pEpIOSToolbox
+#endif
 
 final class SettingsTableViewController: UITableViewController {
 
