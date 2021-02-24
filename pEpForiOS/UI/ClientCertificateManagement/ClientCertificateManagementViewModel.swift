c72f9e13908b7124bf53786fad51e0d31347aa9e
Dirk Zimmermann <dz@pep.security>
2021-02-24 16:08:10 +0100

    IOS-2295 Fix import(s)

--- pEpForiOS/UI/ClientCertificateManagement/ClientCertificateManagementViewModel.swift
+++ pEpForiOS/UI/ClientCertificateManagement/ClientCertificateManagementViewModel.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2020 p≡p Security S.A. All rights reserved.
 //
 
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
 import MessageModel
+#endif
 
 enum ClientCertificateAction {
     case newAccount
