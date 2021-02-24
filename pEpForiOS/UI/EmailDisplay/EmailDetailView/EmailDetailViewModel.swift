d54244a3b29cbd0259dfe898e5eb0fc7436e7a7d
Dirk Zimmermann <dz@pep.security>
2021-02-24 11:20:58 +0100

    IOS-2295 Fix import(s)

--- pEpForiOS/UI/EmailDisplay/EmailDetailView/EmailDetailViewModel.swift
+++ pEpForiOS/UI/EmailDisplay/EmailDetailView/EmailDetailViewModel.swift
@@ -6,8 +6,17 @@
 //  Copyright © 2019 p≡p Security S.A. All rights reserved.
 //
 
+#if EXT_SHARE
+import MessageModelForAppExtensions
+#else
 import MessageModel
+#endif
+
+#if EXT_SHARE
+import PEPIOSToolboxForAppExtensions
+#else
 import pEpIOSToolbox
+#endif
 
 protocol EmailDetailViewModelDelegate: EmailDisplayViewModelDelegate {
 
