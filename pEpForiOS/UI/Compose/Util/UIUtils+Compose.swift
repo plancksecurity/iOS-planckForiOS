4b79475e33d6ba1812617569e943b3f63dae74c2
Dirk Zimmermann <dz@pep.security>
2021-02-25 10:37:40 +0100

    IOS-2295 Revert "UIUtils+DraftsPreview"
    This reverts commit 7dbd97c428169605de95a55a184f5b902763a6db.

--- pEpForiOS/UI/Compose/Util/UIUtils+Compose.swift
+++ pEpForiOS/UI/Compose/Util/UIUtils+Compose.swift
@@ -34,4 +34,33 @@ extension UIUtils {
             UIUtils.show(navigationController: composeNavigationController)
         }
     }
+    
+    /// Modally presents a "Drafts Preview"
+    static public func presentDraftsPreview() {
+        let sb = UIStoryboard(name: Constants.reusableStoryboard, bundle: nil)
+        guard let vc = sb.instantiateViewController(withIdentifier: EmailListViewController.storyboardId) as? EmailListViewController else {
+            Log.shared.errorAndCrash("EmailListViewController needed to presentDraftsPreview is not available!")
+                return
+        }
+        let emailListVM = EmailListViewModel(delegate: vc, folderToShow: UnifiedDraft())
+        vc.viewModel = emailListVM
+        vc.hidesBottomBarWhenPushed = false
+        vc.modalPresentationStyle = .pageSheet
+        vc.modalTransitionStyle = .coverVertical
+        let navigationController =
+            UINavigationController(rootViewController: vc)
+        if let toolbar = navigationController.toolbar {
+            vc.modalPresentationStyle = .popover
+            vc.preferredContentSize = CGSize(width: toolbar.frame.width
+                                                - 16,
+                                             height: 420)
+            vc.popoverPresentationController?.sourceView = vc.view
+            let frame = CGRect(x: toolbar.frame.origin.x,
+                               y: toolbar.frame.origin.y - 10,
+                               width: toolbar.frame.width,
+                               height: toolbar.frame.height)
+            vc.popoverPresentationController?.sourceRect = frame
+        }
+        UIUtils.show(navigationController: navigationController)
+    }
 }
