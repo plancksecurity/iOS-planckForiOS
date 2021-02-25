7dbd97c428169605de95a55a184f5b902763a6db
Dirk Zimmermann <dz@pep.security>
2021-02-25 10:23:26 +0100

    IOS-2295 UIUtils+DraftsPreview

---
+++ pEpForiOS/UI/Compose/Util/UIUtils+DraftsPreview.swift
@@ -0,0 +1,41 @@
+//
+//  UIUtils+DraftsPreview.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 25.02.21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIUtils {
+    /// Modally presents a "Drafts Preview"
+    static public func presentDraftsPreview() {
+        let sb = UIStoryboard(name: Constants.reusableStoryboard, bundle: nil)
+        guard let vc = sb.instantiateViewController(withIdentifier: EmailListViewController.storyboardId) as? EmailListViewController else {
+            Log.shared.errorAndCrash("EmailListViewController needed to presentDraftsPreview is not available!")
+                return
+        }
+        let emailListVM = EmailListViewModel(delegate: vc, folderToShow: UnifiedDraft())
+        vc.viewModel = emailListVM
+        vc.hidesBottomBarWhenPushed = false
+        vc.modalPresentationStyle = .pageSheet
+        vc.modalTransitionStyle = .coverVertical
+        let navigationController =
+            UINavigationController(rootViewController: vc)
+        if let toolbar = navigationController.toolbar {
+            vc.modalPresentationStyle = .popover
+            vc.preferredContentSize = CGSize(width: toolbar.frame.width
+                                                - 16,
+                                             height: 420)
+            vc.popoverPresentationController?.sourceView = vc.view
+            let frame = CGRect(x: toolbar.frame.origin.x,
+                               y: toolbar.frame.origin.y - 10,
+                               width: toolbar.frame.width,
+                               height: toolbar.frame.height)
+            vc.popoverPresentationController?.sourceRect = frame
+        }
+        UIUtils.show(navigationController: navigationController)
+    }
+}
