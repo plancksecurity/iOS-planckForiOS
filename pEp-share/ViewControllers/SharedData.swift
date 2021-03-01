8587fa9aaf22af3151bff409e4f87c0911d70048
Dirk Zimmermann <dz@pep.security>
2021-03-01 09:42:11 +0100

    IOS-2295 Own class SharedData

---
+++ pEp-share/ViewControllers/SharedData.swift
@@ -0,0 +1,14 @@
+//
+//  SharedData.swift
+//  pEp-share
+//
+//  Created by Dirk Zimmermann on 01.03.21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Stores data shared by the user, complete with types.
+class SharedData {
+
+}
