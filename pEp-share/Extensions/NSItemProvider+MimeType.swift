150ec5d1205dc69af30bbcc4f8d4cbb009c9fb47
Dirk Zimmermann <dz@pep.security>
2021-03-02 14:56:57 +0100

    IOS-2295 NSItemProvider+MimeType

---
+++ pEp-share/Extensions/NSItemProvider+MimeType.swift
@@ -0,0 +1,14 @@
+//
+//  NSItemProvider+MimeType.swift
+//  pEp-share
+//
+//  Created by Dirk Zimmermann on 02.03.21.
+//  Copyright © 2021 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+import MobileCoreServices
+
+extension NSItemProvider {
+    
+}
