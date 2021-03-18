fb007b6ee56ff6e31c0150645ae820aebee66d5e
Dirk Zimmermann <dz@pep.security>
2021-03-18 14:21:29 +0100

    IOS-2295 No requiresNetworkConnectivity

--- SubProjects/MessageModel/MessageModel/MessageModel/Interface/Sharing/EncryptAndSendSharing.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/Sharing/EncryptAndSendSharing.swift
@@ -90,7 +90,6 @@ public class EncryptAndSendSharing: EncryptAndSendSharingProtocol {
     @available(iOS 13.0, *)
     private func scheduleAppSend(completion: @escaping (Error?) -> ()) {
         let request = BGProcessingTaskRequest(identifier: backgroundTaskSend)
-        request.requiresNetworkConnectivity = true
 
         do {
             try BGTaskScheduler.shared.submit(request)
