63629efc89f45c5a687fb12d5e374ab14fcf0aa9
Dirk Zimmermann <dz@pep.security>
2021-03-22 10:31:06 +0100

    IOS-2295 More MigrateKeychainServiceTest skeleton

--- SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
@@ -8,26 +8,25 @@
 
 import XCTest
 
+@testable import MessageModel
+
 class MigrateKeychainServiceTest: XCTestCase {
-
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        setupKeychainItems()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testOperation() throws {
+        let expFinished = expectation(description: "expFinished")
+        let op = MigrateKeychainOperation()
+        op.completionBlock = {
+            expFinished.fulfill()
         }
+        op.start()
+        waitForExpectations(timeout: TestUtil.waitTime)
     }
 
+    // MARK: -- Private Helpers
+
+    private func setupKeychainItems() {
+    }
 }
