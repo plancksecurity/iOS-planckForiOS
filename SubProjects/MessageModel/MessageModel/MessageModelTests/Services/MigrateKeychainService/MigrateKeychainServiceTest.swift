43cf39765a2795fd39878c5ffb8c2886657f25cb
Dirk Zimmermann <dz@pep.security>
2021-03-22 10:47:02 +0100

    IOS-2295 Mock add keychain

--- SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
@@ -29,4 +29,20 @@ class MigrateKeychainServiceTest: XCTestCase {
 
     private func setupKeychainItems() {
     }
+
+    private func add(key: String, serverType: String = "Server", password: String) {
+        let query = [
+            kSecClass as String: kSecClassGenericPassword as String,
+            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly as String,
+            kSecAttrService as String: serverType,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: password.data(using: String.Encoding.utf8)!] as [String : Any]
+
+        SecItemDelete(query as CFDictionary)
+
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status != noErr {
+            XCTFail()
+        }
+    }
 }
