9005e194c3d1bb63f7e0ed5d7e07118fd3318276
Dirk Zimmermann <dz@pep.security>
2021-03-22 09:43:17 +0100

    IOS-2295 MigrateKeychainOperation skeleton

---
+++ SubProjects/MessageModel/MessageModel/MessageModel/Services/MigrateKeychainService/MigrateKeychainOperation.swift
@@ -0,0 +1,13 @@
+//
+//  MigrateKeychainOperation.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 22.03.21.
+//  Copyright © 2021 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class MigrateKeychainOperation: BaseOperation {
+    
+}
