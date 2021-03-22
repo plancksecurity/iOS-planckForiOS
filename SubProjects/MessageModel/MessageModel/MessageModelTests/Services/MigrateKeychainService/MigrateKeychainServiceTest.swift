698ef36cc75ae4663817a529ae9a42dedd17c279
Dirk Zimmermann <dz@pep.security>
2021-03-22 10:15:10 +0100

    IOS-2295 MigrateKeychainServiceTest skeleton

---
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
@@ -0,0 +1,33 @@
+//
+//  MigrateKeychainServiceTest.swift
+//  MessageModelTests
+//
+//  Created by Dirk Zimmermann on 22.03.21.
+//  Copyright © 2021 pEp Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class MigrateKeychainServiceTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
