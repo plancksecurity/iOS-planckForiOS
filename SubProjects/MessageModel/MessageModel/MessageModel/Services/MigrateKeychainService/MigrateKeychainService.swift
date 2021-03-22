ff15db2cf394bcf3a68b5c86cdb334202022f347
Dirk Zimmermann <dz@pep.security>
2021-03-22 12:06:43 +0100

    IOS-2295 Docs

--- SubProjects/MessageModel/MessageModel/MessageModel/Services/MigrateKeychainService/MigrateKeychainService.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Services/MigrateKeychainService/MigrateKeychainService.swift
@@ -10,9 +10,14 @@ import Foundation
 
 import pEp4iosIntern
 
+/// Service to migrate passwords, certificates from the default keychain
+/// to either the keychain group `kSharedKeychain` or another one,
+/// specified in the constructor.
 class MigrateKeychainService: OperationBasedService {
     let keychainGroupTarget: String
 
+    /// - parameter keychainGroupTarget: The name of the target keychain
+    /// (where to migrate to), `kSharedKeychain` by default.
     init(keychainGroupTarget: String = kSharedKeychain) {
         self.keychainGroupTarget = keychainGroupTarget
     }
