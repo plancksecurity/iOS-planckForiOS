944bec57b072cc455a553998c2d2e847a34a2fa7
Dirk Zimmermann <dz@pep.security>
2021-03-25 12:23:48 +0100

    IOS-2295 Comment constants

--- SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/MigrateKeychainService/MigrateKeychainServiceTest.swift
@@ -89,7 +89,10 @@ class MigrateKeychainServiceTest: XCTestCase {
 
     let numberOfKeyPasswordPairs = 50
 
-    let keychainTargetGroup = kTeamId + ".security.pep.test.keychain"
+    /// - Note: This must match a defined keychain ID in the app under test.
+    let keychainTargetGroupBase = "security.pep.test.keychain"
+
+    let keychainTargetGroup = kTeamId + "." + keychainTargetGroupBase
 
     private func key(index: Int) -> String {
         return "key_\(index)"
