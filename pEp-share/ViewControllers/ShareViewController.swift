671e6f1107ffdb84596dc6b71f5338f6cdc7be92
Dirk Zimmermann <dz@pep.security>
2021-03-29 09:43:40 +0200

    IOS-2295 Set presentationController?.delegate

--- pEp-share/ViewControllers/ShareViewController.swift
+++ pEp-share/ViewControllers/ShareViewController.swift
@@ -67,6 +67,7 @@ extension ShareViewController {
         }
 
         composeVC.viewModel = composeViewModel
+        presentationController?.delegate = self
 
         navigationController?.pushViewController(composeVC, animated: false)
     }
