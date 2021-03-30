9e423d895fdd4812b068661fe1cfbb8bfb37c730
Dirk Zimmermann <dz@pep.security>
2021-03-30 16:07:12 +0200

    IOS-2295 Fix import

--- pEpForiOS/UI/Util/DynamicHeightScrollView.swift
+++ pEpForiOS/UI/Util/DynamicHeightScrollView.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+#if EXT_SHARE
+import pEpIOSToolboxForExtensions
+#else
 import pEpIOSToolbox
+#endif
 
 protocol DynamicHeightScrollViewDelegate: class {
     /// Bottom constraint of the scroll view. Used to change the hegiht of the scrollView, modifying the constant
