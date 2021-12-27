084f2e68628bde968b253e81488cd78b8c75a7cc
Martin <martin.brude@gmail.com>
2021-12-27 11:52:35 +0100

    IOS-2909 - WIP

--- pEpForiOS/Util/OAuth2/OAuth2Type+Extension.swift
+++ pEpForiOS/Util/OAuth2/OAuth2Type+Extension.swift
@@ -13,15 +13,24 @@ extension OAuth2Type {
         switch self {
         case .google:
             return OAuth2Configuration(
-                oauth2Type: .google, scopes: ["https://mail.google.com/"],
+                oauth2Type: .google,
+                scopes: ["https://mail.google.com/"],
                 clientIDKey: "OAUTH2_GMAIL_CLIENT_ID",
                 redirectURLSchemeKey: "OAUTH2_GMAIL_REDIRECT_URL_SCHEME")
         case .yahoo:
             return OAuth2Configuration(
-                oauth2Type: .yahoo, scopes: ["openid"],
+                oauth2Type: .yahoo,
+                scopes: ["openid"],
                 clientIDKey: "OAUTH2_YAHOO_CLIENT_ID",
                 clientSecretKey: "OAUTH2_YAHOO_CLIENT_SECRET",
                 redirectURLKey: "OAUTH2_YAHOO_REDIRECT_URL")
+        case .o365:
+            return OAuth2Configuration(
+                oauth2Type: .o365,
+                scopes: ["https://outlook.office.com"],
+                clientIDKey: "OAUTH2_O365_CLIENT_ID",
+                clientSecretKey: "OAUTH2_O365_CLIENT_SECRET",
+                redirectURLKey: "OAUTH2_O365_REDIRECT_URL")
         }
     }
 
@@ -34,6 +43,10 @@ extension OAuth2Type {
         case .yahoo:
             return OIDServiceConfiguration(
                 authorizationEndpoint: URL(string: "https://api.login.yahoo.com/oauth2/request_auth")!,
+                tokenEndpoint: URL(string: "https://api.login.yahoo.com/oauth2/get_token")!)
+        case .o365:
+            return OIDServiceConfiguration(
+                authorizationEndpoint: URL(string: "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize")!,
                 tokenEndpoint: URL(string: "https://api.login.yahoo.com/oauth2/get_token")!)
         }
     }
