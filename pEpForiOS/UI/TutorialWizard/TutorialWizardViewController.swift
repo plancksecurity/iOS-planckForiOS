7193be707c9caff8b6af07d2164bcd80ae0252db
Dirk Zimmermann <dz@pep.security>
2022-05-12 13:30:44 +0200

    IOS-2965 Revert "Accessibility identifiers tutorial"
    This reverts commit a2b14978926e8f7cb2ca77d61c66f29a914d621d.

--- pEpForiOS/UI/TutorialWizard/TutorialWizardViewController.swift
+++ pEpForiOS/UI/TutorialWizard/TutorialWizardViewController.swift
@@ -106,21 +106,13 @@ extension TutorialWizardViewController {
     /// Updates the right bar button, regarding if it's the last element or not.
     /// - Parameter lastScreen: Indicates if it's the last screen.
     private func updateNavButton(lastScreen: Bool) {
-        var navBarButtonTitle: String? = nil
-        var accessibilityIdentifier: String? = nil
-
+        var navBarButtonTitle = ""
         if lastScreen {
             navBarButtonTitle = NSLocalizedString("Finish", comment: "Start up tutorial finish button")
-            accessibilityIdentifier = "Tutorial Finish"
         } else {
             navBarButtonTitle = NSLocalizedString("Skip", comment: "Start up tutorial skip button")
-            accessibilityIdentifier = "Tutorial Skip"
         }
-
         let endButton = UIBarButtonItem(title: navBarButtonTitle, style: .done, target: self, action: #selector(closeScreen))
-
-        endButton.accessibilityIdentifier = accessibilityIdentifier
-
         navigationItem.rightBarButtonItem = endButton
     }
 }
