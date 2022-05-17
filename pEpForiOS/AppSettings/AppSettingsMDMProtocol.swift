ab789127fedb6c3e14914d4c7b2350ece78fb2ba
Dirk Zimmermann <dz@pep.security>
2022-05-17 08:26:15 +0200

    IOS-2943 Start MDM settings protocol

---
+++ pEpForiOS/AppSettings/AppSettingsMDMProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  AppSettingsMDMProtocol.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 17.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// App settings that are relevant for MDM.
+public protocol AppSettingsMDMProtocol {
+}
