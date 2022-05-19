67770588171563119350baab7044bcf6e0f3227f
Dirk Zimmermann <dz@pep.security>
2022-05-19 09:47:21 +0200

    IOS-2943 Start MDMPredeployed

---
+++ pEpForiOS/AppSettings/MDM/MDMPredeployed.swift
@@ -0,0 +1,12 @@
+//
+//  MDMPredeployed.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 19.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class MDMPredeployed {
+}
