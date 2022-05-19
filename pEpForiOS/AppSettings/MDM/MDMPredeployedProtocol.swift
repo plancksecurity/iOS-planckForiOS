cb05591729cfde5d452f116bde34b6f9494bd12c
Dirk Zimmermann <dz@pep.security>
2022-05-19 09:14:20 +0200

    IOS-2943 Start MDMPredeployedProtocol

---
+++ pEpForiOS/AppSettings/MDM/MDMPredeployedProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MDMPredeployedProtocol.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 19.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+protocol MDMPredeployedProtocol {
+}
