289a9616cd270a59a9422fe550b980a5238c21e0
Dirk Zimmermann <dz@pep.security>
2022-05-23 14:18:21 +0200

    IOS-2943 Remove code that's never going to be needed

--- pEpForiOS/AppSettings/MDM/MDMPredeployed.swift
+++ pEpForiOS/AppSettings/MDM/MDMPredeployed.swift
@@ -13,29 +13,6 @@ import MessageModel
 class MDMPredeployed {
 }
 
-/// Represents an MDM-configured server, e.g. an IMAP server.
-struct MDMPredeployedServer {
-  let host: String
-  let port: UInt16
-}
-
-struct MDMPredeployedAccount {
-  /// The IMAP server for this account.
-  let imapServer: MDMPredeployedServer
-
-  /// The SMTP server for this account.
-  let smtpServer: MDMPredeployedServer
-
-  /// The _login_ user name (that is, for logging in to any of the servers).
-  let loginName: String
-
-  /// The real name of the user.
-  let userName: String
-
-  /// The password to be used for all accounts.
-  let password: String
-}
-
 // MARK: - Dictionary key constants
 
 extension MDMPredeployed {
