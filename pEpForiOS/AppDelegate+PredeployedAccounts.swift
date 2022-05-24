2e5f5c99f39f56a31adee6968919381ad95cae92
Dirk Zimmermann <dz@pep.security>
2022-05-24 10:07:08 +0200

    IOS-2943 TMP Temporary test code

---
+++ pEpForiOS/AppDelegate+PredeployedAccounts.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate+PredeployedAccounts.swift
+//  pEpForiOS
+//
+//  Created by Dirk Zimmermann on 24.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+extension AppDelegate {
+}
