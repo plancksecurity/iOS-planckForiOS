0729030a292c04c1e773c548667fd48efc69c3fc
Dirk Zimmermann <dz@pep.security>
2022-05-24 10:20:03 +0200

    IOS-2943 TMP predeployAccount

--- pEpForiOS/AppDelegate+PredeployedAccounts.swift
+++ pEpForiOS/AppDelegate+PredeployedAccounts.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
+private typealias SettingsDict = [String:Any]
+
 extension AppDelegate {
+    /// Temporary test function for adding MDM account data.
+    public func predeployAccount(userName: String,
+                                 userAddress: String,
+                                 loginName: String,
+                                 password: String,
+                                 imapServerName: String,
+                                 imapServerPort: Int,
+                                 smtpServerName: String,
+                                 smtpServerPort: Int) {
+        let imapServerDict = serverDictionary(name: imapServerName, port: imapServerPort)
+    }
+
+    private func serverDictionary(name: String, port: UInt16) -> SettingsDict {
+        return [MDMPredeployed.keyServerName: name,
+                MDMPredeployed.keyServerPort: NSNumber(value: port)]
+    }
+
+    private func accountDictionary(userName: String,
+                                   userAddress: String,
+                                   loginName: String,
+                                   password: String,
+                                   imapServer: SettingsDict,
+                                   smtpServer: SettingsDict) -> SettingsDict {
+        return [MDMPredeployed.keyUserName: userName,
+                MDMPredeployed.keyUserAddress: userAddress,
+                MDMPredeployed.keyLoginName: loginName,
+                MDMPredeployed.keyPassword: password,
+                MDMPredeployed.keyImapServer: imapServer,
+                MDMPredeployed.keySmtpServer: smtpServer]
+    }
 }
