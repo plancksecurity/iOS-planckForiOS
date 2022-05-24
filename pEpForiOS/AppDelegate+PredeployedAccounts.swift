82afc99254f4656ee4fab7b0c52ef77bdb687547
Dirk Zimmermann <dz@pep.security>
2022-05-24 13:34:01 +0200

    Revert "IOS-2943 TMP predeployAccount"
    This reverts commit 0729030a292c04c1e773c548667fd48efc69c3fc.

--- pEpForiOS/AppDelegate+PredeployedAccounts.swift
+++ pEpForiOS/AppDelegate+PredeployedAccounts.swift
@@ -23,35 +23,4 @@ extension AppDelegate {
             Log.shared.errorAndCrash(error: error)
         }
     }
-
-    /// Temporary test function for adding MDM account data.
-    public func predeployAccount(userName: String,
-                                 userAddress: String,
-                                 loginName: String,
-                                 password: String,
-                                 imapServerName: String,
-                                 imapServerPort: Int,
-                                 smtpServerName: String,
-                                 smtpServerPort: Int) {
-        let imapServerDict = serverDictionary(name: imapServerName, port: imapServerPort)
-    }
-
-    private func serverDictionary(name: String, port: UInt16) -> SettingsDict {
-        return [MDMPredeployed.keyServerName: name,
-                MDMPredeployed.keyServerPort: NSNumber(value: port)]
-    }
-
-    private func accountDictionary(userName: String,
-                                   userAddress: String,
-                                   loginName: String,
-                                   password: String,
-                                   imapServer: SettingsDict,
-                                   smtpServer: SettingsDict) -> SettingsDict {
-        return [MDMPredeployed.keyUserName: userName,
-                MDMPredeployed.keyUserAddress: userAddress,
-                MDMPredeployed.keyLoginName: loginName,
-                MDMPredeployed.keyPassword: password,
-                MDMPredeployed.keyImapServer: imapServer,
-                MDMPredeployed.keySmtpServer: smtpServer]
-    }
 }
