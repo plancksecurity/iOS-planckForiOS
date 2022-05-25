576dca54ef20a6472e8fe363a7004c02bbc3bae0
Dirk Zimmermann <dz@pep.security>
2022-05-25 16:18:34 +0200

    IOS-2943 TMP Testing

--- pEpForiOS/AppDelegate+PredeployedAccounts.swift
+++ pEpForiOS/AppDelegate+PredeployedAccounts.swift
@@ -23,3 +23,51 @@ extension AppDelegate {
         }
     }
 }
+
+// MARK: - TMP Testing
+
+private typealias SettingsDict = [String:Any]
+
+extension AppDelegate {
+    /// Temporary test function for adding MDM account data.
+    private func predeployAccount(userName: String,
+                                  userAddress: String,
+                                  loginName: String,
+                                  password: String,
+                                  imapServerName: String,
+                                  imapServerPort: Int,
+                                  smtpServerName: String,
+                                  smtpServerPort: Int) {
+        let imapServerDict = serverDictionary(name: imapServerName, port: UInt16(imapServerPort))
+        let smtpServerDict = serverDictionary(name: smtpServerName, port: UInt16(smtpServerPort))
+        let predeployedAccount = accountDictionary(userName: userName,
+                                                   userAddress: userAddress,
+                                                   loginName: loginName,
+                                                   password: password,
+                                                   imapServer: imapServerDict,
+                                                   smtpServer: smtpServerDict)
+
+        let mdm: SettingsDict = [MDMPredeployed.keyMDM: [predeployedAccount]]
+
+        UserDefaults.standard.register(defaults: mdm)
+    }
+
+    private func serverDictionary(name: String, port: UInt16) -> SettingsDict {
+        return [MDMPredeployed.keyServerName: name,
+                MDMPredeployed.keyServerPort: NSNumber(value: port)]
+    }
+
+    private func accountDictionary(userName: String,
+                                   userAddress: String,
+                                   loginName: String,
+                                   password: String,
+                                   imapServer: SettingsDict,
+                                   smtpServer: SettingsDict) -> SettingsDict {
+        return [MDMPredeployed.keyUserName: userName,
+                MDMPredeployed.keyUserAddress: userAddress,
+                MDMPredeployed.keyLoginName: loginName,
+                MDMPredeployed.keyPassword: password,
+                MDMPredeployed.keyImapServer: imapServer,
+                MDMPredeployed.keySmtpServer: smtpServer]
+    }
+}
