48becc66317d244f3ef350ae1dad60def54ccada
Dirk Zimmermann <dz@pep.security>
2022-05-26 10:01:14 +0200

    IOS-2943 Tell the app delegate

--- pEpForiOS/AppDelegate+PredeployedAccounts.swift
+++ pEpForiOS/AppDelegate+PredeployedAccounts.swift
@@ -14,12 +14,13 @@ extension AppDelegate {
     /// Checks for predeployed accounts, and acts on them.
     ///
     /// - Note: Silently fails if there was an error is the account description.
-    public func predeployAccounts() {
+    public func predeployAccounts() -> Bool {
         let predeployer: MDMPredeployedProtocol = MDMPredeployed()
         do {
-            try predeployer.predeployAccounts()
+            return try predeployer.predeployAccounts()
         } catch {
             Log.shared.logError(message: "Error during MDM account predeployment: \(error)")
+            return false
         }
     }
 }
