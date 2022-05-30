5e99fdc41ca4d3274683ca29a50036753451a4f8
Dirk Zimmermann <dz@pep.security>
2022-05-30 12:37:04 +0200

    IOS-2943 MDMAccountPredeploymentViewModel

---
+++ pEpForiOS/UI/MDMAccountPredeployment/MDMAccountPredeploymentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MDMAccountPredeploymentViewModel.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 30.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+class MDMAccountPredeploymentViewModel {
+}
