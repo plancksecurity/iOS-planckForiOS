b8b82adbf8a269a6c3ff903e20e79948d8b1ce7c
Dirk Zimmermann <dz@pep.security>
2022-05-30 11:30:54 +0200

    IOS-2943 MDMAccountPredeploymentViewController

---
+++ pEpForiOS/UI/MDMAccountPredeployment/MDMAccountPredeploymentViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MDMAccountPredeploymentViewController.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 30.05.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import UIKit
+
+class MDMAccountPredeploymentViewController: UIViewController {
+}
