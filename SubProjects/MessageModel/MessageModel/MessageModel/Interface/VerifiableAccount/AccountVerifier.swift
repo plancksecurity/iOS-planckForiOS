3312ebd0758b70f875ca7f6fef2a4c08026b884b
Dirk Zimmermann <dz@pep.security>
2022-06-13 14:44:32 +0200

    IOS-2943 Move the params to verify()

--- SubProjects/MessageModel/MessageModel/MessageModel/Interface/VerifiableAccount/AccountVerifier.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/VerifiableAccount/AccountVerifier.swift
@@ -18,30 +18,28 @@ public class AccountVerifier {
 
     public typealias AccountVerifierCallback = (_ error: Error?) -> ()
 
-    init(address: String? = nil,
-         userName: String? = nil,
-         authMethod: AuthMethod? = nil,
-         imapPassword: String? = nil,
-         smtpPassword: String? = nil,
-         accessToken: OAuth2AccessTokenProtocol? = nil,
-         loginNameIMAP: String? = nil,
-         serverIMAP: String? = nil,
-         portIMAP: UInt16 = 993,
-         transportIMAP: ConnectionTransport = ConnectionTransport.TLS,
-         loginNameSMTP: String? = nil,
-         serverSMTP: String? = nil,
-         portSMTP: UInt16 = 587,
-         transportSMTP: ConnectionTransport = ConnectionTransport.startTLS,
-         automaticallyTrustedImapServer: Bool = false,
-         manuallyTrustedImapServer: Bool = false,
-         keySyncEnable: Bool = true,
-         containsCompleteServerInfo: Bool = false,
-         usePEPFolderProvider: UsePEPFolderProviderProtocol? = nil,
-         originalImapPassword: String? = nil,
-         originalSmtpPassword: String? = nil) {
-    }
-
-    public func verify(verifiedCallback: @escaping AccountVerifierCallback) {
+    public func verify(address: String? = nil,
+                       userName: String? = nil,
+                       authMethod: AuthMethod? = nil,
+                       imapPassword: String? = nil,
+                       smtpPassword: String? = nil,
+                       accessToken: OAuth2AccessTokenProtocol? = nil,
+                       loginNameIMAP: String? = nil,
+                       serverIMAP: String? = nil,
+                       portIMAP: UInt16 = 993,
+                       transportIMAP: ConnectionTransport = ConnectionTransport.TLS,
+                       loginNameSMTP: String? = nil,
+                       serverSMTP: String? = nil,
+                       portSMTP: UInt16 = 587,
+                       transportSMTP: ConnectionTransport = ConnectionTransport.startTLS,
+                       automaticallyTrustedImapServer: Bool = false,
+                       manuallyTrustedImapServer: Bool = false,
+                       keySyncEnable: Bool = true,
+                       containsCompleteServerInfo: Bool = false,
+                       usePEPFolderProvider: UsePEPFolderProviderProtocol? = nil,
+                       originalImapPassword: String? = nil,
+                       originalSmtpPassword: String? = nil,
+                       verifiedCallback: @escaping AccountVerifierCallback) {
         self.verifiedCallback = verifiedCallback
         // TODO Kick off the verification
     }
