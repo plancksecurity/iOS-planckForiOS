de826c31af3f9b3b12c1097dd54b214df9115b2f
Dirk Zimmermann <dz@pep.security>
2022-06-17 12:53:30 +0200

    IOS-2943 Test for basic (local) failure

--- SubProjects/MessageModel/MessageModel/MessageModelTests/Services/AccountVerifierTest.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/AccountVerifierTest.swift
@@ -8,29 +8,35 @@
 
 import XCTest
 
+import MessageModel
+
 class AccountVerifierTest: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testBasicFail() throws {
+        let expVerification = expectation(description: "expVerification")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let verifier = AccountVerifier()
+        verifier.verify(address: "blagrg@example.com",
+                        userName: "none",
+                        password: "none",
+                        loginName: "none",
+                        serverIMAP: "localhost",
+                        portIMAP: 9999,
+                        serverSMTP: "localhost",
+                        portSMTP: 9999) { maybeError in
+            guard let _ = maybeError else {
+                XCTFail()
+                return
+            }
+            expVerification.fulfill()
         }
-    }
 
+        wait(for: [expVerification], timeout: TestUtil.waitTimeLocal)
+    }
 }
