718069828fc084620cf9a353f98a66c73d6c3a0c
Dirk Zimmermann <dz@pep.security>
2022-07-04 11:42:01 +0200

    IOS-2943 Start MDMAccountPredeploymentViewModelTest

---
+++ pEpForiOSTests/UI/MDM/MDMAccountPredeploymentViewModelTest.swift
@@ -0,0 +1,18 @@
+//
+//  MDMAccountPredeploymentViewModelTest.swift
+//  pEpForiOSTests
+//
+//  Created by Dirk Zimmermann on 04.07.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import XCTest
+
+class MDMAccountPredeploymentViewModelTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
