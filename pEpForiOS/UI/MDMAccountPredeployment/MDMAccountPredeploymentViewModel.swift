afe5aa3b6d163e4351a83a3b3993c93e95cd2d68
Dirk Zimmermann <dz@pep.security>
2022-07-04 12:08:55 +0200

    IOS-2943 Make the VM Result Equatable

--- pEpForiOS/UI/MDMAccountPredeployment/MDMAccountPredeploymentViewModel.swift
+++ pEpForiOS/UI/MDMAccountPredeployment/MDMAccountPredeploymentViewModel.swift
@@ -11,7 +11,7 @@ import Foundation
 import pEpIOSToolbox
 
 class MDMAccountPredeploymentViewModel {
-    enum Result {
+    enum Result: Equatable {
         /// An error ocurred during the pre-deployment
         case error(message: String)
 
