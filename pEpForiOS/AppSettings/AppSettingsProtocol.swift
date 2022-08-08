c63a50221366e3b8fb9cace65ad729f2a749abee
Martin <martin.brude@gmail.com>
2022-08-08 09:48:20 +0200

    IOS-3052 - add some comments

--- pEpForiOS/AppSettings/AppSettingsProtocol.swift
+++ pEpForiOS/AppSettings/AppSettingsProtocol.swift
@@ -13,14 +13,28 @@ import MessageModel
 #endif
 
 public protocol AppSettingsProtocol {
+    /// Indicates if keySync is enabled
     var keySyncEnabled: Bool { get set }
+
+    /// Indicates if the PEP folder is enabled
     var usePEPFolderEnabled: Bool { get set }
+
+    /// Indicates if the extra keys are editable.
     var extraKeysEditable: Bool { get set }
+
+    /// Indicates if the unencryped subject is enabled.
     var unencryptedSubjectEnabled: Bool { get set }
+
     var threadedViewEnabled: Bool { get set }
+
+    /// Indicates if Passive mode is on.
     var passiveMode: Bool { get set }
+
+    /// The email address of the default account. 
     var defaultAccount: String? { get set }
+
     var lastKnownDeviceGroupState: DeviceGroupState { get set }
+
     var shouldShowTutorialWizard: Bool { get set }
     /// Whether or not the user has already answered the "Do you want to allow pEp app to access 
     /// your contacts"
@@ -30,10 +44,10 @@ public protocol AppSettingsProtocol {
     var unsecureReplyWarningEnabled: Bool { get set }
 
     /// Should the logging be verbose, or not?
-    var verboseLogginEnabled: Bool { get set }
+    var verboseLogginEnabled : Bool { get set }
 
     /// The list of the accepted languages codes for truswords. 
-    var acceptedLanguagesCodes: [String] { get set }
+    var acceptedLanguagesCodes : [String] { get set }
 
     // MARK: - MDM
 
@@ -41,31 +55,31 @@ public protocol AppSettingsProtocol {
     /// https://confluence.pep.security/pages/viewpage.action?pageId=10223902 (see Settings meaning and structure)
 
     /// Enable or disable pEp privacy protection for the user or device's account.
-    var mdmPepEnablePrivacyProtection : Bool { get set }
+    var mdmPepEnablePrivacyProtection: Bool { get set }
 
     /// Extra keys can be provided using this setting
     /// extra keys can be removed if the setting is provided and all fingerprint elements are blank.
     /// So the lack of extra keys could be nil or empty.
-    var mdmPepExtraKeys : [String]? { get set }
+    var mdmPepExtraKeys: [String]? { get set }
 
     /// This is an advanced feature.
     /// It allows using a handshake method to establish trust between two users.
-    var mdmPepUseTrustwords : Bool { get set }
+    var mdmPepUseTrustwords: Bool { get set }
 
     /// When this setting is enabled, the unsecure Recipients in the "to", "cc", "bcc" fields from Message Compose screen
     ///  will appear highlighted in red color.
     /// Default is true.
-    var mdmUnsecureDeliveryWarning : Bool { get set }
+    var mdmUnsecureDeliveryWarning: Bool { get set }
 
     /// This is an advanced feature.
     /// When enabled, a dedicated pEp folder is used for pEp sync messages.
     /// When disabled, Inbox folder will be user for these messages instead.
     /// Default is true.
-    var mdmPepSyncFolder : Bool { get set }
+    var mdmPepSyncFolder: Bool { get set }
 
     /// This is an advanced feature. When enabled, debug log can be displayed in a console.
     /// Default is false.
-    var mdmDebugLogging : Bool { get set }
+    var mdmDebugLogging: Bool { get set }
 
     /// Number of mails displayed in Message List screen by default.
     /// The user can always refresh more mails from the server.
@@ -74,34 +88,34 @@ public protocol AppSettingsProtocol {
     var mdmAccountDisplayCount: Int { get set }
 
     /// Max folders to check with push. Advanced setting
-    var mdmMaxPushFolders : Int { get set }
+    var mdmMaxPushFolders: Int { get set }
 
     /// Defaults to MDM's {{username}}.
     /// If not provided or empty, email address will be used instead.
     /// Name that will be displayed in several places of the app, associated with the user account.
     /// Setting this value is optional for deployment.
-    var mdmAccountDescription : String? { get set }
+    var mdmAccountDescription: String? { get set }
 
     /// Defaults to MDM's {{username}}, recommended for deployment.
     /// If not provided or empty, email address will be used instead.
-    var mdmCompositionSenderName : String? { get set }
+    var mdmCompositionSenderName: String? { get set }
 
     /// Whether to include sender signature when composing emails.
     /// Default is true.
-    var mdmCompositionUseSignature : String? { get set }
+    var mdmCompositionUseSignature: String? { get set }
 
     /// Signature to include in outgoing emails when composition_use_signature is enabled
-    var mdmCompositionSignature : String? { get set }
+    var mdmCompositionSignature: String? { get set }
 
     /// Whether to position the sender's signature before the quoted message in replies/forwards.
     /// (Signature after quoted message by default).
     /// Default is false.
-    var mdmCompositionSignatureBeforeQuotedMessage : String? { get set }
+    var mdmCompositionSignatureBeforeQuotedMessage: String? { get set }
 
     /// Whether to position sender signature before the quoted message in replies/forwards.
     /// (Signature after quoted message by default).
     /// Default is false.
-    var mdmDefaultQuotedTextShown : String? { get set }
+    var mdmDefaultQuotedTextShown: String? { get set }
 
     /// Folders that the application will use as special archive, drafts, sent, spam, trash folders.
     /// By default all of them are empty.
@@ -115,29 +129,29 @@ public protocol AppSettingsProtocol {
     /// - spam_folder: Folder where mails marked as spam are stored.
     /// - trash_folder: Folder where deleted mails are temporarily stored until they are permanently deleted.
     /// - The special value "-NONE-" can be entered to unassign a special folder.
-    var mdmAccountDefaultFolders : [String]? { get set }
+    var mdmAccountDefaultFolders: [String]? { get set }
 
     /// When enabled, a button for remote search will appear in local search screen,
     /// so that the user can get more search results from the server.
     /// Default is true.
-    var mdmRemoteSearchEnabled : Bool { get set }
+    var mdmRemoteSearchEnabled: Bool { get set }
 
     /// Number of messages retrieved when a remote search is performed.
     /// Default is 50.
-    var mdmAccountRemoteSearchNumResults : Int { get set }
+    var mdmAccountRemoteSearchNumResults: Int { get set }
 
     /// This is an advanced feature. Whether to enable the account to perform pEp sync.
     /// Default is true.
-    var mdmPepSaveEncryptedOnServer : Bool { get set }
+    var mdmPepSaveEncryptedOnServer: Bool { get set }
 
     /// This is an advanced feature. Whether to enable the account to perform pEp sync.
     /// Default is true.
-    var mdmPepEnableSyncAccount : Bool { get set }
+    var mdmPepEnableSyncAccount: Bool { get set }
 
     /// This setting can be enabled from MDM to allow to add new devices for an existing user (mail address).
     /// After the sync is done it should be set to false again.
     /// Default is false.
-    var mdmAllowPepSyncNewDevices : Bool { get set }
+    var mdmAllowPepSyncNewDevices: Bool { get set }
 
     // MARK: - Collapsing State
 
