abc1341d7a27bc8eaab58543cadbfaa81dfd506b
Dirk Zimmermann <dz@pep.security>
2022-08-09 12:21:31 +0200

    IOS-3051 Add SwiftUI for MDM deployment

---
+++ pEpForiOS/AppSettings/MDM/MDMPredeployedUI.swift
@@ -0,0 +1,21 @@
+//
+//  MDMPredeployedUI.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 09.08.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import SwiftUI
+
+struct MDMPredeployedUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MDMPredeployedUI_Previews: PreviewProvider {
+    static var previews: some View {
+        MDMPredeployedUI()
+    }
+}
