8ec5621aee9dbddc14ca2490aafa1d1556a0326e
Dirk Zimmermann <dz@pep.security>
2022-08-16 09:05:54 +0200

    IOS-3051 Add swift ui view

---
+++ pEpForiOS/UI/MDMAccountDeployment/MDMAccountDeploymentView.swift
@@ -0,0 +1,21 @@
+//
+//  MDMAccountDeploymentView.swift
+//  pEp
+//
+//  Created by Dirk Zimmermann on 16.08.22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+import SwiftUI
+
+struct MDMAccountDeploymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MDMAccountDeploymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MDMAccountDeploymentView()
+    }
+}
