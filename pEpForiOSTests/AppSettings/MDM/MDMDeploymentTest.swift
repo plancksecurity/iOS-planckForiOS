28f5073bb9a76f2e2d01d89a26a1fea18af8acab
Dirk Zimmermann <dz@pep.security>
2022-08-16 10:20:09 +0200

    IOS-3051 Remove outdated code

--- pEpForiOSTests/AppSettings/MDM/MDMDeploymentTest.swift
+++ pEpForiOSTests/AppSettings/MDM/MDMDeploymentTest.swift
@@ -16,7 +16,7 @@ private typealias SettingsDict = [String:Any]
 
 class MDMDeploymentTest: XCTestCase {
     override func setUpWithError() throws {
-        setupAccountData = []
+        setupAccountData = nil
     }
 
     override func tearDownWithError() throws {
@@ -25,31 +25,9 @@ class MDMDeploymentTest: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func testSingleAccountNetworkError() throws {
+    func testNetworkError() throws {
         XCTAssertFalse(MDMDeployment().haveAccountToDeploy)
-        setupSingleFailingPredeployedAccount()
-        XCTAssertTrue(MDMDeployment().haveAccountToDeploy)
-
-        do {
-            try predeployAccounts()
-            XCTFail()
-        } catch MDMDeploymentError.networkError {
-        } catch {
-            XCTFail()
-        }
-
-        XCTAssertFalse(MDMDeployment().haveAccountToDeploy)
-
-        let accounts = Account.all()
-        XCTAssertEqual(accounts.count, 0)
-    }
-
-    func testAllExistingAccountsHaveBeenWipedAfterNetworkFail() throws {
-        let _ = createAccount(baseName: "acc1", portBase: 555, index: 1)
-        let _ = createAccount(baseName: "acc2", portBase: 556, index: 2)
-
-        XCTAssertFalse(MDMDeployment().haveAccountToDeploy)
-        setupSingleFailingPredeployedAccount()
+        setupDeployableAccountData()
         XCTAssertTrue(MDMDeployment().haveAccountToDeploy)
 
         do {
@@ -94,106 +72,9 @@ class MDMDeploymentTest: XCTestCase {
 
     // MARK: - Setup Util
 
-    /// Contains account data for double-checking what has been set up.
-    struct AccountStruct {
-        let userAddress: String
-        let imapServer: String
-        let imapPort: UInt16
-        let smtpServer: String
-        let smtpPort: UInt16
-        let userName: String
-        let loginName: String
-        let password: String
-    }
+    /// The MDM account data that is expected to be deployed.
+    var setupAccountData: MDMDeployment.AccountData? = nil
 
-    /// An array of all accounts that are expected to be set up
-    var setupAccountData = [AccountStruct]()
-
-    func setupSingleFailingPredeployedAccount(appendixNumber: Int = 0) {
-        let accountDict = failingAccountWithServerDictionary(appendixNumber: appendixNumber)
-        let predeployedAccounts: SettingsDict = [MDMDeployment.keyPredeployedAccounts:[accountDict]]
-        UserDefaults.standard.set(predeployedAccounts, forKey: MDMDeployment.keyMDM)
-    }
-
-    func createAccount(baseName: String, portBase: Int, index: Int) -> Account {
-        let session = Session.main
-        let id = Identity.init(address: "\(baseName)\(index)@example.com",
-                               userID: "\(baseName)\(index)",
-                               addressBookID: nil,
-                               userName: "\(baseName)\(index)",
-                               session: session)
-        let creds = ServerCredentials.init(loginName: "\(baseName)\(index)", clientCertificate: nil)
-        creds.password = "password_\(portBase)\(index)"
-
-        let imap = Server.create(serverType: .imap,
-                                 port: UInt16(portBase),
-                                 address: "imap\(baseName)\(portBase)\(index)",
-                                 transport: .tls,
-                                 credentials: creds)
-        let smtp = Server.create(serverType: .smtp,
-                                 port: UInt16(portBase + 1),
-                                 address: "smtp\(baseName)\(portBase + 1)\(index)",
-                                 transport: .tls,
-                                 credentials: creds)
-        let acc = Account.init(user: id, servers: [imap, smtp], session: session)
-        session.commit()
-
-        return acc
-    }
-
-    func indexed(string: String, index: Int) -> String {
-        return "\(string)\(index)"
-    }
-
-    // MARK: - Setup Util Util
-
-    private func failingAccountWithServerDictionary(appendixNumber: Int = 0) -> SettingsDict {
-        let accountData = AccountStruct(userAddress: "account\(appendixNumber)@example.com",
-                                        imapServer: "imapServer\(appendixNumber)",
-                                        imapPort: 993,
-                                        smtpServer: "smtpServer\(appendixNumber)",
-                                        smtpPort: 587,
-                                        userName: "userName\(appendixNumber)",
-                                        loginName: "loginName\(appendixNumber)",
-                                        password: "password\(appendixNumber)")
-        if setupAccountData.count > appendixNumber {
-            XCTFail()
-            return [:]
-        }
-        setupAccountData.append(accountData)
-
-        let imapServer = serverDictionary(name: accountData.imapServer,
-                                          port: accountData.imapPort)
-        let smtpServer = serverDictionary(name: accountData.smtpServer,
-                                          port: accountData.smtpPort)
-        let accountDict = accountDictionary(userName: accountData.userName,
-                                            userAddress: accountData.userAddress,
-                                            loginName: accountData.loginName,
-                                            password: accountData.password,
-                                            imapServer: imapServer,
-                                            smtpServer: smtpServer)
-
-        return accountDict
-    }
-
-    // TODO: Adapt to new account setup
-    private func serverDictionary(name: String, port: UInt16) -> SettingsDict {
-        return ["MDMDeployment.keyServerName": name,
-                "MDMDeployment.keyServerPort": NSNumber(value: port)]
-    }
-
-    // TODO: Adapt to new account setup
-    private func accountDictionary(userName: String,
-                                   userAddress: String,
-                                   loginName: String,
-                                   password: String,
-                                   imapServer: SettingsDict,
-                                   smtpServer: SettingsDict) -> SettingsDict {
-        return ["MDMDeployment.keyUserName": userName,
-                "MDMDeployment.keyUserAddress": userAddress,
-                "MDMDeployment.keyLoginName": loginName,
-                "MDMDeployment.keyPassword": password,
-                "MDMDeployment.keyImapServer": imapServer,
-                "MDMDeployment.keySmtpServer": smtpServer]
+    func setupDeployableAccountData() {
     }
 }
