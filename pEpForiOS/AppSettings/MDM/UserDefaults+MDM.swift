01ce828d145b49106c43aeaeb2e819a76588d0d1
Martin <martin.brude@gmail.com>
2022-09-05 09:18:44 +0200

    PEMA-35 - add documentation

--- pEpForiOS/AppSettings/MDM/UserDefaults+MDM.swift
+++ pEpForiOS/AppSettings/MDM/UserDefaults+MDM.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+// This extension is used to observe MDM settings via KVO.
+//
+// To do it, we add the property with @objc dynamic modifiers,
+// then we use the variable name as keypath.
+//
+// Usage example:
+//    userDefaults.observe(\.mdmSettings, options: [.old, .new],
+//      changeHandler: { (defaults, change) in
+//    })
 extension UserDefaults {
     @objc dynamic var mdmSettings: Dictionary<String, Any>? {
         return dictionary(forKey: MDMPredeployed.keyMDM)
