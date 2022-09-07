70441470e30859a399794027ebdee335d9a14fd3
Martin <martin.brude@gmail.com>
2022-09-07 11:27:46 +0200

    PEMA-35 - extract to observer class

--- pEpForiOS/AppSettings/AppSettings.swift
+++ pEpForiOS/AppSettings/AppSettings.swift
@@ -26,41 +26,11 @@ public final class AppSettings: KeySyncStateProvider, AppSettingsProtocol {
     // MARK: - Singleton
     
     static public let shared = AppSettings()
-    private var mdmDictionary: [String: Any] = [:]
 
     private init() {
         setup()
         registerForKeySyncDeviceGroupStateChangeNotification()
         registerForKeySyncDisabledByEngineNotification()
-        startObserver()
-    }
-
-    public func startObserver() {
-        if let values = UserDefaults.standard.dictionary(forKey: MDMPredeployed.keyMDM) {
-            mdmDictionary = values
-        }
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(userDefaultsDidChange),
-                                               name: UserDefaults.didChangeNotification,
-                                               object: nil)
-    }
-
-    @objc private func userDefaultsDidChange(notification: NSNotification) {
-        // We only care about standard user default settings, and specifically mdm settings
-        guard let defaults = notification.object as? UserDefaults,
-              defaults == UserDefaults.standard,
-              let mdm = defaults.dictionary(forKey: MDMPredeployed.keyMDM) else {
-            //Nothing to do
-            return
-        }
-
-        // As ´Any´ does not conform to Equatable
-        // we use NSDictionary to easily compare these dictionaries.
-        let mdmSettingsHasChanged = !NSDictionary(dictionary: mdm).isEqual(to: mdmDictionary)
-        if  mdmSettingsHasChanged {
-            mdmDictionary = mdm
-            NotificationCenter.default.post(name:.pEpMDMSettingsChanged, object: mdm, userInfo: nil)
-        }
     }
 
     // MARK: - KeySyncStateProvider
---
+++ pEpForiOS/AppSettings/AppSettingsObserver.swift
@@ -0,0 +1,60 @@
+//
+//  AppSettingsObserver.swift
+//  pEp
+//
+//  Created by Martín Brude on 7/9/22.
+//  Copyright © 2022 p≡p Security S.A. All rights reserved.
+//
+
+protocol AppSettingsObserverProtocol {
+
+    static var shared: AppSettingsObserverProtocol { get }
+}
+
+class AppSettingsObserver: AppSettingsObserverProtocol {
+
+    private var mdmDictionary: [String: Any] = [:]
+
+    static public let shared: AppSettingsObserverProtocol = AppSettingsObserver()
+
+    // MARK: - Private
+
+    private init() {
+        startObserver()
+    }
+
+    private func startObserver() {
+        if let values = UserDefaults.standard.dictionary(forKey: MDMPredeployed.keyMDM) {
+            mdmDictionary = values
+        }
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(userDefaultsDidChange),
+                                               name: UserDefaults.didChangeNotification,
+                                               object: nil)
+    }
+
+    @objc private func userDefaultsDidChange(notification: NSNotification) {
+        // We only care about standard user default settings, and specifically mdm settings
+        guard let defaults = notification.object as? UserDefaults,
+              defaults == UserDefaults.standard,
+              let mdm = defaults.dictionary(forKey: MDMPredeployed.keyMDM) else {
+            //Nothing to do
+            return
+        }
+
+        // As ´Any´ does not conform to Equatable
+        // we use NSDictionary to easily compare these dictionaries.
+        let mdmSettingsHasChanged = !NSDictionary(dictionary: mdm).isEqual(to: mdmDictionary)
+        if  mdmSettingsHasChanged {
+            mdmDictionary = mdm
+            NotificationCenter.default.post(name:.pEpMDMSettingsChanged, object: mdm, userInfo: nil)
+        }
+    }
+
+    // MARK: - Deinit
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
