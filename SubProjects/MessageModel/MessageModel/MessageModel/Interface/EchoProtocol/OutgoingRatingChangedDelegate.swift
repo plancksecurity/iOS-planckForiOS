9a536cb04c5814063c099e294597574e9b58f789
Dirk Zimmermann <dz@pep.security>
2022-09-07 15:02:06 +0200

    PEMA-24 OutgoingRatingChangedService

---
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/EchoProtocol/OutgoingRatingChangedDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  OutgoingRatingChangedDelegate.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 07.09.22.
+//  Copyright © 2022 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+protocol OutgoingRatingChangedDelegate: NSObject {
+    /// The echo protocol determined in the background that some partner identities changed their rating,
+    /// so outgoing ratings have to be re-computed.
+    func outgoingRatingChanged()
+}
---
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/EchoProtocol/OutgoingRatingChangedService.swift
@@ -0,0 +1,17 @@
+//
+//  OutgoingRatingChangedService.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 07.09.22.
+//  Copyright © 2022 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Service for listening to outgoing rating changes, caused by the echo protocol.
+///
+/// Usage: Instantiate an instance of this class, and set the (weak) delegate. Keep the instance alive
+/// as long as you want to receive updates to the delegate.
+class OutgoingRatingChangedService {
+    public weak var delegate: OutgoingRatingChangedDelegate?
+}
