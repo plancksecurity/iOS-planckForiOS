965c362b7b388632d14b35bbda9190519d19a244
Dirk Zimmermann <dz@pep.security>
2022-09-12 14:10:19 +0200

    PEMA-25 Correct naming

--- pEpForiOS/UI/Compose/ViewModel/ComposeViewModelState.swift
+++ pEpForiOS/UI/Compose/ViewModel/ComposeViewModelState.swift
@@ -157,12 +157,12 @@ extension ComposeViewModel {
         }
 
         public func validate() {
-            updatePepRating()
+            updatepEpRating()
             validateForSending()
         }
 
         public func reevaluatePepRating() {
-            updatePepRating()
+            updatepEpRating()
         }
 
         private func setup() {
@@ -216,7 +216,7 @@ extension ComposeViewModel.ComposeViewModelState {
         return bccRecipients.count > 0
     }
 
-    private func updatePepRating() {
+    private func updatepEpRating() {
         guard !isForceUnprotectedDueToBccSet else {
             rating = .unencrypted
             return
