889f340c7106d19b2af1239356180c4a40494f8d
Dirk Zimmermann <dz@pep.security>
2022-10-25 13:47:51 +0200

    PEMA-8 Util for bundling the setting of MDM settings

---
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/Util/MDMSettingsUtil.swift
@@ -0,0 +1,23 @@
+//
+//  MDMSettingsUtil.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 25.10.22.
+//  Copyright © 2022 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Bundles account/engine setup together, so the code can be shared by both MDM-account setup logic
+/// and the MDM-setting changes.
+public class MDMSettingsUtil {
+    /// Expose the init to outsiders.
+    public init() {}
+
+    public func configure() {
+        MediaKeysUtil().configure(mediaKeyDictionaries: AppSettings.shared.mdmMediaKeys)
+        EchoProtocolUtil().enableEchoProtocol(enabled: AppSettings.shared.mdmEchoProtocolEnabled)
+        TrustedServerUtil().setStoreSecurely(newValue: AppSettings.shared.mdmPEPSaveEncryptedOnServerEnabled)
+        ExtraKeysUtil().configure(extraKeyDictionaries: AppSettings.shared.mdmPEPExtraKeys)
+    }
+}
