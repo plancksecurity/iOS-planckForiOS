94024081d2c4dc83d8fd59d83d0d4445695e21fa
Dirk Zimmermann <dz@pep.security>
2022-10-25 10:52:50 +0200

    PEMA-8 Load test account data from plist

--- pEpForiOS/UI/MDMAccountDeployment/FakeAccountData.swift
+++ pEpForiOS/UI/MDMAccountDeployment/FakeAccountData.swift
@@ -12,27 +12,8 @@ private typealias SettingsDict = [String:Any]
 
 class FakeAccountData {
     func setupDeployableAccountData() {
-        let loginname = "login_name"
-
-        let compositionSettingsDict = [AppSettings.keyCompositionSenderName: "sender_name"]
-
-        let imapSettingsDict: SettingsDict = [MDMDeployment.keyIncomingMailSettingsServer: "imap_server",
-                                              MDMDeployment.keyIncomingMailSettingsSecurityType: "SSL/TLS",
-                                              MDMDeployment.keyIncomingMailSettingsPort: NSNumber(value: 1993),
-                                              MDMDeployment.keyIncomingMailSettingsUsername: loginname]
-
-        let smtpSettingsDict: SettingsDict = [MDMDeployment.keyOutgoingMailSettingsServer: "smtp_server",
-                                              MDMDeployment.keyOutgoingMailSettingsSecurityType: "STARTTLS",
-                                              MDMDeployment.keyOutgoingMailSettingsPort: NSNumber(value: 1465),
-                                              MDMDeployment.keyOutgoingMailSettingsUsername: loginname]
-
-        let mailSettingsDict: SettingsDict = [MDMDeployment.keyUserAddress: "email@example.com",
-                                              MDMDeployment.keyIncomingMailSettings: imapSettingsDict,
-                                              MDMDeployment.keyOutgoingMailSettings: smtpSettingsDict]
-
-        let mdmDict = [MDMDeployment.keyCompositionSettings: compositionSettingsDict,
-                       MDMDeployment.keyAccountDeploymentMailSettings: mailSettingsDict]
-
-        UserDefaults.standard.set(mdmDict, forKey: MDMDeployment.keyMDM)
+        let url = Bundle.main.url(forResource: "MDM", withExtension: "plist")!
+        let plistDict = NSDictionary.init(contentsOf: url)!
+        UserDefaults.standard.set(plistDict, forKey: MDMDeployment.keyMDM)
     }
 }
