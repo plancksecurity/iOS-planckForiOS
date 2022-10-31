2cfb781c1adf4e6f4d1f3ac5b86cad3e695d1e50
Dirk Zimmermann <dz@pep.security>
2022-10-31 10:41:49 +0100

    PEMA-8 Remove unneeded code

--- pEpForiOS/AppSettings/MDM/MDMSettingsUtil.swift
+++ pEpForiOS/AppSettings/MDM/MDMSettingsUtil.swift
@@ -13,9 +13,6 @@ import MessageModel
 /// Bundles account/engine setup together, so the code can be shared by both MDM-account setup logic
 /// and the MDM-setting changes.
 public class MDMSettingsUtil {
-    /// Expose the init to outsiders.
-    public init() {}
-
     /// - Note: For some of these settings it's vital that they are set, e.g. at DB level, _before_ the corresponding
     /// service starts up the first time, e.g. `KeySyncService`.
     public func configure(completion: @escaping (Result<Void, Error>) -> Void) {
