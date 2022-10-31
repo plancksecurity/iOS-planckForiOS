a592d3b752c5b66d2c1cc946647ba845520d8855
Dirk Zimmermann <dz@pep.security>
2022-10-31 10:11:38 +0100

    PEMA-8 KeySyncUtil

---
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/Util/KeySyncUtil.swift
@@ -0,0 +1,17 @@
+//
+//  KeySyncUtil.swift
+//  MessageModel
+//
+//  Created by Dirk Zimmermann on 31.10.22.
+//  Copyright © 2022 pEp Security S.A. All rights reserved.
+//
+
+import Foundation
+
+public class KeySyncUtil {
+
+    public init() {}
+
+    public func enable(keySync: Bool) {
+    }
+}
