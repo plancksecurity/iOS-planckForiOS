7cd034e0bbf69b74db81de006c3519aa02e2506a
Dirk Zimmermann <dz@pep.security>
2022-11-04 14:36:22 +0100

    PEMA-8 Remove TODO. This doesn't happen.

--- SubProjects/MessageModel/MessageModel/MessageModel/Interface/VerifiableAccount/AccountVerifier.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/VerifiableAccount/AccountVerifier.swift
@@ -118,7 +118,6 @@ extension AccountVerifier: AccountVerifierProtocol {
 extension AccountVerifier: VerifiableAccountDelegate {
     public func didEndVerification(result: Result<Void, Error>) {
         guard let cb = verifiedCallback else {
-            // TODO: This actually happens. Fix.
             Log.shared.errorAndCrash(message: "No verifiedCallback")
             return
         }
