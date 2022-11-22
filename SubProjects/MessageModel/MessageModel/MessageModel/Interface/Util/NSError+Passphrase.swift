bc6963718ab0faa112cb88e2945331c3b157f80f
Dirk Zimmermann <dz@pep.security>
2022-11-22 16:34:43 +0100

    PEMA-105 Use PEPObjCEngineStatusErrorDomain (new engine error domain)

--- SubProjects/MessageModel/MessageModel/MessageModel/Interface/Util/NSError+Passphrase.swift
+++ SubProjects/MessageModel/MessageModel/MessageModel/Interface/Util/NSError+Passphrase.swift
@@ -15,7 +15,7 @@ public extension NSError {
     /// Indicates if the error is PASSPHRASE related.
     /// That means: if it is a passphraseRequired error or a wrong passphrase error.
     var isPassphraseError: Bool {
-        if domain == PEPObjCAdapterEngineStatusErrorDomain &&
+        if domain == PEPObjCEngineStatusErrorDomain &&
             code == Int(PEPStatus.passphraseRequired.rawValue) ||
             code == Int(PEPStatus.wrongPassphrase.rawValue) {
             return true
