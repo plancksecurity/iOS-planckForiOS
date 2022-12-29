1997edfc74c40b3156dbecb6844b03bd8e60894d
Dirk Zimmermann <dz@pep.security>
2022-12-29 09:00:26 +0100

    PEMA-118 This test will need a (test) account

--- SubProjects/MessageModel/MessageModel/MessageModelTests/Services/FetchNumberOfNewMailsServiceTest.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/Services/FetchNumberOfNewMailsServiceTest.swift
@@ -8,7 +8,9 @@
 
 import XCTest
 
-final class FetchNumberOfNewMailsServiceTest: XCTestCase {
+@testable import MessageModel
+
+final class FetchNumberOfNewMailsServiceTest: PersistentStoreDrivenTestBase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
