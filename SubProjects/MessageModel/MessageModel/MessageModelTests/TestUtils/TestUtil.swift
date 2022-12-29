43feacafcc974d44d8274e17f3ad7620640b8817
Dirk Zimmermann <dz@pep.security>
2022-12-29 09:13:51 +0100

    PEMA-118 Dead code

--- SubProjects/MessageModel/MessageModel/MessageModelTests/TestUtils/TestUtil.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/TestUtils/TestUtil.swift
@@ -241,63 +241,6 @@ extension TestUtil {
         return acc
     }
 
-    // MARK: - Sync Loop
-
-    static public func syncAndWait(cdAccountsToSync:[CdAccount]? = nil,
-                                   testCase: XCTestCase,
-                                   context: NSManagedObjectContext? = nil) {
-
-        let context: NSManagedObjectContext = context ?? Stack.shared.mainContext
-        guard let accounts = cdAccountsToSync ?? CdAccount.all(in: context) as? [CdAccount] else {
-            XCTFail("No account to sync")
-            return
-        }
-        // Serial queue
-        let queue = OperationQueue()
-        queue.maxConcurrentOperationCount = 1
-
-        let errorPropagator = ErrorPropagator()
-
-        // Array to own services as long as they are in use.
-        var services = [ServiceProtocol]()
-
-        for cdaccount in accounts {
-            // Send all
-            guard let sendService = cdaccount.sendService(errorPropagator: errorPropagator) else {
-                // This account does not offer a send send service. That might be a valid case for
-                // protocols supported in the future.
-                continue
-            }
-            services.append(sendService)
-            queue.addOperations(sendService.operations(), waitUntilFinished: false)
-
-            // Fetch & Sync all
-            guard
-                let replicationService = cdaccount.replicationService(errorPropagator: errorPropagator)
-                else {
-                // This account does not offer a replication send service. That might be a valid case for
-                // protocols supported in the future.
-                continue
-            }
-            services.append(replicationService)
-            queue.addOperations(replicationService.operations(), waitUntilFinished: false)
-        }
-
-        // Decrypt all
-        let decryptService = DecryptService(errorPropagator: errorPropagator)
-        services.append(decryptService)
-        queue.addOperations(decryptService.operations(), waitUntilFinished: false)
-
-
-        let expSynced = testCase.expectation(description: "expSynced")
-        DispatchQueue.global(qos: .utility).async {
-            queue.waitUntilAllOperationsAreFinished()
-            expSynced.fulfill()
-        }
-
-        testCase.wait(for: [expSynced], timeout: waitTime)
-    }
-
     // MARK: - Moved from App target. Needs love, review, ideally remove
 
     static func checkForExistanceAndUniqueness(uuids: [MessageID],
