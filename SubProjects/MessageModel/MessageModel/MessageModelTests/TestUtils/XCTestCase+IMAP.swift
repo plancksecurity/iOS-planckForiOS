9e060e367b34d024429be1e5b4af3f9796ddabfc
Dirk Zimmermann <dz@pep.security>
2022-12-29 09:00:47 +0100

    PEMA-118 XCTest version of syncAndWait

--- SubProjects/MessageModel/MessageModel/MessageModelTests/TestUtils/XCTestCase+IMAP.swift
+++ SubProjects/MessageModel/MessageModel/MessageModelTests/TestUtils/XCTestCase+IMAP.swift
@@ -75,6 +75,64 @@ extension XCTestCase {
         })
     }
 
+    // MARK: - Sync Loop
+
+    public func syncAndWait(cdAccountsToSync:[CdAccount]? = nil,
+                            context: NSManagedObjectContext? = nil) {
+
+        let context: NSManagedObjectContext = context ?? Stack.shared.mainContext
+
+        guard let accounts = cdAccountsToSync ?? CdAccount.all(in: context) as? [CdAccount] else {
+            XCTFail("No account to sync")
+            return
+        }
+
+        // Serial queue
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+
+        let errorPropagator = ErrorPropagator()
+
+        // Array to own services as long as they are in use.
+        var services = [ServiceProtocol]()
+
+        for cdaccount in accounts {
+            // Send all
+            guard let sendService = cdaccount.sendService(errorPropagator: errorPropagator) else {
+                // This account does not offer a send send service. That might be a valid case for
+                // protocols supported in the future.
+                continue
+            }
+            services.append(sendService)
+            queue.addOperations(sendService.operations(), waitUntilFinished: false)
+
+            // Fetch & Sync all
+            guard
+                let replicationService = cdaccount.replicationService(errorPropagator: errorPropagator)
+                else {
+                // This account does not offer a replication send service. That might be a valid case for
+                // protocols supported in the future.
+                continue
+            }
+            services.append(replicationService)
+            queue.addOperations(replicationService.operations(), waitUntilFinished: false)
+        }
+
+        // Decrypt all
+        let decryptService = DecryptService(errorPropagator: errorPropagator)
+        services.append(decryptService)
+        queue.addOperations(decryptService.operations(), waitUntilFinished: false)
+
+
+        let expSynced = expectation(description: "expSynced")
+        DispatchQueue.global(qos: .utility).async {
+            queue.waitUntilAllOperationsAreFinished()
+            expSynced.fulfill()
+        }
+
+        wait(for: [expSynced], timeout: TestUtil.waitTime)
+    }
+
     public func fetchNumberOfNewMails(errorContainer: ErrorContainerProtocol,
                                       context: NSManagedObjectContext) -> Int? {
         let expNumMails = expectation(description: "expNumMails")
