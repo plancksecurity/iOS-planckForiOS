dceeac90dacf60ada2a87f4976fb73cd975db1b7
Dirk Zimmermann <dz@pep.security>
2023-01-23 10:53:12 +0100

    PEMA-195 Remove crashing test that does not reflect reality
    SettingsViewModelTest.testDeleteAccountWithMoreThanOneAccount

--- pEpForiOSTests/UI/Settings/Setting/SettingsViewModelTest.swift
+++ pEpForiOSTests/UI/Settings/Setting/SettingsViewModelTest.swift
@@ -114,25 +114,6 @@ class SettingsViewModelTest: AccountDrivenTestBase {
         waitForExpectations(timeout: TestUtil.waitTime)
     }
 
-    func testDeleteAccountWithMoreThanOneAccount() {
-        givenThereAreTwoAccounts()
-        let delegate = SettingsViewModeldelegate()
-        let removeFolderViewCollapsedStateOfAccountWithExpectation = expectation(description: "removeFolderViewCollapsedStateOfAccountWithExpectation")
-        let appSettingsMock = MockAppSettings(removeFolderViewCollapsedStateOfAccountWithExpectation: removeFolderViewCollapsedStateOfAccountWithExpectation)
-        setupViewModel(delegate: delegate, appSettings: appSettingsMock)
-        let firstIndexPath = IndexPath(row: 0, section: 0)
-        let firstSection = settingsVM.section(for: firstIndexPath)
-        let cellsBefore = firstSection.rows.count
-        let firstSectionRows = firstSection.rows
-        if let row = firstSectionRows.first as? SettingsViewModel.ActionRow,
-            let action = row.action {
-            action()
-        }
-        let cellsAfter = settingsVM.section(for: firstIndexPath).rows.count
-        XCTAssertEqual(cellsBefore, cellsAfter + 1)
-        waitForExpectations(timeout: TestUtil.waitTime)
-    }
-
     func testHandleExportDBsPressed() {
         let delegate = SettingsViewModeldelegate()
         let exportDBsexpectation = expectation(description: "export dbs")
