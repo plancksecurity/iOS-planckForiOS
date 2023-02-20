0daef0f486139f2de24ebb2c000ad5f65a7d2e84
Dirk Zimmermann <dz@pep.security>
2023-02-20 15:38:06 +0100

    E3-3 Enable echo protocol

--- pEpForiOS/AppDelegate.swift
+++ pEpForiOS/AppDelegate.swift
@@ -56,9 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     private func setupServices() {
-        if !MDMUtil.isEnabled() {
-            EchoProtocolUtil().enableEchoProtocol(enabled: false)
-        }
+        EchoProtocolUtil().enableEchoProtocol(enabled: true)
 
         messageModelService = MessageModelService(errorPropagator: errorPropagator,
                                                   cnContactsAccessPermissionProvider: AppSettings.shared,
