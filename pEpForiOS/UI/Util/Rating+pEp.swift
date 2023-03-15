d7832bf2ed0df6a61335d229e2b5502b6a2b9ee8
Sascha Bacardit <saschabacardit@gmail.com>
2023-03-15 11:48:07 +0100

    PEMA-341 Updated Rating+pEp
    Rating is how icons are determined now, not colors.

--- pEpForiOS/UI/Util/Rating+pEp.swift
+++ pEpForiOS/UI/Util/Rating+pEp.swift
@@ -25,34 +25,34 @@ extension Rating {
         case .undefined:
             return nil
         case .cannotDecrypt:
-            return withText ? UIImage(named: "pEp-status-msg-red") : UIImage(named: "pEp-status-red_white-border")
+            return withText ? UIImage(named: "pEp-status-cannot-decrypt") : UIImage(named: "pEp-status-red_white-border")
         case .haveNoKey:
             return withText ? UIImage(named: "pEp-status-msg-red") : UIImage(named: "pEp-status-red_white-border")
-        case .unencrypted: //I think this is nil?
-            return nil
-        case .mediaKeyEncryption: // AKA Basic Protection
+        case .unencrypted:
+            return withText ? UIImage(named: "pEp-status-not-encrypted") : UIImage(named: "pEp-status-red_white-border")
+        case .mediaKeyEncryption:
             if enabled {
                 return withText ? UIImage(named: "pEp-status-msg-basic-protection") : UIImage(named: "pEp-status-green_white-border")
             } else {
                 return withText ? UIImage(named: "pEp-status-msg-disabled-secure") : nil
             }
-        case .unreliable: // AKA Weak protection
+        case .unreliable:
             if enabled {
-                return withText ? UIImage(named: "pEp-status-msg-weak-protection") : UIImage(named: "pEp-status-green_white-border")
+                return withText ? UIImage(named: "pEp-status-weak-encryption") : UIImage(named: "pEp-status-green_white-border")
             } else {
                 return withText ? UIImage(named: "pEp-status-msg-disabled-secure") : nil
             }
-        case .b0rken:
-            return nil
+        case .underAttack:
+            return withText ? UIImage(named: "pEp-status-dangerous-triangle") : UIImage(named: "pEp-status-red_white-border")
         case .reliable:
             if enabled {
-                return withText ? UIImage(named: "pEp-status-msg-yellow") : UIImage(named: "pEp-status-green_white-border")
+                return withText ? UIImage(named: "pEp-status-encrypted-green") : UIImage(named: "pEp-status-green_white-border")
             } else {
                 return withText ? UIImage(named: "pEp-status-msg-disabled-secure") : nil
             }
         case .trusted:
             if enabled {
-                return withText ? UIImage(named: "pEp-status-msg-yellow") : UIImage(named: "pEp-status-green_white-border")
+                return withText ? UIImage(named: "pEp-status-trusted") : UIImage(named: "pEp-status-green_white-border")
             } else {
                 return withText ? UIImage(named: "pEp-status-msg-disabled-secure") : nil
             }
@@ -69,9 +69,10 @@ extension Rating {
                 return withText ? UIImage(named: "pEp-status-msg-disabled-trusted") : nil
             }
         case .mistrust:
-            return withText ? UIImage(named: "pEp-status-msg-red") : UIImage(named: "pEp-status-red_white-border")
-        case .underAttack:
-            return withText ? UIImage(named: "pEp-status-msg-red") : UIImage(named: "pEp-status-red_white-border")
+            return withText ? UIImage(named: "pEp-status-dangerous-circle") : UIImage(named: "pEp-status-red_white-border")
+        case .b0rken:
+            return withText ? UIImage(named: "pEp-status-dangerous-triangle") : UIImage(named: "pEp-status-red_white-border")
+
         }
     }
 
