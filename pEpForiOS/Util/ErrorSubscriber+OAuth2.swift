f22bb2fd54cf90f017848fde70583b854b5d9e52
Dirk Zimmermann <dz@planck.security>
2024-01-24 16:29:10 +0100

    EFI-182 Move some functions into object scope

--- pEpForiOS/Util/ErrorSubscriber+OAuth2.swift
+++ pEpForiOS/Util/ErrorSubscriber+OAuth2.swift
@@ -12,50 +12,50 @@ import MessageModel
 import PlanckToolbox
 
 extension ErrorSubscriber {
+    func oauthType(scopes: [String]) -> OAuth2Type {
+        let scopes = Set(scopes)
+
+        // Note the default value, just in case.
+        var oauthType = OAuth2Type.o365
+
+        for authType in OAuth2Type.allCases {
+            let configScopes = authType.oauth2Config()?.scopes ?? []
+            let s1 = Set(configScopes)
+            if !scopes.intersection(s1).isEmpty {
+                oauthType = authType
+                break
+            }
+        }
+
+        return oauthType
+    }
+
+    func accountType(oauthType: OAuth2Type) -> VerifiableAccount.AccountType {
+        switch oauthType {
+        case .google: return VerifiableAccount.AccountType.gmail
+        case .o365: return VerifiableAccount.AccountType.o365
+        }
+    }
+
+    /// Handles the OAuth2 reauthorization
+    func handleReauthorization(accountEmail: String, scopes: [String]) {
+        if oauthAuthorizers[accountEmail] != nil {
+            let oauthType = oauthType(scopes: scopes)
+            let vc = UIApplication.currentlyVisibleViewController()
+            let oauthAuthorizer = OAuthAuthorizer()
+            oauthAuthorizers[accountEmail] = oauthAuthorizer
+            oauthAuthorizer.delegate = self
+            let oauth2Authorizer = OAuth2ProviderFactory().oauth2Provider().createOAuth2Authorizer()
+
+            oauthAuthorizer.authorize(authorizer: oauth2Authorizer,
+                                      accountType: accountType(oauthType: oauthType),
+                                      viewController: vc)
+        }
+    }
+
     func handleOAuth2AuthorizationError(error: Error) -> Bool {
         guard let displayError = DisplayUserError(withError: error) else {
             return false
-        }
-
-        func oauthType(scopes: [String]) -> OAuth2Type {
-            let scopes = Set(scopes)
-
-            // Note the default value, just in case.
-            var oauthType = OAuth2Type.o365
-
-            for authType in OAuth2Type.allCases {
-                let configScopes = authType.oauth2Config()?.scopes ?? []
-                let s1 = Set(configScopes)
-                if !scopes.intersection(s1).isEmpty {
-                    oauthType = authType
-                    break
-                }
-            }
-
-            return oauthType
-        }
-
-        func accountType(oauthType: OAuth2Type) -> VerifiableAccount.AccountType {
-            switch oauthType {
-            case .google: return VerifiableAccount.AccountType.gmail
-            case .o365: return VerifiableAccount.AccountType.o365
-            }
-        }
-
-        /// Handles the OAuth2 reauthorization
-        func handleReauthorization(accountEmail: String, scopes: [String]) {
-            if oauthAuthorizers[accountEmail] != nil {
-                let oauthType = oauthType(scopes: scopes)
-                let vc = UIApplication.currentlyVisibleViewController()
-                let oauthAuthorizer = OAuthAuthorizer()
-                oauthAuthorizers[accountEmail] = oauthAuthorizer
-                oauthAuthorizer.delegate = self
-                let oauth2Authorizer = OAuth2ProviderFactory().oauth2Provider().createOAuth2Authorizer()
-
-                oauthAuthorizer.authorize(authorizer: oauth2Authorizer,
-                                          accountType: accountType(oauthType: oauthType),
-                                          viewController: vc)
-            }
         }
 
         /// Presents the user a dialog with the error, giving him a choice of doing the reauthorization or not.
@@ -68,7 +68,7 @@ extension ErrorSubscriber {
             }
 
             func handleReAuth() {
-                handleReauthorization(accountEmail: accountEmail, scopes: scopes)
+                self.handleReauthorization(accountEmail: accountEmail, scopes: scopes)
             }
 
             UIUtils.showTwoButtonAlert(withTitle: displayError.title,
