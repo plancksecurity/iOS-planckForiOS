73caae15ea38ee8d91edfb1bb5723935f1537e00
Dirk Zimmermann <dz@planck.security>
2024-01-24 13:34:29 +0100

    EFI-182 Extension for ErrorSubscriber

---
+++ pEpForiOS/Util/ErrorSubscriber+OAuth2.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorSubscriber+OAuth2.swift
+//  planckForiOS
+//
+//  Created by Dirk Zimmermann on 24/1/24.
+//  Copyright © 2024 p≡p Security S.A. All rights reserved.
+//
+
+import Foundation
+
+extension ErrorSubscriber {
+    
+}
